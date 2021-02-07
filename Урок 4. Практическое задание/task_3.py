82e607cad2038e88b5e6517728e49f8540cee6cb
Dmitry Chitalov <cdi9@yandex.ru>
2021-02-07 19:04:03 +0500

    4

---
+++ Урок 4. Практическое задание/task_3.py
@@ -0,0 +1,38 @@
+"""
+Задание 3.
+
+Приведен код, формирующий из введенного числа
+обратное по порядку входящих в него
+цифр и вывести на экран.
+
+Сделайте профилировку каждого алгоритма через cProfile и через timeit
+
+Сделайте вывод, какая из трех реализаций эффективнее и почему!!!
+
+И можете предложить еще свой вариант решения!
+Без аналитики задание считается не принятым
+"""
+
+
+def revers_1(enter_num, revers_num=0):
+    if enter_num == 0:
+        return revers_num
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        return revers_1(enter_num, revers_num)
+
+
+def revers_2(enter_num, revers_num=0):
+    while enter_num != 0:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+    return revers_num
+
+
+def revers_3(enter_num):
+    enter_num = str(enter_num)
+    revers_num = enter_num[::-1]
+    return revers_num
