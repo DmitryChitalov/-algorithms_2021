f558374d65e45526dbf6589753e3827206e30d98
User <>
2021-02-12 21:38:22 +0300

    complete 3

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -5,7 +5,46 @@
 1) придумайте 2-3 решения (не менее двух)
 2) оцените сложность каждого решения в нотации О-большое
 3) сделайте вывод, какое решение эффективнее и почему
+"""
+# 1 решение
 
+company_dict = {'company_1': 1000,
+                'company_2': 2000,
+                'company_3': 3000,
+                'company_4': 4000,
+                'company_5': 5000
+                }
+
+# Сложность решения O(3n)
+
+
+def search_max(my_company_dict):
+    final_list = []
+    number_of_key = ''
+    max_item = 0
+    for elem in range(3):
+        for keys, values in my_company_dict.items():  # O(n)
+            if values > max_item:
+                max_item = values
+                number_of_key = keys
+        final_list.append(number_of_key)
+        final_list.append(max_item)
+        my_company_dict.pop(number_of_key, max_item)
+        max_item = 0
+    return final_list
+
+
+print(search_max(company_dict))
+
+# 2 решение
+
+# Сложность решения O(n log n)
+
+list_of_company = list(company_dict.items())
+list_of_company.sort(key=lambda enum: enum[1], reverse=True)  # O(n log n)
+
+
+"""
 Примечание:
 Без выполнения пунктов 2 и 3 задание считается нерешенным. Пункты 2 и 3 можно выполнить
 через строки документации в самом коде.
