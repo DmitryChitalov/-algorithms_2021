b70166d6ce3510ce7e78c11056465b095ed5d1e7
sitdikov_denis <fomk@mail.ru>
2021-02-12 10:13:07 +0300

    сделал 1 задачу 2 домашки

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -28,3 +28,86 @@
 Вы вместо трехзначного числа ввели строку (((. Исправьтесь
 Введите операцию (+, -, *, / или 0 для выхода):
 """
+
+
+def addition(num1, num2):
+    return num1 + num2
+
+
+def subtraction(num1, num2):
+    return num1 - num2
+
+
+def multiplication(num1, num2):
+    return num1 * num2
+
+
+def division(num1, num2):
+    return num1 / num2
+
+
+class Calculator():
+    available_operation = {'+': addition,
+                           '-': subtraction,
+                           '*': multiplication,
+                           '/': division,
+                           '0': 'exit'}
+
+    exit_symbol = '0'
+
+    def __init__(self):
+        self.num1 = ''
+        self.num2 = ''
+        self.operator = ''
+
+    @staticmethod
+    def num_check(num):
+        try:
+            int(num)
+            return True
+        except ValueError:
+            return False
+
+    def operator_check(self, operator):
+        return operator in self.available_operation
+
+    def get_operation_func(self, operator):
+        return self.available_operation[operator]
+
+    def data_input(self):
+        while not self.num_check(self.num1):
+            num1 = input("Введите первое число: ")
+            if self.num_check(num1):
+                self.num1 = int(num1)
+
+        while not self.num_check(self.num2):
+            num2 = input("Введите второе число: ")
+            if self.num_check(num2):
+                self.num2 = int(num2)
+
+        while not self.operator_check(self.operator):
+            operator = input("Введите операцию (+, -, *, / или 0 для выхода): ")
+            if self.operator_check(operator):
+                self.operator = operator
+
+    def calculation(self):
+        self.data_input()
+
+        if self.operator == self.exit_symbol:
+            return
+        else:
+            func = self.get_operation_func(self.operator)
+            try:
+                res = func(self.num1, self.num2)
+                print(res)
+            except ZeroDivisionError:
+                print("На ноль делить нельзя")
+
+            self.num1 = ''
+            self.num2 = ''
+            self.operator = ''
+            return self.calculation()
+
+
+calc = Calculator()
+calc.calculation()
