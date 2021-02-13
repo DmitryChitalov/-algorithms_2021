42a76ade3dc5010f49d74ad742bbf95833893231
User <>
2021-02-13 11:41:20 +0300

    complete 5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -23,3 +23,41 @@
 # 1) созд-е экземпляров стека (если стопка - класс)
 # 2) lst = [[], [], [], [],....]
 """
+
+
+class PlateStack:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.elements = []
+
+    def is_empty(self):
+        return self.elements == [[]]
+
+    def push(self, elem):
+        if len(self.elements[len(self.elements)-1]) < self.max_size:
+            self.elements[len(self.elements)-1].append(elem)
+        else:
+            self.elements.append([])
+            self.elements[len(self.elements)-1].append(elem)
+
+    def pop(self):
+        pop_plate = self.elements[len(self.elements)-1].pop()
+        if len(self.elements[len(self.elements)-1]) == 0:
+            self.elements.pop()
+        return pop_plate
+
+    def val(self):
+        return self.elements[len(self.elements)-1][len(self.elements[len(self.elements)-1])-1]
+
+    def plate_size(self):
+        sum_of_plates = 0
+        for stack in self.elements:
+            sum_of_plates += len(stack)
+        return sum_of_plates
+
+    def stack_sum(self):
+        return len(self.elements)
+
+
+
+
