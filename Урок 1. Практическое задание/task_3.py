e502fbb515dd3fd1338693f23963e8c507952adc
captaindamien <damiengo@rambler.ru>
2021-02-14 19:56:04 +1100

    hw_1
    Задания 1-5 для 1 урока

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -21,3 +21,31 @@
 Реализуйте поиск трех компаний с наибольшей годовой прибылью.
 Выведите результат.
 """
+
+companies = {'first_company': 10, 'second_company': 20, 'third_company': 30,
+             'fourth_company': 40, 'fifth_company': 50}
+
+# 1) Линейно-логарифмическая сложность O(n log n)
+income = sorted(companies.values())[2]
+for key, value in companies.items():
+    if value >= income:
+        print(f"{key}: ${value}m.")
+
+
+# 2) Линейная сложность O(n)
+new_companies_dict = {}
+for i in range(3):
+    company_name = ''
+    company_income = 0
+    for el in companies:
+        if companies.get(el) > company_income:
+            company_name = el
+            company_income = companies.get(el)
+    new_companies_dict.update({company_name: companies.pop(company_name)})
+
+companies.update(new_companies_dict)
+
+for key, vale in new_companies_dict.items():
+    print(f"{key}: ${value}m.")
+
+# Второе решение более эффективное, потому что имеет линейную сложность O(n) -> быстрее отработает
