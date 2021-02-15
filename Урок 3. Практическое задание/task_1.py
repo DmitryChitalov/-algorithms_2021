51715794ee9a3ffdc10420146ba569ed2a4e757c
sitdikov_denis <fomk@mail.ru>
2021-02-15 23:41:55 +0300

    Сделал 1 задание 3 домашки без выводов

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -19,10 +19,72 @@ import time
 
 
 def time_stamp(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         start = time.time()
-        func()
+        return_value = func(*args, **kwargs)
         end = time.time()
-        print(f'[*] Время выполнения: {end - start} секунд.')
+        print('[*] Время выполнения: {} секунд.'.format(end - start))
+        return return_value
 
     return wrapper
+
+
+@time_stamp
+def get_list():
+    return [i for i in range(9999999)]
+
+
+@time_stamp
+def get_dict():
+    return {i: i for i in range(9999999)}
+
+
+my_list = get_list()
+my_dict = get_dict()
+
+"""
+"""
+
+
+@time_stamp
+def get_item_from_list(items_list):
+    return items_list[10000]
+
+
+@time_stamp
+def get_item_from_dict(dict_of_items):
+    return dict_of_items[10000]
+
+
+get_item_from_list(my_list)
+get_item_from_dict(my_dict)
+"""
+"""
+
+
+@time_stamp
+def add_item_to_list(items_list, item):
+    items_list.append(item)
+
+
+@time_stamp
+def add_item_to_dict(dict_of_items, item):
+    dict_of_items[item] = item
+
+
+add_item_to_list(my_list, 1111111111111)
+add_item_to_dict(my_dict, 1111111111111)
+
+
+@time_stamp
+def remove_from_list(items_list, item):
+    items_list.remove(item)
+
+
+@time_stamp
+def remove_from_dict(dict_of_items, item):
+    dict_of_items.pop(item)
+
+
+remove_from_list(my_list, 1111111111111)
+remove_from_dict(my_dict, 1111111111111)
