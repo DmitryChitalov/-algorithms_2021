5ba8e454f6d824434548f5703351cc9040d794df
MegaLexa <kukh99@mail.ru>
2021-02-18 18:15:12 +0500

    task_3 done

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,20 @@
 """
 
 # hash?
+
+
+def count_unique(word):
+    hash_dict = {}
+    for ind in range(len(word) + 1):
+        for ind_2 in range(ind + 1, len(word) + 1):
+            update_word = word[ind:ind_2]
+            hash_dict[hash(update_word)] = update_word
+    hash_dict.pop(hash(word))
+    return hash_dict
+
+
+user_word = input('Введите любое слово: ')
+user_in_word = count_unique(user_word)
+print(f'В строке {user_word} {len(user_in_word)} подстрок:')
+for i in user_in_word.values():
+    print(i)
