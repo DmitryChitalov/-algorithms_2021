b2f56cc8090a3a306346e8c18c9504a7737d36c5
cherenkov <17rolex@gmail.com>
2021-02-18 17:37:44 +0300

    solved task3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,16 @@
 """
 
 # hash?
+
+
+def func(some_string):
+    hash_list = []
+    for i in range(len(some_string) + 1):
+        for j in range(i + 1, len(some_string) + 1):
+            if hash(some_string[i:j]) not in hash_list and some_string[i:j] != some_string:
+                hash_list.append(hash(some_string[i:j]))
+    print(len(hash_list))
+
+
+some_string = 'papa'
+func(some_string)
