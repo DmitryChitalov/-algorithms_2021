104e9be88878c269f725456068ee7e41c7489c9b
Cher-Study <mainsail_645@inbox.ru>
2021-02-18 23:43:35 +0300

    hometask3 done

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -9,3 +9,36 @@
 Можете условжнить задачу, реализовав ее через ООП
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+import hashlib
+
+memory = {}
+
+
+def memorize(func):
+    def g(n):
+        r = memory.get(n)
+        if r is None:
+            r = func(n)
+            memory[n] = r
+            return 'Адрес добавлен в кэш'
+        return 'Адрес уже существует'
+    return g
+
+
+@memorize
+def hashing(url_adress):
+    salt = url_adress[:7:-1]
+    result = hashlib.sha256(url_adress.encode() + salt.encode())
+    return result
+
+
+print(hashing('https://geekbrains.ru/education'))
+print(hashing('https://geekbrains.ru/education'))
+
+print(hashing('https://geekbrains.ru/lessons/109621'))
+
+
+for key, val in memory.items():
+    print(f'{key} : {val.hexdigest()}')
+
+
