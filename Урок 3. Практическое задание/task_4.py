e7bacd52ed46eaf16062ce67838c9520d884a1d7
NeverGiveUppp <70692430+NeverGiveUppp@users.noreply.github.com>
2021-02-19 14:46:04 +0500

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -9,3 +9,25 @@
 Можете условжнить задачу, реализовав ее через ООП
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+import hashlib
+from uuid import uuid4
+
+repository = {}
+salt = uuid4().hex
+
+
+def url_save(url):
+    salted_minced_meat = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+    if repository.get(url):
+        print(f'Адрес уже есть в кэше {url}')
+    else:
+        repository[url] = salted_minced_meat
+        print(f'Сохраняем в кэш: {url} ')
+
+
+url_save('https://geekbrains.ru/')
+url_save('https://www.youtube.com/')
+url_save('https://geekbrains.ru/')
+url_save('https://www.google.ru/')
+
+print(f'Уже в кэше {repository}')
