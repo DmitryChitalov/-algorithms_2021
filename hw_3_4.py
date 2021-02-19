11f195054b504353c000681aeaeb0680821e706f
Anna <annyshka1997@yandex.ru>
2021-02-19 15:52:49 +0300

    HW3

---
+++ hw_3_4.py
@@ -0,0 +1,34 @@
+"""
+Задание 4.
+Реализуйте скрипт "Кэширование веб-страниц"
+
+Функция должна принимать url-адрес и проверять
+есть ли в кэше соответствующая страница, если нет, то вносит ее в кэш
+
+Подсказка: задачу решите обязательно с применением 'соленого' хеширования и хеш-таблиц
+Можете условжнить задачу, реализовав ее через ООП
+Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
+"""
+import hashlib
+from uuid import uuid4
+
+
+def memoize(func):
+    def g(n, memory={}):
+        r = memory.get(n)
+        if r is None:
+            r = func(n)
+            memory[n] = r
+        return r
+
+    return g
+
+
+@memoize
+def hash_func(url ):
+    salt = uuid4().hex
+    return hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+
+
+hash_func('www')
+hash_func('www')
