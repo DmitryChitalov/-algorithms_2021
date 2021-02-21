728875a64663fb1347defdf9d8ffbaa8ff455b97
Alex Miruk <kws.nightmen@gmail.com>
2021-02-21 18:19:58 +0300

    HomeWork_4_complited

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,10 @@
 Без аналитики задание считается не принятым
 """
 
+import cProfile
+from timeit import timeit
+from random import randint
+
 
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
@@ -36,3 +40,44 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def memo(f):
+    cache = {}
+
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return wrapper
+
+
+@memo
+def revers_4(enter_num, revers_num=0):
+    if enter_num == 0:
+        return revers_num
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        return revers_1(enter_num, revers_num)
+
+
+num = randint(100000000, 10000000000000)
+cProfile.run('revers_3(num),revers_2(num),revers_1(num),revers_4(num)')
+print('Первая функция рекурсии выполниласб за:', timeit("revers_1(num)", globals=globals()))
+print('Втарая функция выполнилась за:', timeit("revers_2(num)", globals=globals()))
+print('Третья функция выполнилась за:', timeit("revers_3(num)", globals=globals()))
+print('Четвертая функция рекурсия с мемоизацией  выполнилась за:', timeit("revers_4(num)", globals=globals()))
+
+"""
+Больше всего времени занимает первая функция так как это рекурсия 
+и требует времени на создание стека вызовов и после выполнения операций но если добавить мемоизацию то она 
+выполняется быстрее всех остольных так как позволяет пропускать одинаковые вызовы рекурсии(но если все будут уникальны 
+то мемоизация будет минусом).
+Функция с циклом быстрее простой рекурсии но все же она выполняет много действий.
+Функция со срезом самая быстрая потому что она возвращает копию строки с шагом -1.
+Интерестно что сProfile видит функцию 4 но ее выполнение считает для функции один хотя названия их разные 
+"""
--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -16,9 +16,10 @@
 Укажите формулу сложности О-нотация каждого алгоритма
 и сделайте обоснвование рез-ам
 """
+from timeit import timeit
 
 
-def simple(i):
+def simple(i):  # O(n**2)
     """Без использования «Решета Эратосфена»"""
     count = 1
     n = 2
@@ -38,5 +39,64 @@ def simple(i):
     return n
 
 
-i = int(input('Введите порядковый номер искомого простого числа: '))
-print(simple(i))
+def sieve(q):  # O(n)
+    """Этот цикл я нашел в интернете и чуть подогнал к требованиям"""
+    n = q ** 2
+    m = (n - 1) // 2
+    b = [True] * m
+    i, p, ps = 0, 3, [2]
+    while p * p < n:
+        if b[i]:  # O(n)
+            ps.append(p)
+            j = 2 * i * i + 6 * i + 3
+            while j < m:
+                b[j] = False  # O(n)
+                j = j + 2 * i + 3
+        i += 1
+        p += 2
+    while i < m:
+        if b[i]:
+            ps.append(p)
+        i += 1
+        p += 2
+        if len(ps) == q:
+            break
+    return ps[q - 1]
+
+
+def my_func(c):  # O(n)
+    """Этот придумал я но он не сказать чтобы был оптимальный, но я смог его сделать быстрее остольных"""
+    result = [2]
+    end = 20
+    while len(result) < c:  # O(1)
+        counter = 0
+        list_numb = [i for i in range(1, end, 2)]  # O(n)
+        list_numb[0] = 0
+        while counter < end // 2:  # O(1)
+            cur_numb = list_numb[counter]  # O(n)
+            if cur_numb and cur_numb not in result:  # O(n)
+                result.append(cur_numb)
+                for f in range(counter + 1, len(list_numb)):  # O(n)
+                    if list_numb[f] % cur_numb == 0:  # O(n)
+                        list_numb[f] = False
+            counter += 1
+            if len(result) == c:
+                break
+        end += c
+    return result[-1]
+
+
+print('Время выполнения моей функции для 10:', timeit('my_func(10)', globals=globals(), number=100))
+print('Время выполнения решето функции для 10:', timeit('sieve(10)', globals=globals(), number=100))
+print('Время выполнения перебора функции для 10:', timeit('simple(10)', globals=globals(), number=100))
+print('Время выполнения моей функции для 100:', timeit('my_func(100)', globals=globals(), number=100))
+print('Время выполнения решето функции для 100:', timeit('sieve(100)', globals=globals(), number=100))
+print('Время выполнения перебора функции для 100:', timeit('simple(100)', globals=globals(), number=100))
+print('Время выполнения моей функции для 1000:', timeit('my_func(1000)', globals=globals(), number=100))
+print('Время выполнения решето функции для 1000:', timeit('sieve(1000)', globals=globals(), number=100))
+print('Время выполнения перебора функции для 1000:', timeit('simple(1000)', globals=globals(), number=100))
+
+'''
+Алгоритм эростофера быстрее обычного перебора но на малых значениях перебор может быть быстрее
+'''
+
