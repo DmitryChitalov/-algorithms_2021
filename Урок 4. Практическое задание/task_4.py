aa8b7e37ed81a7ca79acbe7fdca373d82f5519bd
sharp2point <sharppoint@yandex.ru>
2021-02-23 11:23:23 +0300

    Less4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,8 +11,11 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+import random
 
-array = [1, 3, 1, 3, 4, 5, 1]
+array = [int(random.randrange(0,10)) for _ in range(100)]
+#array = [1, 3, 1, 3, 4, 5, 1]
 
 
 def func_1():
@@ -41,3 +44,49 @@ def func_2():
 
 print(func_1())
 print(func_2())
+
+############################################################
+
+def counter_num():
+    '''
+    Поиск по сортированному списку
+    '''
+    res = sorted([(i,array.count(i)) for i in set(array)],key=lambda t:t[1])[-1]
+    return f'Чаще всего встречается число {res[0]}, ' \
+           f'оно появилось в массиве {res[1]} раз(а)'
+
+def max_count():
+    res = max(array, key=array.count)
+    return f'Чаще всего встречается число {res}, ' \
+           f'оно появилось в массиве {array.count(res)} раз(а)'
+
+
+print(f"counter_num: {counter_num()}")
+print(f"max_count: {max_count()}")
+
+print(f"func_1 time: {timeit('func_1()',setup='from __main__ import func_1,array',number=1000)}")
+print(f"func_2 time: {timeit('func_2()',setup='from __main__ import func_2,array',number=1000)}")
+print(f"counter_num() time: {timeit('counter_num()',setup='from __main__ import counter_num,array',number=1000)}")
+print(f"max_count() time: {timeit('max_count()',setup='from __main__ import max_count,array',number=1000)}")
+##########################################################################
+
+'''
+
+
+Для 100 элементов:
+
+func_1 time: 0.24799100000000002
+func_2 time: 0.19747110000000007
+counter_num() time: 0.031702600000000025
+max_count() time: 0.19539099999999998
+
+Для 1000 элементов:
+
+func_1 time: 15.1299451
+func_2 time: 14.521449000000002
+counter_num() time: 0.16786860000000203
+max_count() time: 14.425566200000002
+
+Из замеров следует что функция counter_num() рализованная 
+через встроенную функцию (sorted() алгоритм Timsort) наиболее эффективна 
+'''
