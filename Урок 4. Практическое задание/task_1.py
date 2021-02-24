0baf093b5fe9f1c9bd230b0293441ba94cd8ff65
COBECTb <qweik747@yandex.ru>
2021-02-24 19:56:49 +0300

    Task 4 complete.

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -14,9 +14,29 @@
 """
 
 
+from timeit import timeit
+
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+user_lst = [i for i in range(100)]
+print(timeit("func_1(user_lst)", globals=globals()))  # 5.1447363
+
+print(timeit("func_2(user_lst)", globals=globals()))  # 4.1955155
+
+"""
+Аналитика.
+Для ускорения было использована конструкция спискового включения.
+Списковое включение выполняется быстрее цикла с условием и наполнением списка.
+"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -14,6 +14,10 @@
 """
 
 
+from timeit import timeit
+from cProfile import run
+
+
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -36,3 +40,30 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+usr_num = 12345678901234
+
+
+def main():
+    revers_1(usr_num)
+    revers_2(usr_num)
+    revers_3(usr_num)
+
+
+print("Revers_1:", timeit("revers_1(usr_num)", globals=globals()))
+print("Revers_2:", timeit("revers_2(usr_num)", globals=globals()))
+print("Revers_3:", timeit("revers_3(usr_num)", globals=globals()))
+
+run("main()")
+
+
+"""
+Аналитика:
+Revers_1 выделяется как в cProfile - много вызовов, так и в timeit - самое продолжительное выполнение, в связи
+с рекурсией.
+Revers_2 в cProfile не выделился - 1 вызов и быстрое выполнение, в timeit выделился- немногим быстрее рекурсии
+из-за отсутствия многочисленных, одинаковых вызовов.
+Revers_3 в cProfile не отличается от предыдущего. В timeit значительно различи от второго, так как используется
+встроенная инструкция реверса.
+"""
