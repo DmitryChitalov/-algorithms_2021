24137e39fcd993619213b655cd2574d5e0364193
MegaLexa <kukh99@mail.ru>
2021-02-27 13:14:40 +0500

    task_2 done

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -33,3 +33,68 @@ class HexNumber:
 hx = HexNumber
 hx + hx
 """
+
+# Через defaultdict:
+from collections import defaultdict
+
+
+def add_to_dict(num, add_dict):
+    for i in num:
+        add_dict[num].append(i)
+
+
+def to_sixteen(num):
+    num_str = '0123456789ABCDEF'
+    if num // 16 == 0:
+        return num_str[num]
+    else:
+        return to_sixteen(num // 16) + num_str[num % 16]
+
+
+num_dict = defaultdict(list)
+
+num_1 = input('Введите первое число шестнадцатиричной системы: ')
+num_2 = input('Введите второе число шестнадцатиричной системы: ')
+
+add_to_dict(num_1, num_dict)
+add_to_dict(num_2, num_dict)
+
+num_sum = to_sixteen(int(num_1, 16) + int(num_2, 16))
+num_mul = to_sixteen(int(num_1, 16) * int(num_2, 16))
+
+add_to_dict(num_sum, num_dict)
+add_to_dict(num_mul, num_dict)
+
+print(f'{num_dict[num_1]} + {num_dict[num_2]} = {num_dict[num_sum]}')
+print(f'{num_dict[num_1]} * {num_dict[num_2]} = {num_dict[num_mul]}')
+
+'''
+Не совсем понял, нужно ли было взаимодействовать именно со списками из словаря или же
+словарь здесь исключительно для хранения чисел в виде списков, но сделал вот так
+'''
+
+print('-' * 200)
+
+
+# Через ООП:
+class SNum:
+    def __init__(self, num):
+        self.num = num
+        self.list_num = list(self.num)
+
+    def __str__(self):
+        return f'{self.list_num}'
+
+    # Если бы до этого не вводил функцию to_sixteen, добавил бы здесь статический метод
+    def __mul__(self, other):
+        return list(to_sixteen(int(self.num, 16) * int(other.num, 16)))
+
+    def __add__(self, other):
+        return list(to_sixteen(int(self.num, 16) + int(other.num, 16)))
+
+
+num_1 = SNum(num_1)
+num_2 = SNum(num_2)
+
+print(f'{num_1} + {num_2} = {num_1 + num_2}')
+print(f'{num_1} * {num_2} = {num_1 * num_2}')
