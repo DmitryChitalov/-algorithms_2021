35c65e10779fdf1a88a8de5738ad7bf5d2efc7a3
Nick <ellan@list.ru>
2021-02-28 17:40:14 +0700

    lesson 5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -15,3 +15,33 @@ deque – это обобщение стеков и очередей.
 
 И добавить аналитику, так ли это или нет.!
 """
+
+import collections
+import timeit
+
+
+list1 = [n for n in range(5000)]
+deque1 = collections.deque([n for n in range(5000)])
+
+#
+print(timeit.timeit('list1.append(0)', globals=globals()))
+print(timeit.timeit('deque1.append(0)', globals=globals()))
+
+print(timeit.timeit('list1.insert(0,0)', globals=globals(), number=10000))
+print(timeit.timeit('deque1.appendleft(0)', globals=globals(), number=10000))
+
+print(timeit.timeit('list1.pop()', globals=globals()))
+print(timeit.timeit('deque1.pop()', globals=globals()))
+
+print(timeit.timeit('list1.pop(0)', globals=globals(), number=10000))
+print(timeit.timeit('deque1.popleft()', globals=globals(), number=10000))
+
+print(timeit.timeit('list1[:] = [1, 2, 3] + list1', globals=globals(), number=10000))
+print(timeit.timeit('deque1.extendleft([1, 2, 3])', globals=globals(), number=1000))
+
+print(timeit.timeit('list1.extend([1, 2, 3])', globals=globals()))
+print(timeit.timeit('deque1.extend([1, 2, 3])', globals=globals()))
+
+
+#Во всех операциях представленных выше, deque оказался быстрее, чем лист, в частности, стоит отметить добавление элемента,
+#либо же ещё одного списка, в начало списка, здесь deque оказался значительно быстрее в обоих случаях
