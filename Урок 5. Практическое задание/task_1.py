b8a975860693e1b3cc383c52982ba3f2a3740a2d
Kaddo <message.chaos628@gmail.com>
2021-03-01 18:59:49 +0300

    added home work lesson_5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,88 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+
+from collections import namedtuple
+from numpy import mean
+
+
+class NotEnough(Exception):
+    pass
+
+
+def mean_all(lst, num, sum_all=0):
+    for firm in lst:
+        sum_firm = firm.qr_1 + firm.qr_2 + firm.qr_3 + firm.qr_4
+        sum_all += sum_firm
+
+    return sum_all / num
+
+
+def above_mean(lst, sum_mean, result=''):
+    for firm in lst:
+        sum_firm = firm.qr_1 + firm.qr_2 + firm.qr_3 + firm.qr_4
+        if sum_firm > sum_mean:
+            result = firm.name + ', ' + result
+    if result[-2] == ',':
+        result = result[:-2]
+
+    return result
+
+
+def lower_mean(lst, sum_mean, result=''):
+    for firm in lst:
+        sum_firm = firm.qr_1 + firm.qr_2 + firm.qr_3 + firm.qr_4
+        if sum_firm < sum_mean:
+            result = firm.name + ', ' + result
+    if result[-2] == ',':
+        result = result[:-2]
+
+    return result
+
+
+ENT = namedtuple('firm', 'name qr_1 qr_2 qr_3 qr_4')
+list_firm = []
+i = 0
+
+try:
+    num_ent = int(input('Введите колличество предприятий чтобы рассчитать прибыль: '))
+    if num_ent == 1:
+        raise NotEnough('Колличество предприятий должно быть больше 1!')
+
+    while i < num_ent:
+        i += 1
+        name_ent = input(f'Введите название предприятия {i}: ')
+        profit_ent = input('Через пробел введите прибыль данного предприятия\n'
+                           'за каждый квартал(Всего 4 квартала): ')
+        profit_ent = profit_ent.split(' ')
+        if len(profit_ent) == 4:
+            ENTERPRISE = ENT(
+                name=name_ent,
+                qr_1=int(profit_ent[0]),
+                qr_2=int(profit_ent[1]),
+                qr_3=int(profit_ent[2]),
+                qr_4=int(profit_ent[3])
+            )
+        else:
+            raise NotEnough('Нужно ввести 4 числа через пробел!')
+
+        list_firm.append(ENTERPRISE)
+
+    mean_prof = mean_all(list_firm, num_ent)
+    print()
+    print(f'Средняя годовая прибыль всех предприятий: {mean_prof} млрд')
+    print(f'Предприятия, с прибылью выше среднего значения: {above_mean(list_firm, mean_prof)}')
+    print(f'Предприятия, с прибылью ниже среднего значения: {lower_mean(list_firm, mean_prof)}')
+
+except ValueError:
+    print('Нужно ввести цифру, а вы ввели буквы или символы!')
+except ZeroDivisionError:
+    print('Колличество предприятий должно быть больше 1!')
+
+'''
+Пример компания и их прибыль в млрд:
+Pepsico 14 30 48 70
+The Coca-Cola Company 8 19 28 37 
+Вимм-Билль-Данн 2 8 10 11
+IDS Borjomi Group 5 9 10 13
+'''
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,52 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+import collections
+import timeit
+
+my_dict = {1: 111, 2: 222, 3: 333}
+ord_dict = collections.OrderedDict([(1, 111), (2, 222), (3, 333)])
+
+
+def fill(dct):
+    start = list(dct.keys())[-1]
+    for i in range(start + 1, 11005):
+        dct.update({i: int(str(f'{i}{i}{i}'))})
+
+
+print(timeit.timeit("my_dict.keys()", globals=globals(), number=1000000))
+print(timeit.timeit("my_dict.update({4:444})", globals=globals(), number=1000000))
+print(timeit.timeit("my_dict.get(4)", globals=globals(), number=1000000))
+fill(my_dict)
+print(timeit.timeit("my_dict.popitem()", globals=globals(), number=10000))
+print()
+print(timeit.timeit("ord_dict.keys()", globals=globals(), number=1000000))
+print(timeit.timeit("ord_dict.update({4:444})", globals=globals(), number=1000000))
+print(timeit.timeit("ord_dict.get(4)", globals=globals(), number=1000000))
+fill(ord_dict)
+print(timeit.timeit("ord_dict.popitem(last=True)", globals=globals(), number=10000))
+
+'''
+Результаты замеров на моём пк:
+0.06542928999988362
+0.13076091899893072
+0.045632885998202255
+0.0006581819998245919
+
+0.054205677999561885
+0.21997439699953247
+0.048066485000163084
+0.0013319619993126253
+
+Я сильно загрузил словарь и OrderedDict так как на 10000 результат был одинаков.
+На 10000000 повторений мы уже видим небольшую разницу, а именно в добавление пары
+ключ значение немного медленнее в OrderedDict. А вот достать пару ключ значение с
+конца в словаре существенно быстрее чем в OrderedDict.
+В python3 до версии 3.6 словари были неупорядоченными. А OrderedDict упорядочивает словарь. 
+Поэтому в этом модуле было больше смысла чем в современых версиях python3. Правда если 
+вам потребуется достать пару ключ значение с начала, то в OrderedDict есть такая возможность
+popitem(last=False). У словаря нет аналогичной функции и нужно будет либо знать название
+либо пройтись по парам ключ значение в цикле и нагромаждать код. Общий вывод такой что
+в современном Python3 нужды в OrderedDict практически нет.
+'''
