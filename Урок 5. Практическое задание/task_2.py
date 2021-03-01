925f94d2216d846e16c19214d744560ff224221e
maksim <kuban.flowers@gmail.com>
2021-03-02 00:53:32 +0300

    add homework_5

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -33,3 +33,112 @@ class HexNumber:
 hx = HexNumber
 hx + hx
 """
+
+from collections import defaultdict
+
+
+def hex_number():
+    hex_1 = list(input("Введите шестнадцатеричное число: "))
+    hex_dct = defaultdict(list)
+    for el in range(len(hex_1) - 1, -1, -1):
+        hex_dct[el].append(hex_1[len(hex_1) - el - 1])
+    return hex_dct
+
+
+def sum_hex_number(dst_1, dst_2):
+    """Сложение столбиком"""
+    num_str = '0123456789ABCDEF'
+    summ = defaultdict(list)
+    for i in range(max(len(dst_1), len(dst_2))):
+        x = "".join(dst_1[i])
+        y = "".join(dst_2[i])
+        summ[i].append(num_str.index(x) + num_str.index(y))
+    res = []
+    i = 0
+    for j in summ.values():
+        z = i + j[0]
+        if z < 16:
+            res.append(num_str[z])
+            i = 0
+        else:
+            res.append(num_str[z % 16])
+            i = z // 16
+    res.reverse()
+    return res
+
+
+def mul_hex_number(dst_1, dst_2):
+    """Умножение столбиком"""
+    num_str = '0123456789ABCDEF'
+    mul = []
+    for i in range(len(dst_1)):
+        mul_1 = defaultdict(list)
+        for j in range(len(dst_2)):
+            x = "".join(dst_1[i])
+            y = "".join(dst_2[j])
+            mul_1[j].append(num_str.index(x) * num_str.index(y))
+        res = []
+        i = 0
+        for j in mul_1.values():
+            z = i + j[0]
+            if z < 16:
+                res.append(num_str[z])
+                i = 0
+            else:
+                res.append(num_str[z % 16])
+                i = z // 16
+        res.append(str(i))
+        mul.append(res)
+    mul_2 = []
+    for i in range(len(mul[0]) + 1):
+        if i == 0:
+            mul_2.append(num_str.index(mul[0][0]))
+        elif i == len(mul[0]):
+            mul_2.append(num_str.index(mul[1][-1]))
+        else:
+            mul_2.append(num_str.index(mul[0][i]) + num_str.index(mul[1][i - 1]))
+    mul_2.reverse()
+    res = []
+    i = 0
+    for j in mul_2:
+        z = i + j
+        if z < 16:
+            res.append(num_str[z])
+            i = 0
+        else:
+            res.append(num_str[z % 16])
+            i = z // 16
+    return res
+
+
+num1 = hex_number()
+num2 = hex_number()
+print(f"Сумма шестнадцатеричных чисел: {sum_hex_number(num1, num2)}")
+print(f"Произведение шестнадцатеричных чисел: {mul_hex_number(num1, num2)}")
+
+"""
+Через ООП
+"""
+
+
+class HexNumber:
+    def __init__(self, hex_number):
+        self.hex_number = list(str(hex_number))
+
+    def __add__(self, other):
+        el_1 = int("".join(self.hex_number), 16)
+        el_2 = int("".join(other.hex_number), 16)
+        return list(hex(el_1 + el_2))[2:]
+
+    def __mul__(self, other):
+        el_1 = int("".join(self.hex_number), 16)
+        el_2 = int("".join(other.hex_number), 16)
+        return list(hex(el_1 * el_2))[2:]
+
+
+a = "A2"
+b = "C4F"
+num_1 = HexNumber(a)
+num_2 = HexNumber(b)
+print(f"Сумма шестнадцатеричных чисел: {num_1 + num_2}")
+print(f"Произведение шестнадцатеричных чисел: {num_1 * num_2}")
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -15,3 +15,75 @@ deque – это обобщение стеков и очередей.
 
 И добавить аналитику, так ли это или нет.!
 """
+
+from collections import deque
+from timeit import timeit
+
+
+def append_left_list(check_list, el):
+    check_list.insert(0, el)
+
+
+def append_left_deque(check_deque, el):
+    check_deque.appendleft(el)
+
+
+def pop_left_list(check_list):
+    check_list.pop(0)
+
+
+def pop_left_deque(check_deque):
+    check_deque.popleft()
+
+
+def extend_left_list(check_list, el):
+    for i in el:
+        check_list.insert(0, i)
+
+
+def extend_left_deque(check_deque, el):
+    check_deque.extendleft(el)
+
+
+def pop_list(check_list):
+    check_list.pop()
+
+
+def pop_deque(check_deque):
+    check_deque.pop()
+
+
+some_list = [el for el in range(100000)]
+some_deque = deque(some_list)
+some_el = True
+some_lst = [1, 2]
+
+print('appendleft list')
+print(timeit('append_left_list(some_list, some_el)', globals=globals(), number=10000))
+
+print('appendleft deque')
+print(timeit('append_left_deque(some_deque, some_el)', globals=globals(), number=10000))
+
+print('popleft list')
+print(timeit('pop_left_list(some_list)', globals=globals(), number=10000))
+
+print('popleft deque')
+print(timeit('pop_left_deque(some_deque)', globals=globals(), number=10000))
+
+print('extendleft list')
+print(timeit('extend_left_list(some_list, some_lst)', globals=globals(), number=100))
+
+print('extendleft deque')
+print(timeit('extend_left_deque(some_deque, some_lst)', globals=globals(), number=100))
+
+print('pop list')
+print(timeit('pop_list(some_list)', globals=globals(), number=100))
+
+print('pop deque')
+print(timeit('pop_deque(some_deque)', globals=globals(), number=100))
+
+'''
+В результате замеров времени получилось, что встроенные в deque операции
+работают во много раз быстрее, чем аналогичные в простом списке. Информация в документации
+соответствует дейтсвительности. При этом, если делать замеры одинаковых фукнций - время сопоставимо.
+'''
