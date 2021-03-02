ae12434b20e51cfacdc612d3191039eba1f60ae8
Islam <centurionislam98@gmail.com>
2021-03-02 06:26:45 +0600

    Наработка

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -10,4 +10,35 @@
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
 
-# begin
+from hashlib import pbkdf2_hmac
+from re import findall
+
+urls = {}
+url = 'https//developer.org/test'
+if findall(r'^(https?://)([\w.]+)\.([a-z]{2,6})(/[\w/]+)?$', url):
+    print(True)
+
+#
+# def hash_url(url):
+#     if findall(r'https?://(?:[-\w.]|(?:%[\da-fA-F]))+', url):
+#         salt = url.split('.')
+#         url_h = sha256(salt[-2].encode() + url.encode()).hexdigest()
+#         if not url_h in table_url.values():
+#             if len(table_url) == 0:
+#                 table_url[1] = url_h
+#                 return f'Хеш добавлен - {url_h}'
+#             else:
+#                 key = max(table_url.keys()) + 1
+#                 table_url[key] = url_h
+#                 return f'Хеш добавлен - {url_h}'
+#         else:
+#             return f'{url} - url-адрес уже есть в хеш-таблице!'
+#     else:
+#         return f'{url} - неверный url адрес!'
+#
+#
+# print(hash_url('https://www.chipdip.ru/'))
+# print(hash_url('https://www.arduino.cc/'))
+# print(hash_url('https://amperka.ru/'))
+# print(hash_url('https://www.chipdip.ru/'))
+# print(hash_url('https/amperka.ru/'))
