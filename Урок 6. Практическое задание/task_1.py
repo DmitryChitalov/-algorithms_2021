094812a9326e61d8409742ff493b2cd7188ba7d1
sitdikov_denis <fomk@mail.ru>
2021-03-04 00:24:43 +0300

    Почти доделал 1 задание

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -22,13 +22,34 @@
 С одновременным замером времени (timeit.default_timer())!
 """
 
-
 """
 1) Реализуем сложение двух матриц стандартными средствами и средствами numpy
-В качестве матриц используем список списков
+2) Сравним списковое включение и генератор списка
 """
-
+########################################################################################################################
+import memory_profiler
 import numpy as np
+import timeit
+
+count = 2000
+
+
+def memory_measurement(func):
+    def wrapper(*args, **kwargs):
+        start_memory = memory_profiler.memory_usage()
+        start_time = timeit.default_timer()
+
+        res = func(*args, **kwargs)
+
+        end_memory = memory_profiler.memory_usage()
+        end_time = timeit.default_timer()
+
+        mem_diff = end_memory[0] - start_memory[0]
+        time_diff = end_time - start_time
+
+        return res, mem_diff, time_diff
+
+    return wrapper
 
 
 class Matrix:
@@ -36,31 +57,97 @@ class Matrix:
         self.m = data
 
     def __add__(self, other):
-        if len(self.m) < len(other.m):
-            max_count = len(self.m)
-            smaller_matrix = self
-        else:
-            max_count = len(other.m)
-            smaller_matrix = other
-
         m = []
-        for line_index in range(max_count):
+        for line_index in range(len(self.m)):
             line = []
-            for elem_index in range(len(smaller_matrix.m[line_index])):
+            for elem_index in range(len(self.m[line_index])):
                 line.append(self.m[line_index][elem_index] + other.m[line_index][elem_index])
             m.append(line)
         return Matrix(m)
 
 
-list1 = [[1, 1, 1], [2, 2, 2], [2, 2, 2]]
-list2 = [[4, 4, 4], [5, 5, 50], [5, 5, 50]]
+@memory_measurement
+def matrix_sum(m1, m2):
+    """
+    функция считает сумму двух матриц, состоящих из списков
+    """
+    return m1 + m2
+
+
+@memory_measurement
+def m_sum_gen(m1, m2):
+    """
+    функция считает сумму двух матриц, состоящих из генераторов
+    """
+    for line1, line2 in zip(m1.m, m2.m):
+        line = []
+        for elem_index in range(len(line1)):
+            line.append(line1[elem_index] + line2[elem_index])
+        yield line
+
+
+@memory_measurement
+def matrix_lc():
+    """
+    генерация списка через  lc
+    """
+    return [[i for i in range(count)] for j in range(count)]
+
+
+@memory_measurement
+def matrix_generator():
+    """
+    создание объекта генератора
+    """
+    for i in range(count):
+        yield [j for j in range(count)]
+
+
+@memory_measurement
+def gen_iter(gen):
+    for elem in gen:
+        c = elem
+
+
+# замерим результаты создания списка и генератора
+list1, mem_diff1, time_diff1 = matrix_lc()
+list2, mem_diff2, time_diff2 = matrix_generator()
+
+# получим все данные из генератора
+c, mem_diff2, time_diff2 = gen_iter(list2)
+
+print(f'Генерация через LC затратила = {mem_diff1} mib, time_diff = {time_diff1} msec')
+print(f'Генерация через generator затратила = {mem_diff2} mib, time_diff = {time_diff2} msec')
+
+"""
+Генерация через LC затратила = 139.734375 mib, time_diff = 0.2783290070001385 msec
+Генерация через generator затратила = 0.0 mib, time_diff = 0.10070318699945346 msec
+Вывод №1: наглядно видим, что генератор использует гораздо меньше и времени и памяти
+"""
+
 
 m1 = Matrix(list1)
-m2 = Matrix(list2)
+m2 = Matrix(list1)
 
-m3 = m1 + m2
-print(m3.m)
+sum, mem_diff3, time_diff3 = matrix_sum(m1, m2)
 
 m_np1 = np.array(list1)
-m_np2 = np.array(list2)
-print(m_np1 + m_np2)
+m_np2 = np.array(list1)
+sum, mem_diff4, time_diff4 = matrix_sum(m_np1, m_np2)
+
+print(f'Вычисление суммы списка списков затратила = {mem_diff3} mib, time_diff = {time_diff3} msec')
+print(f'Вычисление суммы массивов NumPy затратила = {mem_diff4} mib, time_diff = {time_diff4} msec')
+"""
+Вычисление суммы списка списков затратила = 147.68359375 mib, time_diff = 0.86058907000006 msec
+Вычисление суммы массивов NumPy затратила = 30.4765625 mib, time_diff = 0.1176289610002641 msec
+Вывод №2 - очевидно, что использование массивов NumPy требует гораздо меньше ресурсов
+"""
+
+
+m1_gen = Matrix(list2)
+m2_gen = Matrix(list2)
+
+# m3, mem_diff5, time_diff5 = m_sum_gen(m1_gen, m2_gen)
+# получим все данные из генератора
+c, mem_diff5, time_diff5 = gen_iter(m_sum_gen(m1_gen, m2_gen))
+print(f'Вычисление суммы генераторов затратило = {mem_diff5} mib, time_diff = {time_diff5} msec')
