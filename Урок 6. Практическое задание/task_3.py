65f979079c42c1e875b9da6f539a6e80ccad0e69
sergey <samael.env@gmail.com>
2021-03-04 15:45:43 +0300

    commit
    commit

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,23 @@
 Придумать как это решить!
 Есть очень простое решение!
 """
+from memory_profiler import profile
+
+
+@profile
+def cnt(number):
+    def count_recur(num=number, even=0, odd=0):
+        if num % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+        num = num // 10
+        if num == 0:
+            return print(f"evens {even}\nodds {odd}")
+        count_recur(num, even, odd)
+    return count_recur(number)
+
+
+cnt(35608909)
+"""Если замерять как обычно, то получаем таблицу на каждый вызов функции
+Для замера нужно обернуть фйнкцию в другую функцию и замерить результ работы фнции 'обёртки'"""
