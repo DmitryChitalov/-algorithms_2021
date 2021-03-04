109a981c6961c0035975357f06702efdf10c3847
Nikita <lukonenko.nikitka@mail.ru>
2021-03-04 13:09:23 +0300

    HW_6 Ready

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -21,3 +21,85 @@
 Попытайтесь дополнительно свой декоратор используя ф-цию memory_usage из memory_profiler
 С одновременным замером времени (timeit.default_timer())!
 """
+from collections import namedtuple
+from recordclass import recordclass
+import gc
+import memory_profiler
+from numpy import array
+lst_obj = [el for el in range(1000000)]
+obj = tuple(el for el in range(1000000))
+
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        m1 = memory_profiler.memory_usage()
+        res = func(*args, **kwargs)
+        m2 = memory_profiler.memory_usage()
+        mem_diff = m2[0] - m1[0]
+        print(f'Выполнение заняло: {mem_diff} Mib')
+        return res
+    return wrapper
+
+
+@decor
+def get_sum_1(lst_obj_1):
+    res_1 = 0
+    for el in lst_obj_1:
+        res_1 += el
+    return res_1
+
+
+print(get_sum_1(lst_obj))
+
+
+@decor
+def get_sum_2(lst_obj_2):
+    lst_obj_2 = lst_obj_2
+    res_1 = 0
+    for el in lst_obj_2:
+        res_1 += el
+    gc.collect()
+    return res_1
+
+
+print(get_sum_2(lst_obj))
+
+'''Использование модуля gc.collector сильно экономит память. Также замена списка на кортеж уменьшает кольчество 
+используемой паняти. '''
+
+
+@decor
+def pers_info_1():
+    namedtuple_ = namedtuple('test_1', 'name surname numbers address')
+    info_1 = namedtuple_(name='Pavel', surname='Rachmanov', numbers=[el for el in range(10000)], address='Krasnaya 55')
+    return info_1
+
+
+@decor
+def pers_info_2():
+    recordclass_ = recordclass('test_2', ('name', 'surname', 'numbers', 'address'))
+    info_2 = recordclass_(name='Nikita', surname='Pogromov', numbers=[el for el in range(10000)], address='Pobedy 11')
+    return info_2
+
+
+pers_info_1()
+pers_info_2()
+'''Переменные recordclass действительно используют меньше места по сравнению с namedtuple, 
+особенно при работе с большими объемами данных '''
+
+
+@decor
+def numpy_array():
+    test_1 = array([el for el in range(1000000)])
+    return test_1
+
+
+@decor
+def arr_():
+    test_2 = list(el for el in range(1000000))
+    return test_2
+
+
+numpy_array()
+arr_()
+'''Модуль NumPy array хорошо оптимизирован и использует меньше памяти, чем обычный список'''
