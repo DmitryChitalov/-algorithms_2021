fc0c2578a7df27944236080ff4987210c89258b8
sharp2point <sharppoint@yandex.ru>
2021-03-04 23:45:01 +0300

    Lesson_6

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,30 @@
 Придумать как это решить!
 Есть очень простое решение!
 """
+from timeit import default_timer
+from memory_profiler import memory_usage, profile
+
+#@profile
+def rec_func(num):
+    #print(num)
+    if num < 1:
+        return num
+    return rec_func(num - 1)
+
+@profile
+def wrapp(num):
+    return rec_func(num)
+
+#rec_func(100)
+wrapp(10)
+
+'''
+При использовании @profile c рекурсивной функцией замер происходит каждую иттерацию
+
+Но если рекурсию обернуть в простую функцию то замер пройдёт как положено
+Line #    Mem usage    Increment  Occurences   Line Contents
+============================================================
+    18     19.5 MiB     19.5 MiB           1   @profile
+    19                                         def wrapp(num):
+    20     19.5 MiB      0.0 MiB           1       return rec_func(num)
+'''
