df8d7b40e65e90795e5652e56892b61e259ca0df
Cher-Study <mainsail_645@inbox.ru>
2021-03-06 16:29:03 +0300

    corrected

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -7,6 +7,7 @@
 """
 from memory_profiler import profile
 
+
 @profile
 def rev_numbers(number):
     if len(str(number)) == 1:
@@ -56,6 +57,7 @@ print(recurs_func(1230))
 Еще один вариант:
 '''
 
+
 @profile
 def recurs_func2(num):
     def rev_numbers2(number):
@@ -70,6 +72,7 @@ def recurs_func2(num):
                 number = number // 10
                 return str(el) + rev_numbers(number)
     return rev_numbers2(num)
+
 
 '''
 В данном случае в таблице будет все подробно расписано по строкам, как в таблицах из немодифицированного случая.
