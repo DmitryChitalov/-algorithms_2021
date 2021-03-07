9e6f9bcc393c287550f76ff5f6ad269ec062776c
MegaLexa <kukh99@mail.ru>
2021-03-07 20:06:58 +0500

    task_3 done

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,23 @@
 Придумать как это решить!
 Есть очень простое решение!
 """
+
+from memory_profiler import profile
+
+
+@profile
+def check_recur(mul_num):
+    def factorial(number):
+        if number == 1:
+            return number
+        else:
+            return number * factorial(number - 1)
+    return factorial(mul_num)
+
+
+print(check_recur(900))
+
+'''
+Думаю, минус данного варианта профилирования состоит в том, что мы не можем четко
+увидеть, сколько памяти занимает каждый из вызовов функции
+'''
