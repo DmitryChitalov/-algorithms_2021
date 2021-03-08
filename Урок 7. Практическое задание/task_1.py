712e28608c35c6646ac48b8b591429b8e2fdb48f
AshSmol <ashiryaev84@gmail.com>
2021-03-08 22:21:29 +0300

    lesson 7 task 1, 2

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -19,3 +19,42 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+from random import randint
+from timeit import timeit
+
+
+def bubble_sort(lst):
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - 1):
+            if lst[j] < lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return lst
+
+
+def bubble_sort_improved(lst):
+    flag = False
+    for i in range(len(lst) - 1):
+        if flag:
+            break
+        flag = True
+        for j in range(len(lst) - 1):
+            if lst[j] < lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                flag = False
+    return lst
+
+
+my_lst = [randint(-100, 100) for i in range(10)]
+print(my_lst)
+print(f"Bubble_sort:\n{bubble_sort(my_lst[:])}")
+print(f"bubble_sort_improved:\n{bubble_sort_improved(my_lst[:])}")
+print(f"bubble_sort: {timeit('bubble_sort(my_lst[:])', globals=globals())} сек")
+print(f"bubble_sort_improved: {timeit('bubble_sort_improved(my_lst[:])', globals=globals())} сек")
+
+"""
+Вывод:
+доработка алгоритма незначительно увеличила его эффективность
+Прирост составил менее 2%. Результаты замеров массива 100 элементов 1миллион повторений:
+bubble_sort: 922.9501952 сек
+bubble_sort_improved: 905.2050769000001 сек
+"""
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,126 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+from random import random
+import timeit
+
+"""
+Прямое слияние Боуза-Нельсона - можно рассматривать как разновидность алгоритма сортировки слиянием,
+хотя по сути это сортировочная сеть. Суть ее заключается в том, что все подмассивы можно сортировать параллельно
+на каждом этапе. 
+"""
+
+
+def bose_nelson(data):
+    """
+    Прямое слияние Боуза-Нельсона - можно рассматривать как разновидность алгоритма сортировки слиянием,
+    хотя по сути это сортировочная сеть. Суть ее заключается в том, что все подмассивы можно сортировать параллельно
+    на каждом этапе.
+    """
+
+    def bose_nelson_merge(j, r, m):
+        if j + r < len(data):
+            if m == 1:
+                if data[j] > data[j + r]:
+                    data[j], data[j + r] = data[j + r], data[j]
+            else:
+                m = m // 2
+                bose_nelson_merge(j, r, m)
+                if j + r + m < len(data):
+                    bose_nelson_merge(j + m, r, m)
+                bose_nelson_merge(j + m, r - m, m)
+        return data
+
+    m = 1
+    while m < len(data):
+        j = 0
+        while j + m < len(data):
+            bose_nelson_merge(j, m, m)
+            j = j + m + m
+        m = m + m
+    return data
+
+
+def merge_sort(lst_obj):
+    if len(lst_obj) > 1:
+        center = len(lst_obj) // 2
+        left = lst_obj[:center]
+        right = lst_obj[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        # перестали делить
+        # выполняем слияние
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst_obj[k] = left[i]
+                i += 1
+            else:
+                lst_obj[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst_obj[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            lst_obj[k] = right[j]
+            j += 1
+            k += 1
+        return lst_obj
+
+
+orig_list = [random() * 50 for _ in range(10)]
+
+# замеры 10
+print(
+    timeit.timeit(
+        "merge_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "bose_nelson(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random() * 50 for _ in range(100)]
+
+# замеры 100
+print(
+    timeit.timeit(
+        "merge_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "bose_nelson(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random() * 50 for _ in range(1000)]
+
+# замеры 1000
+print(
+    timeit.timeit(
+        "merge_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "bose_nelson(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+my_lst = [random() * 50 for i in range(10)]
+print(my_lst)
+print(bose_nelson(my_lst[:]))
+print(merge_sort(my_lst))
