1e1d438e13a562ad1c78ddcf3d633200e49da4c3
NeverGiveUppp <70692430+NeverGiveUppp@users.noreply.github.com>
2021-03-10 20:01:05 +0500

    Update task_1.py

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -19,3 +19,50 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+import timeit
+import random
+
+orig_list = [random.randint(-100, 100) for _ in range(100)]
+count = 100
+ordered_array = [random.randint(-100, 100) for _ in range(count)]
+
+
+def bubble_sort(my_list):
+    n = 1
+    while n < len(my_list):
+        for i in range(len(my_list) - n):
+            if my_list[i] > my_list[i + 1]:
+                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+        n += 1
+    return my_list
+
+
+print('Не отсортированный список, превым способом')
+print(timeit.timeit("bubble_sort(orig_list[:])", globals=globals(), number=1000))
+print('Отсортированный список, превым способом')
+print(timeit.timeit("bubble_sort(ordered_array[:])", globals=globals(), number=1000))
+
+
+def buble_sort_2(my_list):
+    exchange = True
+    ln = len(my_list) - 1
+    for i in range(ln):
+        if not exchange:
+            break
+        exchange = False
+        for j in range(ln, 0, -1):
+            if my_list[j] < my_list[j - 1]:
+                my_list[j - 1], my_list[j] = my_list[j], my_list[j - 1]
+                exchange = True
+        ln -= 1
+    return my_list
+
+
+print('Не отсортированный список, после доработки')
+print(timeit.timeit("buble_sort_2(orig_list[:])", globals=globals(), number=1000))
+print('Отсортированный список, после доработки')
+print(timeit.timeit("buble_sort_2(ordered_array[:])", globals=globals(), number=1000))
+
+'''Доработка ускорила работу функции в случае отсортированного и не осортированного списка.
+Доработка состоит в том, цикл может завершится без лишних проходов по уже отсортированному.
+Вероятность того что список будет уже отсортирован очень мала'''
