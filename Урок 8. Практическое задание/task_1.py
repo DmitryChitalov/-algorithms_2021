d61f392037838ed3454b7c7e32243c5f073ee69a
Cher-Study <mainsail_645@inbox.ru>
2021-03-14 23:09:14 +0300

    homework 8 done

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -10,3 +10,74 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+from collections import Counter, deque
+import queue
+
+
+class Node:
+    def __init__(self, x, k=-1, l=None, r=None, c=''):
+        self.freq = x
+        self.key = k
+        self.left = l
+        self.right = r
+        self.code = c
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+
+def huffman_code(data):
+    node_list = []
+    que = queue.PriorityQueue()
+    code_table = {}
+
+    freq_table = Counter(data)
+    freq_table = deque(sorted(freq_table.items(), key=lambda item: item[1]))
+
+    # Huffman tree init
+    for (k, v) in freq_table:
+        node_list.append(Node(v, k))
+        que.put(node_list[-1])
+
+    # Huffman tree generate
+    while que.qsize() > 1:
+        node1 = que.get()
+        node2 = que.get()
+        node1.code = '1'
+        node2.code = '0'
+        nn = Node(node1.freq + node2.freq, l=node1, r=node2)
+        node_list.append(nn)
+        que.put(node_list[-1])
+
+    # get Huffman code
+    def get_huffman_code(p, code_str=[]):
+        code_str.append(p.code)
+        if p.left:
+            get_huffman_code(p.left, code_str.copy())
+            get_huffman_code(p.right, code_str.copy())
+        else:
+            code_table[p.key] = ''.join(code_str)
+
+    get_huffman_code(node_list[-1])
+
+    return code_table
+
+
+def huffman_encodind(code_table, string):
+    result = []
+    for el in string:
+        result.append(code_table[el])
+    return ' '.join(result)
+
+
+# строка для кодирования
+s = "beep boop beer!"
+
+# функция заполняет кодовую таблицу (символ-его код)
+# {'b': '11', 'p': '101', 'r': '1001', '!': '1000', 'e': '01', 'o': '001', ' ': '000'}
+for k, v in huffman_code(s).items():
+    print(k, '-', v)
+
+# закодированная строка
+print(huffman_encodind(huffman_code(s), s))
+
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,7 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -22,12 +23,17 @@ class BinaryTree:
     # добавить левого потомка
     def insert_left(self, new_node):
         # если у узла нет левого потомка
-        if self.left_child == None:
+        if self.left_child is None:
+            if new_node >= self.get_root_val():
+                raise ValueError('Левый потомок должен быть меньше родительского узла!')
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.left_child = BinaryTree(new_node)
         # если у узла есть левый потомок
         else:
+            if new_node >= self.get_root_val() or new_node < self.get_left_child().get_root_val():
+                print('Значение родительского узла:', self.get_root_val())
+                raise ValueError('Левый потомок должен быть меньше родительского узла!')
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
@@ -37,12 +43,17 @@ class BinaryTree:
     # добавить правого потомка
     def insert_right(self, new_node):
         # если у узла нет правого потомка
-        if self.right_child == None:
+        if self.right_child is None:
+            if new_node < self.get_root_val():
+                raise ValueError('Правый потомок должен быть больше родительского узла или равен ему!')
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.right_child = BinaryTree(new_node)
         # если у узла есть правый потомок
         else:
+            if new_node < self.get_root_val() or new_node > self.get_right_child().get_root_val():
+                print('Значение родительского узла:', self.get_root_val())
+                raise ValueError('Правый потомок должен быть больше родительского узла или равен ему!')
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
@@ -66,14 +77,53 @@ class BinaryTree:
         return self.root
 
 
-r = BinaryTree(8)
+'''r = BinaryTree(8)
 print(r.get_root_val())
 print(r.get_left_child())
-r.insert_left(40)
+r.insert_left(4)
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
 r.insert_right(12)
 print(r.get_right_child())
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+print(r.get_right_child().get_root_val())'''
+
+answer = input('Введите корень бинарного дерева: ')
+r = BinaryTree(int(answer))
+while answer != '#':
+    answer = input('Выберите действие: \nДля ввода левого узла нажмите "l", \nдля ввода правого '
+                   'узла нажмите "r", \nдля выхода нажмите "#": ')
+
+    if answer == 'l':
+        try:
+            val = int(input('Введите значение: '))
+            r.insert_left(val)
+        except ValueError:
+            print('Значение левого потомка должно быть меньше значения родительского узла '
+                  '\nи больше следующего левого потомка!\n')
+        else:
+            print(r.get_left_child())
+            print('Успешно')
+
+    elif answer == 'r':
+        try:
+            val = input('Введите значение: ')
+            r.insert_right(int(val))
+        except ValueError:
+            print('Значение правого потомка должно быть больше значения родительского узла или равно ему '
+                  '\nи меньше значения следующего правого потомка!\n')
+        else:
+            print(r.get_right_child())
+            print('Успешно')
+
+print('Программа завершена')
+
+
+"""
+Новая версия программы проверяет вводимые данные на соответствие правилам построения бинарного дерева.
+Она не только сравнивает потомка со значением родительского узла, но и с введенным ранее дочерним потомком, 
+который должен стать потомком нового узла.
+Реализован клиентский код для проверки правильности работы программы с данной доработкой.
+
+"""
