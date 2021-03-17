c4a0c578f4cf1c45319a09571c55df6fa358aadc
kovsa <sergeyakovalev@yandex.ru>
2021-03-17 18:56:57 +0300

    HW.8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -10,3 +10,96 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+
+
+from collections import Counter
+
+
+class HuffmanString:
+
+    def __init__(self, value, left=None, right=None):
+        self.right = right
+        self.left = left
+        self.value = value
+
+
+def get_code(root, codes=dict(), code=''):
+    if root is None:
+        return
+
+    if isinstance(root.value, str):
+        codes[root.value] = code
+        return codes
+
+    get_code(root.left, codes, code + '0')
+    get_code(root.right, codes, code + '1')
+
+    return codes
+
+
+def get_tree(string):
+    string_count = Counter(string)
+
+    if len(string_count) <= 1:
+        node = HuffmanString(None)
+
+        if len(string_count) == 1:
+            node.left = HuffmanString([key for key in string_count][0])
+            node.right = HuffmanString(None)
+
+        string_count = {node: 1}
+
+    while len(string_count) != 1:
+        node = HuffmanString(None)
+        spam = string_count.most_common()[:-3:-1]
+
+        if isinstance(spam[0][0], str):
+            node.left = HuffmanString(spam[0][0])
+
+        else:
+            node.left = spam[0][0]
+
+        if isinstance(spam[1][0], str):
+            node.right = HuffmanString(spam[1][0])
+
+        else:
+            node.right = spam[1][0]
+
+        del string_count[spam[0][0]]
+        del string_count[spam[1][0]]
+        string_count[node] = spam[0][1] + spam[1][1]
+
+    return [key for key in string_count][0]
+
+
+def coding(string, codes):
+    res = ''
+
+    for symbol in string:
+        res += codes[symbol]
+
+    return res
+
+
+def decoding(string, codes):
+    res = ''
+    i = 0
+
+    while i < len(string):
+
+        for code in codes:
+
+            if string[i:].find(codes[code]) == 0:
+                res += code
+                i += len(codes[code])
+
+    return res
+
+
+code_str = 'beep boop beer!'
+
+print(code_str)
+tree = get_tree(code_str)
+
+codes = get_code(tree)
+print(codes)
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,17 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
+from sys import exit
+
+
+class Error(Exception):
+    def __init__(self, text, root, new_node):
+        self.txt = text
+        self.root = root
+        self.new_node = new_node
+
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -22,32 +33,45 @@ class BinaryTree:
     # добавить левого потомка
     def insert_left(self, new_node):
         # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        try:
+            if new_node > self.root:
+                raise Error("Значение для добавления левого потомка должно быть меньше корня!!!", self.root, new_node)
+            if self.left_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
+        except Error as e:
+            print(f"{e.txt}\nКорень: {e.root}\nновый элемент: {e.new_node}")
+            exit(1)
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        try:
+            if new_node <= self.root:
+                raise Error("Значение для добавления правого потомка должно быть >= корню!!!", self.root, new_node)
+            # если у узла нет правого потомка
+            if self.right_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
+        except Error as e:
+            print(f"{e.txt}\nКорень: {e.root}\nновый элемент: {e.new_node}")
+            exit(1)
 
     # метод доступа к правому потомку
     def get_right_child(self):
