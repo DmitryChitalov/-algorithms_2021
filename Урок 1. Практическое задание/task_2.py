f117943cc6827e844c7cf1c52243d52fd06f54a9
nevidimk0 <nevidimk0@inbox.ru>
2021-03-19 22:39:05 +0300

    Update task_2.py

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -19,3 +19,37 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+
+import random
+import time
+
+
+def algorithm_1(lst_obj):   # O(n**2)
+    for element_1 in lst_obj:
+        is_minimal = 1
+        for element_2 in lst_obj:
+            if element_1 > element_2:
+                is_minimal = 0
+                break
+        if is_minimal == 1:
+            minimal_value = element_1    
+        
+    return minimal_value
+    
+def algorithm_2(lst_obj):   # O(n)
+    minimal_value = None
+    for element in lst_obj:
+        if(minimal_value == None):
+            minimal_value = element
+        else:
+            if element < minimal_value:
+                minimal_value = element
+        
+    return minimal_value    
+
+for j in (50, 500, 1000, 5000, 1000):
+    lst = random.sample(range(-100000, 100000), j)
+
+
+print(algorithm_1(lst)) # O(n**2)
+print(algorithm_2(lst)) # O(n)
