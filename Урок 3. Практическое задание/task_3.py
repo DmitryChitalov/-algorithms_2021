85608d3b732a26375d5c9f02c54aec7757fdcb7c
Fred <fred17@mail.ru>
2021-03-20 23:05:20 +0300

    lesson 3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,30 @@
 """
 
 # hash?
+
+str_val = "zasqr"
+
+substrings = set()
+len_str = len(str_val)
+count = 0
+end_count = len_str - 1
+val_bool = True
+
+for i in str_val:
+    substrings.add(hash(i))
+
+while count <= len_str - 2:
+    if val_bool:
+        for i in range(0, end_count):
+            if str_val[i:] != "" and str_val[i:] != str_val:
+                substrings.add(hash(str_val[i:]))
+        val_bool = False
+    else:
+        for i in range(0, end_count):
+            if str_val[count:-i] != "":
+                substrings.add(hash(str_val[count - 1:-i]))
+
+    count += 1
+
+print(substrings)
+print("{} уникальных подстрок - {}".format(str_val, len(substrings)))
