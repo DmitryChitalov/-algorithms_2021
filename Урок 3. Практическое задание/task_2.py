b2e77d6349d1fb303a7b94a941896514a05c4ffd
nikitinserg <nikitin.serg.alex@gmail.com>
2021-03-20 13:36:53 +0900

    lesson 3 h/w

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -18,3 +18,69 @@
 Допускаются любые усложения задания - валидация, подключение к БД, передача данных в файл
 """
 # sqlite, postgres, db_api, orm
+
+# т.к. решил использовать sqlite, пришлось немного изменить задание, чтоб соответствовать духу СУБД
+
+from hashlib import pbkdf2_hmac
+import sqlite3
+
+sqlite_connection = sqlite3.connect(':memory:')
+cursor = sqlite_connection.cursor()
+cursor.execute("""DROP TABLE IF EXISTS users;""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+                    user_id INTEGER primary key autoincrement
+                    ,login varchar(10)
+                    ,password varchar(50)
+                );""")
+cursor.execute("""INSERT INTO users (login, password) VALUES
+                ('user1', 'pass1')
+                ,('user2', 'pass2')
+                ,('user3', 'pass3')
+                ,('user4', 'pass4')
+                ,('user5', 'pass5')
+                ,('user6', 'pass1')
+                ;""")  # чтоб не мучать пользователя вводом данных, заполним сами
+sqlite_connection.commit()
+
+
+def get_hash(usr, pwd):
+    user_hash = pbkdf2_hmac('sha256', pwd.encode(), usr.encode(), 100000)
+    return user_hash.hex()
+
+
+def hashing():
+    """
+    заменим в таблице users пароли на хэши паролей
+    """
+    cursor.execute(f"""SELECT login, password FROM users""")
+    records = cursor.fetchall()
+    print('Список пользователей и паролей и хэшей: ')
+    for row in records:
+        print(f'login: {row[0]}\t password: {row[1]}\t hash: ', end='')
+        cursor.execute(f"""UPDATE users SET password='{get_hash(row[0], row[1])}' WHERE login='{row[0]}';""")
+        print(get_hash(row[0], row[1]))
+        sqlite_connection.commit()
+    # после выполнения цикла пароли необратимо удалены, остались только хэши
+hashing()
+
+def login_validation(login=input("Логин: "), wrong_pass_try=3):
+    """
+    Проверка логина/пароля
+    :param login: запрашивается логин
+    :param wrong_pass_try: количество попыток неправильного ввода пароля
+    """
+    cursor.execute(f"""SELECT password FROM users WHERE login = '{login}';""")
+    result = cursor.fetchone()
+    if result is None:
+        print("Учетная запись не найдена")
+        login_validation(input("Логин: "))
+    elif result[0] == get_hash(login, input('Введите пароль: ')):
+        print(f"Добро пожаловать, {login}")
+    else:
+        if wrong_pass_try >= 1:
+            print(f'Неправильный пароль. Осталось попыток: {wrong_pass_try}')
+            login_validation(login, wrong_pass_try - 1)
+        else:
+            print('Попытки закончились')
+
+login_validation()
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -9,3 +9,38 @@
 Можете условжнить задачу, реализовав ее через ООП
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+
+from hashlib import pbkdf2_hmac
+
+
+class CacheUrl:
+    def __init__(self):
+        self.cache_dict = {}
+        self.__salt = 'salty_salt'
+
+    def add_url(self, url):
+        if not self.cache_dict.get(url):
+            self.cache_dict[url] = pbkdf2_hmac(hash_name='sha256',
+                                               password=url.encode('UTF-8'),
+                                               salt=self.__salt.encode('UTF-8'),
+                                               iterations=100000)
+            print(f'{url} занесен в кэш')
+        else:
+            print(f'{url} уже есть в кэше')
+    def __str__(self): # вывод всего кэша
+        dict_print = 'Весь словарь: \n'
+        for  k, v in self.cache_dict.items():
+            dict_print += str(k) + ' : ' + str(v.hex()) + '\n'
+        return dict_print
+
+
+
+
+my_cache = CacheUrl()
+my_cache.add_url('https://google.com')
+my_cache.add_url('https://yandex.ru')
+my_cache.add_url('https://geekbrains.ru')
+my_cache.add_url('https://geekbrains.ru')
+
+
+print(my_cache)
