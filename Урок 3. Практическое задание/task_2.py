32c01a725cdd26cb3cd324b66819156fbf175cd7
Terramora <terramorka@gmail.com>
2021-03-21 09:57:53 +0300

    no message

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -17,4 +17,69 @@
 
 Допускаются любые усложения задания - валидация, подключение к БД, передача данных в файл
 """
-# sqlite, postgres, db_api, orm
+import random
+import string
+from hashlib import sha256
+
+bd = {}
+ls = []
+
+
+def create_random_password(length=12):
+    from os import urandom
+    salt = urandom(length)
+    symbols = string.ascii_letters + string.digits + '!#$!&_+-?@^_^'
+    password = ''.join(random.choice(symbols) for _ in range(length))
+    password_hash = sha256(password.encode('UTF-8') + salt).hexdigest()
+    return password, password_hash, salt
+
+
+def generate_hash(pwd):
+    from os import urandom
+    salt = urandom(12)
+    hash_password = sha256(pwd.encode('utf-8') + salt).hexdigest()
+    print(hash_password)
+    return hash_password, salt
+
+
+def check_hash(password, user_data):
+    import hashlib
+
+    if hashlib.sha256(
+            password.encode('utf-8') + user_data['salt']).hexdigest() == user_data['password']:
+        return True
+    else:
+        return False
+
+
+def generate_password():
+    for i in range(10):
+        pwd, hash_pwd, salt = create_random_password()
+        print(f'user{i}: {pwd}')
+        bd[f'user{i}'] = {'password': hash_pwd, 'salt': salt}
+
+
+def main():
+    pwd = input('Введите пароль: ').lower()
+    ls.append(generate_hash(pwd))
+    pwd = input('Введите пароль еще раз для проверки: ')
+    if sha256(
+            pwd.encode('utf-8') + ls[0][1]).hexdigest() == ls[0][0]:
+        print(True)
+    else:
+        return False
+
+
+main()
+
+# def main():
+#     #   generate_password()
+#     print(bd)
+#     user_name, pwd = input('Введите логин: ').lower(), input('Введите пароль: ')
+#     if bd.get(user_name):
+#         if check_hash(pwd, bd[user_name]):
+#             print('Доступ разрешен.')
+#         else:
+#             print('Доступ запрещен.')
+
+#   main()
--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -16,4 +16,19 @@
 а
 """
 
+
 # hash?
+
+
+def distinct_substr(text):
+    dt = {}
+    for i in range(len(text) + 1):
+        for j in range(i + 1, len(text) + 1):
+            new_substr = text[i:j]
+            if new_substr != text:
+                dt[hash(new_substr)] = new_substr
+    print(f'{text} - {len(dt)} уникальных подстрок\n')
+    print('\n'.join(dt.values()))
+
+
+distinct_substr('papa')
