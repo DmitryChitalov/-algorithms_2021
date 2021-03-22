1e7a7d4a2acb2723d4ba3ded45628d2d73aa6b89
wifi-for-fridges <timofeewdanila@mail.ru>
2021-03-22 20:46:03 +0300

    ДЗ к уроку 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -1,22 +1,71 @@
 """
 Задание 1.
-
 Приведен код, который позволяет сохранить в
 массиве индексы четных элементов другого массива
-
 Сделайте замеры времени выполнения кода с помощью модуля timeit
-
 Оптимизируйте, чтобы снизить время выполнения
 Проведите повторные замеры
-
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание считается не принятым
 """
 
+from timeit import default_timer, timeit
 
+
+# собственный декоратор для проверки замеров времени с помощью timeit
+def exec_time_decorator(count):
+    def exec_time(func):
+        def wrapper(*args):
+            result = 0
+
+            for number in range(count):
+                start = default_timer()  # левая отсечка времени
+                func(args[0])
+                tmp_res = default_timer() - start  # правая отсечка времени
+                result += tmp_res  # формирование итогового значения
+
+            print(f'время выполнения: {result}')
+
+        return wrapper
+
+    return exec_time
+
+
+# исходная функция, уже присутствующая в задании
+def func_1_for_timeit(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+# для генерации нового списка использовал LC, поскольку это лаконичнее и выполняется быстрее, нежели вариант с циклом for.
+# К тому же для генерации нового списка производится меньшее количество операций (не создаем новый промежуточный список
+# new_arr и не используем append() для его заполнения)  => выигрыш в скорости выполнения и в объеме занимаемой памяти
+def get_index_in_list_for_timeit(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+# исходная функция, уже присутствующая в задании, только с применением декоратора
+@exec_time_decorator(10000)
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+# собственный вариант оптимизации, только с применением декоратора
+@exec_time_decorator(10000)
+def get_index_in_list(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+numbers_list = [i for i in range(100)]
+print(timeit("func_1_for_timeit(numbers_list)", number=10000, globals=globals()))  # результат: 0.085 с
+print(timeit('get_index_in_list_for_timeit(numbers_list)', number=10000, globals=globals()))  # результат: 0.07 с
+
+func_1(numbers_list)  # результат: 0.082 с
+get_index_in_list(numbers_list)  # результат: 0.068 с => получили близкие значения временных замеров
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,17 +1,16 @@
 """
 Задание 3.
-
 Приведен код, формирующий из введенного числа
 обратное по порядку входящих в него
 цифр и вывести на экран.
-
 Сделайте профилировку каждого алгоритма через cProfile и через timeit
-
 Сделайте вывод, какая из трех реализаций эффективнее и почему!!!
-
 И можете предложить еще свой вариант решения!
 Без аналитики задание считается не принятым
 """
+
+import timeit
+from cProfile import run
 
 
 def revers_1(enter_num, revers_num=0):
@@ -36,3 +35,41 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def reverses_number(enter_num):
+    revers_1(enter_num)
+    revers_2(enter_num)
+    revers_3(enter_num)
+
+# собственный вариант решения, похожий на функцию revers_2
+def own_reverse_number(enter_number):
+
+    new_number = 0
+    while enter_number > 0:
+        digit = enter_number % 10
+        enter_number //= 10
+        new_number *= 10
+        new_number += digit
+
+    return new_number
+
+number = 12345678912345678912345678912345678912345678900000000000000000000065463234253256572123415225353643
+
+# revers_1 - рекурсивная функция, также в реализации присутствует много операций вычисления и присваивания =>
+# наиболее долгое время выполнения
+# revers_2 - итеративное решение, которое по времени выполнения выигрывает перед рекурсивной реализацией
+# revers_3 - в решении используется встроенная функция str(), а также срез в обратном порядке всего массива, полностью
+# отстутвуют вычислительные операции => данная функция будет отрабатывать быстрее, чем reverse_2
+print(timeit.timeit('revers_1(number)', number=10000, globals=globals()))  # результат: 0.63 с
+print(timeit.timeit('revers_2(number)', number=10000, globals=globals()))  # результат 0.41 с
+print(timeit.timeit('revers_3(number)', number=10000, globals=globals()))  # результат 0.01 с
+
+# в выводе получаем, что все значения в столбце cumtime для каждой из функций revers_1, revers_2 и revers_3 равны 0.000
+run('reverses_number(number)')
+
+# для собственной функции результат выполнения 0.43 с => оптимизация не удалась, результат близкий к reverse_2
+print(timeit.timeit('own_reverse_number(number)', number=10000, globals=globals()))
+
+# cumtime = 0.000
+run('own_reverse_number(number)')
