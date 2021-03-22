44c34f0e4a8b440b2dd008eb2abba90683199c11
ASV108 <aver.serg1870@gmail.com>
2021-03-22 12:08:07 +0300

    lesson_3 tasks uploaded

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,36 @@
 """
 
 # hash?
+
+while True:
+    print('-' * 50)
+    string = input('Для выхода введите "0"\n'
+                   'Введите строчное значение: ')
+    #tring = 'papa'
+
+    if string == '0':
+        break
+    string_list = []
+    len_list = len(string)
+
+    for i in range(len(string)):
+        a = string[len(string) - len_list:]
+        len_sub = 0
+        for j in range(len(a)):
+            b = a[:len(a) - len_sub]
+            string_list.append(hash(b))
+            len_sub += 1
+        len_list -= 1
+
+    string_set = set(string_list[1:])
+    print(f'Введенная строка имеет {len(string_set)} подстр.')
+
+
+
+
+
+
+
+
+
+
