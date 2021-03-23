f026b21110075128d7c7ac160b26769575fb0cc3
Your Name <you@example.com>
2021-03-23 09:59:33 +0300

    задания к уроку 4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -12,6 +12,8 @@
 И можете предложить еще свой вариант решения!
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
 
 
 def revers_1(enter_num, revers_num=0):
@@ -36,3 +38,59 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+num = 1234567893475857349857348957
+print(f'Функция_1: {timeit("revers_1(num)", globals=globals())}')
+print(f'Функция_2: {timeit("revers_2(num)", globals=globals())}')
+print(f'Функция_3: {timeit("revers_3(num)", globals=globals())}')
+print('=' * 100)
+run("revers_1(num)")
+print('=' * 100)
+run("revers_2(num)")
+print('=' * 100)
+run("revers_3(num)")
+"""
+Функция_1: 6.0483078
+Функция_2: 4.1057546
+Функция_3: 0.39394419999999997
+====================================================================================================
+3 функция самая быстрая, т.к. она не выполняет никаких вычислений, берется просто срез.
+
+cProfile
+Показывает нам что первый вариант исполнения функции вызывает себя множество раз
+
+         32 function calls (4 primitive calls) in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+     29/1    0.000    0.000    0.000    0.000 task_3.py:19(revers_1)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+====================================================================================================
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:29(revers_2)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+====================================================================================================
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:37(revers_3)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+"""
