7bd334e22ad9ebb2e83fec15954eeb00a4af4e4a
Demid <bydimko@yandex.ru>
2021-03-24 02:49:12 +0300

    lesson1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -21,3 +21,46 @@
 Реализуйте поиск трех компаний с наибольшей годовой прибылью.
 Выведите результат.
 """
+
+base_company = {
+    'Mail.ru Group': 1000,
+    'Yandex': 2000,
+    'Rambler': 3000,
+    'gmail': 1500,
+    'google': 2600,
+    'Yahoo!': 3700,
+    'Bing': 4080,
+    'msn': 500
+}
+
+
+# первый вариант - O(N^2)
+def sorted_1(company_dict):
+    company_lst = [el for el in company_dict.values()]
+    company_lst.sort(reverse=True)
+    for el in company_lst[:3]:
+        for i, j in company_dict.items():
+            if j == el:
+                print(i, j)
+
+
+# второй вариант - O(N log N)
+list_from_dictionary = list(base_company.items())
+list_from_dictionary.sort(key=lambda i: i[1], reverse=True)
+for i in range(3):
+    print(list_from_dictionary[i][0], ':', list_from_dictionary[i][1])
+
+
+# третий вариант - O(N)
+def three_max(list_input):
+    input_max = {}
+    list_d = dict(list_input)
+    for i in range(3):
+        maximum = max(list_d.items(), key=lambda k_v: k_v[1])
+        del list_d[maximum[0]]
+        input_max[maximum[0]] = maximum[1]
+    return input_max
+
+
+print(three_max(base_company))
+sorted_1(base_company)
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -26,3 +26,41 @@
 Для реализации хранилища можно применить любой подход,
 который вы придумаете, например, реализовать словарь.
 """
+
+# Предпочтителена функция authorization2,
+# в ней не используется цикл, поиск в словаре по ключу имеет сложность O(1)
+
+
+# О(N)
+def authorization1(users, user_name, user_password):
+    for key, value in users.items():
+        if key == user_name:
+            if value['password'] == user_password and value['activation']:
+                return "Добро пожаловать! Доступ к ресурсу предоставлен"
+            elif value['password'] == user_password and not value['activation']:
+                return "Учетная запись не активна! Пройдите активацию!"
+            elif value['password'] != user_password:
+                return "Пароль не верный"
+    return "Данного пользователя не существует"
+
+
+# общая сложность O(1)
+def authorization2(users, user_name, user_password):
+    if users.get(user_name):
+        if users[user_name]['password'] == user_password and users[user_name]['activation']:
+            return "Добро пожаловать! Доступ к ресурсу предоставлен"
+        elif users[user_name]['password'] == user_password and not users[user_name]['activation']:
+            return "Учетная запись не активна! Пройдите активацию!"
+        elif users[user_name]['password'] != user_password:
+            return "Пароль не верный"
+    else:
+        return "Данного пользователя не существует"
+
+
+my_users = {'user1': {'password': '11111', 'activation': True},
+            'user2': {'password': '11111', 'activation': False},
+            'user3': {'password': '11111', 'activation': True},
+            'user4': {'password': '11111', 'activation': False}
+            }
+
+print(authorization2(my_users, 'user1', '11111'))
