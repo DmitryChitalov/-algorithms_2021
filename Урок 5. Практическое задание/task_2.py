0fe89bd167c801af59357f55db6ec4655e89437d
nikitinserg <nikitin.serg.alex@gmail.com>
2021-03-24 14:29:50 +0900

    lesson 5 h/w

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -33,3 +33,49 @@ class HexNumber:
 hx = HexNumber
 hx + hx
 """
+
+# 1. вариант
+from collections import defaultdict
+
+
+def hexnum(num_1, num_2):
+    """
+    Возвращает арифметические операции + и * с шестнадцатиричными числами
+    :param num_1: первое число
+    :param num_2: второе число
+    :return: ответ в виде массива
+    """
+    d = defaultdict(list)
+    d[num_1] = list(num_1)
+    d[num_2] = list(num_2)
+    d['add'] = list(hex(int(num_1, 16) + int(num_2, 16))[2:])
+    d['mul'] = list(hex(int(num_1, 16) * int(num_2, 16))[2:])
+    return f'{num_1} + {num_2} = {d["add"]}\n {num_1} * {num_2} = {d["mul"]}'
+
+
+print(f'def hexnum:\n{hexnum("a2", "c4f")}')
+
+# 2. вариант
+class HexNumber:
+    """
+    Возвращает арифметические операции + и * с шестнадцатиричными числами
+    """
+    def __init__(self, number):
+        self.number = number
+
+    def __add__(self, other):
+        result = hex(int(self.number, 16) + int(other.number, 16))
+        return list(result[2:])
+
+    def __mul__(self, other):
+        result = hex(int(self.number, 16) * int(other.number, 16))
+        return list(result[2:])
+
+    def __str__(self):
+        return f'Полученное число: {list(self.number)}'
+
+
+hex_num_1 = HexNumber('a2')
+hex_num_2 = HexNumber('c4f')
+print('HexNumber __add__: ', hex_num_1 + hex_num_2)
+print('HexNumber __mul__:', hex_num_1 * hex_num_2)
