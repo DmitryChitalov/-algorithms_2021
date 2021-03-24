1d54048b7d6c638c82c416789df264a325e5888b
IrinaMilekhina <i.mezhova@gmail.com>
2021-03-25 00:40:53 +0300

    lesson 4 (1-4)

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,8 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание считается не принятым
 """
+import timeit
+from random import randint
 
 
 def func_1(nums):
@@ -20,3 +22,18 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+n = [randint(0, 1000) for i in range(500)]
+
+print('Выполнение первой функции составило', timeit.repeat("func_1(n)", globals=globals(), repeat=3, number=1000))
+print('Выполнение второй функции составило', timeit.repeat("func_2(n)", globals=globals(), repeat=3, number=1000))
+
+
+# Я использовала для второго варианта LC, замеры показали, что он быстрее, что логично, т.к. не нужно создавать
+# промежуточный список и нет метода append()
