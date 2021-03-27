303232b9011e4f2d333a3ad1ceb8c1ee50ad5bfe
Your Name <you@example.com>
2021-03-27 19:48:04 +0300

    задания к уроку 5

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -33,3 +33,37 @@ class HexNumber:
 hx = HexNumber
 hx + hx
 """
+from collections import defaultdict
+
+
+def function_1(num_1, num_2):
+    def_dict = defaultdict(list)
+    def_dict[num_1] = list(num_1)
+    def_dict[num_2] = list(num_2)
+    def_dict['sum'] = list(hex(int(num_1, 16) + int(num_2, 16))[2:])
+    def_dict['multiplication'] = list(hex(int(num_1, 16) * int(num_2, 16))[2:])
+    return f'{num_1} + {num_2} = {"".join(def_dict["sum"])}\n' \
+           f'{num_1} * {num_2} = {"".join(def_dict["multiplication"])}\n' \
+           f'{def_dict[num_1]} + {def_dict[num_2]} = {def_dict["sum"]}\n' \
+           f'{def_dict[num_1]} * {def_dict[num_2]} = {def_dict["multiplication"]}'
+
+
+print(f'{function_1.__name__}\n{function_1("A2", "C4F")}')
+print('=' * 60)
+
+
+class HexNumber:
+    def __init__(self, num):
+        self.num = num
+
+    def __add__(self, other):
+        return list(hex(int(self.num, 16) + int(other.num, 16))[2:])
+
+    def __mul__(self, other):
+        return list(hex(int(self.num, 16) * int(other.num, 16))[2:])
+
+
+number_1 = HexNumber('A2')
+number_2 = HexNumber('C4F')
+print(f'Сложение: {number_1 + number_2}')
+print(f'Умножение: {number_1 * number_2}')
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,50 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from collections import OrderedDict
+from random import randint
+from timeit import timeit
+
+dict_min = 1
+dict_max = 1000
+
+
+def classic_dict(classic):
+    classic = {key: randint(dict_min, dict_max) for key in range(dict_max)}
+    return classic
+
+
+def classic_dict_get(classic):
+    return classic.get(dict_max - dict_min)
+
+
+def classic_dict_pop(classic):
+    for item in range(len(classic)):
+        classic.pop(item)
+    return classic
+
+
+to_test = {}
+dict_ord = OrderedDict()
+
+print(f'Обычный словарь, создание/заполнение: {timeit("classic_dict(to_test)", globals=globals(), number=10000)}')
+print(f'Обычный словарь, получение: {timeit("classic_dict_get(to_test)", globals=globals(), number=10000)}')
+print(f'Обычный словарь, удаление: {timeit("classic_dict_pop(to_test)", globals=globals(), number=10000)}')
+print('=' * 50)
+print(f'OrderDict словарь, создание/заполнение: {timeit("classic_dict(dict_ord)", globals=globals(), number=10000)}')
+print(f'OrderDict словарь, получение: {timeit("classic_dict_get(dict_ord)", globals=globals(), number=10000)}')
+print(f'OrderDict словарь, удаление: {timeit("classic_dict_pop(dict_ord)", globals=globals(), number=10000)}')
+"""
+Результат на моем ноутбуке:
+
+Обычный словарь, создание/заполнение: 6.560466
+Обычный словарь, получение: 0.00118029999999969
+Обычный словарь, удаление: 0.0016126999999999114
+==================================================
+OrderDict словарь, создание/заполнение: 6.7765341999999995
+OrderDict словарь, получение: 0.0013980000000000103
+OrderDict словарь, удаление: 0.0015562999999989557
+
+Исходя из тестов можно сделать вывод, что результаты равны, видимая разница на уровне погрешности вычислений.
+Смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях - отсутствует.
+"""
