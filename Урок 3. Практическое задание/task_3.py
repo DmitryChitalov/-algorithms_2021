be05d01f0197094dca37a7aa7702d429c069a988
nevidimk0 <nevidimk0@inbox.ru>
2021-03-28 20:50:07 +0300

    task3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,18 @@
 """
 
 # hash?
+
+given_string = input("Input your string: ")
+result_set = set()
+
+for i in range(len(given_string)):
+    last_string = given_string[i:]
+    for length in range(1, len(last_string) + 1):
+        substring = given_string[i:i + length]
+        if substring != given_string:
+            hashed_substring = hash(substring)
+            result_set.add(hashed_substring)
+
+print(f"Total number of substrings: {len(result_set)}")
+
+
