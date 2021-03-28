e65846ed3c9af25a210a4eb437e2ba73cfeb3dd4
Kuznetsov-Serg <ksn1974@mail.ru>
2021-03-28 22:38:16 +0300

    HomeWork#04

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,10 +13,71 @@
 Без аналитики задание считается не принятым
 """
 
+from timeit import Timer
+from timeit import timeit
+from random import randint
 
-def func_1(nums):
+def fill_list(amount):
+    my_list = []
+    for i in range(amount):
+        my_list.append(randint(0, 10000))
+    return my_list
+
+def func_1(nums):       # Эталонный вариант
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+def func_2(nums):       # Попробуем через встроенные lambda и enumerate, чтобы индекс не "тащить" отдельно
+                        # вторя map+lambda для очистки получаемого списка кортежей от кортежей
+    return list(map(lambda x: x[0], filter(lambda x: x[1] % 2 == 0, enumerate(nums))))
+
+def func_3(nums):       # повторяет func_2, но для очистки встроенное преобразование к dict и обратно в list
+    return list(dict(filter(lambda x: x[1] % 2 == 0, enumerate(nums))))
+
+def func_4(nums):       # уберем все "лишнее", оставив встроенную enumerate
+    return [i for i,x in enumerate(nums) if not x%2]
+
+def func_5(nums):       # попроубуем какой-то выигрыш от "не вытаскивать" значенеи массива по индексу
+    new_arr = []
+    for i, x in enumerate(nums):
+        if x % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+func_list = ['func_1', 'func_2', 'func_3', 'func_4', 'func_5', 'func_1']
+my_list = [0, 5, 3, 7, 8, 4]
+print('Проверим, что все функции "верно" выбирают список индексов из списка: ', my_list)
+f = globals()
+for el in func_list:        # Убедимся, что все функции верно выбирают индексы элементов с четным значением
+    print(f'Функция {el}: {f[el](my_list)}')
+
+# my_list = range(10000)
+my_list = fill_list(10000)
+print('Теперь список с размерностью ', len(my_list))
+for el in func_list:
+    print(f'Функция {el}:', timeit(el+'(my_list)', number=1000, globals=globals()))
+
+#t1 = Timer("func_1(my_list)", setup="my_list=fill_list(10000)", globals=globals())
+
+'''
+Результат:
+
+Функция func_1: 1.753318042
+Функция func_2: 3.7037612830000004
+Функция func_3: 3.546011859
+Функция func_4: 1.394236509999999
+Функция func_5: 1.811383407000001
+Функция func_1: 1.6717626939999999 
+(для проверки, завершим с первоначальным вариантом) 
+'''
+
+'''
+Вывод:
+    пробовал, как на шаблонных списках range(1000...), так и на randint() - результаты идентичные
+    ранее, считал, что использование встроенных lambda, filter, zip сокращают время - нет, только "красоту"
+    Самый эффективный алгоритм (№4), одновременно оказался и самым "простым" с т.з. внешней записи
+    (нет этих множественных вложенностей, преобразований и т.д.)
+'''
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -18,13 +18,16 @@ from random import randint
 
 def recursive_reverse(number):
     if number == 0:
-        return str(number % 10)
+        return ''
     return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+def num_10000():
+    return randint(100000000, 10000000000000)
 
 
 num_100 = randint(10000, 1000000)
 num_1000 = randint(1000000, 10000000)
-num_10000 = randint(100000000, 10000000000000)
+# num_10000 = randint(100000000, 10000000000000)
 
 print('Не оптимизированная функция recursive_reverse')
 print(
@@ -37,9 +40,9 @@ print(
         "recursive_reverse(num_1000)",
         setup='from __main__ import recursive_reverse, num_1000',
         number=10000))
-print(
+print('вх.параметр генерится при каждом вызове: ',
     timeit(
-        "recursive_reverse(num_10000)",
+        "recursive_reverse(num_10000())",
         setup='from __main__ import recursive_reverse, num_10000',
         number=10000))
 
@@ -64,6 +67,20 @@ def recursive_reverse_mem(number):
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
 
+def reverse_for(number):
+    in_str = str(number)
+    out_str = ''
+    for i in range(len(in_str)-1,-1,-1):
+        out_str += in_str[i]
+    return out_str
+
+def reverse_integrated(number):
+    return reversed(str(number))
+
+# n = 1234567890
+# print(n, reverse_for(n))
+# exit()
+
 print('Оптимизированная функция recursive_reverse_mem')
 print(
     timeit(
@@ -75,8 +92,55 @@ print(
         'recursive_reverse_mem(num_1000)',
         setup='from __main__ import recursive_reverse_mem, num_1000',
         number=10000))
-print(
+print('вх.параметр генерится при каждом вызове: ',
     timeit(
-        'recursive_reverse_mem(num_10000)',
+        'recursive_reverse_mem(num_10000())',
         setup='from __main__ import recursive_reverse_mem, num_10000',
         number=10000))
+
+func_list = ['recursive_reverse', 'recursive_reverse_mem', 'reverse_for', 'reverse_integrated']
+print('Теперь сравним варианты :')
+for el in func_list:
+    print(f'Функция {el}:', timeit(el+'(num_10000())', number=10000, globals=globals()))
+
+
+
+'''
+Запуск "как есть"
+Не оптимизированная функция recursive_reverse
+0.053577714
+0.06022617599999999
+0.09217042800000003
+Оптимизированная функция recursive_reverse_mem
+0.003592898000000011
+0.0036067840000000295
+0.0037577209999999917
+
+Результаты настораживают. В случае кэширования, при изменении "работы" в 10-100 раз, результат практически не меняется.
+Сделаем предположение, что входной параметр (num_100..) функции остается прежним все 10000 раз запусков.
+Сделаем его принудительно-обновляемым (завернем в пользовательскую функцию - 'num_10000()'):
+
+Не оптимизированная функция recursive_reverse
+0.04855514300000002
+0.058233810999999996
+вх.параметр генерится при каждом вызове:  0.113600427
+Оптимизированная функция recursive_reverse_mem
+0.003647235999999998
+0.003517429999999988
+вх.параметр генерится при каждом вызове:  0.18852120000000006
+
+Вот, теперь уже больше похоже на отсутствие примуществ кэширования рекурсии (последняя строка).
+Мы на кэширование тратим больше ресурсов, нежели на выгоду от их использования при  
+Ответ:
+        КЭШирование при реализации такой рекурсии не дает преимуществ по скорости.
+        
+Добавим пару вариантов функций:
+- reverse_for (переворачивает число в простом цикле)
+- reverse_integrated (переворачивает число при помощи встроенной функции reversed)
+
+Результат вполен прогнозируемый (встроенная функция самая "быстрая", потом уже обычный перебор,...):
+Функция recursive_reverse: 0.113733855
+Функция recursive_reverse_mem: 0.18921550899999995
+Функция reverse_for: 0.05399973300000005
+Функция reverse_integrated: 0.024709552000000023
+'''
