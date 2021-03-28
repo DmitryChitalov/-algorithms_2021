fbab27eda9efb1f9e34fd9b69710805cfef48686
yarikmik <yarik.mik@mail.ru>
2021-03-28 12:41:56 +0700

    lesson_3 complete

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -9,3 +9,31 @@
 Можете условжнить задачу, реализовав ее через ООП
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+
+from hashlib import sha256
+
+class UrlCash:
+    def __init__(self):
+        self.__url_dict = {}
+        self.__salt = 'Url_salt'
+
+    def add_url(self, url):
+        hash_url = sha256(url.encode() + self.__salt.encode()).hexdigest()
+        if hash_url in self.__url_dict.values():
+            print('Url уже есть в кеше')
+        else:
+            self.__url_dict[url] = hash_url
+
+    def show_url_cash(self):
+        for key, value in self.__url_dict.items():
+            print(f'{key}: {value}')
+
+
+a = UrlCash()
+a.add_url('https://finance.yahoo.com/')
+a.add_url('https://travel.ru/')
+a.add_url('https://travel.ru/')
+a.add_url('https://nalog.ru/')
+a.add_url('https://mail.ru/')
+
+a.show_url_cash()
