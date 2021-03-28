dcbc17756221358aa1fdacf95932626d75e4e4c7
Dmitriy Rumyantsev <dr2moscow@yandex.ru>
2021-03-28 19:51:00 +0300

    Второе задание выполнено, начинаю понимать нотацию

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -19,3 +19,28 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+
+
+# 1. Функция поиска минимального значания из списка, сложность O(n^2) - квадратичная
+def min_search(min_list):
+	min_o = min_list[0]
+	for el in min_list:
+		for el2 in min_list:
+			if el < el2 and el < min_o:
+				min_o = el
+	return min_o
+
+
+# 2. Функция поиска минимального значания из списка, сложность O(n) - линейная
+def min_search_2(min_list):
+	min_o = min_list[0]
+	for el in min_list:
+		if el < min_o:
+			min_o = el
+	return min_o
+
+
+l = [2, 7, 9, 12, 33, 5, 0, -2, -5]
+
+print(min_search(l))
+print(min_search_2(l))
