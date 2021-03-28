7dc2619c795db30098a1e9e6fef7c489b29d9f3c
nevidimk0 <nevidimk0@inbox.ru>
2021-03-28 21:43:28 +0300

    task1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,7 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание считается не принятым
 """
+import timeit
 
 
 def func_1(nums):
@@ -20,3 +21,28 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+def fill_list(number_of_items):
+    test_list = list(range(number_of_items))
+    return test_list
+
+
+numbers = fill_list(100000)
+print(func_1(numbers))
+print(func_2(numbers))
+
+print(timeit.timeit("func_1(numbers)", globals=globals(), number=1000))  # 19.684575000000002
+print(timeit.timeit("func_2(numbers)", globals=globals(), number=1000))  # 15.374730999999997
+
+"""
+Снизил время выполнения функции за счет использования list comprehension.
+list comprehension работают эффективней простого цикла в связи с отсутсвием цикла и постоянных вызовов append.
+list comprehension изначально создана для построения списков из итерируемых объектов.
+"""
+
