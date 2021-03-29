e2d4eb0c22bf9a61488e060f179712e324f2fef4
Dmitriy Rumyantsev <dr2moscow@yandex.ru>
2021-03-29 11:13:53 +0300

    Скорректировал решение, после решения других заданий по 1 уроку

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -14,3 +14,70 @@
 
 После реализации структуры, проверьте ее работу на различных сценариях
 """
+
+
+class TaskBoard:
+    """Класс структуры"""
+    def __init__(self):
+        self.__solved = []  # Список решенных задач
+        self.__basic = Queue()  # Очередь решаемых задач
+        self.__revision = Queue()  # Очередь на доработку
+
+    def add_task(self, task):
+        """Добавляет задачу в список решаемых"""
+        self.__basic.add(task)
+
+    def move_to_revision(self):
+        """Перемещает последнюю задачу из решаемых на доработку"""
+        self.__revision.add(self.__basic.remove())
+
+    def move_to_solved(self):
+        """Перемещает последнюю задачу из решаемых в решенные"""
+        self.__solved.append(self.__basic.remove())
+
+    def complete_revision(self):
+        """Перемещает последнюю задачу из списка на дорабоке в решенные"""
+        self.__solved.append(self.__revision.remove())
+
+    def __str__(self):
+        return 'Решаемые задачи: ' + str(self.__basic) + '\nЗадачи на доработку: ' + str(self.__revision) + \
+               '\nЗавершенные задачи: ' + ', '.join(self.__solved) + '\n'
+
+
+class Queue:
+    """Класс очереди"""
+    def __init__(self):
+        self.__lst = []
+
+    def add(self, element):
+        """Добавляет элемент в очередь"""
+        self.__lst.insert(0, element)
+
+    def remove(self):
+        """Убирает элемент из очереди"""
+        return self.__lst.pop()
+
+    def __len__(self):
+        """Возвращает длину очереди"""
+        return len(self.__lst)
+
+    def __str__(self):
+        return ', '.join(reversed(self.__lst))
+
+
+task_board = TaskBoard()
+
+task_board.add_task('Собрать команду')
+task_board.add_task('Придумать план')
+print(task_board)
+
+task_board.move_to_revision()
+print(task_board)
+
+task_board.add_task('Назначить встречу')
+task_board.move_to_solved()
+print(task_board)
+
+task_board.complete_revision()
+task_board.move_to_solved()
+print(task_board)
