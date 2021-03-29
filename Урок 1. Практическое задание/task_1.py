201f6d530a2c36c412cee8433186aba990a8a0e0
vvmokrenko <v.v.mokrenko@gmail.com>
2021-03-29 13:54:12 +0300

    HomeWork 1 by V.Mokrenko

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -27,10 +27,10 @@ def check_1(lst_obj):
     Алгоритм 3:
     Создать множество из списка
 
-    Сложность: !!!.
+    Сложность: O(n).
     """
-    lst_to_set = set(lst_obj)  # !!!
-    return lst_to_set  # !!!
+    lst_to_set = set(lst_obj)  # O(n) - линейная, где n = len(lst_obj)
+    return lst_to_set  # 0 - просто возвращаем указатель на множество
 
 
 #############################################################################################
@@ -42,12 +42,12 @@ def check_2(lst_obj):
     что такой элемент отстутствует
     в оставшихся справа элементах
 
-    Сложность: !!!.
+    Сложность: O(n^2).
     """
-    for j in range(len(lst_obj)):          # !!!
-        if lst_obj[j] in lst_obj[j+1:]:    # !!!
-            return False                   # !!!
-    return True                            # !!!
+    for j in range(len(lst_obj)):          # O(n) - линейная, где n = len(lst_obj)
+        if lst_obj[j] in lst_obj[j+1:]:    # 0(n) - линейная
+            return False                   # 0 - просто возвращаем значение 0
+    return True                            # 0 - просто возвращаем значение 1
 
 
 #############################################################################################
@@ -58,14 +58,14 @@ def check_3(lst_obj):
     Вначале выполним для списка сортировку, далее, сравниваем элементы попарно
     Если присутствуют дубли, они будут находиться рядом.
 
-    Сложность: !!!
+    Сложность: O(n log n)
     """
-    lst_copy = list(lst_obj)                 # !!!
-    lst_copy.sort()                          # !!!
-    for i in range(len(lst_obj) - 1):        # !!!
-        if lst_copy[i] == lst_copy[i+1]:     # !!!
-            return False                     # !!!
-    return True                              # !!!
+    lst_copy = list(lst_obj)                 # O(n) - линейная, где n = len(lst_obj)
+    lst_copy.sort()                          # O(n log n) - линейно-логарифмическая
+    for i in range(len(lst_obj) - 1):        # O(n) - линейная
+        if lst_copy[i] == lst_copy[i+1]:     # O(1) - константная
+            return False                     # 0
+    return True                              # 0
 
 #############################################################################################
 
