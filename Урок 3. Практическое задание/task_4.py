2c396510ab681fcaf46c63c34a4fd5bb571dec43
Demid <bydimko@yandex.ru>
2021-03-30 11:26:15 +0300

    lesson3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -9,3 +9,25 @@
 Можете условжнить задачу, реализовав ее через ООП
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+
+from hashlib import sha256
+from uuid import uuid4
+
+
+class Caching:
+    def __init__(self):
+        self.__cache = {}
+        self.__salt = uuid4().hex
+
+    def get_page(self, url):
+        if self.__cache.get(url):
+            print(f'Данный адрес: {url} присутствует в кэше')
+        else:
+            res = sha256(self.__salt.encode() + url.encode()).hexdigest()
+            self.__cache[url] = res
+            print(self.__cache)
+
+
+cache_url = Caching()
+cache_url.get_page('https://geekbrains.ru/')
+cache_url.get_page('https://geekbrains.ru/')
