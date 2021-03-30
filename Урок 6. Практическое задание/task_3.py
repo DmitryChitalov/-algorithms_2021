829c245c2fe7635c4fc2e603194237bde6beefa9
nikitinserg <nikitin.serg.alex@gmail.com>
2021-03-30 15:51:08 +0900

    Lesson 6 h/w (со второй задачей возникли проблемы)

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,43 @@
 Придумать как это решить!
 Есть очень простое решение!
 """
+
+from memory_profiler import profile
+
+numerical_series = [1]
+
+def numerical_series_sum(user_number, next_number = 1.0):
+    """
+    Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
+    :param user_number: вводимое число
+    :param next_number:
+    :return:
+    """
+    if user_number > 0:
+        new_number = -next_number / 2
+        numerical_series.append(new_number)
+        user_number -= 1
+        return numerical_series_sum(user_number - 1, new_number)
+    else:
+        return sum(numerical_series)
+
+# user_number = int(input('Введите количество элементов: '))
+user_number = 10
+
+@profile
+def sub_num(func):
+    return func
+
+func_num = sub_num(user_number)
+print(f'Количество элементов: {user_number}, их сумма: {func_num}')
+
+"""
+Записал рекурсивную фукцию в другую фукцию (этакий аналог декоратора)
+и теперь профилировщик вызывается один раз
+
+Line #    Mem usage    Increment  Occurences   Line Contents
+============================================================
+    31     18.2 MiB     18.2 MiB           1   @profile
+    32                                         def sub_num(func):
+    33     18.2 MiB      0.0 MiB           1       return func
+"""
