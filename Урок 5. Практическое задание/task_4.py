8f68b8d8fded43a5f373db127f41b3d9f8bdd637
Nikitarious <59883385+Nikitarious@users.noreply.github.com>
2021-03-30 19:01:26 +0300

    Add files via upload

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -1,8 +1,28 @@
 """
 Задача 4.
 Поработайте с обычным словарем и OrderedDict.
-Выполните операции, равные по смыслу, с каждым из объектов и сделайте замеры.
+Выполните различные операции с каждым из объектов и сделайте замеры.
 Опишите полученные результаты, сделайте выводы.
+"""
+from collections import OrderedDict
+from timeit import timeit
 
-И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
+my_dict = {i: i + 2 for i in range(1000000)}
+my_ordered = OrderedDict(my_dict)
+
+print('OrderedDict pop')
+print(timeit('my_ordered.popitem()', globals=globals(), number=1000000))
+print('Dict pop')
+print(timeit('my_dict.popitem()', globals=globals(), number=1000000))
+print('OrderedDict values')
+print(timeit('my_ordered.values', globals=globals(), number=1000000))
+print('Dict values')
+print(timeit('my_dict.values', globals=globals(), number=1000000))
+print('OrderedDict keys')
+print(timeit('my_ordered.keys', globals=globals(), number=1000000))
+print('Dict keys')
+print(timeit('my_dict.keys', globals=globals(), number=1000000))
+
+"""Cмысла в использовании OrderedDict нет, так как словарь в новых версиях Python уже упорядоченный,
+а увеличение в скорости незначительно
 """
