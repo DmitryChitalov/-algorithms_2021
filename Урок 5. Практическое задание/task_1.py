5729394fdfa4580693e2c70261877252ba9a63a8
nevidimk0 <nevidimk0@inbox.ru>
2021-03-31 22:25:11 +0300

    task1

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,31 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+from collections import namedtuple
+
+
+company_template = namedtuple('Company', 'q1 q2 q3 q4')
+number_of_companies = int(input("Введите количество предприятий для расчета прибыли: "))
+
+total_companies = {}
+income_collector = 0
+for i in range(number_of_companies):
+    current_company_name = input("Введите название предприятия: ")
+    current_company_stats = input("через пробел введите прибыль данного предприятия: ")
+    current_company_stats_lst = list(map(int, current_company_stats.split()))
+    current_company = company_template._make(current_company_stats_lst)
+    income_collector += sum(current_company)
+    total_companies[current_company_name] = current_company
+
+average_income = income_collector / number_of_companies
+print(f"Средняя годовая прибыль всех предприятий: {average_income}")
+print("Предприятия, с прибылью выше среднего значения: ", end="")
+for company, stats in total_companies.items():
+    if sum(stats) > average_income:
+        print(company, end=" ")
+print()
+print("Предприятия, с прибылью ниже среднего значения: ", end="")
+for company, stats in total_companies.items():
+    if sum(stats) < average_income:
+        print(company, end=" ")
+print()
