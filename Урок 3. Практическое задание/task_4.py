48541fd13bfbd219ffe3c102b2171dd1bbfc93d5
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2021-03-31 04:15:27 +0300

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -9,3 +9,25 @@
 Можете условжнить задачу, реализовав ее через ООП
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+
+import hashlib
+
+
+def site(url):
+    salt = 'https://'
+    print(salt)
+    if dict_of_site.get(url):
+        print("такой сайт уже есть")
+    else:
+        hash_for_dict = hashlib.sha256(salt.encode()+url.encode()).hexdigest()
+        dict_of_site[url] = hash_for_dict
+        print(dict_of_site)
+
+
+dict_of_site = {}
+
+site('https://stackoverflow.com/questions/48613002/sha-256-hashing-in-python')
+site('https://stackoverflow.com/questions/48613002/sha-256-hashing-in-python')
+site('https://docs-python.ru/standart-library/modul-hashlib-python/')
+site('https://python-scripts.com/sqlite')
+site('https://python-scripts.com/sqlite')
