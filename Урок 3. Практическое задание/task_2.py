6efda4bbbc03bad92f3528527ec835f63d7cee21
yanagerasimenko <yanagerasimenko@bk.ru>
2021-03-31 18:33:18 +0700

    Задание 3 (Герасименко Яна)

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -17,4 +17,58 @@
 
 Допускаются любые усложения задания - валидация, подключение к БД, передача данных в файл
 """
-# sqlite, postgres, db_api, orm
+
+import hashlib
+
+list_hash = []
+
+
+def psw_to_hash(psw):
+
+    slt = str(1234567)
+
+    result = hashlib.sha256(slt.encode() + psw.encode()).hexdigest()
+
+    list_hash.append(result)
+
+    return result
+
+
+f = open('text.txt', 'r+')
+
+for line in f:
+
+    list_hash.append(line)
+
+print(list_hash)
+
+print(psw_to_hash(input('Введите пароль: ')), file=f)
+
+
+print(f'Ваш номер будет: {len(list_hash) - 1}, запомните его хорошенько')
+
+f.close()
+
+print(list_hash[len(list_hash)-1])
+
+login = input('Введите выданный Вам номер: ')
+
+psw_ = input('Введите пароль: ')
+
+
+if list_hash[int(login)] == hashlib.sha256(str(1234567).encode() + psw_.encode()).hexdigest():
+
+    print('Вы авторизировались, поздравляю.')
+
+    print(f'Список первоначальный {list_hash[int(login)]}')
+
+    print(f'Список получивщийся {hashlib.sha256(str(1234567).encode() + psw_.encode()).hexdigest()}')
+
+else:
+    print('Вы не прошли авторизацию, что-то пошло не так!!!')
+
+    print(list_hash[int(login) - 1])
+
+    print(f'Список первоначальный {list_hash[int(login)]}')
+
+    print(f'Список получивщийся {hashlib.sha256(str(1234567).encode() + psw_.encode()).hexdigest()}')
--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -14,6 +14,25 @@
 ара
 р
 а
+a
 """
 
 # hash?
+
+input_string = str(input("Введите произвольную строку: "))
+
+
+def hash_function(string):
+
+    string_set = set()
+
+    for i in range(len(string)):
+
+        for j in range(len(string) - 1 if i == 0 else len(string), i, -1):
+
+            string_set.add(hash(string[i:j]))
+
+    return f"Количество различных подстрок в этой строке: {len(string_set)}"
+
+
+print(hash_function(input_string))
