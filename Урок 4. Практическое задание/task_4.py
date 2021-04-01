39264a24cedb7d84e3926dc4fae45a9b72fd64f9
vashchuk <vashchuk.work@gmail.com>
2021-04-01 16:18:14 +0300

    added less4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -12,6 +12,7 @@
 Без аналитики задание считается не принятым
 """
 
+import timeit
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -39,5 +40,23 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    total = (max(array, key=array.count))
+    total_n = array.count(total)
+    return f'Чаще всего встречается число {total} '\
+    f'оно появилось в массиве {total_n} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print('1 -- ', timeit.timeit('func_1()', 'from __main__ import func_1', number=1000))
+print('2 -- ', timeit.timeit('func_2()', 'from __main__ import func_2', number=1000))
+print('3 -- ', timeit.timeit('func_3()', 'from __main__ import func_3', number=1000))
+
+#1 --  0.0018369999999999984
+#2 --  0.0022369
+#3 --  0.0016716000000000023
+
+#в первых двух случаях происходят переборы и сравнение,
+# в третьем варианте используется встроеная функция она по скорости чуть быстрее
