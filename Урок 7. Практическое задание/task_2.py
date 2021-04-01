c4f8e9dcf7a58e80388c30e80ffed5db593c64c0
wifi-for-fridges <timofeewdanila@mail.ru>
2021-04-01 18:06:42 +0300

    ДЗ к уроку 7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -2,14 +2,198 @@
 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
-
 Хотя в примерах к уроку уже есть вариант реализации слияния,
 попробуйте предложить другой (придумать или найти)
-
 И попытаться сделать замеры на массивах разной длины: 10, 100, 1000, ...
-
 Пример:
 Введите число элементов: 5
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+from timeit import timeit
+from memory_profiler import profile
+from random import uniform
+
+
+# вариант реализации (похожий на код с урока), взятый с сайта
+# https://tproger.ru/translations/sorting-algorithms-in-python/
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    # Длина списков часто используется, поэтому создадим переменные для удобства
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            # Сравниваем первые элементы в начале каждого списка
+            # Если первый элемент левого подсписка меньше, добавляем его
+            # в отсортированный массив
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            # Если первый элемент правого подсписка меньше, добавляем его
+            # в отсортированный массив
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        # Если достигнут конец левого списка, элементы правого списка
+        # добавляем в конец результирующего списка
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        # Если достигнут конец правого списка, элементы левого списка
+        # добавляем в отсортированный массив
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+
+
+def merge_sort(nums):
+    # Возвращаем список, если он состоит из одного элемента
+    if len(nums) <= 1:
+        return nums
+
+    # Для того чтобы найти середину списка, используем деление без остатка
+    # Индексы должны быть integer
+    mid = len(nums) // 2
+
+    # Сортируем и объединяем подсписки
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+
+    # Объединяем отсортированные списки в результирующий
+    return merge(left_list, right_list)
+
+
+# код с урока
+def merge_sort_from_lesson(lst_obj):
+    if len(lst_obj) > 1:
+        center = len(lst_obj) // 2
+        left = lst_obj[:center]
+        right = lst_obj[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        # перестали делить
+        # выполняем слияние
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst_obj[k] = left[i]
+                i += 1
+            else:
+                lst_obj[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst_obj[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            lst_obj[k] = right[j]
+            j += 1
+            k += 1
+        return lst_obj
+
+
+@profile
+def mrg_sort_mem_profile(list_to_sort):
+    merge_sort(list_to_sort)
+
+
+@profile
+def mrg_sort_from_lesson_mem_profile(list_to_sort):
+    merge_sort_from_lesson(list_to_sort)
+
+
+numbers_list = [uniform(0, 49) for _ in range(10)]
+print(f'исходный массив: {numbers_list}')
+print(f'отсортированный массив: {merge_sort(numbers_list[:])}')
+
+# время - 0.017 с, память - 20 MiB
+print('новая реализация:')
+print(f"время выполения: {timeit('merge_sort(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_mem_profile(numbers_list[:])
+
+# время - 0.017 с, память - 20 MiB
+print('реализация с урока:')
+print(f"время выполения: {timeit('merge_sort_from_lesson(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_from_lesson_mem_profile(numbers_list[:])
+
+numbers_list = [uniform(0, 49) for _ in range(100)]
+
+# время - 0.24 с, память - 20 MiB
+print('новая реализация:')
+print(f"время выполения: {timeit('merge_sort(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_mem_profile(numbers_list[:])
+
+# время - 0.023 с, память - 20 MiB
+print('реализация с урока:')
+print(f"время выполения: {timeit('merge_sort_from_lesson(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_from_lesson_mem_profile(numbers_list[:])
+
+numbers_list = [uniform(0, 49) for _ in range(1000)]
+
+# время - 3.18 с, память - 20.3 MiB
+print('новая реализация:')
+print(f"время выполения: {timeit('merge_sort(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_mem_profile(numbers_list[:])
+
+# время - 3.28 с, память - 20 MiB
+print('реализация с урока:')
+print(f"время выполения: {timeit('merge_sort_from_lesson(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_from_lesson_mem_profile(numbers_list[:])
+
+numbers_list = [uniform(0, 49) for _ in range(10000)]
+
+# время - 40.29 с, память - 21.9 MiB
+print('новая реализация:')
+print(f"время выполения: {timeit('merge_sort(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_mem_profile(numbers_list[:])
+
+# время - 40.23 с, память - 21.7 MiB
+print('реализация с урока:')
+print(f"время выполения: {timeit('merge_sort_from_lesson(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_from_lesson_mem_profile(numbers_list[:])
+
+numbers_list = [uniform(0, 49) for _ in range(100000)]
+
+# время - 486.45 с, память - 25.4 MiB
+print('новая реализация:')
+print(f"время выполения: {timeit('merge_sort(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_mem_profile(numbers_list[:])
+
+# время - 494.23 с, память - 25.9 MiB
+print('реализация с урока:')
+print(f"время выполения: {timeit('merge_sort_from_lesson(numbers_list[:])', number=1000, globals=globals())}")
+print('занимаемая память:\n')
+mrg_sort_from_lesson_mem_profile(numbers_list[:])
+
+'''
+результаты временных замеров показали, что сложность алгоритма действительно O(NlogN), поскольку с ростом длины 
+входного массива время выполенения алгоритма сортировки (в сравнении с длиной входных данных) увеличивается 
+незначительно. при этом замеры памяти показали, что с ростом длины входных данных требуемый объем памяти 
+увеличивается существенно: 20.3 МиБ = 2660761.6 байт для входных данных длины 1000 и 25.4 = 3329228.8 байт для 
+входных данных длины 100000 причиной такого увеличения требуемого объема памяти является увеличение длины входного 
+массива чисел, также на потребляемый объем памяти влияет рекурсивная реализация алгоритма сортировки
+также результаты замеров времени выполнения и памяти показали, что реализация алгоритма из интернета несущественно
+уступает реализации алгоритма, приведенной на уроке. разница в показателях замеров соответствует погрешности
+'''
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -42,6 +42,207 @@ for i in
     left == right
     left.clear()
     right.clear()
-
-
 """
+
+from random import randint
+from timeit import timeit
+from memory_profiler import profile
+from statistics import median
+
+
+# решение с использованием гномьей сортировки
+
+# итоговая сложность O(n^2)
+def gnome_sort(list_to_sort):
+    i = 1
+    while i < len(list_to_sort):
+        if list_to_sort[i - 1] > list_to_sort[i]:
+            list_to_sort[i - 1], list_to_sort[i] = list_to_sort[i], list_to_sort[i - 1]
+
+            if i > 1:
+                i -= 1
+        else:
+            i += 1
+
+    return list_to_sort
+
+
+# итоговая сложность O(n^2), хотя и присутствует усовершенствование:
+# запоминаем позицию, с которой начались обмены, затем сразу же перепрыгнуть в эту позицию при завершении обменов
+def upgrade_gnome_sort(list_to_sort):
+    i = 1
+    j = 2
+    while i < len(list_to_sort):
+        if list_to_sort[i - 1] > list_to_sort[i]:
+            list_to_sort[i - 1], list_to_sort[i] = list_to_sort[i], list_to_sort[i - 1]
+            i -= 1
+
+            if i == 0:
+                i, j = j, j + 1
+        else:
+            i, j = j, j + 1
+
+    return list_to_sort
+
+
+@profile
+def gnome_sort_mem_prof(list_to_sort):
+    gnome_sort(list_to_sort)
+
+
+@profile
+def upgr_gnome_sort_mem_prof(list_to_sort):
+    upgrade_gnome_sort(list_to_sort)
+
+
+# решение без использования сортировки, итоговая сложность O(n^2)
+def get_median_variant_1(numbers_list):
+    left = []
+    right = []
+    for i in range(len(numbers_list)):
+        for j in range(len(numbers_list)):
+            if j == i:
+                continue
+            elif numbers_list[j] == numbers_list[i]:
+                if j < i:
+                    left.append(numbers_list[j])
+                else:
+                    right.append(numbers_list[j])
+            elif numbers_list[j] < numbers_list[i]:
+                left.append(numbers_list[j])
+            else:
+                right.append(numbers_list[j])
+
+        if len(left) == len(right):
+            return numbers_list[i]
+        else:
+            left.clear()
+            right.clear()
+
+
+@profile
+def get_med_var_1_mem_prof(numbers_list):
+    get_median_variant_1(numbers_list)
+
+
+# итоговая сложность O(n^2)
+def get_median_variant_2(numbers_list):
+    for i in range(len(numbers_list) // 2):     # сложность цикла O(n)
+        numbers_list.remove(max(numbers_list))  # сложность remove() - O(n), max() - O(n) => итоговая O(2n) = (n)
+
+    return max(numbers_list)                    # O(n)
+
+
+@profile
+def get_med_var_2_mem_prof(numbers_list):
+    get_median_variant_2(numbers_list)
+
+
+while True:
+    try:
+        list_len = int(input('введите желаемую длину списка: '))
+
+        if list_len % 2 == 0:
+            raise Exception
+        else:
+            break
+
+    except ValueError:
+        print('длина списка должна быть целым числом!')
+    except Exception:
+        print('список может быть лишь нечетной длины!')
+
+
+list_numbers = [randint(1, 100) for _ in range(list_len)]
+print(f'исходный список: {list_numbers}')
+
+list_numbers_sorted = gnome_sort(list_numbers[:])
+print(f'отсортированный список: {list_numbers_sorted}')
+print(f'медиана списка: {list_numbers_sorted[(len(list_numbers) - 1) // 2]}')  # 2m+1 = len() => m = (len()-1)/2
+
+print(f'медиана списка, найденная с помощью get_median_variant_1: {get_median_variant_1(list_numbers)}')
+print(f'медиана списка, найденная с помощью get_median_variant_2: {get_median_variant_2(list_numbers[:])}')
+print(f'медиана, найденная с помощью модуля statistics: {median(list_numbers)}')
+
+list_numbers = [randint(1, 100) for _ in range(11)]
+
+# время - 0.015 c, память - 19.7 MiB
+print(f"обычный вариант: {timeit('gnome_sort(list_numbers[:])', number=1000, globals=globals())}")
+gnome_sort_mem_prof(list_numbers[:])
+
+# время - 0.01 c, память - 19.7 MiB
+print(f"улучшенный вариант: {timeit('upgrade_gnome_sort(list_numbers[:])', number=1000, globals=globals())}")
+upgr_gnome_sort_mem_prof(list_numbers[:])
+
+# время - 0.015 c, память - 20.2 MiB
+print(f"get_med_var_1: {timeit('get_median_variant_1(list_numbers)', number=1000, globals=globals())}")
+get_med_var_1_mem_prof(list_numbers)
+
+# время - 0.0025 c, память - 20.2 MiB
+print(f"get_med_var_2: {timeit('get_median_variant_2(list_numbers[:])', number=1000, globals=globals())}")
+get_med_var_2_mem_prof(list_numbers[:])
+
+# время - 0.00053 с
+print(f"встроенная функция median(): {timeit('median(list_numbers)', number=1000, globals=globals())}")
+
+list_numbers = [randint(1, 100) for _ in range(101)]
+
+# время - 1.31 c, память - 19.7 MiB
+print(f"обычный вариант: {timeit('gnome_sort(list_numbers[:])', number=1000, globals=globals())}")
+gnome_sort_mem_prof(list_numbers[:])
+
+# время - 0.85 c, память - 19.7 MiB
+print(f"улучшенный вариант: {timeit('upgrade_gnome_sort(list_numbers[:])', number=1000, globals=globals())}")
+upgr_gnome_sort_mem_prof(list_numbers[:])
+
+# время - 1.26 c, память - 20.2 MiB
+print(f"get_med_var_1: {timeit('get_median_variant_1(list_numbers)', number=1000, globals=globals())}")
+get_med_var_1_mem_prof(list_numbers)
+
+# время - 0.083 c, память - 20.2 MiB
+print(f"get_med_var_2: {timeit('get_median_variant_2(list_numbers[:])', number=1000, globals=globals())}")
+get_med_var_2_mem_prof(list_numbers[:])
+
+# время - 0.0039 с
+print(f"встроенная функция median(): {timeit('median(list_numbers)', number=1000, globals=globals())}")
+
+list_numbers = [randint(1, 100) for _ in range(1001)]
+
+# время - 146.25 c, память - 19.7 MiB
+print(f"обычный вариант: {timeit('gnome_sort(list_numbers[:])', number=1000, globals=globals())}")
+gnome_sort_mem_prof(list_numbers[:])
+
+# время - 95.33 c, память - 19.7 MiB
+print(f"улучшенный вариант: {timeit('upgrade_gnome_sort(list_numbers[:])', number=1000, globals=globals())}")
+upgr_gnome_sort_mem_prof(list_numbers[:])
+
+# время - 167.26 c, память - 20.7 MiB
+print(f"get_med_var_1: {timeit('get_median_variant_1(list_numbers)', number=1000, globals=globals())}")
+get_med_var_1_mem_prof(list_numbers)
+
+# время - 7.17 c, память - 20.7 MiB
+print(f"get_med_var_2: {timeit('get_median_variant_2(list_numbers[:])', number=1000, globals=globals())}")
+get_med_var_2_mem_prof(list_numbers[:])
+
+# время - 0.071 с
+print(f"встроенная функция median(): {timeit('median(list_numbers)', number=1000, globals=globals())}")
+
+'''
+поскольку сложность получения конкретного элемента в списке O(1), будем считать, что данным временем выполнения 
+можно пренебречь => у варианта решения через сортировку замеры проводились лишь для реализаций самой сортировки. 
+результаты проведения временных замеров подтвердили тот факт, что теоретическая сложность алгоритма гномьей 
+сортировки O(n^2). однако, несмотря на это, улучшенный алгоритм показал существенно лучшую скорость выполнения в 
+сравнении с исходным алгоритмом сортировки. объем занимаемой памяти у обоих алгоритмов одинаков, это связано с тем, 
+что на вход обоих алгоритмов передаются копии различной длины одного и того же массива
+
+также было приведено 2 решения без использования сортировки. результаты замеров показали, что на массивах небольшой 
+длины (а именно, 11) скорость выполнения, примерно, одинакова. на массивах длины 101 появляется разница во времени 
+выполнения, на массивах большой длины вариант решения get_median_variant_2 через функции max() и remove() существенно 
+быстрее, чем вариант решения get_median_variant_1, также существенно быстрее любого из варианта сортировки. это 
+связано с тем, что происходит постоянное сокращение длины переданного в функцию массива. замеры памяти показали, 
+что оба варианта расходуют одинаковое количество памяти, это связано с тем, что в них передаются массивы одной и той 
+же длины, состоящие из одних и тех же типов данных 
+
+результаты временных замеров времени для встроенной функции median() показали, что она работает существенно быстрее
+всех приведенных вариантов решений во всех случаях
+'''
