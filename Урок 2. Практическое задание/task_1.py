32ac9a56a85d61f6081a343d71fe33b5c216d71d
kt <kogan.t@bssg.ru>
2021-04-03 14:50:03 +0300

    task 2.1

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -28,3 +28,36 @@
 Вы вместо трехзначного числа ввели строку (((. Исправьтесь
 Введите операцию (+, -, *, / или 0 для выхода):
 """
+
+import re
+
+
+def calculator() -> None:
+    sign = input("Введите операцию (+, -, *, / или 0 для выхода):")
+    print(bool(re.compile("^[+-/*]+$").match(sign)))
+    try:
+        if int(sign) == 0:
+            return None
+    except ValueError:
+        pass
+    try:
+        aa = input("Введите первое число:")
+        bb = input("Введите второе число:")
+        if sign.replace(" ", "") == "/" and float(bb) == 0.0:
+            print(" mda... ")
+        #
+        #
+        # this does not work, in multi condition!
+        # bool(re.compile("^[+-/*]+$").match(sign))
+        # why???
+        # if (bool(re.compile("^[+-/*]+$").match(sign)) and (len(sign) == 1)) or float(aa) or float(bb):
+        if len(sign) == 1:
+            if bool(re.compile("^[+-/*]+$").match(sign)) or float(aa) or float(bb):
+                print(eval(str(aa + sign + bb)))
+    except:
+        print("incorrect input, try again!..")
+    calculator()
+
+
+if __name__ == '__main__':
+    calculator()
