e9e97648f9aab33a3860d7419a840a5a8214e896
yanagerasimenko <yanagerasimenko@bk.ru>
2021-04-03 15:03:07 +0700

    Задание 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -1,22 +1,76 @@
 """
 Задание 1.
-
 Приведен код, который позволяет сохранить в
 массиве индексы четных элементов другого массива
-
 Сделайте замеры времени выполнения кода с помощью модуля timeit
-
 Оптимизируйте, чтобы снизить время выполнения
 Проведите повторные замеры
-
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание считается не принятым
 """
+import timeit
+
+
+numbers_list = [1, 2, 3, 4, 5, 6, 7]
 
 
 def func_1(nums):
-    new_arr = []
+
+    new_list = []
+
     for i in range(len(nums)):
+
         if nums[i] % 2 == 0:
-            new_arr.append(i)
-    return new_arr
+
+            new_list.append(i)
+
+    return new_list
+
+
+print('1 -- ', timeit.timeit('func_1(numbers_list)', 'from __main__ import func_1, numbers_list', number=1000))
+
+
+def func_2(numbers_list):
+
+    total = [i for j, i in enumerate(numbers_list) if not j % 2]
+
+
+print('2 -- ', timeit.timeit('func_2(numbers_list)', 'from __main__ import func_2, numbers_list', number=1000))
+
+
+def func_3(numbers_list):
+
+   numbers_list[::2]
+
+
+print('3 -- ', timeit.timeit('func_3(numbers_list)', 'from __main__ import func_3, numbers_list', number=1000))
+
+
+def func_4(numbers_list):
+
+    for i in range(0, len(numbers_list), 2):
+
+        True
+
+
+print('4 -- ', timeit.timeit('func_4(numbers_list)', 'from __main__ import func_4, numbers_list', number=1000))
+
+func_2(numbers_list)
+
+"""
+
+1ый вариант медленнее 4го, потому чтоу  нас просиходит перебор абсолютно всех элементов и только потом проверка
+деления на остаток. В 4ом варианте мы перебираем по одному, из-за этого перебор становится в 2 раза меньше.
+3ий вариант тоже быстрее исходного, так как мы береа по каждому 2ому элементу (не перебираем все, поэтому и быстрее).
+2ой вариант тоже быстрее исходного, так как enumerate сам перебирает индексы, а потом идет
+обычная проверка на четность (этот варинт самый быстрый).
+
+Значения с моего компьюетра:
+1 --  0.0006564000000000014
+2 --  0.0005964999999999998
+3 --  0.00012090000000000017
+4 --  0.0002488000000000004
+с помощью их, можно проверить точность моей аналитики.
+
+"""
+
--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -18,25 +18,97 @@
 """
 
 
-def simple(i):
+from timeit import timeit
+
+
+def func_1(i):  # O(N^2)
+
     """Без использования «Решета Эратосфена»"""
     count = 1
+
     n = 2
+
     while count <= i:
+
         t = 1
+
         is_simple = True
+
         while t <= n:
+
             if n % t == 0 and t != 1 and t != n:
+
                 is_simple = False
+
                 break
+
             t += 1
+
         if is_simple:
+
             if count == i:
+
                 break
+
             count += 1
+
         n += 1
+
     return n
 
 
-i = int(input('Введите порядковый номер искомого простого числа: '))
-print(simple(i))
+number = int(input('Введите порядковый номер искомого простого числа: '))
+
+print(func_1(number))
+
+
+def func_2(n):  # O(NlogN)
+
+    find = n
+
+    n = n * 100
+
+    a = [el for el in range(n + 1)]
+
+    a[1] = 0
+
+    number = 2
+
+    while number <= n:  # O(N)
+
+        if a[number] != 0:
+
+            j = number + number
+
+            while j <= n:  # O(logN)
+
+                a[j] = 0
+
+                j += number
+        number += 1
+
+    a = set(a)
+
+    a.remove(0)
+
+    a = sorted(list(a))
+
+    return a[find - 1]
+
+
+print(timeit('func_1(10)', globals=globals(), number=1000))
+
+print(timeit('func_2(10)', globals=globals(), number=1000))
+
+print(timeit('func_1(100)', globals=globals(), number=1000))
+
+print(timeit('func_2(100)', globals=globals(), number=1000))
+
+print(timeit('func_1(1000)', globals=globals(), number=1000))
+
+print(timeit('func_2(1000)', globals=globals(), number=1000))
+
+
+"""
+Чем больше взять порядковый номер, тем выше будет эффективность. Но на небольших числах эффективнее простой алгоритм.
+"""
