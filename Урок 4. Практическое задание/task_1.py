b452a1322c7ca8fc484b50f7224ca0ddbb5d1570
Demid <bydimko@yandex.ru>
2021-04-03 15:53:33 +0300

    lesson4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,8 @@
 Без аналитики задание считается не принятым
 """
 
+import timeit
+
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +22,50 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    # list comprehension
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+def func_3(nums):
+    # list comprehension + enumerate
+    return [i for i, el in enumerate(nums) if el % 2 == 0]
+
+
+print(f'{5 * "*"}1000{5 * "*"}')
+my_list = [el for el in range(1000)]
+func_list = ['func_1', 'func_2', 'func_3']
+for func in func_list:
+    print(f'Функция {func}:', timeit.timeit(func + '(my_list)', number=1000, globals=globals()))
+print(f'{5 * "*"}10000{5 * "*"}')
+# 10000
+my_list = [el for el in range(10000)]
+func_list = ['func_1', 'func_2', 'func_3']
+for func in func_list:
+    print(f'Функция {func}:', timeit.timeit(func + '(my_list)', number=1000, globals=globals()))
+print(f'{5 * "*"}100000{5 * "*"}')
+# 100000
+my_list = [el for el in range(100000)]
+func_list = ['func_1', 'func_2', 'func_3']
+for func in func_list:
+    print(f'Функция {func}:', timeit.timeit(func + '(my_list)', number=1000, globals=globals()))
+
+"""
+*****1000*****
+Функция func_1: 0.1058539
+Функция func_2: 0.07406679999999999
+Функция func_3: 0.09814900000000001
+*****10000*****
+Функция func_1: 1.2394902
+Функция func_2: 0.8881659000000002
+Функция func_3: 0.9372547
+*****100000*****
+Функция func_1: 14.745923699999999
+Функция func_2: 11.9250279
+Функция func_3: 14.097612299999998
+
+Все функции О(n) - линейная сложность
+Списковые включения отрабатывают быстрее, чем привычная реализация итераторов
+"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,10 @@
 Без аналитики задание считается не принятым
 """
 
+from timeit import timeit
+from random import randint
+from cProfile import run
+
 
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
@@ -36,3 +40,47 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def main(count_iter, number):
+    for i in range(count_iter):
+        revers_1(number)
+        revers_2(number)
+        revers_3(number)
+
+
+print('Замеры Timeit:')
+numb = randint(1000000, 1000000000)
+func_list = ['revers_1', 'revers_2', 'revers_3']
+for func in func_list:
+    print(f'Функция {func}:', timeit(func + '(numb)', number=1000000, globals=globals()))
+
+print('\nЗамеры cProfile:')
+run('main(1000000, numb)')
+
+"""
+Замеры Timeit:
+Функция revers_1: 2.9163433
+Функция revers_2: 1.7984789
+Функция revers_3: 0.3989035000000003
+
+Замеры cProfile:
+         12000004 function calls (3000004 primitive calls) in 7.723 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    7.723    7.723 <string>:1(<module>)
+10000000/1000000    4.497    0.000    4.497    0.000 task_3.py:21(revers_1)
+  1000000    2.060    0.000    2.060    0.000 task_3.py:31(revers_2)
+  1000000    0.441    0.000    0.441    0.000 task_3.py:39(revers_3)
+        1    0.725    0.725    7.723    7.723 task_3.py:45(main)
+        1    0.000    0.000    7.723    7.723 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+        
+revers_3 - оптимальный вариант O(n). Срез наиболее быстрый в плане выполнения
+задачи.  
+Рекурсия и цикл имеют арифмитические действия поэтому они проигрывают
+в скорости по сравнению со срезом в котором отсутствуют арифмитические действия.
+"""
