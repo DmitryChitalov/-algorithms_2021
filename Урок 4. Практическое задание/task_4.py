a8efce66a22afc1277762099dc819cb3952ca126
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2021-04-03 13:28:35 +0300

    Update task_4.py

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -1,18 +1,19 @@
 """
 Задание 4.
-
 Приведены два алгоритма. В них определяется число,
 которое встречается в массиве чаще всего.
-
 Сделайте профилировку каждого алгоритма через timeit
-
 Попытайтесь написать третью версию, которая будет самой быстрой.
 Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
-
 Без аналитики задание считается не принятым
 """
+import timeit
+from collections import Counter
+from statistics import mode
+from random import randint
 
-array = [1, 3, 1, 3, 4, 5, 1]
+#array = [1, 3, 1, 3, 4, 5, 1]
+array = [randint(-10, 10) for i in range(100)]
 
 
 def func_1():
@@ -39,5 +40,33 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    return f'Чаще всего встречается число {max(set(array), key = array.count)}'
+
+
+def func_4():
+    occurence_count = Counter(array)
+    return f'Чаще всего встречается число {occurence_count.most_common(1)[0][0]}'
+
+
+def func_5():
+    return f'Чаще всего встречается число {(mode(array))}'
+
+
 print(func_1())
+print("func_1", timeit.timeit("func_1()", globals=globals(), number=1000))
 print(func_2())
+print("func_2", timeit.timeit("func_2()", globals=globals(), number=1000))
+print(func_3())
+print("func_3", timeit.timeit("func_3()", globals=globals(), number=1000))
+print(func_4())
+print("func_4", timeit.timeit("func_4()", globals=globals(), number=1000))
+print(func_5())
+print("func_5", timeit.timeit("func_5()", globals=globals(), number=1000))
+
+"""
+из 5 вариантов решения первый остается самым быстрым, следующий по скорости вариант три,
+далее варианты: 2, 5, 4. НО на массивах больших размеров картина меняется и наиболее быстрым
+показывает себя метод 4 и 5 сильно обгоняя другие способы. следующий по эффективности способ 3.
+1 и 2 способы показывают самую низкую эффективность.
+"""
