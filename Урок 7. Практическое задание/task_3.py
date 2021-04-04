a3f959d47f705e07b9689c13ffc4c0e558033359
Fred <fred17@mail.ru>
2021-04-04 12:49:08 +0300

    lesson 7

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -54,23 +54,14 @@ def my_median(lst, n):
     lst = lst.copy()
     max_val = False
     while len(lst) > 1:
-        lst_var = []
         i = 0
-        retry = False
         if max_val:
             i = min(lst)
             max_val = False
         else:
             i = max(lst)
             max_val = True
-
-        for j in range(len(lst)):
-            if lst[j] != i or retry:
-                lst_var.append(lst[j])
-            else:
-                retry = True
-            if j + 1 == len(lst):
-                lst = lst_var
+        lst.remove(i)
     return lst[0]
 
 
