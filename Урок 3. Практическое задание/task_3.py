28e1d5db0e107be0e61ab9aef2af6857b1aabdd9
AndrewSanat <73751254+AndrewSanat@users.noreply.github.com>
2021-04-06 20:10:43 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,17 @@
 """
 
 # hash?
+
+
+def count_substrings(u_str):
+    rez_set = set()
+    for i in range(len(u_str)):
+        for j in range(i + 1, len(u_str) + 1):
+            rez_set.add(hash(u_str[i:j]))
+            print(f'{u_str[i:j]}, ', end='')
+    rez_set.remove(hash(u_str[:len(u_str)]))
+    return len(rez_set)
+
+
+user_str = 'zxxxx'
+print(f'\nВ строке "{user_str}" содержится {count_substrings(user_str)} уникальных подстрок')
