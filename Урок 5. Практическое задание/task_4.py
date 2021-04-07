8df51b262f1939b6bb042f3797cd45034cec471d
korshunov418 <Tolmagrad@mail.ru>
2021-04-07 21:33:26 +0700

    Update task_4.py

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,42 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from time import time
+from collections import OrderedDict
+
+dict_list = dict()
+or_dict = OrderedDict()
+### start insert ###
+start = time()
+for i in range(10000):
+    dict_list[str(i)]=i
+end = time()
+print(end - start)
+### end insert ###
+
+### start pop ###
+start = time()
+for i in range(10000):
+    dict_list.pop(str(i))
+end = time()
+print(end - start)
+### end pop ###
+
+
+### start update ###
+start = time()
+for i in range(10000):
+    or_dict.update({str(i):i})
+end = time()
+print(end - start)
+### end update ###
+
+### start pop ###
+start = time()
+for i in range(10000):
+    or_dict.pop(str(i))
+end = time()
+print(end - start)
+### end pop ###
+
+#OrderedDict с базовыми операциями работает хуже
