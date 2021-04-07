98bdbd5d928d2af06f9a0e18f5443ab38f0c5d96
d.paklin <d.paklin@gmail.com>
2021-04-07 19:35:16 +0300

    hw6

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,32 @@
 Придумать как это решить!
 Есть очень простое решение!
 """
+
+
+import memory_profiler
+
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        m1 = memory_profiler.memory_usage()
+        res = func(*args, **kwargs)
+        m2 = memory_profiler.memory_usage()
+        mem_diff = m2[0] - m1[0]
+        print(f'Выполнение заняло: {mem_diff} Mib')
+        return res
+    return wrapper
+
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+@decor
+def funct_():
+    return factorial(7)
+
+
+print(funct_())
