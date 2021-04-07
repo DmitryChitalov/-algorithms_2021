b989a91b5121d3b294b70d8cda990275a912f259
vetazakharova <53183417+vetazakharova@users.noreply.github.com>
2021-04-07 17:59:39 +0200

    tasks 1, 3, 4 done
    почему-то на функциях с командами pop и в 3, и в 4 заданиях возникает ошибка, не смогла это замерить

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,35 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+from collections import namedtuple
+
+def firm_rev_count():
+    num_firms = int(input("Введите количество компаний: "))
+    FIRMS = namedtuple("Firm", "name first_q second_q third_q fourth_q")
+    average_rev = {}
+
+    for i in range(num_firms):
+        FIRM = FIRMS(
+        name = input("введите название компании: "),
+        first_q = int(input("прибыль за первый квартал: ")),
+        second_q = int(input("прибыль за второй квартал: ")),
+        third_q = int(input("прибыль за третий квартал: ")),
+        fourth_q = int(input("прибыль за четвертый квартал: "))
+        )
+        average_rev[FIRM.name] = (FIRM.first_q + FIRM.second_q + FIRM.third_q + FIRM.fourth_q) / 4
+
+    average_rev_all = 0
+    for i in average_rev.values():
+        average_rev_all += i
+    average_rev_fin = average_rev_all / num_firms
+
+    print("Средняя годовая прибыль всех предприятий:", average_rev_fin)
+    for key, value in average_rev.items():
+        if value < average_rev_fin:
+            print(f"Предприятие, с прибылью ниже среднего значения: {key}")
+        elif value > average_rev_fin:
+            print(f"Предприятие, с прибылью выше среднего значения:{key}")
+        else:
+            print(f"Прибыль предприятия {key} равна среднему значению")
+
+firm_rev_count()
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -15,3 +15,93 @@ deque – это обобщение стеков и очередей.
 
 И добавить аналитику, так ли это или нет.!
 """
+from collections import deque
+import timeit
+
+list1 = [i for i in range(1000)]
+de = deque([i for i in range(1000)])
+# print(list1)
+# print(de)
+
+
+
+def l_append(lvar):
+    for i in range(1000):
+        lvar.append(i)
+
+def d_append(dvar):
+    for i in range(1000):
+        dvar.append(i)
+
+def l_insert(lvar):
+    for i in range(100):
+        lvar.insert(0, i)
+
+def d_apleft(dvar):
+    for i in range(100):
+        dvar.appendleft(i)
+
+def l_pop(lvar):
+    for i in range(100):
+        a = lvar.pop()
+
+def d_pop(dvar):
+    for i in range(100):
+        a = dvar.pop()
+
+def l_popleft(lvar):
+    for i in range(100):
+        a = lvar.pop(0)
+
+def d_popleft(dvar):
+    for i in range(100):
+        a = dvar.popleft()
+
+def l_reverse(lvar):
+    lreversed = lvar.reverse()
+
+def d_reverse(dvar):
+    dreversed = dvar.reverse()
+
+num = 10000
+
+# print("Measurments for the append command")
+# print("List: ", timeit.timeit('l_append(list1)', globals = globals(), number = num))
+# print("Deque: ", timeit.timeit('d_append(de)', globals = globals(), number = num))
+# print("Measurments for the insert command")
+# print("List: ", timeit.timeit('l_insert(list1)', globals = globals(), number = num))
+# print("Deque: ", timeit.timeit('d_apleft(de)', globals = globals(), number = num))
+print("Measurments for the pop command")
+print("List: ", timeit.timeit('l_pop(list1)', globals = globals(), number = num))
+print("Deque: ", timeit.timeit('d_pop(de)', globals = globals(), number = num))
+print("Measurments for the popleft command")
+print("List: ", timeit.timeit('l_popleft(list1)', globals = globals(), number = num))
+print("Deque: ", timeit.timeit('d_popleft(de)', globals = globals(), number = num))
+print("Measurments for the reverse command")
+print("List: ", timeit.timeit('l_reverse(list1)', globals = globals(), number = num))
+print("Deque: ", timeit.timeit('d_reverse(de)', globals = globals(), number = num))
+
+
+'''
+на коммандах добавления в начало списка/очереди очередь гораздо быстрее (для чего она и используется), предполагаю что на popleft была бы пожая картина.
+
+
+Measurments for the append command
+List:  0.7916738
+Deque:  0.5679875000000001
+
+Measurments for the insert command:
+List:  394.4995175
+Deque:  0.047056100000000003
+
+Measurments for the reverse command
+List:  0.0038516999999999996
+Deque:  0.009213599999999999
+
+
+
+
+
+
+
+'''
