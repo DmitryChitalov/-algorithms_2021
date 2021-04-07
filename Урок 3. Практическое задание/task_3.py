83727a178f04393a49ecb071dbef5fadf7c99a72
korshunov418 <Tolmagrad@mail.ru>
2021-04-07 21:30:49 +0700

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -16,4 +16,15 @@
 а
 """
 
-# hash?
+
+def equal_string(srt_val):
+    equal_r={}
+    for i in range(len(srt_val)):
+        for length in range(1, len(srt_val[i:]) + 1):
+            sub_str = srt_val[i:i + length]
+            if (srt_val != sub_str):
+                equal_r[hash(sub_str)]=sub_str
+    print( f"Уникальных строк{len(equal_r)}")
+    print( f"{ equal_r.values()}")
+    
+equal_string('papa')
