97499800b2485cd98e76ee6ad33385c456857178
vashchuk <vashchuk.work@gmail.com>
2021-04-08 15:45:55 +0300

    added less5

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,44 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+from timeit import timeit
+from collections import OrderedDict
+
+
+def dict_filler(my_dict):
+    my_dict = {k: k**2 for k in range(10000)}
+    return my_dict
+
+
+def dict_search(my_dict):
+    return my_dict[9000]
+
+
+def dict_pop(my_dict):
+    my_dict = {k: k ** 2 for k in range(10000)}
+    for i in range(1000, 9000):
+        my_dict.pop(i)
+    return my_dict
+
+
+my_dict = {}
+o_dict = OrderedDict()
+
+print(timeit('dict_filler(my_dict)', 'from __main__ import dict_filler, my_dict', number=100))
+print(timeit('dict_filler(o_dict)', 'from __main__ import dict_filler, o_dict', number=100))
+
+my_dict = dict_filler(my_dict)
+o_dict = dict_filler(o_dict)
+
+assert list(my_dict.items()) == list(o_dict.items())
+
+print(timeit('dict_filler(my_dict)', 'from __main__ import dict_filler, my_dict', number=100))
+print(timeit('dict_filler(o_dict)', 'from __main__ import dict_filler, o_dict', number=100))
+
+print(timeit('dict_pop(my_dict)', 'from __main__ import dict_pop, my_dict', number=100))
+print(timeit('dict_pop(o_dict)', 'from __main__ import dict_pop, o_dict', number=100))
+
+
+# Замеры времени показали одинаковые результаты.
+# Смысла использовать OrderedDict вместо обычного нет.
