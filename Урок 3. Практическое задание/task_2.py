109fda95b226371042adbf5609d563aa9fe0c400
Forseven <nutaru@gmail.com>
2021-04-08 23:04:05 +0700

    Lesson 3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -18,3 +18,48 @@
 Допускаются любые усложения задания - валидация, подключение к БД, передача данных в файл
 """
 # sqlite, postgres, db_api, orm
+from peewee import *
+import datetime
+from uuid import uuid4
+from hashlib import pbkdf2_hmac
+from binascii import hexlify
+
+db = SqliteDatabase('my_database.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    login = CharField(unique=True)
+    password_hash = CharField(unique=True)
+    salt = CharField(unique=False)
+
+
+def password_hash(password,salt):
+
+    obj = pbkdf2_hmac(hash_name='sha256',
+                      password=password.encode(),
+                      salt=salt.encode(),
+                      iterations=100000)
+    return hexlify(obj)
+
+db.connect()
+salt = uuid4().hex
+db.create_tables([User])
+login = (input('Введите login: '))
+password = (input('Введите пароль: '))
+
+
+user, created = User.get_or_create(login=login,password_hash=password_hash(password,salt),salt=salt)
+
+
+print(f'В базе данных хранится строка: {user.password_hash}')
+
+check_password = (input('Введите пароль еще раз для проверки: '))
+if password_hash(check_password,user.salt) == user.password_hash:
+    print('Вы ввели правильный пароль')
+else:
+    print('Вы ввели не правильный пароль')
