fc8d2511e4be1bcd1a45ceeec0f9d59aa90f5f23
asv108 <aver.serg1870@gmail.com>
2021-04-10 09:44:51 +0300

    lesson_8 tasks added

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,70 +10,90 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
-class BinaryTree:
+class Tree:
+    """Класс Бинарное дерево"""
     def __init__(self, root_obj):
-        # корень
+        """
+        Конструктор принимает
+        :param root_obj (int): корень дерева
+        """
+
         self.root = root_obj
-        # левый потомок
-        self.left_child = None
-        # правый потомок
-        self.right_child = None
+        self.left_child = None  # левый потомок
+        self.right_child = None  # правый потомок
 
-    # добавить левого потомка
-    def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+    def __str__(self):
+        return str(self.root)
 
-    # добавить правого потомка
-    def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+    def add_left(self, new_node):
+        """Метод добавления левого потомка"""
 
-    # метод доступа к правому потомку
+        try:
+            if self.root >= new_node:
+                if self.left_child is None:
+                    self.left_child = Tree(new_node)
+                else:
+                    # тогда вставляем новый узел
+                    tree_obj = Tree(new_node)
+                    # и спускаем имеющегося потомка на один уровень ниже
+                    tree_obj.left_child = self.left_child
+                    self.left_child = tree_obj
+            else:
+                raise Exception('Ошибка значения')
+        except Exception as e:
+            print(e)
+
+    def add_right(self, new_node):
+        """Метод добавления правого потомка"""
+        try:
+            if self.root <= new_node:
+                if self.right_child is None:
+                    self.right_child = Tree(new_node)
+                else:
+                    tree_obj = Tree(new_node)
+                    tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+            else:
+                raise Exception('Ошибка значения')
+        except Exception as e:
+            print(e)
+
     def get_right_child(self):
-        return self.right_child
+        """Метод доступа к правому потомку"""
+        try:
+            return self.right_child
+        except AttributeError:
+            print('Ошибка доступа к атрибуту класса.')
 
-    # метод доступа к левому потомку
     def get_left_child(self):
-        return self.left_child
+        """Метод доступа к левому потомку"""
+        try:
+            return self.left_child
+        except AttributeError:
+            print('Ошибка доступа к атрибуту класса.')
 
-    # метод установки корня
     def set_root_val(self, obj):
+        """Метод установки корня"""
         self.root = obj
 
-    # метод доступа к корню
     def get_root_val(self):
-        return self.root
+        """Метод доступа к корню"""
+        try:
+            return self.root
+        except AttributeError:
+            print('Ошибка доступа к атрибуту класса.')
 
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+if __name__ == '__main__':
+    r = Tree(10)
+    print(r.get_root_val())
+    print(r.get_left_child())
+    r.add_left(44)
+    r.add_left(5)
+    print(r.get_left_child())
+    print(r.get_left_child().get_root_val())
+    r.add_right(18)
+    print(r.get_right_child())
+    print(r.get_right_child().get_root_val())
+    r.get_right_child().set_root_val(14)
+    print(r.get_right_child().get_root_val())
