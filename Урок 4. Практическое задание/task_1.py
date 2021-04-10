32cd97adb299e77bc4c11735f2e47705f02a7ba9
ipavka_work <ipavka@ukr.net>
2021-04-10 17:32:17 +0300

    Lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,8 @@
 Без аналитики задание считается не принятым
 """
 
+from timeit import timeit
+
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +22,30 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+# Список для теста
+data_list = [i for i in range(1000)]
+
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+def func_3(nums):
+    return [i for i, el in enumerate(nums) if el % 2 == 0]
+
+
+if __name__ == '__main__':
+    for execut in [1, 10, 20, 50]:
+        print(f'Количество выполнений {execut}')
+        print(f'Вариант_1 {timeit("func_1(data_list)", number=execut, globals=globals())}')
+        print(f'Вариант_2 {timeit("func_2(data_list)", number=execut, globals=globals())}')
+        print(f'Вариант_3 {timeit("func_3(data_list)", number=execut, globals=globals())}')
+
+
+"""
+По O-нотации все три варианта, вместе с исходным имею линейную сложность O(n), но вариант 2 "func_2" самая быстрая,
+за счет быстрой реализации "List Comprehension". 3й вариант с "enumerate" и списковым включением почти 
+идентичен по скорости со вторым, но чуть проигрывает. 
+"""
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -12,6 +12,8 @@
 Без аналитики задание считается не принятым
 """
 
+from timeit import timeit
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -39,5 +41,67 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+# print(func_1())
+# print(func_2())
+
+
+# ***********************************************************************************
+
+
+def func_3():
+    """ List Comprehension """
+    new_array = [array.count(i) for i in array]
+    max_2 = max(new_array)
+    elem = array[new_array.index(max_2)]
+    return f'Чаще всего встречается число {elem}, ' \
+           f'оно появилось в массиве {max_2} раз(а)'
+
+
+def func_4():
+    """ Встроенная функция max() и аргумент key """
+    max_2 = max(array, key=array.count)
+    elem = array.count(max_2)
+    return f'Чаще всего встречается число {max_2}, ' \
+           f'оно появилось в массиве {elem} раз(а)'
+
+
+def func_5():
+    m = 0
+    num = 0
+    for i in set(array):
+        count = array.count(i)
+        if count > m:
+            m = count
+            num = i
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {m} раз(а)'
+
+
+if __name__ == '__main__':
+    print(f'func_1 {timeit("func_1()", number=1000000, globals=globals())}')
+    print(f'func_2 {timeit("func_2()", number=1000000, globals=globals())}')
+    print(f'func_3 {timeit("func_3()", number=1000000, globals=globals())}')
+    print(f'func_4 {timeit("func_4()", number=1000000, globals=globals())}')
+    print(f'func_5 {timeit("func_5()", number=1000000, globals=globals())}')
+
+# Результаты замеров
+"""
+func_1 1.034237611
+func_2 1.4021699449999998
+func_3 1.2803982509999998
+func_4 1.099538726
+func_5 0.8992509269999998
+"""
+
+"""
+Сделал три варианта: "func_3" "func_4" "func_5" 
+- func_3 почти идентична исходной "func_2" заменил цикл for на списковое включение, 
+эффективность чуть увеличилась.
+- func_4 быстрее за счет встроенной функции max() и аргумент "key", как вы и подсказывали а лекции,
+по идее это самый быстрый вариант.
+Но!!!
+Исходя из замеров
+- func_5 чуть быстрее за счет set() для исходного списка, там самым сокращая кол-во итераций в цикле 
+по исходному списку
+
+"""
