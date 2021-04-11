c7defe3316f8eee83f32ade1eb09618298a4e4db
YanaGerasimenko <yanagerasimenko@bk.ru>
2021-04-11 22:50:29 +0700

    ДЗ 8 (Герасименко Яна)

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,7 +10,9 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
 class BinaryTree:
+
     def __init__(self, root_obj):
         # корень
         self.root = root_obj
@@ -21,6 +23,19 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
+
+        try:
+
+            if new_node > self.root:
+
+                raise ValueError
+
+        except ValueError:
+
+            print(f'Вставляемое значение {new_node} > значения корня {self.root}')
+            print('Завершение программы (аварийное)')
+            exit(1)
+
         # если у узла нет левого потомка
         if self.left_child == None:
             # тогда узел просто вставляется в дерево
@@ -36,6 +51,20 @@ class BinaryTree:
 
     # добавить правого потомка
     def insert_right(self, new_node):
+
+        try:
+
+            if new_node <= self.root:
+
+                raise ValueError
+
+        except ValueError:
+
+            print(f'Вставляемое значение {new_node} <= значения корня {self.root}')
+
+            print('Завершение программы (аварийное)')
+
+            exit(1)
         # если у узла нет правого потомка
         if self.right_child == None:
             # тогда узел просто вставляется в дерево
@@ -48,32 +77,62 @@ class BinaryTree:
             # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.right_child = self.right_child
             self.right_child = tree_obj
-
     # метод доступа к правому потомку
+
     def get_right_child(self):
+
         return self.right_child
 
     # метод доступа к левому потомку
     def get_left_child(self):
+
         return self.left_child
 
     # метод установки корня
     def set_root_val(self, obj):
+
         self.root = obj
 
     # метод доступа к корню
     def get_root_val(self):
+
         return self.root
 
 
 r = BinaryTree(8)
+
 print(r.get_root_val())
+
 print(r.get_left_child())
+
 r.insert_left(40)
+
 print(r.get_left_child())
+
 print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
+
 print(r.get_right_child().get_root_val())
+
+r.insert_right(12)
+
+print(r.get_right_child())
+
+print(r.get_right_child().get_root_val())
+
 r.get_right_child().set_root_val(16)
+
+print(r.get_right_child().get_root_val())
+
+r.insert_left(5)
+
+print(r.get_left_child())
+
+print(r.get_left_child().get_root_val())
+
+r.insert_right(6)
+
+print(r.get_left_child().get_root_val())
+
+print(r.get_right_child())
+
 print(r.get_right_child().get_root_val())
