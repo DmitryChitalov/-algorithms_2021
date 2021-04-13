45c9b319a18e3149d237f6c0d40949faac9813a2
Demid <bydimko@yandex.ru>
2021-04-13 22:02:17 +0300

    lesson7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -19,3 +19,77 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+
+from random import randint
+from timeit import timeit
+
+
+def bubble_sort(arr):
+    n = 1
+    while n < len(arr):
+        for i in range(len(arr) - n):
+            if arr[i] < arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        n += 1
+    return arr
+
+
+def bubble_sort_upd(arr):
+    n = 1
+    while n < len(arr):
+        swap = False
+        for i in range(len(arr) - n):
+            if arr[i] < arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swap = True
+        if not swap:
+            return arr
+        n += 1
+    return arr
+
+
+list_rnd = [randint(-100, 100) for _ in range(200)]
+list_opt = [el for el in range(200, 0, -1)]
+list_neg = [el for el in range(200)]
+
+print('Замеры на рандомном массиве')
+print(timeit('bubble_sort(list_rnd[:])',
+             number=1000,
+             globals=globals()))
+print(timeit('bubble_sort_upd(list_rnd[:])',
+             number=1000,
+             globals=globals()))
+print('\nЗамеры на оптимальном массиве')
+print(timeit('bubble_sort(list_opt[:])',
+             number=1000,
+             globals=globals()))
+print(timeit('bubble_sort_upd(list_opt[:])',
+             number=1000,
+             globals=globals()))
+print('\nЗамеры на худшем варианте массива')
+print(timeit('bubble_sort(list_neg[:])',
+             number=1000,
+             globals=globals()))
+print(timeit('bubble_sort_upd(list_neg[:])',
+             number=1000,
+             globals=globals()))
+
+"""
+Замеры на рандомном массиве
+4.6160997
+4.4848432
+
+Замеры на оптимальном массиве
+2.2553229000000012
+0.04780640000000069
+
+Замеры на худшем варианте массива
+6.6131954
+6.879781899999998
+
+Исходя из полученых результатов видно, что при рандомных входящих данных и самом негативном варинте результаты схожи.
+При оптимальном варианте массива доработаная сортировка пузырьком ожидаемо работает гараздо быстрее.
+Поскольку оптимизированая функция не имеет проигрыша во времени на случайных данных, но может быть гараздо быстрее
+при позитивных варинтах массива доработка оправдана.
+"""
