409d3854d03fc848d8dba77b9f8a23a88df10f7b
riazantcev <d.riazantcev@gmail.com>
2021-04-13 15:47:24 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,16 @@
 """
 
 # hash?
+
+
+def unique_substr(str_obj):
+    substr_list = set()
+    for i in range(1, len(str_obj)):
+        for j in range(0, len(str_obj)):
+            el = str_obj[j:j+i]
+            substr_list.add(hash(el.encode('utf-8')))
+    return len(substr_list)
+
+
+my_str = 'papa'
+print(unique_substr(my_str))
