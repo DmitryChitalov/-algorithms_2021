3b8fc4ad0cdb9c8d708324b2880da1c946d43fc4
fosemberg <fosemberg@gmail.com>
2021-04-13 19:05:01 +0300

    3.3 add test cases

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,28 @@
 """
 
 # hash?
+
+def count_uniq_sub_strings(string):
+    return string
+
+test_cases = [
+    {
+        'props': 'рара',
+        'result': 6,
+    },
+    {
+        'props': 'pa',
+        'result': 2,
+    },
+]
+
+passed_tests = 0
+for test_case in test_cases:
+    props = test_case["props"]
+    expected_result = test_case["result"]
+    real_result = count_uniq_sub_strings(props)
+    is_ok = real_result == expected_result
+    if is_ok:
+        passed_tests += 1
+    print(f'{is_ok} {count_uniq_sub_strings.__name__}({props}) => {real_result}{(f", but {expected_result} expected", "")[is_ok]}')
+print(f'passed tests: {passed_tests}/{len(test_cases)}\n')
