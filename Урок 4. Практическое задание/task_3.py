5bc6221c7ffd3f1f572f94fcaef3010b02f53cfd
Roman-Ch-32 <82414197+Roman-Ch-32@users.noreply.github.com>
2021-04-15 19:47:55 +0300

    Add files via upload

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -12,6 +12,8 @@
 И можете предложить еще свой вариант решения!
 Без аналитики задание считается не принятым
 """
+import cProfile
+from timeit import timeit
 
 
 def revers_1(enter_num, revers_num=0):
@@ -36,3 +38,21 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+num = 123123131
+
+cProfile.run('revers_1(num)')
+cProfile.run('revers_2(num)')
+cProfile.run('revers_3(num)')
+
+print(f'1 - {timeit("revers_1(num)", "from __main__ import revers_1, num", number=10000)}')
+print(f'2 - {timeit("revers_2(num)", "from __main__ import revers_2, num", number=10000)}')
+print(f'3 - {timeit("revers_3(num)", "from __main__ import revers_3, num", number=10000)}')
+
+
+"""
+Третья функция самая быстрая, т.к. написана на основе встроенных функций
+Вторая немного быстрее первой, т.к. в ней применён цикл.
+Первая самая медленная из-за использования рекурсии.
+"""
