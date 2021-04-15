1a1be4204e0d748353d78c77ede06b4b3e601f60
EgorDubkov <egord2323@gmail.com>
2021-04-15 17:52:13 +0300

    Update task_5.py

--- Урок 2. Практическое задание/task_5.py
+++ Урок 2. Практическое задание/task_5.py
@@ -23,3 +23,19 @@
 и допускается комб-е - цикл и рекурсия
 """
 
+from random import random
+n = round(random() * 100)
+i = 1
+print("Компьютер загадал число. Отгадайте его. У вас 10 попыток")
+while i <= 10:
+    u = int(input(str(i) + '-я попытка: '))
+    if u > n:
+        print('Много')
+    elif u < n:
+        print('Мало')
+    else:
+        print('Вы угадали с %d-й попытки' % i)
+        break
+    i += 1
+else:
+    print('Вы исчерпали 10 попыток. Было загадано', n)
