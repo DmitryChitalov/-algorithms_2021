ae2d1456a7ffdb3512d980a6ad1d6b7f7612a776
EgorDubkov <egord2323@gmail.com>
2021-04-15 17:55:02 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -14,9 +14,25 @@
 """
 
 
+
+from timeit import timeit
+
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+data = [i for i in range(1000)]
+
+print(f'Вариант_1 {timeit("func_1(data)", number=1000, globals=globals())}')
+print(f'Вариант_2 {timeit("func_2(data)", number=1000, globals=globals())}')
+
+#вариант 2 "func_2" быстрее, чем 1, так как реализация не использует append 
