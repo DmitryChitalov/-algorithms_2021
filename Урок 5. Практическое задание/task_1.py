cc707e225e4c3fbdebdee3052bd28529b1b33efd
fbatch <fyodor.batchkala@gmail.com>
2021-04-17 11:23:09 +0300

    hw-5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -5,7 +5,7 @@
 и вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 
-Подсказка:
+Подсказка: namedtuple (но не обязательно)
 Для решения задачи обязательно примените какую-нибудь коллекцию из модуля collections
 
 Пример:
@@ -23,3 +23,40 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+from collections import namedtuple
+
+
+amount_of_firms = int(input('Введите количество компаний для расчета прибыли: '))
+firms_list = []
+firm = namedtuple('firm', 'name profit')
+
+for i in range(amount_of_firms):
+    firms_list.append(firm(
+        name=input('Введите название компании: '),
+        profit=list(map(int, input('Через пробел введите прибыль компании за каждый квартал '
+                                   '(Всего 4 квартала): ').split()))
+    ))
+
+
+average_profit = 0
+for i in firms_list:
+    average_profit += sum(i.profit)/len(firms_list)
+
+
+def func(companies, average, top_firms = [], low_firms = []):
+    if len(companies) <= 0:
+        return top_firms, low_firms
+    else:
+        add_firm, profit = companies.pop()
+        if sum(profit) > average:
+            top_firms.append(add_firm)
+        else:
+            low_firms.append(add_firm)
+        return func(companies, average, top_firms, low_firms)
+
+
+higher_company, lower_company = func(firms_list, average_profit)
+
+print(f'Средняя годовая прибыль всех предприятий: {average_profit}')
+print(f'Предприятия, с прибылью выше среднего значения: {", ".join(higher_company)}')
+print(f'Предприятия, с прибылью ниже среднего значения: {", ".join(lower_company)}')
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -15,3 +15,57 @@ deque – это обобщение стеков и очередей.
 
 И добавить аналитику, так ли это или нет.!
 """
+from collections import deque
+from timeit import timeit
+
+
+dque = deque(range(1, 100))
+lst = [i for i in range(100)]
+other_lst = ['a', 'b', 'c']
+
+def append_list(lst):
+    lst.insert(0, 5)
+    return lst
+
+print('Добавление элементов в list:')
+print(timeit("append_list(lst)", globals=globals(), number=100000))
+
+def append_deque(dque):
+    dque.appendleft(5)
+    return dque
+
+print('Добавление элементов в deque: ')
+print(timeit("append_deque(dque)", globals=globals(), number=100000))
+
+def pop_list(lst):
+    lst.pop(0)
+    return lst
+
+print('Удаление первого элемента list:')
+print(timeit("pop_list(lst)", globals=globals(), number=100000))
+
+def pop_deque(dque):
+    dque.popleft()
+    return dque
+
+print('Удаление первого элемента deque:')
+print(timeit("pop_deque(dque)", globals=globals(), number=100000))
+
+def ext_list(lst):
+    lst = other_lst + lst
+    return lst
+
+print('Добавление элемента из другого списка в list')
+print(timeit("ext_list(lst)", globals=globals(), number=100000))
+
+def ext_deque(dque):
+    dque.extendleft(lst)
+    return dque
+
+print('Добавление элемента из другого списка в deque')
+print(timeit("ext_deque(dque)", globals=globals(), number=100000))
+
+# Метод deque имеет большую скорость при добавлении элементов в лист и
+# при удалении первого элемента.
+# Но добавление элемента из другого списка методом extendleft работает медленее.
+# Иногда, метод deque будет работать быстрее.
