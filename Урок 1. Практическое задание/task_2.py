3e85cc6d1003ea4a96eadcafc8e61c88cae67198
rtretyakov <rtretyakov@outlook.com>
2021-04-22 07:17:40 +1000

    commit and push

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -6,13 +6,53 @@
 Первый, в виде функции, должен обеспечивать поиск минимального значения для списка.
 В основе алгоритма должно быть сравнение каждого числа со всеми другими элементами списка.
 Сложность такого алгоритма: O(n^2) - квадратичная.
+"""
+import random
 
+
+def find_min_item_quad(lst):                # O(N^2)
+
+    min_value = lst[0]                      # O(1)
+
+    for i in range(len(lst)):               # O(N)
+        for j in range(len(lst)):           # O(N)
+            if i != j:                      # O(1)
+                if lst[i] < lst[j]:         # O(1)
+                    if lst[i] < min_value:  # O(1)
+                        min_value = lst[i]  # O(1)
+                else:
+                    if lst[j] < min_value:  # O(1)
+                        min_value = lst[j]  # O(1)
+
+    return min_value
+
+
+"""
 Второй, в виде функции, должен обеспечивать поиск минимального значения для списка.
 Сложность такого алгоритма: O(n) - линейная.
+"""
 
+
+def find_min_item_line(lst):        # O(N)
+
+    min_value = lst[0]              # O(1)
+
+    for i in range(len(lst)-1):     # O(N)
+        if lst[i+1] < min_value:    # O(1)
+            min_value = lst[i+1]    # O(1)
+
+    return min_value
+
+
+"""
 Не забудьте указать сложность каждого из двух алгоритмов. Для лучшего закрепления темы
 можете определить и указать сложность еще и у каждого выражения этих двух алгоритмов.
 
 Примечание:
 Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
 """
+
+sample_lst = random.sample(range(-100000, 100000), 100)
+print(sample_lst)
+print(find_min_item_quad(sample_lst))
+print(find_min_item_line(sample_lst))
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -20,3 +20,56 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+import random
+
+
+def get_companies_ver01(max_count):                 # O(N) - линейная сложность
+
+    comp_dict = {'Компания № ' + str(i + 1): random.randint(0, 10000) for i in range(max_count)}    # O(len(lst))
+    print('Список компаний: ', comp_dict)           # O(len(lst))
+
+    print('3 компании с максимальной прибылью:')
+    for i in range(1, 4):                           # O(1)
+        max_profit = 0                              # O(1)
+        name_company = ''                           # O(1)
+
+        for el in comp_dict.items():                # O(len(lst))
+            if el[1] > max_profit:                  # O(1)
+                max_profit = el[1]                  # O(1)
+                name_company = el[0]                # O(1)
+
+        print(f'{i} место: {name_company}, {max_profit}')   # O(1)
+        comp_dict.pop(name_company)                         # O(1)
+
+
+def get_companies_ver02(max_count):                 # O(N) - линейная сложность
+
+    comp_lst = ['Компания № ' + str(i + 1) for i in range(max_count)]   # O(N)
+    profit_lst = [random.randint(0, 10000) for i in range(max_count)]   # O(N)
+
+    print(comp_lst)                                 # O(N)
+    print(profit_lst)                               # O(N)
+
+    print('3 компании с максимальной прибылью:')
+    for i in range(1, 4):
+        max_profit = 0                              # O(1)
+        name_company = ''                           # O(1)
+        id_company = 0                              # O(1)
+
+        for j in range(len(profit_lst)):            # O(N)
+            if profit_lst[j] > max_profit:          # O(1)
+                max_profit = profit_lst[j]          # O(1)
+                name_company = comp_lst[j]          # O(1)
+                id_company = j
+
+        print(f'{i} место: {name_company}, {max_profit}')   # O(1)
+        comp_lst.pop(id_company)                            # O(N)
+        profit_lst.pop(id_company)                          # O(N)
+
+
+get_companies_ver01(20)
+get_companies_ver02(20)
+
+# В приведенных примерах сложность линейная, зависит от количества элементов в словаре/списке.
+# Тем не менее при использовании словаря скорость выполнения будет выше, так как меньшее количество
+# операций имеет сложность O(N)
