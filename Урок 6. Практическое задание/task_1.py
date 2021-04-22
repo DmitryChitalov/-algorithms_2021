17f27be402de38d0fd47f8fccacfe256dd0e4bda
EgorDubkov <egord2323@gmail.com>
2021-04-22 18:51:33 +0300

    Update task_1.py

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -21,3 +21,74 @@
 Попытайтесь дополнительно свой декоратор используя ф-цию memory_usage из memory_profiler
 С одновременным замером времени (timeit.default_timer())!
 """
+
+from timeit import default_timer
+from memory_profiler import memory_usage
+
+
+def memory_profile(func):
+    def wrapper(*args):
+        start_time = default_timer()
+        start_memory = memory_usage()
+        func(*args)
+        end_time = default_timer()
+        end_memory = memory_usage()
+        print(f'Временные затраты: {end_time - start_time}\n Затраты памяти: {end_memory[0] - start_memory[0]}')
+
+    return wrapper
+
+# script 1
+
+@memory_profile
+def iter_used(lst):
+    for i in range(len(lst)):
+        lst[i] = str(lst[i])
+    return lst
+
+
+@memory_profile
+def map_used(lst):
+    return list(map(str, lst))
+
+
+my_list = [x+10 for x in range(1000000)]
+print('\n\nСкрипт 2 \nБез map():')
+iter_used(my_list)
+print('\nС map():')
+map_used(my_list)
+
+'''
+Без использования функции map():
+Временные затраты: 0.385755195
+Затраты памяти: 36.07421875
+С использованием функции map():
+Временные затраты: 0.2230677710000002
+Затраты памяти: 6.78515625
+Функция map_used эффективнее
+
+# script 2
+
+@memory_profile
+def get_list(n):
+    return [i**2 for i in range(n)]
+
+
+@memory_profile
+def get_dict(n):
+    return {i**2: i for i in range(n)}
+
+k = 100000
+
+print('\n\nСкрипт 3\nСоздание словаря:')
+get_dict(k)
+print('\nСоздание списка:')
+get_list(k)
+
+'''
+Создание словаря:
+Временные затраты: 0.15319952100000012
+Затраты памяти: 10.96875
+Создание списка:
+Временные затраты: 0.14219764099999987
+Затраты памяти: 5.98828125
+Создание словаря занимает больше памяти, чем создание списка
