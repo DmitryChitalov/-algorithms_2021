5516806547ae19c7ac3395fda70c3411165bb621
Roman-Ch-32 <82414197+Roman-Ch-32@users.noreply.github.com>
2021-04-22 17:02:18 +0300

    Add files via upload

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,29 @@
 Придумать как это решить!
 Есть очень простое решение!
 """
+
+from memory_profiler import profile
+
+
+
+def sum_n(n):
+    if n == 0:
+        return 0
+    else:
+        return n + sum_n(n-1)
+
+
+print(sum_n(5))
+
+
+@profile()
+def alt(n):
+    def sum_n1(*args):
+        if n == 0:
+            return 0
+        else:
+            return n + sum_n1(n - 1)
+    return alt, print(sum_n(n))
+
+
+print(alt(5))
