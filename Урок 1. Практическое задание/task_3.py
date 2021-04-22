d9322fb6de0e35434cf1143e48ed5364348aae91
valvandor <sincov.vitalii@gmail.com>
2021-04-22 15:17:59 +0300

    refactor task3, test from PyCharm

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -39,13 +39,13 @@ def get_richest_firms_2(income_dict):
         Возвращает список, состоящий из кортежей компаний и их наибольшей годовой прибыли (3 штуки)
         Сложность алгоритма: O(N^2) - квадратичная  (сортировка методом выбора)
     """
-    richest_firms = [(key, value) for key, value in income_dict.items()]  # O(N) + O(N)
-    for pos in range(len(richest_firms) - 1):  # O(N)
-        for k in range(pos + 1, len(richest_firms)):  # O(N)
-            if richest_firms[k][1] < richest_firms[pos][1]:  # сравниваем значения из словарей
-                richest_firms[k], richest_firms[pos] = richest_firms[pos], richest_firms[k]  # O(1)
-    richest_firms.reverse()  # O(N)
-    return richest_firms[:3]
+    all_firms = [(key, value) for key, value in income_dict.items()]  # O(N) + O(N)
+    for pos in range(len(all_firms) - 1):  # O(N)
+        for k in range(pos + 1, len(all_firms)):  # O(N)
+            if all_firms[k][1] < all_firms[pos][1]:  # сравниваем значения из словарей
+                all_firms[k], all_firms[pos] = all_firms[pos], all_firms[k]  # O(1)
+    all_firms.reverse()  # O(N)
+    return all_firms[:3]
 
 
 def get_richest_firms_3(income_dict):
