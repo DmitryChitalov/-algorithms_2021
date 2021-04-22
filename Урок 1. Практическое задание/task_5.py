124c33f13aedaf319957fa021f9bd9b5e07322a6
valvandor <sincov.vitalii@gmail.com>
2021-04-22 22:23:09 +0300

    complete tasks 5-7

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,71 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class StacksOfPlates:
+    """ Класс реализует структуру данных набор стэков,
+        представляющий собой список, каждый элемент которого является стеком с ограничением по числу max_height.
+    """
+    def __init__(self, number_plates: int, max_height: int = 10):  # по умолчанию пусть будет в стопке по 10
+        self.max_height = max_height
+        self.number_plates = number_plates
+        self.stacks = []  # [[10], [10], [3]]
+        if self.number_plates:
+            self.stacks.append(self.number_plates)
+            while self.stacks[-1] > self.max_height:
+                self.stacks.append(self.stacks[-1] - self.max_height)
+                self.stacks[-2] = self.max_height
+
+    def is_empty(self):
+        """ Возвращает булевское значение, пустой ли список """
+        return self.stacks == [] or self.stacks == [0]
+
+    def get_size_last_element(self):
+        """ Возвращает число последнего стека"""
+        if self.is_empty():
+            return 0
+        elif self.stacks[-1] == self.max_height:
+            return 10
+        else:
+            return self.stacks[-1]
+
+    def push_in(self, plates: int):
+        """ Добавляет тарелки в последний стек. Если стек переполнен - создает новый"""
+        if self.is_empty():
+            self.stacks.append(plates)
+        else:
+            self.stacks[-1] += plates
+        while self.stacks[-1] > self.max_height:
+            self.stacks.append(self.stacks[-1] - self.max_height)
+            self.stacks[-2] = self.max_height
+
+    def pop_out(self, plates: int):
+        """ Забирает тарелки с последнего стека. Если стек заканчивается - забирает со следующего с конца.
+            Если тарелок не хватает, то опрерация не выполняется.
+        """
+        if self.is_empty():
+            print('Тарелок нет')
+        else:
+            while plates > self.stacks[-1]:
+                if len(self.stacks) == 1 and plates > self.stacks[-1]:
+                    print('Столько тарелок нет')
+                    break
+                plates -= self.stacks[-1]
+                self.stacks.pop()
+            else:
+                self.stacks[-1] -= plates
+
+
+if __name__ == '__main__':
+    stack_plates = StacksOfPlates(15)
+    print(stack_plates.stacks)
+    print(stack_plates.is_empty())
+    stack_plates.push_in(9)
+    print(stack_plates.stacks)
+    print(stack_plates.get_size_last_element())
+    stack_plates.pop_out(11)
+    print(stack_plates.stacks)
+    stack_plates.pop_out(39)
+    print(stack_plates.is_empty())
+    stack_plates.pop_out(1)
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,3 +18,90 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class TasksDesc:
+    """
+        Класс представляет собой структуру данных в виде наборов из очередей,
+        представленных в виде списков дел.
+
+        Надо дорабатывать и включать блоки try/except
+    """
+    def __init__(self):
+        self.income_tasks = []
+        self.in_progress = []
+        self.need_to_fix = []
+        self.completed = []
+
+    def any_new_task(self):
+        """ Проверяет, есть ли доступные новыве задачи"""
+        return self.income_tasks == []
+
+    def add_new_task(self, task: str):
+        """ Добавляет новую задачу в очередь """
+        self.income_tasks.insert(0, task)
+
+    def get_new_task(self):
+        """ Печатает на экран очередную новую задачу, если она существует """
+        print(f'Очередная новая задача: {self.income_tasks[-1]}') if len(self.income_tasks) else print('Задач нет')
+
+    def move_to_do(self):
+        """ Перемещает очередную новую задачу в список, которые решаются """
+        self.in_progress.insert(0, self.income_tasks.pop())
+
+    def what_to_do(self):
+        """ Печатает на экран очередную выполняемую задачу, если она существует """
+        print(f'Очередная выполняемая задача: {self.in_progress[-1]}') if len(self.in_progress) else print('Задач нет')
+
+    def move_to_fix(self):
+        """ Перемещает очередную выполняемую задачу в список задач, которые надо исправить """
+        self.need_to_fix.insert(0, self.in_progress.pop())
+
+    def complete_done(self):
+        """ Перемещает очередную выполняемую задачу в список завершенных задач """
+        self.completed.insert(0, self.in_progress.pop())
+
+    def what_to_fix(self):
+        """ Печатает на экран очередную перевыполняемую задачу, если она существует """
+        print(f'Очередная перевыполняемая задача: {self.need_to_fix[-1]}') if len(self.need_to_fix) else print('Задач '
+                                                                                                               'нет')
+
+    def complete_fixed(self):
+        """ Перемещает очередную перевыполняемую задачу в список завершенных задач """
+        self.completed.insert(0, self.need_to_fix.pop())
+
+    def get_all_tasks(self):
+        """ Выводит на экран список всех задач по категориям """
+        all_desc = (self.income_tasks, self.in_progress, self.need_to_fix, self.completed)
+        categories = {1: '"New tasks"', 2: '"In progress"', 3: '"Need to fix"', 4: '"Completed"'}
+        key = 0
+        for desc in all_desc:
+            key += 1
+            if desc:
+                print(f'Задачи из категории {categories.get(key)}', end='\n================================\n')
+                for task in range(len(desc) - 1, -1, -1):  # тут фича просмотра первых в очереди сверху
+                    print(f'{desc[task]}')
+
+
+my_desc = TasksDesc()
+my_desc.add_new_task('Have to go to a shop')
+my_desc.add_new_task('Have to eat')
+my_desc.add_new_task('Have to sleep')
+
+my_desc.get_new_task()
+my_desc.move_to_do()
+my_desc.what_to_do()
+print('В магазин уже иду...')
+
+my_desc.get_new_task()
+print('Глянем, что там надо сделать...', end='\n\n')
+my_desc.get_all_tasks()
+
+print('Забыл хлеб...')
+my_desc.move_to_fix()
+my_desc.complete_fixed()
+print(my_desc.completed)
+my_desc.move_to_do()
+print(my_desc.in_progress)
+my_desc.complete_done()
+my_desc.get_all_tasks()
