e1690a267b784c0f64eac9b3be2126b4e4179f1a
Forseven <nutaru@gmail.com>
2021-04-22 22:17:19 +0700

    Lesson 7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -19,3 +19,79 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+from random import randint
+from timeit import timeit
+
+
+def bubble_sort(sort_list):
+    n = 1
+    while n <= len(sort_list):
+        for i in range(len(sort_list) - n):
+            if sort_list[i] < sort_list[i + 1]:
+                sort_list[i], sort_list[i + 1] = sort_list[i + 1], sort_list[i]
+        n += 1
+    return sort_list
+
+
+def bubble_sort_updated(sort_list):
+    n = 1
+    while n <= len(sort_list):
+        с = 0
+        for i in range(len(sort_list) - n):
+            if sort_list[i] < sort_list[i + 1]:
+                sort_list[i], sort_list[i + 1] = sort_list[i + 1], sort_list[i]
+                с += 1
+        if с == 0:
+            break
+        n += 1
+    return sort_list
+
+data_list = [randint(-100, 100) for _ in range(10)]
+ord_list = list(range(100, -10, -1))
+
+print(f'Исходный: {data_list}\n'
+      f'Отсортированный: {bubble_sort_updated(data_list[:])}')
+
+'''
+Дороботка заключается в том, что если за проходку не насчитается ни одной
+замены, то цикл прекратится. 
+'''
+print('unsorted')
+
+print('bubble_sort')
+
+print(timeit('bubble_sort(data_list[:])', globals=globals(), number=10000))
+
+print('bubble_sort_updated:')
+
+print(timeit('bubble_sort_updated(data_list[:])', globals=globals(), number=1000))
+
+print('-' * 10)
+
+
+print('sorted')
+print('bubble_sort')
+
+print(timeit('bubble_sort(ord_list[:])', globals=globals(), number=10000))
+
+print('bubble_sort_updated')
+
+print(timeit('bubble_sort_updated(ord_list[:])', globals=globals(), number=10000))
+
+
+#Модифицированая сортировка на уже отсортированых списках отрабатывает значительно быстрее. На практике уже отсортированные
+#списки встречаются ну очень редко.
+
+Исходный: [-26, -53, 79, -36, 73, 78, -99, -16, 96, 81]
+Отсортированный: [96, 81, 79, 78, 73, -16, -26, -36, -53, -99]
+unsorted
+bubble_sort
+0.10573649999999998
+bubble_sort_updated:
+0.011058600000000002
+----------
+sorted
+bubble_sort
+4.3953561
+bubble_sort_updated
+0.07868860000000044
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -45,3 +45,87 @@ for i in
 
 
 """
+from random import randint
+from statistics import StatisticsError
+
+from memory_profiler import memory_usage
+from timeit import default_timer
+
+def heapify(nums, heap_size, root_index):
+
+    largest = root_index
+    left_child = (2 * root_index) + 1
+    right_child = (2 * root_index) + 2
+
+    if left_child < heap_size and nums[left_child] > nums[largest]:
+        largest = left_child
+
+    if right_child < heap_size and nums[right_child] > nums[largest]:
+        largest = right_child
+
+    if largest != root_index:
+        nums[root_index], nums[largest] = nums[largest], nums[root_index]
+
+        heapify(nums, heap_size, largest)
+def heap_sort(nums):
+    n = len(nums)
+
+    for i in range(n, -1, -1):
+        heapify(nums, n, i)
+
+    for i in range(n - 1, 0, -1):
+        nums[i], nums[0] = nums[0], nums[i]
+        heapify(nums, i, 0)
+
+
+def my_decor(func):
+    def check_optimize(*args):
+        t1 = default_timer()
+        m1 = memory_usage()
+        result = func(*args)
+        m2 = memory_usage()
+        t2 = default_timer()
+        mem_res = m2[0] - m1[0]
+        time_res = t2 - t1
+        return f'Память: {mem_res},\nВремя: {time_res},\nРезультат: {result}'
+    return check_optimize
+
+@my_decor
+def my_median(list):
+
+    heap_sort(list)
+    l = len(list)
+
+    mid = (l - 1) // 2
+
+    if (l % 2 == 0):
+        return (list[mid] + list[mid + 1]) / 2
+    else:
+        return list[mid]
+@my_decor
+def median(data):
+
+    data = sorted(data)
+    n = len(data)
+    if n == 0:
+        raise StatisticsError("no median for empty data")
+    if n%2 == 1:
+        return data[n//2]
+    else:
+        i = n//2
+        return (data[i - 1] + data[i])/2
+
+data_list = [randint(-100, 100) for i in range(10000 * 2 + 1)]
+
+
+print(my_median(data_list[:]))
+print(median(data_list[:]))
+
+#При использовании сортировки функция получаеться дастаточно отимизированой
+Память: 0.00390625,
+Время: 0.3525551,
+Результат: 0
+
+Память: 0.1484375,
+Время: 0.21860960000000007,
+Результат: 0
