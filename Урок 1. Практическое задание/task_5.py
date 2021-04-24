7467ad81f6824adb24d623cea4fef2ba7b685c24
gobiodon <yuri.molchanov@gmail.com>
2021-04-24 13:40:32 +0300

    ДЗ 1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,33 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+class PlateStackClass:
+    def __init__(self, max_stack_size = 10):
+        self.elems = [[]]                               # список стопок
+        self.one_stack_size = max_stack_size            # макс. размер стопки
+        self.stack_count = 0                            # количество стопок - 1
+        self.cur_stack = self.elems[self.stack_count]   # последняя стопка
+
+    def is_empty(self):
+        return (self.cur_stack == []) and (self.stack_count == 0)
+
+    def push_in(self, el):
+        if len(self.cur_stack) >= self.one_stack_size:
+            self.elems.append([])
+            self.stack_count += 1
+            self.cur_stack = self.elems[self.stack_count]
+        self.cur_stack.append(el)
+
+    def pop_out(self):
+        result = self.cur_stack.pop()
+        if (len(self.cur_stack) == 0) and (self.stack_count > 0):
+            self.stack_count -= 1
+            self.elems.pop()
+            self.cur_stack = self.elems[self.stack_count]
+        return result
+
+    def get_val(self):
+        return self.cur_stack[len(self.cur_stack) - 1]
+
+    def stack_size(self):
+        return self.stack_count * self.one_stack_size + len(self.cur_stack)
