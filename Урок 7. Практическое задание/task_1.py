27500d3b4b0ba90215029e694ae296b0cd0bd240
Roman-Ch-32 <82414197+Roman-Ch-32@users.noreply.github.com>
2021-04-26 00:56:57 +0300

    Add files via upload

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,7 +9,7 @@
 Обязательно доработайте алгоритм (сделайте его умнее)!
 
 Идея доработки: если за проход по списку не совершается ни одной сортировки,
-то завершение.
+то завершение
 Обязательно сделайте замеры времени обеих реализаций
 и обосновать дала ли оптимизация эффективность
 
@@ -19,3 +19,65 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+
+import timeit
+import random
+
+
+def bubble_sort(lst_obj):
+    n = 1
+    out = 0
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+                out = 1
+        if out == 0:
+            return lst_obj
+        n += 1
+    return lst_obj
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+
+# замеры 10
+print(
+    timeit.timeit(
+        "bubble_sort(orig_list[:])",
+        globals=globals(),
+        number=1000),
+        orig_list[:], '\n',
+        bubble_sort(orig_list[:])
+        )
+
+orig_list = [random.randint(-100, 100) for _ in range(100)]
+
+# замеры 100
+print(
+    timeit.timeit(
+        "bubble_sort(orig_list[:])",
+        globals=globals(),
+        number=1000),
+        orig_list[:], '\n',
+        bubble_sort(orig_list[:])
+        )
+
+orig_list = [random.randint(-100, 100) for _ in range(1000)]
+
+# замеры 1000
+print(
+    timeit.timeit(
+        "bubble_sort(orig_list[:])",
+        globals=globals(),
+        number=1000),
+        orig_list[:], '\n',
+        bubble_sort(orig_list[:])
+        )
+
+'''
+с помощью доработки с добавлением флага в функции были отсеяны сортировки уже отсортированных массивов
+было проверено с помощью функции "sorted()" 
+на уже отсортированных массивах функция работает со сложностью O(N)
+
+'''
