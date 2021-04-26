124c41c558e7e4b4ccce4d02eba6f5d4f476f23e
fosemberg <fosemberg@gmail.com>
2021-04-26 22:57:22 +0300

    7.1 completed

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -29,14 +29,32 @@ def sorted_reverse(array):
 
 
 def bubble_sort_plain(array):
-    return array
+    array_copy = array[:]
+    n = 1
+    while n < len(array_copy):
+        for i in range(len(array_copy) - n):
+            if array_copy[i] < array_copy[i + 1]:
+                array_copy[i], array_copy[i + 1] = array_copy[i + 1], array_copy[i]
+        n += 1
+    return array_copy
 
 
 def bubble_sort_optimized(array):
-    return array
+    array_copy = array[:]
+    n = 1
+    while n < len(array_copy):
+        is_changed = False
+        for i in range(len(array_copy) - n):
+            if array_copy[i] < array_copy[i + 1]:
+                is_changed = True
+                array_copy[i], array_copy[i + 1] = array_copy[i + 1], array_copy[i]
+        if not is_changed:
+            break
+        n += 1
+    return array_copy
 
 
-test_array_names = ['10', '100', '201', '10_000']
+test_array_names = ['10', '100', '201']
 test_fns = [sorted_reverse, bubble_sort_plain, bubble_sort_optimized]
 
 test_array_10 = [10 - i for i in range(10)]
@@ -50,10 +68,6 @@ shuffle(shuffled_test_array_100)
 test_array_201 = [100 - i for i in range(201)]
 shuffled_test_array_201 = test_array_201[:]
 shuffle(shuffled_test_array_201)
-
-test_array_10_000 = [10_000 - i for i in range(10_000)]
-shuffled_test_array_10_000 = test_array_10_000[:]
-shuffle(shuffled_test_array_10_000)
 
 for test_array_name in test_array_names:
     print(f'test_array_{test_array_name}')
@@ -80,12 +94,6 @@ test_cases = [
     {
         'props': shuffled_test_array_201,
         'result': test_array_201,
-        'is_detailed': False,
-    },
-    {
-        'props': shuffled_test_array_10_000,
-        'result': test_array_10_000,
-        'is_detailed': False,
     },
 ]
 
@@ -110,3 +118,35 @@ def unit_test_fn(fn):
 
 for test_fn in test_fns:
     unit_test_fn(test_fn)
+
+"""
+Результаты замеров скорости:
+test_array_10
+sorted_reverse
+0.0006026140000000013
+bubble_sort_plain
+0.012117010999999997
+bubble_sort_optimized
+0.011706633000000001
+
+test_array_100
+sorted_reverse
+0.004201337999999999
+bubble_sort_plain
+0.67413488
+bubble_sort_optimized
+0.644094771
+
+test_array_201
+sorted_reverse
+0.008047460000000006
+bubble_sort_plain
+2.5250704070000003
+bubble_sort_optimized
+2.5063744939999997
+
+Самый быстрый - встроенная функция sorted.
+Затем идет оптимизированная сортировка пузырьком
+(когда нету перестановок во внутреннем цикле - мы выходим из цикла)
+Затем идет сортировка пузырьком без оптимизации
+"""
