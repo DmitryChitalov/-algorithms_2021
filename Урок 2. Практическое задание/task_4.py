28782ebe744dfcbafa1901174f4b2937938f5ffd
harver1 <harver.one@gmail.com>
2021-04-28 01:57:18 +0300

    Дз ко 2 уроку

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -12,3 +12,29 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+# Рекурсивная функция для подсчета элементов в ряде чисел
+# def sum_series(n, base=1):
+def sum_series(n, base):
+    if n == 0:  # базовый(нулевой) случай
+        return f'сумма элементов равна 0'
+    if n == 1:  # базовый случай
+        return base
+    while n > 0:
+        return base + sum_series(n-1, base*-0.5)
+
+
+def program():
+    try:
+        num = int(input('Введите количество элементов: '))
+        rez = sum_series(num, 1)
+        # rez = sum_series(num)
+        print(f'Количество элементов - {num} \nсумма элементов - {rez}')
+    except ValueError:
+        print('Необходимо количество!!!')
+        program()
+
+
+if __name__ == '__main__':
+    program()
