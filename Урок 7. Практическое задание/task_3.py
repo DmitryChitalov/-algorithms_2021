8b69ed7a744291170b853a3d20a2115f116001b6
fosemberg <fosemberg@gmail.com>
2021-04-27 18:57:15 +0300

    7.completed

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -51,11 +51,26 @@ from statistics import median
 
 
 def custom_median(array):
-    return array[len(array) % 2]
+    for i in range(len(array) - 1):
+        el = array[i]
+        l = []
+        m = []
+        r = []
+        for el2 in array:
+            if el2 < el:
+                l.append(el2)
+            elif el2 > el:
+                r.append(el2)
+            else:
+                m.append(el2)
+        len_m = len(m)
+        if abs((len(l) - len_m) - (len(r) - len_m)) < len_m:
+            return el
+    return array[len(array) - 1]
 
 
 def make_shuffled_odd_array(number=10):
-    return [randint(10, 100) for _ in range(2 * number + 1)]
+    return [randint(10, 20) for _ in range(2 * number + 1)]
 
 
 shuffled_odd_array_1 = make_shuffled_odd_array(1)
@@ -64,6 +79,26 @@ shuffled_odd_array_100 = make_shuffled_odd_array(100)
 shuffled_odd_array_500 = make_shuffled_odd_array(500)
 
 test_cases = [
+    {
+        'description': 'at the start',
+        'props': [21, 11, 12, 13, 31, 32, 33],
+        'result': 21,
+    },
+    {
+        'description': 'at the end',
+        'props': [11, 12, 13, 31, 32, 33, 21],
+        'result': 21,
+    },
+    {
+        'description': 'in the middle',
+        'props': [11, 12, 13, 21, 31, 32, 33],
+        'result': 21,
+    },
+    {
+        'description': 'in the middle',
+        'props': [41, 19, 40],
+        'result': 40,
+    },
     {
         'props': shuffled_odd_array_1,
         'result': median(shuffled_odd_array_1),
@@ -87,7 +122,7 @@ test_cases = [
 
 def unit_test_fn(fn):
     passed_tests = 0
-    print(sorted.__name__)
+    print(fn.__name__)
     for test_case in test_cases:
         props = test_case["props"]
         expected_result = test_case["result"]
@@ -99,6 +134,8 @@ def unit_test_fn(fn):
         output = f'{is_ok} {fn.__name__}'
         if is_detailed:
             output += f'({props}) => {real_result}{(f", but {expected_result} expected", "")[is_ok]}'
+        if test_case.get('description'):
+            output += f' // {test_case.get("description")}'
         print(output)
     print(f'passed tests: {passed_tests}/{len(test_cases)}\n')
 
