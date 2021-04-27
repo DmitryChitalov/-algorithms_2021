86c7d5cef7beda3ede180ff9b3821dd9c9542e4a
fosemberg <fosemberg@gmail.com>
2021-04-27 16:52:17 +0300

    7.3 add unit_tests. TODO: realize custom_median

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -45,3 +45,68 @@ for i in
 
 
 """
+
+from random import randint
+from statistics import median
+
+
+def custom_median(array):
+    return array[len(array) % 2]
+
+
+def make_shuffled_odd_array(number=10):
+    return [randint(10, 100) for _ in range(2 * number + 1)]
+
+
+shuffled_odd_array_1 = make_shuffled_odd_array(1)
+shuffled_odd_array_10 = make_shuffled_odd_array(10)
+shuffled_odd_array_100 = make_shuffled_odd_array(100)
+shuffled_odd_array_500 = make_shuffled_odd_array(500)
+
+test_cases = [
+    {
+        'props': shuffled_odd_array_1,
+        'result': median(shuffled_odd_array_1),
+    },
+    {
+        'props': shuffled_odd_array_10,
+        'result': median(shuffled_odd_array_10),
+    },
+    {
+        'props': shuffled_odd_array_100,
+        'result': median(shuffled_odd_array_100),
+        'is_detailed': False,
+    },
+    {
+        'props': shuffled_odd_array_500,
+        'result': median(shuffled_odd_array_500),
+        'is_detailed': False,
+    },
+]
+
+
+def unit_test_fn(fn):
+    passed_tests = 0
+    print(sorted.__name__)
+    for test_case in test_cases:
+        props = test_case["props"]
+        expected_result = test_case["result"]
+        is_detailed = test_case.get("is_detailed") != False
+        real_result = fn(props)
+        is_ok = real_result == expected_result
+        if is_ok:
+            passed_tests += 1
+        output = f'{is_ok} {fn.__name__}'
+        if is_detailed:
+            output += f'({props}) => {real_result}{(f", but {expected_result} expected", "")[is_ok]}'
+        print(output)
+    print(f'passed tests: {passed_tests}/{len(test_cases)}\n')
+
+
+test_fns = [
+    median,
+    custom_median,
+]
+
+for test_fn in test_fns:
+    unit_test_fn(test_fn)
