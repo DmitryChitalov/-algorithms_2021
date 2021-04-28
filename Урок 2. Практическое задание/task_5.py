adef42e89ccf73bd54651626b92d09578133ce7d
MALDERus <ssh7@bk.ru>
2021-04-28 03:49:51 +0300

    Решения к 6 заданиям 2 урока - 1,2,3,4,5,7.

--- Урок 2. Практическое задание/task_5.py
+++ Урок 2. Практическое задание/task_5.py
@@ -21,3 +21,15 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+def print_ascii_char(number = 32):
+    if number < 32 or number > 127:
+        return
+
+    if number != 32 and (number - 32) % 10 == 0:
+        print()
+    print(f'{number} - {chr(number)}', end = '\t')
+    print_ascii_char(number + 1)
+
+
+print_ascii_char()
