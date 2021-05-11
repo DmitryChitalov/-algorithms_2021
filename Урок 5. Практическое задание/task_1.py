5e511d7fc958452105be08990e9d79345f644846
Stas <ly150652@gmail.com>
2021-05-11 22:02:46 +1000

    Task 5.1

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 """
 1.	Пользователь вводит данные о количестве предприятий, их наименования и прибыль
 за 4 квартала (т.е. 4 отдельных числа) для каждого предприятия.
@@ -23,3 +25,55 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+
+Firm_pattern = namedtuple('Company', ['name', 'profit_1', 'profit_2', 'profit_3', 'profit_4'])
+
+
+def main():
+    firms = []
+    while True:
+        number_of_companies = input('Enter the number of companies: ')
+        try:
+            number_of_companies = int(number_of_companies)
+            break
+        except ValueError:
+            print(f'You only have to enter one number!\n')
+    for i in range(1, number_of_companies + 1):
+        name = input('Enter company name: ')
+        if name in [firm.name for firm in firms]:
+            print(f'The firm already exists.\n')
+            continue
+        try:
+            total_profit = input('Enter the profit of the company for 4 quarters, separated by a space: ').split()
+            firm = Firm_pattern(name,
+                                float(total_profit[0]),
+                                float(total_profit[1]),
+                                float(total_profit[2]),
+                                float(total_profit[3]))
+        except ValueError:
+            print(f'You must enter numeric values!\n')
+            continue
+        except IndexError:
+            print(f'You must enter 4 values!\n')
+            continue
+        firms.append(firm)
+    return avg(firms)
+
+
+def avg(firms):
+    total = 0
+    for firm in firms:
+        total += sum(firm[1:])
+    return output(total / len(firms), firms)
+
+
+def output(avg_val, firms):
+    above = [firm.name for firm in firms if avg_val > sum(firm[1:])]
+    print(len(firms[0])-1)
+    below = [firm.name for firm in firms if avg_val < sum(firm[1:])]
+    return f'Average annual profit of all firms: {avg_val}\n' \
+           f'Firms with above average profits: {above}\n' \
+           f'Firms with below average profits: {below}\n'
+
+
+print(main())
