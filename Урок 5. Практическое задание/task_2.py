1558401dae6ce4e2477d623ba1974a3256ec41bb
Alexander <fcalsm@yandex.ru>
2021-05-12 14:59:32 +0300

    Lesson 5
    GB

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -34,3 +34,76 @@ hx = HexNumber
 hx + hx
 hex()
 """
+from collections import defaultdict, deque
+from functools import reduce
+from timeit import timeit
+
+
+print('1 вариант')
+
+
+def summa_composition16():
+    try:
+        dict_lst = defaultdict()
+        number1 = input('Введите первое шестнадцатиричное число: ').upper()
+        number2 = input('Введите второе шестнадцатиричное число: ').upper()
+        dict_lst[1] = list(number1)
+        dict_lst[2] = list(number2)
+        summa = int(reduce(lambda a, b: a + b, dict_lst[1]), 16) + int(reduce(lambda a, b: a + b, dict_lst[2]), 16)
+        composition = int(reduce(lambda a, b: a + b, dict_lst[1]), 16) * int(reduce(lambda a, b: a + b, dict_lst[2]), 16)
+        summa = list(hex(summa)[2:].upper())
+        composition = list(hex(composition)[2:].upper())
+        dict_lst[3] = summa
+        dict_lst[4] = composition
+        return f'Сумма чисел: {dict_lst[3]}\nПроизведение чисел: {dict_lst[4]}'
+    except ValueError:
+        print('Неверный ввод')
+        return summa_composition16()
+
+
+print(summa_composition16())
+print('_________________________')
+print('2 Вариант')
+
+
+class HexNumber:
+
+    def __init__(self, string):
+        self.string = string
+
+    def __add__(self, other):
+        # Можно конечно было без deque, но очень понравилось как он превращает строку (быстрее)
+        final = deque(hex(int(self.string, 16) + int(other.string, 16)).upper()[2:])
+        final = str(final)[6:][:-1]
+        return f'Сумма чисел: {final}'
+
+    def __mul__(self, other):
+        final = deque(hex(int(self.string, 16) * int(other.string, 16)).upper()[2:])
+        final = str(final)[6:][:-1]
+        return f'Произведение чисел: {final}'
+
+
+a = HexNumber('a2')
+b = HexNumber('c4f')
+print(a+b)
+print(a*b)
+
+print('_________________________')
+# Замеры list и deque
+print('Замеры list и deque')
+
+
+def list_str(n):
+    return list(n)
+
+
+def deque_str(n):
+    return deque(n)
+
+
+n = '7c9fe'
+print(list_str(n))
+print(deque_str(n))
+
+print(f'list: {timeit("list_str(n)", globals=globals())}')
+print(f'deque: {timeit("deque_str(n)", globals=globals())}')
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,120 @@ deque – это обобщение стеков и очередей.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+from timeit import timeit
+from collections import deque
+
+
+my_list = []
+my_deque = deque()
+
+
+def full_list(n):
+    for el in range(n):
+        my_list.append(el)
+
+
+def full_deque(n):
+    for el in range(n):
+        my_deque.append(el)
+
+
+print(f'__________________________________________')
+print(f'Добавление в лист')
+print(f'{timeit("full_list(10000)",globals=globals(),number=1)}'[:10])
+print(f'Добавление в deque')
+print(f'{timeit("full_deque(10000)",globals=globals(),number=1)}'[:10])
+print('Добавление быстрее deque')
+
+
+def one_position_list(n):
+    for el in range(n):
+        my_list.insert(0, el)
+
+
+def one_position_deque(n):
+    for el in range(n):
+        my_deque.appendleft(el)
+
+
+print(f'__________________________________________')
+print(f'Добавление в начало лист')
+print(f'{timeit("one_position_list(10000)",globals=globals(),number=1)}'[:10])
+print(f'Добавление в начало deque')
+print(f'{timeit("one_position_deque(10000)",globals=globals(),number=1)}'[:10])
+print('Добавление быстрее deque')
+
+
+def index_list():
+    count = len(my_list) // 7
+    while count <= 0:
+        my_list.index(count, my_list)
+        count -= 1
+
+
+def index_deque():
+    count = len(my_deque) // 7
+    while count <= 0:
+        my_deque.index(count, my_deque)
+        count -= 1
+
+
+print(f'__________________________________________')
+print(f'Поиск индекса элемента лист')
+print(f'{timeit("index_list()",globals=globals(),number=1)}'[:10])
+print(f'Поиск индекса элемента deque')
+print(f'{timeit("index_deque()",globals=globals(),number=1)}'[:10])
+print('Поиск индекса быстрее лист')
+
+
+def mid_add_list():
+    mid = len(my_list) // 2
+    my_list.insert(mid, mid)
+
+
+def mid_add_deque():
+    mid = len(my_deque) // 2
+    my_deque.insert(mid, mid)
+
+
+print(f'__________________________________________')
+print(f'Добавление в середину лист')
+print(f'{timeit("mid_add_list()",globals=globals(),number=10000)}'[:10])
+print(f'Добавление в середину deque')
+print(f'{timeit("mid_add_deque()",globals=globals(),number=10000)}'[:10])
+print('Добавление в середину быстрее list')
+
+
+def pop_one_list():
+    my_list.pop(0)
+
+
+def pop_one_deque():
+    my_deque.popleft()
+
+
+print(f'__________________________________________')
+print(f'Удаление первого элемента лист')
+print(f'{timeit("pop_one_list()",globals=globals(),number=10000)}'[:10])
+print(f'Удаление первого элемента deque')
+print(f'{timeit("pop_one_deque()",globals=globals(),number=10000)}'[:10])
+print('Удаление первого элемента быстрее deque')
+
+
+def pop_list():
+    my_list.pop()
+
+
+def pop_deque():
+    my_deque.pop()
+
+
+print(f'__________________________________________')
+print(f'Удаление последнего элемента лист')
+print(f'{timeit("pop_list()",globals=globals(),number=10000)}'[:10])
+print(f'Удаление последнего элемента deque')
+print(f'{timeit("pop_deque()",globals=globals(),number=10000)}'[:10])
+print('Удаление последнего элемента быстрее deque')
+print(f'__________________________________________')
+print('Добавление в конец, добавление в начало, удаление последнего и первого элемента -- быстрее deque')
+print('Добавление в середину, поиск индекса -- быстрее list')
