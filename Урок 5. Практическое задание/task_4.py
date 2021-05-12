353e905ec23f8a469784d49e78fb34178bcefa2f
Arcateon <arcateon@gmail.com>
2021-05-12 20:51:10 +0300

    tasks for less_5

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,37 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+from collections import OrderedDict
+from timeit import timeit
+
+my_dict = {'banana': 3, 'apple': 4, 'pear': 1, 'orange': 2}
+
+my_order_dict = OrderedDict(my_dict)
+
+
+print(timeit("""
+my_dict = {'banana': 3, 'apple': 4, 'pear': 1, 'orange': 2}
+"""))
+
+print(timeit("""
+from collections import OrderedDict
+my_order_dict = OrderedDict([('banana', '1'), ('apple', '4'), ('pear', '1'), ('orange', '2')])
+"""))
+
+print(timeit("""
+my_dict = {'banana': 3, 'apple': 4, 'pear': 1, 'orange': 2}
+my_dict['pineapple'] = '5'
+"""))
+
+print(timeit("""
+from collections import OrderedDict
+my_order_dict = OrderedDict([('banana', '1'), ('apple', '4'), ('pear', '1'), ('orange', '2')])
+my_order_dict.update({'pineapple': '5'})
+"""))
+
+"""
+Полагаю, что использование OrderedDict останется актуальным, несмотря на реалтзации многих
+методов в обычных словарях, так как некоторые методы для него остаются уникальными. Например,
+move_to_end. Однако, замеры времени показали, что обычные словари работают быстрее.
+"""
