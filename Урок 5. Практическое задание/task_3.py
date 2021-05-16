c6de85e802acba83743c7843f6aea1c67e410fb3
Kirill <knizamutdinov@outlook.com>
2021-05-17 00:01:52 +0500

    Lesson_5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -15,3 +15,52 @@ deque – это обобщение стеков и очередей.
 
 И добавить аналитику, так ли это или нет.!
 """
+
+
+from timeit import timeit
+from collections import deque
+
+lst = [i for i in range(10000)]
+deq = deque(lst)
+elem = 10000
+
+
+def append_left_list(check_list, el):
+    check_list.insert(0, el)
+
+
+def append_left_deque(check_deque, el):
+    check_deque.appendleft(el)
+
+
+def pop_left_list(check_list):
+    check_list.pop(0)
+
+
+def pop_left_deque(check_deque):
+    check_deque.popleft()
+
+
+def pop_list(check_list):
+    check_list.pop()
+
+
+def pop_deque(check_deque):
+    check_deque.pop()
+
+
+print('append_left_list: ' + str(timeit('append_left_list(lst, elem)', globals=globals(), number=10000)))
+print('append_left_deque: ' + str(timeit('append_left_deque(deq, elem)', globals=globals(), number=10000)) + '\n')
+
+print('pop_left_list: ' + str(timeit('pop_left_list(lst)', globals=globals(), number=10000)))
+print('pop_left_deque: ' + str(timeit('pop_left_deque(deq)', globals=globals(), number=10000)) + '\n')
+
+print('pop_list: ' + str(timeit('pop_list(lst)', globals=globals(), number=10000)))
+print('pop_deque: ' + str(timeit('pop_deque(deq)', globals=globals(), number=10000)))
+
+
+"""
+Добавление елемента в начало - очередь быстрее
+Удаление первого элемента - очередь быстрее
+Удаление последнего элемента - одинаково
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,23 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+
+from collections import OrderedDict
+from timeit import timeit
+
+my_dict = {i: i + 2 for i in range(1000000)}
+my_ordered = OrderedDict(my_dict)
+
+print('OrderedDict pop: ' + str(timeit('my_ordered.popitem()', globals=globals(), number=1000000)))
+print('Dict pop: ' + str(timeit('my_dict.popitem()', globals=globals(), number=1000000)) + '\n')
+
+print('OrderedDict values: ' + str(timeit('my_ordered.values', globals=globals(), number=1000000)))
+print('Dict values: ' + str(timeit('my_dict.values', globals=globals(), number=1000000)) + '\n')
+
+print('OrderedDict keys: ' + str(timeit('my_ordered.keys', globals=globals(), number=1000000)))
+print('Dict keys: ' + str(timeit('my_dict.keys', globals=globals(), number=1000000)))
+
+"""
+Нет смысла использовать OrderedDict, т.к. словарь в новых версиях Python уже упорядоченный и быстрее
+"""
