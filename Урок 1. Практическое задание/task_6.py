790bbb103d489061cecbb99b63d0e0562356880e
redaxel <97jertov97@gmail.com>
2021-05-16 22:51:33 +0300

    lesson-1

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,3 +18,42 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class Queue:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+
+    def print_queue(self):
+        return self.elems
+
+
+def tasks_queue(tasks_lst):
+    queue_r = Queue()
+    queue_m = Queue()
+    queue_adj = Queue()
+    for tsk in tasks_lst:
+        queue_m.to_queue(tsk)
+    for tsk in reversed(queue_m.print_queue()):
+        a = input(f'Вы сделали задание "{tsk}"? ')
+        if a.lower() == 'yes':
+            queue_r.to_queue(tsk)
+        else:
+            queue_adj.to_queue(tsk)
+        queue_m.from_queue()
+    return f'Осталось только: {queue_adj.print_queue()}, вы справитесь!'
+
+
+print(tasks_queue(["Вымыть посуду", "Вынести мусор", "Протереть пыль", "Пропылесосить", "Отдохнуть"]))
