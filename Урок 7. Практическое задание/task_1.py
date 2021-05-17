7adcad6590640dcead612cb69efc3f28666bbf1b
ilyaSerebrennikov <ty-ru@yandex.ru>
2021-05-17 23:24:08 +0300

    7.2

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -19,3 +19,25 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+import random
+
+def bubble_sort(list):
+    n = 1
+    while n < len(list):
+        count = 0
+        for i in range(len(list) - 1 - (n - 1)):
+            if list[i] < list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i] # вот она обратная сортировка
+                count += 1
+        if count == 0:
+            break
+        n += 1
+
+SIZE = 10
+MIN_ITEM = -100
+MAX_ITEM = 99
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+
+print('Массив:', array, sep='\n')
+bubble_sort(array)
+print('Он же, но после сортировки:', array, sep='\n')
