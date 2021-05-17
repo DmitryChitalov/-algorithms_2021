7d3a37310180b541d1395eca84fda38de27f33ed
ilyaSerebrennikov <ty-ru@yandex.ru>
2021-05-17 23:26:35 +0300

    7.3

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,35 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+
+def merge_sort(arr):
+    def merge(frst, snd):
+        res = []
+        x, z = 0, 0
+        while x < len(frst) and z < len(snd):
+            if frst[x] < snd[z]:
+                res.append(frst[x])
+                x += 1
+            else:
+                res.append(snd[z])
+                z += 1
+        res.extend(frst[x:] if x < len(frst) else snd[z:])
+        return res
+    def div_half(lst):
+        if len(lst) == 1:
+            return lst
+        elif len(lst) == 2:
+            return lst if lst[0] <= lst[1] else lst[::-1]
+        else:
+            return merge(div_half(lst[:len(lst)//2]), div_half(lst[len(lst)//2:]))
+
+    return div_half(arr)
+
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 50
+array = [random.uniform(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+
+print('Массив:', array, sep='\n')
+print('Он же, но после сортировки:', merge_sort(array), sep='\n')
