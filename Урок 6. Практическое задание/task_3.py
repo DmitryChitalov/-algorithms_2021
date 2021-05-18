3c7c1b16bd3af831b399b360c726691c686cdd1d
NikitaNezgovorov <75495473+NikitaNezgovorov@users.noreply.github.com>
2021-05-18 18:26:21 +0300

    Homework lesson 6

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,41 @@
 Придумать как это решить!
 Есть очень простое решение!
 """
+from memory_profiler import profile
+
+
+"""
+Берем пример с ДЗ 4 урока
+"""
+
+
+@profile
+def recursive_reverse(number):
+    if number == 0:
+        return ''
+    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+print(f'Рекурсивная функция из примера. {recursive_reverse(654)}')
+
+"""
+Когда мы пытаемся профилировать рекурсивную функцию через декоратор, он срабатывает на каждый вызове функции
+"""
+
+
+@profile
+def recursive_reverse_solution():
+
+    def recursive_reverse_new(number):
+        if number == 0:
+            return ''
+        return f'{str(number % 10)}{recursive_reverse_new(number // 10)}'
+
+
+print(f'Вызов рекурсивной функцию внутри другой функции .')
+
+recursive_reverse_solution()
+
+"""
+Вышли из из положения и разместив рекурсивную функцию внутри другой.
+"""
