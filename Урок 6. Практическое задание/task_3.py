e5fda9cf7defe31ea848498f7ab6a3769afbe7b1
Alexander <fcalsm@yandex.ru>
2021-05-19 12:58:31 +0300

    Lesson_6_3

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,31 @@
 Придумать как это решить!
 Есть очень простое решение
 """
+from memory_profiler import profile
+
+
+@profile
+def revers_1(enter_num, revers_num=0):
+    if enter_num == 0:
+        return revers_num
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        return revers_1(enter_num, revers_num)
+
+
+@profile
+def outer_revers_1():
+    def revers_1(enter_num, revers_num=0):
+        if enter_num == 0:
+            return revers_num
+        else:
+            num = enter_num % 10
+            revers_num = (revers_num + num / 10) * 10
+            enter_num //= 10
+            return revers_1(enter_num, revers_num)
+
+print(outer_revers_1())
+
+# Для того, чтобы декоратор не срабатовал на каждом вызове функции, нужно поместить функцию в функцию
