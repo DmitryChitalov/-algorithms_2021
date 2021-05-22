7acd6fd1067bb13b15603f7bc4ba524ded909c26
KirillovaV <victmanio@yandex.ru>
2021-05-22 13:52:46 +0300

    lesson 8 homework

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -6,7 +6,109 @@
 Разрешается и приветствуется изменение имен переменных, выбор других коллекций, различные изменения
 и оптимизации.
 
-
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
+
+******************************************************************************
+
+Реализация через ООП на основе материала урока. (через Counter, deque)
+
+Создан класс "Node" для создания узлов дерева,
+содержащий информацию о правом и левом потомке
+
+Создан класс "HaffmanCode" для кодирования строки:
+- не хранит изначальную строку, сразу создавая словарь с
+кодами и закодированную строку
+- работает для строк из одного символа и вызывает ошибку при
+попытке создать код из пустой строки
+- методы класса реализованы как защищённые для ограничения доступа
+- получить закодированную строку можно через свойство get_code_string
+- получить изначальную строку можно через свойство encode
 """
+from collections import Counter, deque
+
+
+class Node:
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+
+class HaffmanCode:
+
+    def __init__(self, string):
+        if not string:
+            raise ValueError('невозможно создать код из пустой строки')
+        self.__encode_dict = {}
+        self.__create_code(self.__create_tree(string))
+        self.__code_string = self.__create_code_string(string)
+
+    def __create_code_string(self, string):
+        return ' '.join(self.__encode_dict[ch] for ch in string)
+
+    # создаём дерево для дальнейшей обработки
+    def __create_tree(self, string):
+        code_deque = deque(Counter(string).most_common())
+        code_deque.reverse()
+
+        # если элемент единственный, возвращаем дерево с 1 левым потомком
+        if len(code_deque) == 1:
+            return Node(code_deque[0][0])
+
+        while len(code_deque) > 1:
+            # считаем вес и создаём узел дерева
+            weight = code_deque[0][1] + code_deque[1][1]
+            node = Node(code_deque.popleft()[0], code_deque.popleft()[0])
+
+            i = 0
+            while i < len(code_deque):
+                # если находим элемент с весом больше получившегося
+                # вставляем до него
+                if code_deque[i][1] >= weight:
+                    code_deque.insert(i, (node, weight))
+                    break
+                i += 1
+            else:
+                # иначе добавляем в конец
+                code_deque.append((node, weight))
+
+        # возвращаем получившееся дерево
+        return code_deque[0][0]
+
+    # заполняем словарь символами и их кодами
+    def __create_code(self, node, code=''):
+        if isinstance(node, str):
+            self.__encode_dict[node] = code
+        else:
+            self.__create_code(node.left, f'{code}0')
+            # проверка на случай строки из 1 элемента
+            try:
+                self.__create_code(node.right, f'{code}1')
+            except AttributeError:
+                pass
+
+    @property
+    def get_code_string(self):
+        return self.__code_string
+
+    @property
+    def encode(self):
+        codes = self.__code_string.split()
+        encode = []
+        for code in codes:
+            for ch, el in self.__encode_dict.items():
+                if code == el:
+                    encode.append(ch)
+        return ''.join(encode)
+
+
+if __name__ == '__main__':
+
+    tree = HaffmanCode('beep boop beer!')
+    print(tree.get_code_string)
+    print(tree.encode)
+
+    t1 = HaffmanCode('bbbbbb')
+    print(t1.get_code_string)
+    print(t1.encode)
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,72 +8,116 @@
 (например, валидация значений узлов в соответствии с требованиями для бинарного дерева)
 
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
+
+***************************************************************************************
+
+1. В структуру данных добавлена ссылка на родителя и метод его получения.
+2. Убрана вставка левого/правого узла. Теперь новый элемент вставляется
+в дерево автоматически определяя своё место.
+3. Добавлен метод поиска узла в дереве.
+4. Добавлен метод для получения высоты дерева.
+5. Добавлена проверка узла на сбалансированность.
+(По умолчанию проверяет узел из которого вызываетя, или ищет узел по значению)
 """
 
+
 class BinaryTree:
-    def __init__(self, root_obj):
-        # корень
-        self.root = root_obj
-        # левый потомок
+
+    def __init__(self, root_obj, parent=None):
+        self.parent = parent
+        self.value = root_obj
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
-    def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+    def __repr__(self):
+        return f'Node[{self.value}]'
 
-    # добавить правого потомка
-    def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+    # вставляем новый узел
+    def insert_node(self, val):
+        # если значение равно текущему ничего не меняем
+        if val == self.value:
+            self.value = val
+        # если новое значение меньше значения текущего узла
+        elif val < self.value:
+            # вставляем значение в левое поддерево
+            if not self.left_child:
+                self.left_child = BinaryTree(val, self)
+            else:
+                self.left_child.insert_node(val)
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            # иначе вставляем значение в правое поддерево
+            if not self.right_child:
+                self.right_child = BinaryTree(val, self)
+            else:
+                self.right_child.insert_node(val)
 
-    # метод доступа к правому потомку
+    # проверяем наличие узла в дереве
+    # возвращаем узел, если он есть или None
+    def find_node(self, val):
+        if val == self.value:
+            return self
+        try:
+            if val < self.value:
+                return self.left_child.find_node(val)
+            return self.right_child.find_node(val)
+        except AttributeError:
+            return
+
     def get_right_child(self):
         return self.right_child
 
-    # метод доступа к левому потомку
     def get_left_child(self):
         return self.left_child
 
-    # метод установки корня
-    def set_root_val(self, obj):
-        self.root = obj
+    def get_value(self):
+        return self.value
 
-    # метод доступа к корню
-    def get_root_val(self):
-        return self.root
+    # получаем родителя
+    def get_parent(self):
+        return self.parent
+
+    # проверяем наличие потомков
+    def has_children(self):
+        return self.left_child or self.right_child
+
+    # получаем высоту дерева
+    def get_height(self):
+        left_height = self.left_child.get_height() if self.left_child else 0
+        right_height = self.right_child.get_height() if self.right_child else 0
+        return max(left_height, right_height) + 1
+
+    # является ли узел сбалансированным
+    def is_balanced(self, val=None):
+        if val:
+            node = self.find_node(val)
+        else:
+            node = self
+        if node:
+            left_height = node.left_child.get_height() if node.left_child else 0
+            right_height = node.right_child.get_height() if node.right_child else 0
+            return abs(left_height - right_height) <= 1
+        raise ValueError('неверное значение узла')
 
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+if __name__ == '__main__':
+
+    # заполняем дерево
+    r = BinaryTree(8)
+    r.insert_node(5)
+    r.insert_node(3)
+    r.insert_node(1)
+    r.insert_node(15)
+    r.insert_node(6)
+
+    # Выводим на экран
+    print(r)                                        # корень - Node[8]
+    print(r.left_child, r.right_child)              # левого и правого потомка - Node[5] Node[15]
+    print(r.find_node(1).get_parent())              # родителя для узла 1 - Node[3]
+    print(r.find_node(15).get_left_child())         # левого потомка узла 15 - None
+    print(r.get_height())                           # получаем высоту дерева - 4
+
+    # проверяем сбалансированность
+    print(r.is_balanced(15))                        # узла 15 - True
+    print(r.get_right_child().is_balanced())        # правого потомка корня - True
+    print(r.is_balanced())                          # корня - False
+#    print(r.is_balanced(25))                        # вызовет ошибку
