747d29b66a175c4d8f93897cb1dbc0510d786fb8
Dmitry Ageev <dimasikk99@gmail.com>
2021-05-24 19:31:33 +0300

    Update task_6.py

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -11,3 +11,27 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+from random import randint
+
+
+def number_guesser(attempt, number):
+    user_number = int(input(f'Your choice: '))
+    if user_number == number:
+        print(f'You win!')
+        return
+    elif attempt == 1:
+        print('*' * 40)
+        print(f'Wrong! :(')
+        print(f'The hidden number = {number}')
+        return
+    else:
+        print('*' * 40)
+        print('More' if user_number < number else 'Less')
+        print(f'Remaining number of attempts: {attempt - 1}')
+    number_guesser(attempt - 1, number)
+
+
+secret_number = randint(1, 100)
+print('Guess a number from 1 to 100')
+print('*' * 40)
+number_guesser(10, secret_number)
