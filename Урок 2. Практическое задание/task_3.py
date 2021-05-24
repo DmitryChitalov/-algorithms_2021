dfdbf3e435c77086ad82f485d8d7e7e8b99dfd41
Dmitry Ageev <dimasikk99@gmail.com>
2021-05-24 19:12:11 +0300

    Update task_3.py

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -22,3 +22,15 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+def number_flipper(num):
+    if num == 0:
+        return ''
+    else:
+        revers = str(num % 10)
+        num //= 10
+        return revers + number_flipper(num)
+
+
+some_number = int(input(f'Enter the number: '))
+print(number_flipper(some_number))
