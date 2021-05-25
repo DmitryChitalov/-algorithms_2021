bd774d67c8f1fa84f6684e679cf7fb85c6e7b840
MALDERus <ssh7@bk.ru>
2021-05-26 00:28:14 +0300

    8 урок решения к заданиям - 1,2.

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -10,3 +10,83 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+
+from collections import Counter
+
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.right = right
+        self.left = left
+        self.value = value
+
+
+# Получаем из дерева коды символов передаваемой строки.
+def get_code(root, codes=None, code=''):
+    if codes is None:
+        codes = dict()
+    if root is None:
+        return
+    if isinstance(root.value, str):
+        codes[root.value] = code
+        return codes
+    get_code(root.left, codes, code + '0')
+    get_code(root.right, codes, code + '1')
+    return codes
+
+
+# Строим дерево
+def get_tree(string):
+    string_count = Counter(string)
+    if len(string_count) <= 1:
+        node = Node(None)
+        if len(string_count) == 1:
+            node.left = Node([key for key in string_count][0])
+            node.right = Node(None)
+        string_count = {node: 1}
+    while len(string_count) != 1:
+        node = Node(None)
+        spam = string_count.most_common()[:-3:-1]
+        if isinstance(spam[0][0], str):
+            node.left = Node(spam[0][0])
+        else:
+            node.left = spam[0][0]
+        if isinstance(spam[1][0], str):
+            node.right = Node(spam[1][0])
+        else:
+            node.right = spam[1][0]
+        del string_count[spam[0][0]]
+        del string_count[spam[1][0]]
+        string_count[node] = spam[0][1] + spam[1][1]
+    return [key for key in string_count][0]
+
+
+# Кодируем строку полученными кодами
+def coding(string, codes):
+    res = ''
+    for symbol in string:
+        res += codes[symbol]
+    return res
+
+
+# Декодируем строку
+def decoding(string, codes):
+    res = ''
+    i = 0
+    while i < len(string):
+        for code in codes:
+            if string[i:].find(codes[code]) == 0:
+                res += code
+                i += len(codes[code])
+    return res
+
+
+some_string = 'beep boop beer!'
+tree = get_tree(some_string)
+codes = get_code(tree)
+coding_str = coding(some_string, codes)
+decoding_str = decoding(coding_str, codes)
+print(f'Коды символов: {codes}')
+print('Первоначальная строка: ', some_string)
+print('Закодированная строка: ', coding_str)
+print('Декодированная строка: ', decoding_str)
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -19,8 +19,14 @@ class BinaryTree:
         # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
+    # добавить левого потомка с валидацией
     def insert_left(self, new_node):
+        try:
+            if new_node > self.root:
+                raise ValueError
+        except ValueError:
+            print(f'вставляемое значение потомка - {new_node} должно быть меньше корня - {self.root}')
+            exit(1)
         # если у узла нет левого потомка
         if self.left_child == None:
             # тогда узел просто вставляется в дерево
@@ -34,8 +40,14 @@ class BinaryTree:
             tree_obj.left_child = self.left_child
             self.left_child = tree_obj
 
-    # добавить правого потомка
+    # добавить правого потомка с валидацией
     def insert_right(self, new_node):
+        try:
+            if new_node < self.root:
+                raise ValueError
+        except ValueError:
+            print(f'вставляемое значение потомка - {new_node} должно быть больше корня - {self.root}')
+            exit(1)
         # если у узла нет правого потомка
         if self.right_child == None:
             # тогда узел просто вставляется в дерево
@@ -69,7 +81,7 @@ class BinaryTree:
 r = BinaryTree(8)
 print(r.get_root_val())
 print(r.get_left_child())
-r.insert_left(40)
+r.insert_left(6)
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
 r.insert_right(12)
@@ -77,3 +89,11 @@ print(r.get_right_child())
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
 print(r.get_right_child().get_root_val())
+r.insert_left(5)
+print(r.get_left_child())
+print(r.get_left_child().get_root_val())
+r.insert_right(6)
+print(r.get_left_child().get_root_val())
+print(r.get_right_child())
+print(r.get_right_child().get_root_val())
+r.insert_left(40)
