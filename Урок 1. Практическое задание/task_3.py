5a10af7ef1d65a7eff30ebe4138f03585a773b79
Romnako <76477868+Romnako@users.noreply.github.com>
2021-05-25 15:37:18 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -20,3 +20,43 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+brands = {"Coca-Cola": 98990,
+             "Amazon": 188200,
+             "Google": 190000,
+             "Microsoft": 101900,
+             "IBM": 135930,
+             "Apple.Inc": 174343}  # O(len(...))
+
+
+#  Variant_1
+
+def top_three_1(best_world_brand):
+
+    for el in range(len(best_world_brand)): # O(n)
+        value_index = el
+        for index in range(el + 1, len(best_world_brand)): #O(n)
+            if best_world_brand[value_index][1] > best_world_brand[value_index][1]:
+                value_index = index
+        best_world_brand[el], best_world_brand[value_index] = best_world_brand[value_index], best_world_brand[el]
+    return best_world_brand[:3]
+  
+# For Variant_1 O(n^2)-> Two loops, the rest of the lines are constant complexity
+
+top_three_brands = list(brands.items())
+for i in top_three_1(top_three_brands):
+    print(f"Brand {i[0]} has profit ${i[1]} billions")
+
+#Var2
+def top_three_2(best_world_brand_two):
+    top_brand ={}
+
+    for el in range(3): #O(n)
+        max_value = max(best_world_brand_two.items(), key=lambda k_v: k_v[1])
+        del best_world_brand_two[max_value[0]]
+        top_brand[max_value[0]] = max_value[1]
+    return top_brand
+
+
+print(top_three_2(brands))
+
+# For Variant_1 O(n)-> One loop, the rest of the lines are constant complexity
