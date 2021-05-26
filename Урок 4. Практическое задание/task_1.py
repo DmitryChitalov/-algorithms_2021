e8c354992f85c8b435e0131e3b92624cf2cac2ca
ozzzzz <you@example.com>
2021-05-26 16:54:18 +0300

    homework 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,12 @@
 Без аналитики задание не принимается
 """
 
+from timeit import timeit
+
+
+# test_list = list((int(n)) for n in range(1000))
+test_list = list(range(100000))
+
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +26,25 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in nums if i % 2 == 0]
+    return new_arr
+
+
+print(
+    timeit(
+        "func_1(test_list)",
+        globals=globals(),
+        number=1000))
+print(
+    timeit(
+        "func_2(test_list)",
+        globals=globals(),
+        number=1000))
+
+# Итератор с функцией append был заменён на списковое включение; счетчик,
+# идущий по по индексу с дальнейшим обращением к элементу по полученному
+# индексу был заменен на прямое обращение к элементу во время пербора списка в цикле.
+# По итогу: время выполнения задачи уменьшилось примерно вдвое.
