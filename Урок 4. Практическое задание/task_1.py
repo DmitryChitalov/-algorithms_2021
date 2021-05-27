b581f632b83c2f8de274a4ec9d91aa2abd97d0c1
Elina Grineva <elina.grineva@yandex.ru>
2021-05-27 13:42:16 +0300

    added a 4HW

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -14,9 +14,29 @@
 """
 
 
-def func_1(nums):
+# Я оптимизировала второй код за счет лист комприхеншн и энумирейт (на код уходит меньше времени и замеры это доказали)
+
+from timeit import timeit
+
+
+test_code = '''
+def func_1(nums=[1,2,3,4,5,6]):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+'''
+print(f'Время работы 1 кода -')
+print(timeit(test_code, globals=globals()))
+
+test_code_2 = '''
+def func_2(nums=[1,2,3,4,5,6]):
+    return [v for k, v in enumerate(nums) if not k%2]
+'''
+
+print(f'Время работы 2 кода -')
+print(timeit(test_code_2, globals=globals()))
+
+
+
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -14,6 +14,15 @@
 Без аналитики задание считается не принятым
 """
 
+# Предложенный мною вариант revers_4 является самым труднозатратным. Менее затратным является revers_3 вариант -
+# так как он по своей структуре превращается число в строку и переварачивает его с помощью среза. Другие варианты используют рекурсию,
+# циклы, в моей варианте дополнительно заполняется список с элементами.
+
+
+from cProfile import run
+from timeit import timeit
+
+num = 35685332
 
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
@@ -37,3 +46,29 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num, my_list=[]):
+    if enter_num == 0:
+        return ''.join(str(x) for x in my_list)
+    else:
+        c = enter_num % 10
+        my_list.append(c)
+        enter_num = enter_num // 10
+        return revers_4(enter_num)
+
+
+print(timeit("revers_1(num)", globals=globals(), number=1000))
+print(timeit("revers_2(num)", globals=globals(), number=1000))
+print(timeit("revers_3(num)", globals=globals(), number=1000))
+print(timeit("revers_4(num)", globals=globals(), number=1000))
+
+
+def main():
+    res_1 = revers_1(num)
+    res_2 = revers_2(num)
+    res_3 = revers_3(num)
+    res_4 = revers_4(num)
+
+
+run('main()')
