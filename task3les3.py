83f48583a63dd39840056d09c6196906f1b1b893
12262004-m <77743232+12262004-m@users.noreply.github.com>
2021-05-27 23:38:41 +0300

    Add files via upload

---
+++ task3les3.py
@@ -0,0 +1,18 @@
+import hashlib
+
+s = []
+n = 'lala'
+itog = 0
+for i in range(len(n)):
+    for j in range(i + 1, len(n) + 1):
+        s.append(hashlib.sha256(n[i:j].encode()).hexdigest())
+        print(n[i:j], end=' ')
+print(n)
+
+s2 = []
+for x in s:
+    if x not in s2:
+        s2.append(x)
+
+
+print(len(s2))
