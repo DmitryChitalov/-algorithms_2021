e0bc6d9f97b659ecbf42d6a598c934a740ee3765
KuziaMak <74614098+KuziaMak@users.noreply.github.com>
2021-05-28 09:33:59 +0300

    Update task_6.py

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -20,38 +20,38 @@
 """
 
 
-class Ocered():
-    def __init__(self, otvet):
-        self.otvet = otvet
-        self.ver = []
-        self.never = []
+class Que():
+    def __init__(self, answ):
+        self.answ = answ
+        self.rig = []
+        self.no_rig = []
 
-    def verno(self, ed):
-        self.ver.append(ed)
+    def rigs(self, ed):
+        self.rig.append(ed)
 
-    def neverno(self, ed):
-        self.never.append(ed)
+    def no_rigs(self, ed):
+        self.no_rig.append(ed)
 
     def decision(self, dec, num):
-        otv = self.otvet.pop(0)
+        otv = self.answ.pop(0)
         if dec == otv:
-            self.verno(f"{num} задания верное ответ: {otv}")
+            self.rigs(f"{num} задания верное ответ: {otv}")
         else:
-            self.neverno(f"{num} задание неверно ответ: {otv}")
+            self.no_rigs(f"{num} задание неверно ответ: {otv}")
 
     def result(self):
-        return self.ver,self.never
+        return self.rig,self.no_rig
 
 
-def doska():
-    otvet = [1, 2, 5, 2, 6]
+def board():
+    answ = [1, 2, 5, 2, 6]
     dec1, dec2, dec3, dec4, dec5 = int(input("10/10 ")), int(input("корень 4 ")), int(input("20-15 ")), int(input("2*1 ")), int(input("1+1+1+1+1+1 "))
     dec = [dec1, dec2, dec3, dec4, dec5]
-    oceredi = Ocered(otvet)
+    queue = Que(answ)
     num = 0
     for i in dec:
         num+= 1
-        oceredi.decision(i,num)
-    print(oceredi.result())
+        queue.decision(i,num)
+    print(queue.result())
 
-doska()
+board()
