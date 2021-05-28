5fd44039a20762dfa7ec9028ebb753d576356524
Dmitry Ageev <dimasikk99@gmail.com>
2021-05-28 18:16:04 +0300

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,38 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+from hashlib import sha256
+
+
+class Cashing:
+    def __init__(self):
+        self.data_url = {}
+        self.salt = b'any_salt_1'
+
+    def salt_hash(self, site):
+        hash_url = sha256(self.salt + site.encode()).hexdigest()
+        if hash_url not in self.data_url.values():
+            self.data_url[site] = hash_url
+            print(f'Added: {site}')
+        else:
+            print(f'Already added > {site}')
+
+    def cash_data(self):
+        for el, i in enumerate(self.data_url):
+            print(f'{el + 1}) {i}')
+
+
+check = Cashing()
+check.salt_hash('https://geekbrains.ru/')
+check.salt_hash('https://ya.ru/')
+check.salt_hash('https://github.com/')
+check.salt_hash('https://youtube.com/')
+check.salt_hash('https://github.com')
+check.salt_hash('https://google.ru/')
+check.salt_hash('https://geekbrains.ru/')
+check.salt_hash('https://vk.com/')
+print('\n')
+
+print('In cache: ')
+check.cash_data()
