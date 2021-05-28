5eb3f076ffbb14cf178351461c0bab7ae2754791
Dmitry Ageev <dimasikk99@gmail.com>
2021-05-28 18:11:40 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -16,3 +16,19 @@
 р
 а
 """
+
+def search_of_unique(word):
+    temp_hash = {}
+    for i in range(len(word)):
+        for el in range(i, len(word)):
+            element = word[i:el + 1]
+            temp_hash[hash(element)] = element
+    temp_hash.pop(hash(word))
+    print(f'{word} - {len(temp_hash)} unique substrings \n')
+    for num, words in enumerate(temp_hash.values()):
+        print(f'{num + 1}) {words}')
+
+
+search_of_unique('papa')
+print("*" * 40)
+search_of_unique('google')
