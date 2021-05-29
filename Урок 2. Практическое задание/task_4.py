934d20a23d8b3a19d0c3682e1234a5a86cf29801
android156 <n_degtyarev@mail.ru>
2021-05-29 16:58:10 +0300

    hw2 done, commit from pycharm

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -12,3 +12,28 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+class OwnError (Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+def req_calc_row(n):
+    if n == 1:
+        return 1
+    else:
+        return req_calc_row(n-1) + (-1/2)**(n-1)
+
+
+while True:
+    try:
+        user_number = int(input(f"Введите n:"))
+        if user_number <= 0:
+            raise OwnError("Введено не натуральное число!\n")
+        print(f' сумма {user_number} членов ряда: 1 -0.5 0.25 -0.125 ... равна {req_calc_row(user_number)}')
+        break
+    except ValueError:
+        print('Введено не число!\n')
+    except OwnError as err:
+        print(err)
--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -16,3 +16,31 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+class OwnError (Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+def req_summ_n(n):
+    if n == 1:
+        return 1
+    else:
+        return req_summ_n(n-1) + n
+
+
+while True:
+    try:
+        user_number = int(input(f"Введите n:"))
+        if user_number <= 0:
+            raise OwnError("Введено не натуральное число!\n")
+        res1 = req_summ_n(user_number)
+        res2 = user_number * (user_number + 1) / 2
+        print(f'Сумма {user_number} членов ряда: 1 2 3 4 ... n равна {res1}')
+        print(f'Значение выражения {user_number}({user_number}+1)/2 равно {res2}')
+        print(f'Проверка на равенство - {res1 == res2}')
+        break
+    except ValueError:
+        print('Введено не число!\n')
+    except OwnError as err:
+        print(err)
