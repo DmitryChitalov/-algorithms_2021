9c343158d4a74bd8cb968532a92d5c8e47b20856
Aksel89 <denetor89@yandex.ru>
2021-05-30 17:22:01 +0300

    HW7

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,44 @@ for i in
 
 
 """
+from statistics import median
+import random
+
+number_of_el = input('Введите количество элементов: ')
+while not number_of_el.isdigit() or not int(number_of_el):
+    number_of_el = input('Ошибка! Должно быть введено натуральное число.\n Повторите ввод количества элементов: ')
+m = int(number_of_el) * 2 + 1
+my_list = [random.randint(0, m * 50) for _ in range(m)]
+copy_list = my_list[:]
+print('Исходный массив:', my_list)
+
+
+median(copy_list)
+
+
+def median_1(lst):
+    quotient, remainder = divmod(len(lst), 2)
+    if remainder:
+        return sorted(lst)[quotient]
+    return sum(sorted(lst)[quotient - 1:quotient + 1]) / 2
+
+
+def median_2(lst):
+    sort_lst = sorted(lst)
+    len_lst = len(lst)
+    index = (len_lst - 1) // 2
+    if len_lst % 2:
+        return sort_lst[index]
+    else:
+        return (sort_lst[index] + sort_lst[index + 1]) / 2.0
+
+
+def median_3(lst):
+    lst.sort()
+    mid = len(lst) // 2
+    return (lst[mid] + lst[~mid]) / 2
+
+
+print(f"Медиана: {median_1(copy_list)}")
+print(f"Медиана: {median_2(copy_list)}")
+print(f"Медиана: {median_3(copy_list)}")
