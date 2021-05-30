df5a3e72c79e2026027d5b54d995386bebc39210
vlgreb <vlgrebennikov@mail.ru>
2021-05-31 00:51:56 +0300

    made homework 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,11 +12,61 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание не принимается
 """
+from timeit import timeit
+
+setup = """
+from __main__ import func_1, func_2, func_3, func_4
+from random import randint
+nums = [i for i in range(100)]
+"""
 
 
 def func_1(nums):
+    """Сложность O(n)"""
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    """Сложность O(n)"""
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+def func_3(nums):
+    """Сложность O(n)"""
+    new_arr = []
+    for i in enumerate(nums):
+        if i[1] % 2 == 0:
+            new_arr.append(i[0])
+    return new_arr
+
+
+def func_4(nums):
+    new_arr = list(filter(even_fn, nums))
+    return new_arr
+
+
+def even_fn(x):
+    return x % 2 == 0
+
+
+print("Исходная функция   ", timeit("func_1(nums)", setup, number=1000))
+print("List Comprehension ", timeit("func_2(nums)", setup, number=1000))
+print("Enumerate          ", timeit("func_3(nums)", setup, number=1000))
+print("Filter             ", timeit("func_4(nums)", setup, number=1000))
+
+
+"""
+Проведены замеры времени.
+В качестве дополнительного решения поставленной задачи выбрана встроенная функция filter.
+Результаты:
+
+Исходная функция    0.026812399999999986
+List Comprehension  0.025412600000000007
+Enumerate           0.0434957
+Filter              0.038406300000000004
+"""
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,11 @@
 
 Без аналитики задание считается не принятым
 """
+from cProfile import run
+from timeit import timeit
+from random import randint
+
+num_10000 = randint(100000000, 10000000000000)
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +42,79 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+def revers_4(enter_num):
+    enter_num = str(enter_num)
+    revers_num = enter_num[::-1]
+    yield revers_num
+
+run('revers_1(num_10000)')
+print(timeit("revers_1(num_10000)", globals=globals(), number=10000))
+
+run('revers_2(num_10000)')
+print(timeit("revers_2(num_10000)", globals=globals(), number=10000))
+
+run('revers_3(num_10000)')
+print(timeit("revers_3(num_10000)", globals=globals(), number=10000))
+
+run('revers_3(num_10000)')
+print(timeit("revers_4(num_10000)", globals=globals(), number=10000))
+
+"""
+Профилирование через cProfile не находит проблем с функциями. Все работают оптимально.
+Замерим через timeit с number=10000.
+Быстрее всего работает третья функция.
+В качесте оптимизации, сделаем из функции генератор.
+
+Результат: 
+
+         17 function calls (4 primitive calls) in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+     14/1    0.000    0.000    0.000    0.000 task_3.py:23(revers_1)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+0.4674053
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:33(revers_2)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+0.32216619999999985
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:41(revers_3)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+0.023318800000000195
+         4 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:41(revers_3)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+0.016442000000000068
+
+"""
