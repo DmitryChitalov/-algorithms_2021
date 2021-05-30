54a3b43939ba80082a4ad124c168f3d31009b045
AVO777 <seo@ox2.ru>
2021-05-30 19:56:59 +0300

    Домашнее задание к уроку №5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -5,9 +5,52 @@ deque – это обобщение стеков и очередей.
 Вот основное правило: если вам нужно что-то быстро дописать или вытащить, используйте deque.
 Если вам нужен быстрый случайный доступ, используйте list.
 
-Задача: 
+Задача:
 1) создайте простой список (list) и очередь (deque). Сделайте замеры и оцените что заполняется быстрее.
 2) Выполните различные операции с каждым из объектов. Сделайте замеры и оцените, где и какие операции быстрее.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+from collections import deque
+from timeit import timeit
+from random import randint
+
+n = randint(100, 1000)
+
+lst = [randint(0, 100) for el in range(n)]
+deq = deque(randint(0, 100) for el in range(n))
+
+print(f'Время генерации элементов списка '
+      f'{timeit("lst = [randint(0, 100) for el in range(n)]", globals=globals(), number=100)}')
+
+print(f'Время генерации элементов дека '
+      f'{timeit("deq = deque(randint(0, 100) for el in range(n))", globals=globals(), number=100)}')
+
+print(f'Время добавления элементов в начало списка '
+      f'{timeit("lst.insert(0, 1)", globals=globals(), number=100000)}')
+
+print(f'Время добавления элементов в начало дека '
+      f'{timeit("deq.appendleft(1)", globals=globals(), number=100000)}')
+
+print(f'Время удаления элементов c начала списка '
+      f'{timeit("lst.pop(0)", globals=globals(), number=100000)}')
+
+print(f'Время удаления элементов c начала дека '
+      f'{timeit("deq.popleft()", globals=globals(), number=100000)}')
+
+print(f'Время добавления элементов в конец списка '
+      f'{timeit("lst.append(2)", globals=globals(), number=100000)}')
+
+print(f'Время добавления элементов в конец дека '
+      f'{timeit("deq.append(2)", globals=globals(), number=100000)}')
+
+print(f'Время удаления элементов c конца списка '
+      f'{timeit("lst.pop()", globals=globals(), number=100000)}')
+
+print(f'Время удаления элементов c конца дека  '
+      f'{timeit("deq.pop()", globals=globals(), number=100000)}')
+
+'''
+Время генерации элементов дека и списка практически одинаковые как и встроеные функции pop и append. А вот 
+appendleft и popleft во много раз быстрее у дека чем у списка. 
+'''
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,42 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from collections import OrderedDict
+from timeit import timeit
+from random import randint
+
+n = randint(1000, 10000)
+
+dct = {el: randint(0, 1000) for el in range(n + 1)}
+order_dct = OrderedDict(dct)
+
+print(f'Время генерации элементов словаря '
+      f'{timeit("dct ={el: randint(0,1000) for el in range(n)}" ,globals=globals(), number=1000)}')
+
+print(f'Время генерации элементов OrderedDict '
+      f'{timeit("order_dct = OrderedDict.fromkeys([randint(0, 1000) for el in range(n)])",globals=globals(), number=1000)}')
+
+print(f'Время получения элемента из словаря '
+      f'{timeit("dct[randint(100, n)]", globals=globals(), number=100000)}')
+
+print(f'Время получения элемента из OrderedDict '
+      f'{timeit("order_dct[randint(100, n)]", globals=globals(), number=100000)}')
+
+print(f'Время получения пар значений из словаря '
+      f'{timeit("dct.items()", globals=globals(), number=100000)}')
+
+print(f'Время получения пар значений из OrderedDict '
+      f'{timeit("order_dct.items()", globals=globals(), number=100000)}')
+
+print(f'Время удаления всех элементов из словаря '
+      f'{timeit("dct.popitem()", globals=globals(), number=n)}')
+
+print(f'Время удаления всех элементов из OrderedDict '
+      f'{timeit("order_dct.popitem()", globals=globals(), number=n)}')
+
+
+'''
+Судя по всем замерам они практически идентичны, но чутку быстрее обычный словарь. 
+Смысла использовать OrderedDict в версии python 3.6 и выше нет.
+Может в каких нибудь только специфических задачах.
+'''
