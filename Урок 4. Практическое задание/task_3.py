b698993bbc319ac8ee6a2af8a8861dc75b3f1488
Dmitry Ageev <dimasikk99@gmail.com>
2021-05-31 19:23:57 +0300

    Update task_3.py

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -15,6 +15,10 @@
 """
 
 
+from timeit import timeit
+from cProfile import run
+
+
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -37,3 +41,45 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    """Реверс - 1 вариант"""
+    enter_list = list(str(enter_num))
+    enter_list.reverse()
+    return ''.join(enter_list)
+
+
+def revers_5(enter_num):
+    """Реверс - 2 вариант"""
+    return ''.join(reversed(str(enter_num)))
+
+
+if __name__ == '__main__':
+    numb = 123456789 ** 10
+    print(f'************ "timeit" ************')
+    print(f'1) revers_1 > '
+          f'{timeit("revers_1(numb)", number=10000, globals=globals())}')
+    print(f'2) revers_2 > '
+          f'{timeit("revers_2(numb)", number=10000, globals=globals())}')
+    print(f'3) revers_3 > '
+          f'{timeit("revers_3(numb)", number=10000, globals=globals())}')
+    print(f'3) revers_4 > '
+          f'{timeit("revers_4(numb)", number=10000, globals=globals())}')
+    print(f'3) revers_5 > '
+          f'{timeit("revers_5(numb)", number=10000, globals=globals())}')
+    print('\n')
+
+    print(f'********** cProfile ***********')
+    run('revers_1(numb)')
+    run('revers_2(numb)')
+    run('revers_3(numb)')
+    run('revers_4(numb)')
+    run('revers_5(numb)')
+
+"""
+Рекурсивная функция "revers_1" - самая медленная. Профилировка через cProfile показалала 85 вызовов.
+Реализация через цикл в "revers_2" немного быстрее.
+Способ через срез "revers_3" самый эффективный, т.к. без арифметики в цикле и рекурсии.
+Варианты "revers_4" и "revers_5" немного более эффективны цикла, но менее эффективны среза; из-за преобразований встроенных функций.
+"""
