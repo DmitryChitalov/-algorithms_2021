5095daa70b0698cdd6f222280a5f6d054f2dd134
codezerodawn <codezerodawn@gmail.com>
2021-06-01 11:14:32 +0300

    lesson 6 done

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,35 @@
 Придумать как это решить!
 Есть очень простое решение
 """
+
+from memory_profiler import memory_usage
+from random import randint
+
+
+def mem_decorator(some_func):
+    def wrapper(*args, **kwargs):
+        result = some_func(*args, **kwargs)
+        mem_list.append(str(memory_usage()))
+        return result
+    return wrapper
+
+
+
+@mem_decorator
+def recursive_reverse(number):
+    if number == 0:
+        return ''
+    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+
+if __name__ == '__main__':
+    mem_list = []
+    num_10000 = randint(100000000, 10000000000000)
+    print(f'Задействована память до запуска: '
+          f'{str(memory_usage())} MB')
+    recursive_reverse(num_10000)
+    for i, mem in enumerate(list(reversed(mem_list)), 1):
+        print(f'Задействована память после запуска: '
+              f'{i} раз: {mem} MB')
+
