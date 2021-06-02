731aa417b3b3ae451f33b61c26934a0067dcd0c3
AVO777 <seo@ox2.ru>
2021-06-02 23:06:22 +0300

    s

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -24,7 +24,8 @@
 
 from memory_profiler import memory_usage, profile
 from timeit import default_timer
-from collections import namedtuple
+from random import randint
+
 
 def memory_time_profiler(func):
     def wraper(*args):
@@ -37,48 +38,37 @@ def memory_time_profiler(func):
         return result
     return wraper
 
-@profile
-def add_firm(count_firm):
-    all_company = []
-    for el in range(count_firm):
-        name = input('Введите название фирмы')
-        profits_srt = input('Введите через пробел прибыль данного предприятия за каждый квартал: ')
-        profits = profits_srt.split()
-        if len(profits) != 4:
-                print('Вы ввели либо мало, либо много значений')
-                continue
-        try:
-            average_profit = (int(profits[0]) + int(profits[1]) + int(profits[2]) + int(profits[3])) / 4
-            firm_tuple = Firm_template(name_firm=name,
-                                       average_profit_firm=average_profit)
-            all_company.append(firm_tuple)
-        except ValueError:
-            print('Вы ввели не правильное значение прибыли!!!')
-            continue
-    return all_company
 
-@profile
-def aver_profit(all_company):
-    average_profit_all_firm = 0
-    average_profit_less = []
-    average_profit_more = []
-    for el in all_company:
-        average_profit_all_firm = average_profit_all_firm + el.average_profit_firm
-    average_profit_all_firm = average_profit_all_firm / len(all_company)
-    print(f'Средняя годовая прибыль всех предприятий: {average_profit_all_firm}')
-    for el in all_company:
-        if el.average_profit_firm >= average_profit_all_firm:
-            average_profit_more.append(el.name_firm)
+@memory_time_profiler
+def even_and_uneven_shell(n):
+    def even_and_uneven_rec(num, even=0, uneven=0):
+        if num == 0:
+            return f'   even_and_uneven_shell\n' \
+                   f'Even: {even}\n' \
+                   f'Uneven: {uneven}\n'
+        elif num % 2 == 0:
+            even += 1
         else:
-            average_profit_less.append(el.name_firm)
-    print(f'Предприятия, с прибылью выше среднего значения: {average_profit_more}')
-    print(f'Предприятия, с прибылью ниже среднего значения: {average_profit_less}')
+            uneven += 1
+        return even_and_uneven_rec(num // 10, even, uneven)
+
+    return even_and_uneven_rec(n)
 
 
-Firm_template = namedtuple('Firm_Profit', 'name_firm average_profit_firm')
-try:
-    count_firm = int(input('Введите количество предприятий для расчета прибыли: '))
-    aver_profit(add_firm(count_firm))
-except ValueError:
-    print('Вы ввели не число!!!')
+@memory_time_profiler
+def even_and_uneven(num):
+    even = 0
+    uneven = 0
+    for i in str(num):
+        if int(i) % 2 == 0:
+            even += 1
+        else:
+            uneven += 1
+    return f'   even_and_uneven\n' \
+           f'Even: {even}\n' \
+           f'Uneven: {uneven}\n'
 
+
+usr_num = randint(900000, 1000000)
+usr_lst = [randint(0, 100) for el in range(1000)]
+print(usr_num)
