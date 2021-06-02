bb1b5eb1939b0f69132b5330306eb229f02d1ff8
ozzzzz <you@example.com>
2021-06-02 22:51:02 +0300

    homework 6

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,23 @@
 Придумать как это решить!
 Есть очень простое решение
 """
+from memory_profiler import profile
+
+
+@profile
+def profile_rec_stop(*args):
+    def recursion_odd(num, odd=0, even=0):
+        if num == 0:
+            return f'{odd}, {even}'
+        if (num % 10) % 2 == 0:
+            odd += 1
+            return recursion_odd(num // 10, odd, even)
+        else:
+            even += 1
+            return recursion_odd(num // 10, odd, even)
+    return recursion_odd(*args)
+
+
+print(profile_rec_stop(12343534535763567543734))
+
+
