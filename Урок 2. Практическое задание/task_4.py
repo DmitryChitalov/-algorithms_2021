ae2e81a9a2191bbf7aca0a657c9429531c62fdda
Alexey Varlamov <absinthe.hf@gmail.com>
2021-06-02 16:39:07 +0300

    HW L2
    ДЗ к второму уроку

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -12,3 +12,18 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def summation_divided(el_num, el=1, total=0, count=0):
+    if count == el_num:
+        return total
+    else:
+        total += el
+        el /= -2
+        el_num -= 1
+        return summation_divided(el_num, el, total)
+
+
+data = int(input('Введите количество элементов: '))
+print(summation_divided(data))
+
