dc85ddf2528f5c6f334d986d0a5548e23ad533a4
valvandor <sincov.vitalii@gmail.com>
2021-06-02 17:56:54 +0300

    done

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -10,3 +10,54 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+
+from collections import Counter, deque
+
+
+def huffman_tree(str_obj):
+    count = Counter(str_obj)
+    sorted_elements = deque(sorted(count.items(), key=lambda symbol: symbol[1]))
+
+    if len(sorted_elements) != 1:
+        while len(sorted_elements) > 1:
+            weight = sorted_elements[0][1] + sorted_elements[1][1]
+
+            comb = {0: sorted_elements.popleft()[0], 1: sorted_elements.popleft()[0]}
+
+            for index, _count in enumerate(sorted_elements):
+                if weight > _count[1]:
+                    continue
+                else:
+                    sorted_elements.insert(index, (comb, weight))
+                    break
+            else:
+                sorted_elements.append((comb, weight))
+    else:
+        weight = sorted_elements[0][1]
+        comb = {0: sorted_elements.popleft()[0], 1: None}
+        sorted_elements.append((comb, weight))
+
+    return sorted_elements[0][0]
+
+
+code_table = dict()
+
+
+def huffman_code(tree, path=''):
+    if not isinstance(tree, dict):
+        code_table[tree] = path
+    else:
+        huffman_code(tree[0], path=f'{path}0')
+        huffman_code(tree[1], path=f'{path}1')
+
+
+s = "huffman code"
+
+huffman_code(huffman_tree(s))
+
+# выводим коды для каждого символа
+for i in s:
+    print(code_table[i], end=' ')
+print()
+
+# Код идентичен материалу с урока, в доработке не нуждается на мой взгляд. Решение элегантное и эффективное.
