eca9a3bbf54db2a4906762400c54e2067f27599b
Romnako <76477868+Romnako@users.noreply.github.com>
2021-06-03 11:39:37 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -16,3 +16,13 @@
 р
 а
 """
+my_string = input('Enter the string: ')
+my_set = set()
+
+for i in range(len(my_string)):
+    for j in range(len(my_string) - 1 if i == 0 else len(my_string), i, -1):
+        my_set.add(hash(my_string[i:j]))
+
+substrings_count = len(my_set)
+print(f"In the line: '{my_string}' {substrings_count} substrings. \n"
+      f"Hash {my_set}")
