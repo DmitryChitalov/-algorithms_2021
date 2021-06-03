37a922d2a6aa15a8b6f309025848eb95dffd9f6c
Romnako <76477868+Romnako@users.noreply.github.com>
2021-06-03 11:35:36 +0300

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -18,3 +18,57 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+import hashlib
+
+def db_data_inserting(table_name):
+    pass
+
+
+def db_data_reading(users, hashed_user_data):
+    pass
+
+def data_hashing(hashed_data: list):
+        hashed_data[1] = hashlib.sha256(hashed_data[0].encode() + hashed_data[1].encode()).hexdigest()
+        return hashed_data
+
+
+def entering_user_data():
+    new_login = input('Enter your username: ')
+    user_password = input('Enter password: ')
+    user_data = [new_login, user_password]
+    return user_data
+
+
+
+def user_registration():
+    """
+    User registration function
+    """
+    print('To register, enter your username and password.')
+    new_user_data = entering_user_data()
+    return new_user_data
+
+
+def user_authentication():
+    print('To enter your username and password.')
+    hashed_user_data = data_hashing(entering_user_data())
+    if db_data_reading('users', hashed_user_data[0]) == hashed_user_data[1]:
+        return True
+    elif db_data_reading('users', hashed_user_data[0]) is None:
+        return False
+
+
+user_choice = None
+while user_choice != '0':
+    user_choice = input('Enter \n "1" To register a user, \n '
+                         '"2" To login:')
+    if user_choice == '1':
+        user_registration()
+    elif user_choice == '2':
+        if user_authentication():
+            print(print('Authorization was successful! Welcome!'))
+            user_choice = '0'
+        else:
+            print('Username or password is incorrect!')
+    else:
+        print('Input is incorrect')
