c2c29bd10f269ddfc8a9858904cf64f77786c296
redaxel <97jertov97@gmail.com>
2021-06-03 16:02:51 +0300

    Lesson-5 HW

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,40 @@ deque – это обобщение стеков и очередей.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+from timeit import timeit
+from collections import deque
+
+lst = []
+
+deque = deque()
+
+
+def lst_filling():
+    for el in range(1000):
+        lst.append(el)
+
+
+def deq_filling():
+    for el in range(1000):
+        deque.append(el)
+
+
+print(timeit('lst_filling', globals=globals()))
+print(timeit('deq_filling', globals=globals()))
+
+
+def lst_insert():
+    for el in range(10000):
+        lst.insert(0, el)
+
+
+def deque_insert():
+    for el in range(10000):
+        deque.appendleft(el)
+
+
+print(timeit('lst_insert', globals=globals()))
+print(timeit('deque_insert', globals=globals()))
+
+# Судя по замерам можно сказать что они всегда разные, но приблизительно равны, однако в большинстве случаев
+# deque уступает по скорости.
