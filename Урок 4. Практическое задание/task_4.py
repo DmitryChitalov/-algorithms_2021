a618f6777ce7cb648516f5284357bacb86950e39
Dukemegaman <Dukemegaman@gmail.com>
2021-06-05 00:08:13 +0300

    tasks 2, 3, 4 update

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,6 +11,12 @@
 
 Без аналитики задание считается не принятым!
 """
+"""
+func_3 - написание функции в одно строчку оказалось не столь эффективным и использование в ней лямбда-функции 
+привело лишь к увеличению времени выполнения функции
+func_4 - показала самые быстрые результаты за счёт функции set. Обращаясь к списку с готовыми внесёнными значениями
+функция set выполняет меньшее количество итераций
+"""
 from timeit import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
@@ -47,9 +53,24 @@ def func_3():
            f'оно появилось в массиве {count} раз(а)'
 
 
+def func_4():
+    m = 0
+    num = 0
+    for i in set(array):
+        count = array.count(i)
+        if count > m:
+            m = count
+            num = i
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {m} раз(а)'
+
+
 print(timeit('func_1()', globals=globals()))
 print(timeit('func_2()', globals=globals()))
 print(timeit('func_3()', globals=globals()))
-print(func_1())
-print(func_2())
-print(max(array, key=lambda k: array.count(k)))
+print(timeit('func_4()', globals=globals()))
+# print(func_1())
+# print(func_2())
+# print(func_3())
+# print(func_4())
+
