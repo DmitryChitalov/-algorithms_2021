60be829c3dc4b52e10b2b70366c26534390e0195
Dmitry Ageev <dimasikk99@gmail.com>
2021-06-04 16:57:05 +0300

    Update task_1.py

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,25 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+
+
+from collections import namedtuple
+from collections import defaultdict
+
+companies = namedtuple('firms', 'comp_name revenue')
+companies_list = []
+number_of_comp = int(input('Enter the number of companies: '))
+
+for i in range(number_of_comp):
+    company_name = input('Enter the name of the company: ')
+    income = input('Enter the profit of this enterprise for each quarter, separated by a space: ')
+    companies_list.append(companies(company_name, sum(list(map(int, income.split())))))
+
+average_profit = sum(int(c.revenue) for c in companies_list) / number_of_comp
+print(f'{"*" * 40}')
+print(f'Average annual profit of all companies: {average_profit}')
+for i in companies_list:
+    if i.revenue >= average_profit:
+        print(f'Enterprises with above average profit: {i.comp_name}\n')
+    else:
+        print(f'Enterprises with below average profit: {i.comp_name}')
