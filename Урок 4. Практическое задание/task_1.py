70861e116dd8235bf5db574e1a3ff5ce18580411
Romnako <76477868+Romnako@users.noreply.github.com>
2021-06-05 19:07:25 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,17 @@
 Без аналитики задание не принимается
 """
 
+"""
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+   """
+
+from timeit import timeit
+
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +31,26 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+# Measurements of code execution time*Замеры времени выполнения кода
+elements = [i for i in range(200)]
+print(timeit("func_1(elements)", globals=globals()))  # 13.3329138
+
+
+# Algorithm optimization using list comprehension*Оптимизация алгоритма, используя list comprehension
+def func_2(nums):
+    new_arr = [el for el in nums if el % 2 == 0]
+    return new_arr
+
+
+# Runtime measurements after optimization*Замеры времени выполнения после оптимизации
+print(timeit("func_2(elements)", globals=globals()))  # 8.2158034
+
+
+"""
+Using a list comprehension is faster than an iterator with an append method.
+After optimization, the function works 1.62 times faster.
+Использование list comprehension работает быстрее, чем итератор с методом append.
+После оптимизации  функция работает быстрее в 1,62 раза.
+"""
