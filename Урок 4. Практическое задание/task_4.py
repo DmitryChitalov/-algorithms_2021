9e158e6a3c3f672452336b21e6897cd1782d8c9f
Romnako <76477868+Romnako@users.noreply.github.com>
2021-06-05 19:42:18 +0300

    Update task_4.py

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -38,6 +38,35 @@ def func_2():
     return f'Чаще всего встречается число {elem}, ' \
            f'оно появилось в массиве {max_2} раз(а)'
 
+def func_3():
+    max_elem = max(set(array), key=array.count)
+    count = array.count(max_elem)
+    return f'Чаще всего встречается число {max_elem}, ' \
+           f'оно появилось в массиве {count} раз(а)'
+
 
 print(func_1())
 print(func_2())
+print(func_3())
+
+print(f'func_1() - {timeit("func_1()", globals=globals())}')
+print(f'func_2() - {timeit("func_2()", globals=globals())}')
+print(f'func_3() - {timeit("func_3()", globals=globals())}')
+
+
+"""
+The func_3 () function works faster if you pass a set instead of an array of numbers to the max () arguments.
+The iteration will only go over the unique elements of the list. Without this, the speed of func_1 and 3 is the same.
+func_2 () is the slowest because it uses the second list.
+
+Функция func_3() отрабатывает быстрее, если в аргументы max() передать не маcсив чисел, а множество.
+Итерация пойдет только по уникальным элементам списка. Без этого скорость func_1 и 3 одинаковая.
+Функция func_2() самая медленная потому, что в ней используется второй список.
+___________________________________________________________________________________________________________________
+Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+Чаще всего встречается число 1, оно появилось в массиве 3 раз(а)
+func_1() - 1.210743
+func_2() - 1.644316
+func_3() - 1.2244089000000002
+"""
