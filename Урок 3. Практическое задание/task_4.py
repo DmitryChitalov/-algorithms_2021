71eb78e8707773cd0aa6c601f3833463a404b110
Lebedev Sergey <fackaster@gmail.com>
2021-06-05 11:52:34 +0300

    Home work 3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,20 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+import hashlib
+
+
+def check_hash(url, dict_cache):
+    hash_url = hashlib.sha256('your salt is mine'.encode() + url.encode()).hexdigest()
+    if hash_url not in dict_cache.keys():
+        dict_cache[hash_url] = url
+    return
+
+
+db_cache = {}
+list_test_url = ['https://github.com/', 'https://ru.wikipedia.org/', 'https://translate.google.com/',
+                 'https://github.com/', 'https://python-scripts.com/', 'https://ru.wikipedia.org/']
+[check_hash(i, db_cache) for i in list_test_url]
+print(db_cache)
