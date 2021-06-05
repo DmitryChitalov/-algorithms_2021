e4bea407b0d30122f7d24f24210348dbd47760ce
Stas Tyurin <tyurinsy@gmail.com>
2021-06-06 01:49:04 +0300

    Lesson4. Homework

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,7 +12,8 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание не принимается
 """
-
+from timeit import timeit
+import random
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +21,27 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+def func_gen(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+def func_gen_enum(nums):
+    return [i for i, n in enumerate(nums) if n % 2 == 0]
+
+test_arr = [random.randint(1, 100) for i in range(10000)]
+
+print('Функция из задания:', timeit("func_1(test_arr)", globals=globals(), number=1000))
+print('Функция c генератором:', timeit("func_gen_enum(test_arr)", globals=globals(), number=1000))
+print('Функция c генератором и счетчиком:', timeit("func_gen_enum(test_arr)", globals=globals(), number=1000))
+
+"""
+Функция из задания: 0.826497
+Функция c генератором: 0.6844592
+Функция c генератором и enumerate: 0.7028129000000001
+
+Если заменить обход списка в цикле на генератор, то получается быстрее.
+Вероятно из-за особенностей вызова в цикле append против заточенной под это структуры генератора списков
+
+Если к генератору добавить enumerate, то время чуть увеличивается. 
+Предполагаю из-за времени создания кортежа счетчиком
+"""
