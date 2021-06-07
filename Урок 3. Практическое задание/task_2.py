51a859d2497b413ae223b85bfef09a2d16ff528c
MaximGubanov <m_gubanov@bk.ru>
2021-06-07 20:12:00 +0300

    ДЗ_№3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -18,3 +18,75 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+import hashlib
+import sqlite3
+
+conn = sqlite3.connect('sqlite_task2.db')
+cur = conn.cursor()
+"""Создадим тестовую БД"""
+cur.execute(""" CREATE TABLE IF NOT EXISTS users (
+    id INT(11) PRIMARY KEY,
+    user VARCHAR(50),
+    password VARCHAR(256)
+    ); """)
+
+""" Это три польз-ля по умолчанию. Пароли я предварительно захешировал и поместил
+готовые хеши в звпрос
+max - 123, 
+srj - 456, 
+egor - 789
+Для новых польз-ей хеширование происходит в ф-и reg_user() """
+
+cur.execute("""INSERT INTO users (user, password) VALUES
+    ('max', '74ba7e49e963635164033eb502fb4d47ad7b0ee662a7e0370a0ea0c66659ef88'),
+    ('srj', 'ee61e92bd55cd39d85310371195f532109fdb359d418993bb5435a92793e484a'),
+    ('egor', '52842e8d1f4f8b64a888141c5eba075f6ee3237e47c9040687c03a3fe46de80f');
+    """)
+conn.commit()
+
+def check_user(login):
+    """ Ф-я проверят сущ-ет ли пользователь в БД,
+    возвращает True  или False взависимости от рещультата """
+    cur.execute(f"""SELECT user, password FROM users WHERE user = '{login}';""")
+    sample = cur.fetchone()
+    conn.commit()
+    if sample:
+        return sample[1]    # Если поль-ль существует, то возвр-ет его хеш-пароль
+    else:
+        return False
+
+def reg_user(n):
+    """ Ф-я для регистрации новых пользователй, вызывается, если польз-ль
+    не сущ-ет в БД
+    n - Кол-во попыток """
+    login = input('Введите новый логин: ')
+    password = input('Придумайте пароль: ')
+    password_hash = hashlib.sha256(login.encode() + password.encode()).hexdigest()
+    conn = sqlite3.connect('sqlite_task2.db')
+    cur = conn.cursor()
+    sql = f"""INSERT INTO users (user, password) VALUES ('{login}', '{password_hash}');"""
+    if cur.execute(sql):
+        conn.commit()
+        print(f'Регистрация пройдена успешно. Авторизуйтесь!')
+        return auth_user(input('Введите логин: '), input('Введите пароль: '))
+    else:
+        print(f'Попробуйте снова')
+        return reg_user(n - 1)
+
+def auth_user(login, password):
+    """ Ф-я для авторизация польз-ей. В качестве "соли" добавил логин пользователя"""
+    salt = login
+    sample = check_user(login)  # Выполняем проверку польз-ля
+    if sample:
+        hash_password = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+        if hash_password == sample:
+            return f'Вы ввели правильный пароль'
+        else:
+            return f'Пароль не верный'
+    else:
+        print(f'Нет пользователя с таким именем.\nПройдите регистрацию')
+        return reg_user(n=3)
+
+print(auth_user(input('Введите логин: '), input('Введите пароль: ')))
+
+
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,36 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+import requests
+import hashlib
+from uuid import uuid4
+
+URL = 'https://www.avito.ru'
+URL1 = 'https://www.google.com'
+
+def get_url(url):
+    try:
+        response = requests.get(url)
+        return response.status_code
+    except ConnectionError:
+        return requests.exceptions.ConnectionError
+
+salt = uuid4().hex
+cash_url = {}
+
+def get_page(url):
+    if cash_url.get(url):
+        return f'Адрес присутствует в кэше'
+    else:
+        if get_url(url) == 200:
+            data = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+            cash_url[url] = data
+            return cash_url
+        else:
+            return f'Адрес недоступен'
+
+print(get_page(URL))
+print(get_page(URL1))
+
+print(get_page(URL))
+print(get_page(URL1))
