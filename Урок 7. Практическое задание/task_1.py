7bad6414cb84f92946ba1e8492fefbea5e271993
DmitriiFedorov <fdorov_dima@mail.ru>
2021-06-07 16:21:25 +0300

    Задание 7 Заглушка(успел 1(первое) задание сделать)

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,129 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+import timeit
+import random
+
+# как в уроке, но по убыванию
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+
+print(orig_list)
+print(bubble_sort(orig_list))
+
+
+print(
+    timeit.timeit(
+        "bubble_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random.randint(-100, 100) for _ in range(100)]
+
+# замеры 100
+print(
+    timeit.timeit(
+        "bubble_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random.randint(-100, 100) for _ in range(1000)]
+
+# замеры 1000
+print(
+    timeit.timeit(
+        "bubble_sort(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+
+
+"""
+0.007448299999999998
+0.7391278
+80.6800633 - вышел быстрее чем в примере
+"""
+# с прерыванием, не уверен, но работает быстрее
+
+
+def bubble_sort_while(lst_obj):
+    n = 1
+    counter = 0
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+                counter += 1
+        if not counter:
+            break
+        else:
+            n += 1
+    return lst_obj
+
+
+test_list = [[100, 40, 20, 10, 9, 5, 4, 2, 1, -1, -4] for _ in range(10)]
+print(test_list)
+print(bubble_sort_while(test_list))
+
+print(
+    timeit.timeit(
+        "bubble_sort_while(test_list[:])",
+        globals=globals(),
+        number=1000))
+
+test_list = [[100, 40, 20, 10, 9, 5, 4, 2, 1, -1, -4] for _ in range(100)]
+
+# замеры 100
+print(
+    timeit.timeit(
+        "bubble_sort_while(test_list[:])",
+        globals=globals(),
+        number=1000))
+
+test_list = [[100, 40, 20, 10, 9, 5, 4, 2, 1, -1, -4] for _ in range(1000)]
+
+# замеры 1000
+print(
+    timeit.timeit(
+        "bubble_sort_while(test_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+
+print(orig_list)
+print(bubble_sort_while(orig_list))
+
+
+print(
+    timeit.timeit(
+        "bubble_sort_while(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random.randint(-100, 100) for _ in range(100)]
+
+# замеры 100
+print(
+    timeit.timeit(
+        "bubble_sort_while(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random.randint(-100, 100) for _ in range(1000)]
+
+# замеры 1000
+print(
+    timeit.timeit(
+        "bubble_sort_while(orig_list[:])",
+        globals=globals(),
+        number=1000))
