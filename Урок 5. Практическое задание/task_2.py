009159f0cc469b575a1abc7e97ea528440fb9d8a
Stas Tyurin <tyurinsy@gmail.com>
2021-06-08 21:37:10 +0300

    Lesson 5. Homework

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -34,3 +34,45 @@ hx = HexNumber
 hx + hx
 hex()
 """
+
+from collections import defaultdict
+from functools import reduce
+
+def count_hex(list_dict):
+    arr_list_hex = [int(''.join(x), 16) for x in list_dict.values()]
+    sum_hex = list(hex(sum(arr_list_hex)))[2:]
+    multi_hex = list(hex(reduce(lambda x, y: x * y, arr_list_hex)))[2:]
+
+    print('Сумма чисел: ', sum_hex) #[‘C’, ‘F’, ‘1’]
+    print('Произведение чисел: ', multi_hex) #[‘7’, ‘C’, ‘9’, ‘F’, ‘E’]
+
+list_hex = defaultdict(dict)
+number_1 = input("Введите первое hex число: ")
+list_hex[number_1] = list(number_1)
+print(list_hex[number_1])
+
+number_2 = input("Введите второе hex число: ")
+list_hex[number_2] = list(number_2)
+print(list_hex[number_2])
+
+count_hex(list_hex)
+
+#---------------
+class hex_number:
+    def __init__(self, a):
+        self.a = list(a)
+
+    def __add__(self, other):
+        return list(hex(int(''.join(self.a), 16) + int(''.join(other.a), 16)))[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.a), 16) * int(''.join(other.a), 16)))[2:]
+
+hex_num_1 = hex_number(number_1)
+hex_num_2 = hex_number(number_2)
+
+print('--------')
+print('Сделали через класс:')
+print('Сумма чисел: ', hex_num_1 + hex_num_2)
+print('Произведение чисел: ', hex_num_1 * hex_num_2)
+
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,56 @@ deque – это обобщение стеков и очередей.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+from collections import deque
+from timeit import timeit
+
+list_1 = list()
+deq_1 = deque(list_1)
+
+def fill_fun(cur_list):
+    for x in range(1000):
+        cur_list.append(x)
+
+
+def fun_list_1(cur_list):
+    cur_list.insert(0, 500)
+
+
+def fun_deq_1(cur_deq):
+    cur_deq.appendleft(500)
+
+
+def fun_list_2_1(cur_list):
+    cur_list.pop(0)
+
+def fun_list_2_2(cur_list):
+    cur_list.pop()
+
+def fun_deq_2_1(cur_deq):
+    cur_deq.popleft()
+
+def fun_deq_2_2(cur_deq):
+    cur_deq.pop()
+
+print("Заполнение списка", timeit("fill_fun(list_1)", globals=globals(), number=10000))
+print("Заполнение очереди", timeit("fill_fun(deq_1)", globals=globals(), number=10000))
+print("Добавление в начало списка", timeit("fun_list_1(list_1)", globals=globals(), number=10000))
+print("Добавление в начало очердь", timeit("fun_deq_1(deq_1)", globals=globals(), number=10000))
+print("Удаление из начала списка", timeit("fun_list_2_1(list_1)", globals=globals(), number=10000))
+print("Удаление с конца списка", timeit("fun_list_2_2(list_1)", globals=globals(), number=10000))
+print("Удаление из начала очереди", timeit("fun_deq_2_1(deq_1)", globals=globals(), number=10000))
+print("Удаление с конца очереди", timeit("fun_deq_2_1(deq_1)", globals=globals(), number=10000))
+
+# Заполнение списка 0.6838525
+# Заполнение очереди 0.5866668999999999
+# ---Наполнение элементами списка длится чуть дольше, но не критично---
+
+# Добавление в начало списка 70.7593818
+# Добавление в начало очердь 0.0009275999999971418
+# ---Тяжело списку вставить элемент вначало. Время отличается на несколько порядков---
+
+# Удаление из начала списка 86.0461768
+# Удаление с конца списка 0.0013219999999876109
+# Удаление из начала очереди 0.001155299999993531
+# Удаление с конца очереди 0.0011632000000076914
+# ---Удаление первого элемента из списка - тоже очень долго. Очередь на несколько порядков быстрее---
