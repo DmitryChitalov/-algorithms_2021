8811d7e8694fed05b49c5d4838e31003d33ef589
Alexander Bochkov <abochkov@mail.ru>
2021-06-09 15:24:47 +0300

    Домашняя работа к Уроку 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 Задание 1.
 
 Приведен код, который позволяет сохранить в
@@ -13,10 +13,44 @@
 Без аналитики задание не принимается
 """
 
+from timeit import timeit
 
-def func_1(nums):
+
+def func_1(nums):  # O(n)
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):  # O(n)
+    return [i for i, el in enumerate(nums) if el % 2 == 0]
+
+
+my_nums = [el for el in range(1000)]
+print("Количество запусков - 1 000:")
+print("func_1:", timeit("func_1(my_nums)", globals=globals(), number=1000))
+print("func_2:", timeit("func_2(my_nums)", globals=globals(), number=1000))
+print("\nКоличество запусков - 10 000:")
+print("func_1:", timeit("func_1(my_nums)", globals=globals(), number=10000))
+print("func_2:", timeit("func_2(my_nums)", globals=globals(), number=10000))
+print("\nКоличество запусков - 100 000:")
+print("func_1:", timeit("func_1(my_nums)", globals=globals(), number=100000))
+print("func_2:", timeit("func_2(my_nums)", globals=globals(), number=100000))
+
+"""
+Количество запусков - 1 000:
+func_1: 0.169084162
+func_2: 0.109573213
+
+Количество запусков - 10 000:
+func_1: 1.462735754
+func_2: 1.136822446
+
+Количество запусков - 100 000:
+func_1: 14.851849260999998
+func_2: 11.134291457
+
+Во втором варианте функции применен итератор списка, что позволило сократить время выполнения.
+"""
