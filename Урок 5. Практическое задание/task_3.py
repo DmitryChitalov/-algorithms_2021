d7807225669a2d234cc06afdfcab28856c2ae219
margarita-lomakina <margarita.v.i@mail.ru>
2021-06-09 16:52:41 +0300

    Algorithms homework 5
    by Margarita Lomakina, group 897

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,61 @@ deque – это обобщение стеков и очередей.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+
+from collections import deque
+from random import randint
+from timeit import timeit
+
+def fill_list(nums):
+    return [n for n in nums]
+
+def fill_deque_right(nums):
+    new_deque = deque()
+    for item in nums:
+        new_deque.append(item)
+    return new_deque
+
+def fill_deque_left(nums):
+    new_deque = deque()
+    for item in nums:
+        new_deque.appendleft(item)
+    return new_deque
+
+def fill_deque(nums):
+    return deque(nums)
+
+
+"""
+Заполнение дека происходит медленнее, чем самый быстрый вариант заполнения списка. Однако можно заполнить список и 
+конвертировать его в дек - это самый быстрый способ заполнить дек, который мне удалось придумать.
+Извлечение элементов справа в деке происходит быстрее, слева - сильно быстрее.
+Вставка в рандомное место в списке происходит незначительно быстрее.
+Дек точно стоит использовать, когда элементы извлекаются только с краев.
+
+List:  0.258603
+Deque append:  0.6345072
+Deque append left:  0.6312735
+Convert to deque:  0.07645610000000014
+Извлечь слева из дека:  4.870000000001262e-05
+Извлечь слева из списка:  0.0014690000000001646
+Извлечь справа из дека:  5.5200000000033e-05
+Извлечь справа из списка:  0.00014479999999994497
+Вставка в дек:  0.005069400000000002
+Вставка в список:  0.004730699999999866
+"""
+
+num_list = [randint(0, 100) for _ in range(100000)]
+print('List: ', timeit("fill_list(num_list)", globals=globals(), number=1000))
+print('Deque append: ', timeit("fill_deque_right(num_list)", globals=globals(), number=1000))
+print('Deque append left: ', timeit("fill_deque_left(num_list)", globals=globals(), number=1000))
+print('Convert to deque: ', timeit("fill_deque(num_list)", globals=globals(), number=1000))
+
+test_list = fill_list(num_list)
+test_deque = fill_deque(num_list)
+
+print('Извлечь слева из дека: ', timeit("test_deque.popleft()", globals=globals(), number=10000))
+print('Извлечь слева из списка: ', timeit("del(test_list[0])", globals=globals(), number=10000))
+print('Извлечь справа из дека: ', timeit("test_deque.pop()", globals=globals(), number=10000))
+print('Извлечь справа из списка: ', timeit("test_list.pop(len(test_list)-1)", globals=globals(), number=10000))
+print('Вставка в дек: ', timeit("test_deque.insert(randint(0, 79999), randint(0, 100))", globals=globals(), number=10000))
+print('Вставка в список: ', timeit("test_list.insert(randint(0, 79999), randint(0, 100))", globals=globals(), number=10000))
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,49 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+from collections import OrderedDict
+from random import randint
+from timeit import timeit
+
+test_keys = ['Один', 'Два', 'Три', 'Четыре', 'Пять', 'Шесть', 'Семь', 'Восемь', 'Девять', 'Десять']
+test_values = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
+
+
+def fill_dict(k, v):
+    new_dict = {}
+    for i, key in enumerate(k):
+        new_dict[key] = v[i]
+    return new_dict
+
+
+def fill_ordered_dict(k, v):
+    new_dict = OrderedDict()
+    for i, key in enumerate(k):
+        new_dict[key] = v[i]
+    return new_dict
+
+
+def get_value(t_dict):
+    i = randint(0, 9)
+    key = list(t_dict.keys())[i]
+    return t_dict.get(key)
+
+
+test_dict = fill_dict(test_keys, test_values)
+test_ordered_dict = fill_ordered_dict(test_keys, test_values)
+print('Заполнение dict: ', timeit("fill_dict(test_keys, test_values)", globals=globals(), number=10000))
+print('Заполнение OrderedDict: ', timeit("fill_ordered_dict(test_keys, test_values)", globals=globals(), number=10000))
+print('Получение элемента из dict: ', timeit("get_value(test_dict)", globals=globals(), number=10000))
+print('Получение элемента из OrderedDict: ', timeit("get_value(test_ordered_dict)", globals=globals(), number=10000))
+
+'''
+По данным замерам видно, что особых отличий в скорости работы с обычным словарем и OrderedDict нет 
+(но OrderedDict стабильно чуть медленнее). 
+Кажется, что OrderedDict имеет смысл только если требуется менять порядок ключей в словаре.
+
+Заполнение dict:  0.012172100000000005
+Заполнение OrderedDict:  0.0229751
+Получение элемента из dict:  0.012211200000000005
+Получение элемента из OrderedDict:  0.01420650000000001
+'''
