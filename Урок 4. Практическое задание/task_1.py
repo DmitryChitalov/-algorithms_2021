2fb9594985c860c692c5983ccae34c2d80383155
Sergey <skireykov@mail.ru>
2021-06-09 14:40:05 +0300

    Homework4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,7 +12,7 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание не принимается
 """
-
+from timeit import timeit
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +20,28 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+# list comprehension
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+# filter(function, iterable)
+def func_3(nums):
+    new_arr = filter(lambda i: nums[i] % 2 == 0, range(len(nums)))
+    return new_arr
+
+
+n = [i for i in range(100000)]
+
+print(timeit("func_1(n)", globals=globals(), number=10000))
+print(timeit("func_2(n)", globals=globals(), number=10000))
+print(timeit("func_3(n)", globals=globals(), number=10000))
+
+
+'''
+98.1770606 классический for
+85.81944730000001 - list comprehension справляется лучше,чем просто цикл for,но проигрывает функции filter
+0.004345700000015995 - функция filter справляется в разы быстрее
+
+'''
