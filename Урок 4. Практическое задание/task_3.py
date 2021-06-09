eb665839a7237a3512139b386ab752c740efd1e8
Lebedev Sergey <fackaster@gmail.com>
2021-06-09 18:12:57 +0300

    Home Work 4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -15,6 +15,10 @@
 """
 
 
+from timeit import timeit
+import cProfile
+
+
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -37,3 +41,24 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    revers_num = "".join(reversed(str(enter_num)))
+    return revers_num
+
+
+print(timeit('revers_1(15763)', globals=globals()))
+print(timeit('revers_2(15763)', globals=globals()))
+print(timeit('revers_3(15763)', globals=globals()))
+print(timeit('revers_4(15763)', globals=globals()))
+cProfile.run('revers_1(1000000)')
+cProfile.run('revers_2(1000000)')
+cProfile.run('revers_3(1000000)')
+cProfile.run('revers_4(1000000)')
+
+"""
+Самый быстрый вариант - вариант со срезами (так как это встроенная функция и ей не приходится работать
+с перебором данных, работа с индексами происходит быстрее
+Далле по скорости идет reverset, потом цикл и рекурсия
+"""
