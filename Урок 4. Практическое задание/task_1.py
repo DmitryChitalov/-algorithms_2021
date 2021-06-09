9a8614bab8deff2853c6af8b8b635af45c367089
Lina <linavi96@yandex.ru>
2021-06-09 20:01:21 +0300

    Done all tasks

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,8 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание не принимается
 """
+from timeit import timeit
+from random import choice
 
 
 def func_1(nums):
@@ -20,3 +22,20 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [id_el for id_el, val in enumerate(nums) if val % 2 == 0]
+    return new_arr
+
+
+n = [choice([x for x in range(1000)]) for el in range(1000)]
+print(f'Время первоначальной функции: {timeit("func_1(n)", globals=globals(), number=100000)}')
+print(f'Время измененной функции: {timeit("func_2(n)", globals=globals(), number=100000)}')
+
+'''
+Измерения:
+Время первоначальной функции: 22.308479400000003
+Время измененной функции: 15.684319600000002
+Мы ушли от работы с перебором индексов списка и их значением к работе только с самими значениями.
+'''
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,8 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +39,42 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    reverse_num = list(str(enter_num))
+    reverse_num.reverse()
+    return ''.join(reverse_num)
+
+
+num = 121460
+print(revers_1(num))
+print(revers_2(num))
+print(revers_3(num))
+print(revers_4(num))
+print(f'{"-" * 47}timeit{"-" * 47}')
+print(f'Время работы revers_1: {timeit("revers_1(num)", globals=globals(), number=100000)}')
+print(f'Время работы revers_2: {timeit("revers_2(num)", globals=globals(), number=100000)}')
+print(f'Время работы revers_3: {timeit("revers_3(num)", globals=globals(), number=100000)}')
+print(f'Время работы revers_4: {timeit("revers_4(num)", globals=globals(), number=100000)}')
+print(f'{"-" * 46}сProfile{"-" * 46}')
+
+run('revers_1(987416597246957)')
+run('revers_2(987416597246957)')
+run('revers_3(987416597246957)')
+run('revers_4(987416597246957)')
+"""
+ИТОГ:
+Проведя расчет с помощью timeit получили:
+Время работы revers_1: 0.24445830000000002
+Время работы revers_2: 0.1801162
+Время работы revers_3: 0.05399660000000006
+Время работы revers_4: 0.07808389999999998
+
+С помощью cProfile получили практически идентичные таблицы с нулевыми значениями.
+
+Алгоритм, использующий срез, (revers_3) оказался самым быстрым.
+В отличии от других, этот алгоритм имеет константную сложность, ему не нужны дополнительные арифметические операции и
+он не проходит по циклу, выполняя операции раз за разом. Четвертая функция практически повторяет срез, но использует
+операции списка, что тормозит алгоритм.
+"""
