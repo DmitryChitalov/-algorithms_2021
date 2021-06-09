76e9451a8c62906536323236e1653a05ab096699
Sergey <skireykov@mail.ru>
2021-06-09 14:40:05 +0300

    Homework4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,7 +13,8 @@
 
 Без аналитики задание считается не принятым
 """
-
+from timeit import timeit
+from cProfile import run
 
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
@@ -37,3 +38,29 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    return ''.join(reversed(str(enter_num)))
+
+
+en_num = 97862143239478162341789623419856243158792341589234956174235897230147896708691243976584231
+print("TimeIt:")
+print(timeit("revers_1(en_num)", globals=globals(), number=1000000))
+print(timeit("revers_2(en_num)", globals=globals(), number=1000000))
+print(timeit("revers_3(en_num)", globals=globals(), number=1000000))
+print(timeit("revers_4(en_num)", globals=globals(), number=1000000))
+
+run("revers_1(en_num)")
+run("revers_2(en_num)")
+run("revers_3(en_num)")
+run("revers_4(en_num)")
+
+'''
+revers_1 Рекурсией неэффективна. Много строк.
+revers_2 Цикл while практически как и рекурсия.
+revers_3 самая быстрая за счет среза строки.
+revers_4 использование метода reversed. Хуже,чем revers_3,но сразу понятно в плане кода.
+
+Через cProfile выдают только нули. Все функции достоточно быстрые.
+'''
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,8 +11,9 @@
 
 Без аналитики задание считается не принятым!
 """
+from timeit import timeit
 
-array = [1, 3, 1, 3, 4, 5, 1]
+array = [1, 3, 1, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 5, 1, 3, 1, 2, 3, 1, 3, 1, 3, 3, 3, 3, 3, 4, 5, 1, 3, 1, 2, 3, ]
 
 
 def func_1():
@@ -39,5 +40,42 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    elements = {}
+    for el in array:
+        elements[el] = elements.get(el, 0) + 1
+    return f'Чаще всего встречается число {max(elements, key=elements.get)}, ' \
+           f'оно появилось в массиве {max(elements.values())} раз(а)'
+
+
+def func_4():
+    max_elem = max(set(array), key=array.count)
+    count = array.count(max_elem)
+    return f'Чаще всего встречается число {max_elem}, ' \
+           f'оно появилось в массиве {count} раз(а)'
+
+
+print(timeit("func_1()", globals=globals(), number=100000))
+print(timeit("func_2()", globals=globals(), number=100000))
+print(timeit("func_3()", globals=globals(), number=100000))
+print(timeit("func_4()", globals=globals(), number=100000))
+
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+
+
+'''
+3.676304
+3.7807133000000004
+0.6552629999999988
+3.2987439  и 0.5223064999999991(множество)
+
+Первые две функции работают медленне из-за цикла for
+func_3 использует словарь,метод .get сложность O(1)
+func_4 если не использовать список,а сделать множество,то функция справляется быстрее
+
+'''
