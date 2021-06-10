35c057265c0a97f9a8c7cd21631f125d3b546741
nikkav79 <nikolaykapustnikov@gmail.com>
2021-06-10 19:29:33 +0300

    Homework4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,8 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание не принимается
 """
+import timeit as tt
+
 
 
 def func_1(nums):
@@ -20,3 +22,21 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+num = [el for el in range(2000)]
+
+t1 = tt.timeit("func_1(num)", globals=globals(), number = 1000)
+print(f'Время замера составило: {t1}')
+
+def func_2(nums):
+    new_arr = filter(lambda i: nums[i] % 2 == 0, range(len(nums)))
+    return new_arr
+
+t2 = tt.timeit("func_2(num)", globals=globals(), number= 1000)
+print(f'Время замера составило: {t2}')
+
+
+#Замер 1 - 0.1199333
+#Замер 2 - 0.000422
+#Вывод: встроенная функция filter и лямбда-функция работает значительно быстрее чем решение с примененением цикла for
