c8081437dfc5f6cdb53d7a791162dad14d600a9f
Romnako <76477868+Romnako@users.noreply.github.com>
2021-06-10 22:28:04 +0300

    Update task_2.py

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -4,3 +4,36 @@
 для оптимизации памяти и
 доказать!!! (наглядно, кодом) их эффективность (на примере профилировщика).
 """
+import sys
+
+
+dictionary = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6 }
+
+class Point:
+    def __init__(self, a, b, c, d, e, f):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.e = e
+        self.f = f
+
+
+dictionary_class = Point(1, 2, 3, 4, 5, 6)
+
+print(sys.getsizeof(dictionary))
+print(sys.getsizeof(dictionary_class.__dict__))
+print(f'Словарь экземпляра класса занимает в {sys.getsizeof(dictionary) / sys.getsizeof(dictionary_class.__dict__)} '
+      f'раза меньше места в оперативной памяти, чем обычный словарь.')
+'''
+A class instance dictionary takes up less memory space than a regular dictionary.
+'''
+'''
+C:\Users\79115\AppData\Local\Programs\Python\Python39\python.exe "C:/Users/79115/Desktop/2 Урок Алгоритмы/Урок 2. Коды примеров/Algorithm6.2.py"
+360
+144
+Словарь экземпляра класса занимает в 2.5 раза меньше места в оперативной памяти, чем обычный словарь.
+
+Process finished with exit code 0
+
+'''
