216ee4b356503ddb8ca564d4405c7b53f880cb7d
12262004-m <77743232+12262004-m@users.noreply.github.com>
2021-06-10 22:50:27 +0300

    Add files via upload

--- les8task1.py
+++ les8task1.py
@@ -1,46 +1,74 @@
-class BinaryTree:
-    def __init__(self, root_obj):
-        self.root = root_obj
-        self.left_root_obj = None
-        self.right_root_obj = None
+from collections import Counter
 
-    def left_root_obj_adding(self, node):
-        if self.left_root_obj is None:
-            self.left_root_obj = BinaryTree(node)
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.right = right
+        self.left = left
+        self.value = value
+
+
+def get_code(new_root, codes=None, code=''):
+    if codes is None:
+        codes = dict()
+    if new_root is None:
+        return
+    if isinstance(new_root.value, str):
+        codes[new_root.value] = code
+        return codes
+    get_code(new_root.left, codes, code + '0')
+    get_code(new_root.right, codes, code + '1')
+    return codes
+
+
+def get_tree(str1):
+    count = Counter(str1)
+    if len(count) <= 1:
+        node = Node(None)
+        if len(count) == 1:
+            node.left = Node([i for i in count][0])
+            node.right = Node(None)
+        count = {node: 1}
+    while len(count) != 1:
+        node = Node(None)
+        junk = count.most_common()[:-3:-1]
+        if isinstance(junk[0][0], str):
+            node.left = Node(junk[0][0])
         else:
-            binaryTree_obj = BinaryTree(node)
-            binaryTree_obj.left_root_obj = self.left_root_obj
-            self.left_root_obj = binaryTree_obj
-
-    def right_root_obj_adding(self, node):
-        if self.right_root_obj is None:
-            self.right_root_obj = BinaryTree(node)
+            node.left = junk[0][0]
+        if isinstance(junk[1][0], str):
+            node.right = Node(junk[1][0])
         else:
-            binaryTree_obj = BinaryTree(node)
-            binaryTree_obj.right_root_obj = self.right_root_obj
-            self.right_root_obj = binaryTree_obj
-
-    def get_right_root_obj(self):
-        return self.right_root_obj
-
-    def get_left_root_obj(self):
-        return self.left_root_obj
-
-    def set_root_value(self, obj):
-        self.root = obj
-
-    def get_root_value(self):
-        return self.root
+            node.right = junk[1][0]
+        del count[junk[0][0]]
+        del count[junk[1][0]]
+        count[node] = junk[0][1] + junk[1][1]
+    return [i for i in count][0]
 
 
-r = BinaryTree(8)
-print(r.get_root_value())
-print(r.get_left_root_obj())
-r.left_root_obj_adding(25)
-print(r.get_right_root_obj())
-print(r.get_left_root_obj().get_root_value())
-r.right_root_obj_adding(12)
-print(r.get_right_root_obj())
-print(r.get_right_root_obj().get_root_value())
-r.get_right_root_obj().set_root_value(16)
-print(r.get_right_root_obj().get_root_value())
+def coding_process(str, codes):
+    result = ''
+    for symbol in str:
+        result += codes[symbol]
+    return result
+
+
+def decoding_process(str, codes):
+    result = ''
+    i = 0
+    while i < len(str):
+        for code in codes:
+            if str[i:].find(codes[code]) == 0:
+                result += code
+                i += len(codes[code])
+    return result
+
+
+new_str = input("Введите строчку для кодирования: ")
+new_tree = get_tree(new_str)
+new_codes = get_code(new_tree)
+coding_new_str = coding_process(new_str, new_codes)
+decoding_new_str = decoding_process(coding_new_str, new_codes)
+print(f'Коды символов: {new_codes}')
+print('Закодированная строка: ', coding_new_str)
+print('Декодированная строка: ', decoding_new_str)
+
