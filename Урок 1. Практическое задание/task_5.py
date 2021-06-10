8d21397f20d4bb4aa511aa5b060e2ef9dce5a81b
RuslanT <12herekeith@gmail.com>
2021-06-10 14:34:15 +0300

    Переделано

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -20,38 +20,63 @@
 Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
-STACK_LEN = 5
+class StackClassBy:
+    def __init__(self, stack_by_el):
+        self.elems = [[]]
+        self.stack_by_el = stack_by_el  # Количество элементов в стеке
+
+    def is_empty(self):
+        return self.elems == [[]]
+
+    def push_in(self, el):
+        if len(self.elems[-1]) == self.stack_by_el:
+            self.elems.append([])
+        self.elems[-1].append(el)
+
+    def pop_out(self):
+        if len(self.elems[-1]) == 1:
+            last_elem_last_lst = self.elems.pop()
+            return last_elem_last_lst[-1]
+        return self.elems[-1].pop()
+
+    def get_val(self):
+        return self.elems[-1][-1]
+
+    def stack_size(self):
+        sum_el = 0
+        for stack in self.elems:
+            sum_el += len(stack)
+        return f"Sum stack: {len(self.elems)}\n" \
+               f"Sum elements: {sum_el}"
+
+    def stack_view(self):
+        return f"All stack:\n" \
+               f"{self.elems}"
 
 
-class StackOfPlates:
-    def __init__(self):
-        self.stack = {}
-        self.stack_key = 0
+if __name__ == '__main__':
+    SC_OBJ = StackClassBy(3)  # По 3 элемента в стеке
 
-    def append(self, value):
-        if len(self.stack) == 0:
-            self.stack_key = 0
-            self.stack[self.stack_key] = [value]
-        else:
-            use_stack = self.stack[self.stack_key]
-            if len(use_stack) < STACK_LEN:
-                use_stack.append(value)
-            else:
-                self.stack_key = len(self.stack)
-                self.stack[len(self.stack)] = [value]
+    print(SC_OBJ.is_empty())  # True
 
-    def pop(self):
-        if len(self.stack) == 0:
-            print('Stack of plates is empty')
-            return
-        use_stack = self.stack[self.stack_key]
-        if len(use_stack) != 0:
-            return use_stack.pop()
-        else:
-            del self.stack[self.stack_key]
-            self.stack_key = len(self.stack) - 1
-            if len(self.stack) == 0:
-                print('Stack of plates empty')
-                return
-            use_stack = self.stack[self.stack_key]
-            return use_stack.pop()
+    # наполняем стек
+    for i in range(10):
+        SC_OBJ.push_in(f"{i + 1} plate")
+
+    print(SC_OBJ.stack_view())
+
+    print("Pop_out:", SC_OBJ.pop_out())  # 10 plate
+
+    print(SC_OBJ.get_val())  # 9 plate
+
+    print(SC_OBJ.stack_view())
+
+    print("Add '10 plate'")
+
+    SC_OBJ.push_in("10 plate")
+
+    print(SC_OBJ.stack_view())
+
+    print(SC_OBJ.stack_size())
+
+    print(SC_OBJ.is_empty())  # False
