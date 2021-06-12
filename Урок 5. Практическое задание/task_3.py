6a99cba3f5663b930f077e4056f2485ed06a49a8
Lina <linavi96@yandex.ru>
2021-06-12 16:47:58 +0300

    Done all tasks

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -5,9 +5,79 @@ deque – это обобщение стеков и очередей.
 Вот основное правило: если вам нужно что-то быстро дописать или вытащить, используйте deque.
 Если вам нужен быстрый случайный доступ, используйте list.
 
-Задача: 
+Задача:
 1) создайте простой список (list) и очередь (deque). Сделайте замеры и оцените что заполняется быстрее.
 2) Выполните различные операции с каждым из объектов. Сделайте замеры и оцените, где и какие операции быстрее.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+from collections import deque
+from timeit import timeit
+from random import randint
+
+list_obj = []
+deque_obj = deque()
+
+
+def full_list(list_ob):
+    for i in range(1000):
+        list_ob.append(i)
+
+
+def full_dec(dec_ob):
+    for i in range(1000):
+        dec_ob.append(i)
+
+
+def left_dec(dec_ob):
+    for i in range(1000):
+        dec_ob.appendleft(i * 2)
+
+
+def left_list(list_ob):
+    for i in range(1000):
+        list_ob.insert(0, i * 2)
+
+
+def rand_list(list_ob, n=1000):
+    for _ in range(n):
+        num = randint(0, 1999)
+        list_ob[num] = list_ob[num] * 3 + 25
+
+
+def rand_dec(dec_ob, n=1000):
+    for _ in range(n):
+        num = randint(0, 1999)
+        dec_ob[num] = dec_ob[num] * 3 + 25
+
+
+full_list(list_obj)
+full_dec(deque_obj)
+left_list(list_obj)
+left_dec(deque_obj)
+rand_list(list_obj)
+rand_dec(deque_obj)
+print(f'{list_obj}\n{deque_obj}')
+
+print(f'Время заполнения списка: {timeit("full_list(list_obj)", globals=globals(), number=1000)}')
+print(f'Время заполнения дэка: {timeit("full_dec(deque_obj)", globals=globals(), number=1000)}')
+print(f'Время заполнения в начало списка: {timeit("left_list(list_obj)", globals=globals(), number=100)}')
+print(f'Время заполнения в начало дэка: {timeit("left_dec(deque_obj)", globals=globals(), number=100)}')
+print(f'Время изменения элементов списка: {timeit("rand_list(list_obj)", globals=globals(), number=1000)}')
+print(f'Время изменения элементов дэка: {timeit("rand_dec(deque_obj)", globals=globals(), number=1000)}')
+
+'''
+Время заполнения списка: 0.1103283
+Время заполнения дэка: 0.08749869999999998
+Время заполнения в начало списка: 91.3605223
+Время заполнения в начало дэка: 0.01157380000000785
+Время изменения элементов списка: 1.4507932000000068
+Время изменения элементов дэка: 1.5255997000000008
+
+Исходя из измерений можно утверждать, что когда необходимо заполнять список, добавляя новый элемнет в начало списка,
+а не в конец, то Deque работает в разы быстрее чем обычный список. (Для того чтобы провести измерения для операций
+вставки в начало кол-во запусков функции было сокращено с 1000 до 100)
+
+В остальных случаях список и дэк работают практически одинаково. При заполнении значений дэк работает быстрее. 
+При получении случайного элемента быстрее работает список.
+'''
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,72 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from collections import OrderedDict
+from random import randint
+from timeit import timeit, default_timer
+
+
+def time_dict(func):
+    def wrapper(*args):
+        t1 = default_timer()
+        res = func(*args)
+        print(f'Время выполенения {func.__name__}: {default_timer() - t1}. ')
+        return res
+    return wrapper
+
+
+def full_dict(j_dict):
+    for el in range(10000):
+        j_dict[el] = el * 3
+
+
+def change_el_dict(j_dict):
+    for _ in range(2000):
+        j_dict[randint(0, 9999)] /= 3
+
+
+def items_dict(j_dict):
+    for key_dict, item_dict in j_dict.items():
+        if item_dict % 2 == 0:
+            j_dict[key_dict] += 1
+
+@time_dict
+def pop_el_dict(j_dict):
+    for num in range(1000):
+        j_dict.pop(num)
+
+
+just_dict = {}
+order_dict = OrderedDict()
+full_dict(just_dict)
+full_dict(order_dict)
+change_el_dict(just_dict)
+change_el_dict(order_dict)
+items_dict(just_dict)
+items_dict(order_dict)
+
+print(f'Время заполнения простого словаря: {timeit("full_dict(just_dict)", globals=globals(), number=1000)}')
+print(f'Время заполнения упорядоченного словаря: {timeit("full_dict(order_dict)", globals=globals(), number=1000)}')
+print(f'Время изменения простого словаря: {timeit("change_el_dict(just_dict)", globals=globals(), number=1000)}')
+print(f'Время изменения упорядоченного словаря: {timeit("change_el_dict(order_dict)", globals=globals(), number=1000)}')
+print(f'Время изменения (еще раз) простого словаря: {timeit("items_dict(just_dict)", globals=globals(), number=1000)}')
+print(f'Время изменения (еще раз) упорядоченного словаря: '
+      f'{timeit("items_dict(order_dict)", globals=globals(), number=1000)}')
+pop_el_dict(just_dict)
+pop_el_dict(order_dict)
+
+'''
+Время заполнения простого словаря: 1.3896083
+Время заполнения упорядоченного словаря: 1.6636456
+Время изменения простого словаря: 3.0398193
+Время изменения упорядоченного словаря: 3.0861357
+Время изменения (еще раз) простого словаря: 1.3198959000000006
+Время изменения (еще раз) упорядоченного словаря: 1.6988912000000003
+Время выполенения pop_el_dict: 0.00018949999999939848. 
+Время выполенения pop_el_dict: 0.0002870999999995405.
+(Для удаления элемента был использован декоратор)
+
+Обычный словарь заполняется быстрее заполняется, быстрее получает нужный элемент и быстрее удаляет элемент,
+чем OrderedDict.
+Так что смысла нет использовать OrderDict вместо обычного словаря. Тем более, что в Python 3.9 обычный словарь тоже
+фиксирует порядок добавления элементов.'''
