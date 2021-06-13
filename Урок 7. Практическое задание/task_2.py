1ebe7e82b789a3305311e0af422aaac92a793187
MMatyukha <airmara@yandex.ru>
2021-06-13 23:19:06 +0300

    домашнее  задание   за  7   урок

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,47 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+
+
+def merge_sort(arr):
+
+    def merge(fst, snd):
+        res = []
+        i, j = 0, 0
+
+        while i < len(fst) and j < len(snd):
+
+            if fst[i] < snd[j]:
+                res.append(fst[i])
+                i += 1
+
+            else:
+                res.append(snd[j])
+                j += 1
+
+        res.extend(fst[i:] if i < len(fst) else snd[j:])
+
+        return res
+
+    def div_half(lst):
+
+        if len(lst) == 1:
+            return lst
+
+        elif len(lst) == 2:
+            return lst if lst[0] <= lst[1] else lst[::-1]
+
+        else:
+            return merge(div_half(lst[:len(lst)//2]), div_half(lst[len(lst)//2:]))
+
+    return div_half(arr)
+
+
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 50
+array = [random.uniform(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+
+print('Массив:', array, sep='\n')
+print('После сортировки:', merge_sort(array), sep='\n')
