cedd196572e441e4ce5d3c6ad1c77f0e510354f2
MMatyukha <airmara@yandex.ru>
2021-06-13 23:05:40 +0300

    домашнее  задание   за  5   урок

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,40 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+from collections import namedtuple, deque
+
+QUARTERS = 4
+Company = namedtuple('Company', ['name', 'quarters', 'profit'])
+all_companies = set()
+
+num = int(input("Введите количество предприятий для расчета прибыли: "))
+total_profit = 0
+for i in range(1, num + 1):
+    profit = 0
+    quarters = []
+    name = input(f'Введите название предприятия {i}: ')
+
+    for j in range(QUARTERS):
+        quarters.append(int(input(f'Прибыль за {j + 1}-й квартал: ')))
+        profit += quarters[j]
+
+    comp = Company(name=name, quarters=tuple(quarters), profit=profit)
+
+    all_companies.add(comp)
+    total_profit += profit
+
+average = total_profit / num
+
+print(f'\nСредняя прибыль годовая прибыль всех предприятий= {average}')
+print('DEBUG ', comp)
+
+print(f'\nПредприятя с прибылью выше среднего:')
+for comp in all_companies:
+    if comp.profit > average:
+        print(f'Компания {comp.name} заработала {comp.profit}')
+        # print(comp.quarters[0])   # так можно получить доступ к нужной четверти.
+
+print(f'\nПредприятя с прибылью ниже среднего:')
+for comp in all_companies:
+    if comp.profit < average:
+        print(f'Компания {comp.name} заработала {comp.profit}')
