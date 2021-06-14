e89e295a2830a8da7928540d1085c2a8f0530a4c
Romnako <76477868+Romnako@users.noreply.github.com>
2021-06-14 18:33:13 +0300

    Update task_3.py

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,116 @@ for i in
 
 
 """
+
+
+import random
+from statistics import median
+
+m = 5
+orig_list = [random.randint(0, 100) for _ in range(2 * m + 1)]
+
+'''
+Without sorting algorithm.
+Алгоритм без сортировки.
+'''
+def my_median(lst_obj):
+    middle = len(lst_obj) // 2
+    while True:
+        maximum = lst_obj[0]
+        for el in lst_obj:  # O(n)
+            if el > maximum:  # O(1)
+                maximum = el  # O(1)
+        lst_obj.remove(maximum)  # O(1)
+        if middle == 0:
+            return maximum
+        middle -= 1
+
+
+'''
+Shell algorithm.
+Алгоритм Шелла.
+'''
+
+def shell(lst_obj):
+    inc = len(lst_obj) // 2
+    middle = len(lst_obj) // 2
+    while inc:
+        for i, el in enumerate(lst_obj):
+            while i >= inc and lst_obj[i - inc] > el:
+                lst_obj[i] = lst_obj[i - inc]
+                i -= inc
+            lst_obj[i] = el
+        inc = 1 if inc == 2 else int(inc * 5.0 / 11)
+    return lst_obj[middle]
+
+
+'''
+Gnome algorithm.
+Алгоритм Гномья.
+'''
+def gnome(lst_obj):
+    i, size = 1, len(lst_obj)
+    middle = len(lst_obj) // 2
+    while i < size:
+        if lst_obj[i - 1] <= lst_obj[i]:
+            i += 1
+        else:
+            lst_obj[i - 1], lst_obj[i] = lst_obj[i], lst_obj[i - 1]
+            if i > 1:
+                i -= 1
+    return lst_obj[middle]
+
+'''
+Heap sort algorithm.
+Сортировка кучей.
+
+'''
+
+def heapsort(lst_obj):
+    middle = len(lst_obj) // 2
+    for start in range((len(lst_obj) - 2) // 2, -1, -1):
+        siftdown(lst_obj, start, len(lst_obj) - 1)
+    for end in range(len(lst_obj) - 1, 0, -1):
+        lst_obj[end], lst_obj[0] = lst_obj[0], lst_obj[end]
+        siftdown(lst_obj, 0, end - 1)
+    return lst_obj[middle]
+
+
+
+def siftdown(lst_obj, start, end):
+    root = start
+    while True:
+        child = root * 2 + 1
+        if child > end:
+            break
+        if child + 1 <= end and lst_obj[child] < lst_obj[child + 1]:
+            child += 1
+        if lst_obj[root] < lst_obj[child]:
+            lst_obj[root], lst_obj[child] = lst_obj[child], lst_obj[root]
+            root = child
+        else:
+            break
+
+
+print(f'Median :\n{median(orig_list[:])}')
+print(f'Median without sorting algorithm:\n{my_median(orig_list[:])}')
+print(f'Median Shell algorithm:\n{shell(orig_list[:])}')
+print(f'Median Gnome algorithm:\n{gnome(orig_list[:])}')
+print(f'Median Heapsort algorithm:\n{heapsort(orig_list[:])}')
+
+'''
+C:\Users\79115\AppData\Local\Programs\Python\Python39\python.exe "C:/Users/79115/Desktop/2 Урок Алгоритмы/Урок 2. Коды примеров/Algorithm7.3.py"
+Median :
+35
+Median without sorting algorithm:
+35
+Median Shell algorithm:
+35
+Median Gnome algorithm:
+35
+Median Heapsort algorithm:
+35
+
+Process finished with exit code 0
+
+'''
