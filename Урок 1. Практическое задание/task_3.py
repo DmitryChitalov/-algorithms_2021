889ca7660534da15bce9edcc543022428ad7b441
konovalovdv <89204140266kon@gmail.com>
2021-06-14 19:57:56 +0300

    On branch lesson_1 Changes to be committed: 	new file:   .idea/-algorithms_2021.iml 	new file:   .idea/.gitignore 	new file:   .idea/aws.xml 	new file:   .idea/inspectionProfiles/Project_Default.xml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	modified:   "task_1.py" 	modified:   "task_2.py" 	modified:   "task_3.py" 	modified:   "task_4.py" 	modified:   "task_5.py" 	modified:   "task_6.py" 	modified:   "task_7.py"

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -20,3 +20,50 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+def best_company_1(my_dict):                                    # 9 + (n log n) + 3 + 1 = O(n log n)
+    list_from_company = list(my_dict.items())                   # O(len(dict)) = 9
+    list_from_company.sort(key=lambda x: x[1], reverse=True)    # O(n log n)
+    for i in range(3):                                          # O(3)
+        print(f'{i + 1} company is "{list_from_company[i][0]}" with profit = {list_from_company[i][1]}')  # O(1)
+
+
+def best_company_2(my_dict):                   # 9 + 3 + 1 + n^2 = O(n^2)
+    list_from_company = list(my_dict.items())  # O(len(dict)) = 9
+    for i in range(3):                         # O(3)
+        print(f'{i + 1} company is '           # O(1 + n * n)
+              f'{list_from_company.pop(list_from_company.index(max(list_from_company, key=lambda val: val[1])))}')
+
+
+def best_company_3(my_dict):                     # 9 + 1 + 3 + 1 + n^2 + 1 = O(n^2) просто с более красивым выводом
+    list_from_company = list(my_dict.items())    # O(len(dict)) = 9
+    list_of_top_companies = []                   # O(1)
+    for i in range(3):                           # O(3)
+        list_of_top_companies.append\
+                (list_from_company.pop(
+                    list_from_company.index(max(list_from_company, key=lambda val: val[1]))))     # O(1 + n * n)
+        print(f'{i + 1} company is "{list_of_top_companies[i][0]}" with profit = {list_of_top_companies[i][1]}')  # O(1)
+
+
+companies = {
+    'alpha ': 3000,
+    'bravo': 300,
+    'charlie': 6000,
+    'delta': 2550,
+    'echo': 4385,
+    'foxtrot': 99,
+    'golf': 75,
+    'hotel': 5937,
+    'india': 6418
+}
+
+best_company_1(companies)
+best_company_2(companies)
+best_company_3(companies)
+
+"""
+Наиболее эффективным решением задачи является функция best_company_1, т.к.асимптотическая сложность 
+для данного решения равняется O(n log n). Решения 2 и 3 использовать не рекомендуется 
+из-за их высокой асимпотической сложности.
+"""
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,3 +18,77 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+class QueueClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def to_queue(self, item):
+        self.elems.insert(0, item)
+
+    def from_queue(self):
+        return self.elems.pop()
+
+    def size(self):
+        return len(self.elems)
+
+
+class TaskBoard:
+    def __init__(self):
+        self.current_queue = QueueClass()   # Основная очередь
+        self.revision_queue = QueueClass()  # Очередь на доработку
+        self.resolved = []
+
+    def to_current_queue(self, task):
+        self.current_queue.to_queue(task)
+
+    def to_resolved(self):
+        self.resolved.append(self.current_queue.from_queue())
+
+    def to_revision_queue(self):
+        self.revision_queue.to_queue(self.current_queue.from_queue())
+
+    def from_revision_to_current_queue(self):
+        self.current_queue.to_queue(self.revision_queue.from_queue())
+
+    def from_revision_to_resolved(self):
+        self.resolved.append(self.revision_queue.from_queue())
+
+    # Вспомнил, что индексация возможна и в обратном порядке, поэтому вставил вместо [len(self.current_queue.elems) - 1]
+    # просто [-1]. В пятой задаче менять не стал. На сколько это корректно?
+    def task_count(self):
+        return f'Текущая задача - {self.current_queue.elems[-1]}\n' \
+               f'Количество текущих задач: {len(self.current_queue.elems)}\n' \
+               f'Текущая задача на доработке - {self.revision_queue.elems[-1]}\n' \
+               f'Количество задач на доработке: {len(self.revision_queue.elems)}\n' \
+               f'Количество решенных задач {len(self.resolved)}'
+
+
+
+
+
+
+
+
+task_board = TaskBoard()
+task_board.to_current_queue(1)
+task_board.to_current_queue(2)
+task_board.to_current_queue(3)
+task_board.to_current_queue(4)
+task_board.to_current_queue(5)
+task_board.to_current_queue(6)
+print(task_board.current_queue.elems)
+task_board.to_resolved()
+task_board.to_revision_queue()
+task_board.to_revision_queue()
+task_board.to_revision_queue()
+print(task_board.revision_queue.elems)
+task_board.from_revision_to_current_queue()
+task_board.from_revision_to_resolved()
+print(task_board.current_queue.elems)
+print(task_board.revision_queue.elems)
+print(task_board.resolved)
+print(task_board.task_count())
