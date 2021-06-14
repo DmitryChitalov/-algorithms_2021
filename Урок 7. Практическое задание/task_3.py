5f818c74100b1cbca74bc6a667e610e27b591183
Zaynokov <vlerii99@mail.ru>
2021-06-14 17:30:10 +0300

    Complete

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,44 @@ for i in
 
 
 """
+from random import randint
+from statistics import median
+
+m = int(input("Введите натуральное число m "))
+random_list = [randint(0, 10) for _ in range(2 * m + 1)]
+print(f"Исходный массив: {random_list}")
+print(f"Отсортированный массив: {sorted(random_list[:])}")
+
+
+def list_median(lst):
+    while len(lst) > m + 1:
+        lst.remove(max(lst))
+    return max(lst)
+
+
+def gnome_median(lst):
+    i, j, size = 1, 2, len(lst)
+    while i < size:
+        if lst[i - 1] <= lst[i]:
+            i, j = j, j + 1
+        else:
+            lst[i - 1], lst[i] = lst[i], lst[i - 1]
+            i -= 1
+            if i == 0:
+                i, j = j, j + 1
+    return lst[m]
+
+
+print(f"Медиана без сортировки: {list_median(random_list[:])}")
+print(f"Медиана встроенной функцией: {median(random_list[:])}")
+print(f"Медиана с гномьей сортировкой {gnome_median(random_list[:])}")
+
+
+"""
+Введите натуральное число m 5
+Исходный массив: [7, 0, 10, 5, 4, 7, 9, 3, 10, 4, 5]
+Отсортированный массив: [0, 3, 4, 4, 5, 5, 7, 7, 9, 10, 10]
+Медиана без сортировки: 5
+Медиана встроенной функцией: 5
+Медиана с гномьей сортировкой 5
+"""
