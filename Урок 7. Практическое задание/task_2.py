65686e25d5490677fcbf0f20ae0e1bcf7f8c1d2d
roman <afonasev@gmail.com>
2021-06-14 19:57:57 +0300

    Lesson_7 Hometask

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,66 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+from random import uniform
+from timeit import timeit
+
+my_list10 = [uniform(0.0, 50.0) for num0 in range(10)]
+my_list100 = [uniform(0.0, 50.0) for num1 in range(100)]
+my_list1000 = [uniform(0.0, 50.0) for num2 in range(1000)]
+print(f'Несортированный массив: \n {my_list10}')
+
+
+def merge_lists(lst1, lst2):
+    i = j = 0
+    merged_lst = []
+    while i < len(lst1) and j < len(lst2):
+        if lst1[i] < lst2[j]:
+            merged_lst.append(lst1[i])
+            i += 1
+        else:
+            merged_lst.append(lst2[j])
+            j += 1
+    merged_lst += lst1[i:] + lst2[j:]
+    return merged_lst
+
+
+def input_data(any_list):
+    if len(any_list) == 1:
+        return any_list
+    else:
+        lst1 = input_data(any_list[:len(any_list)//2])
+        lst2 = input_data(any_list[len(any_list)//2:])
+        return merge_lists(lst1, lst2)
+
+
+def bubble_sort(any_list):
+    n = len(any_list)
+    for x in range(n):
+        for y in range(len(any_list[x:]) - 1):
+            if any_list[y + 1] > any_list[y]:
+                any_list[y + 1], any_list[y] = any_list[y], any_list[y + 1]
+    return any_list
+
+
+print(f'Проверка сортировки: \n {input_data(my_list10)}')
+
+print('Merge 10 элементов в списке', timeit('input_data(my_list10[:])', globals=globals(),
+                                                           number=10000))
+print('Merge 100 элементов в списке', timeit('input_data(my_list100[:])', globals=globals(),
+                                                            number=10000))
+print('Bubble 100 элементов в списке', timeit('bubble_sort(my_list100[:])', globals=globals(),
+                                                             number=10000))
+print('Merge 1000 элементов в списке', timeit('input_data(my_list1000[:])', globals=globals(),
+                                                             number=10000))
+
+
+"""
+Merge 10 элементов в списке 0.10386949999999999
+Merge 100 элементов в списке 2.0953494
+Bubble 100 элементов в списке 5.454131
+Merge 1000 элементов в списке 27.315076100000002
+
+Пузырьковая сортировка со срезом, которая показала себя лучшей в предыдущем задании, в данном задании проиграла с 
+огромным отрывом методу слияния.
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,63 @@ for i in
 
 
 """
+
+from random import randint
+from statistics import median
+from timeit import timeit
+
+my_list = [randint(-100, 100) for num in range(99)]
+
+
+def sort_by_shell(any_list):
+    center = len(any_list) // 2
+    while center > 0:
+        for i in range(len(any_list) - center):
+            j = i
+            while j >= 0 and any_list[j] > any_list[j + center]:
+                any_list[j], any_list[j + center] = any_list[j + center], any_list[j]
+                j -= 1
+        center //= 2
+
+    return any_list
+
+
+def find_median(any_lst):
+    quot, rem = divmod(len(any_lst), 2)
+    if rem:
+        return any_lst[quot]
+    return sum(any_lst[quot - 1:quot + 1]) / 2
+
+
+def median_unsorted(any_list):
+    center = len(any_list) // 2
+    while True:
+        maximum = any_list[0]
+        for num in any_list:
+            if num > maximum:
+                maximum = num
+        any_list.remove(maximum)
+        if center == 0:
+            return maximum
+        center -= 1
+
+
+print(f'shell_sort:\n{sort_by_shell(my_list[:])}')
+print(f'Медиана для списка выше: {find_median(sort_by_shell(my_list[:]))}')
+print(f'Проверка через median: {median(my_list[:])}')
+print(f'Медиана без сортировки: {median_unsorted(my_list[:])}')
+
+print('Время выполнения find_median(shell_sort): ', timeit('find_median(sort_by_shell(my_list[:]))', globals=globals(),
+                                                           number=10000))
+print('Время выполнения statistics_median: ', timeit('median(my_list[:])', globals=globals(),
+                                                     number=10000))
+print('Время выполнения median_unsorted: ', timeit('median_unsorted(my_list[:])', globals=globals(),
+                                                   number=10000))
+"""
+Время выполнения find_median(shell_sort):  1.3833398
+Время выполнения statistics_median:  0.024225299999999894
+Время выполнения median_unsorted:  0.8888303000000002
+
+Вариант с сортировкой Шелла самый медленный, поиск медианы без сортировки на втором месте т.к. "сортирует" только 
+половину списка, встроенная функция вне конкуренции.
+"""
