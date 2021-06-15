395fe8d065ee83e4afe76d617ed2411ffb5b02bb
Andrey Belyaev <ianbel263@gmail.com>
2021-06-15 21:22:48 +0300

    add binary_search

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -148,10 +148,56 @@ def check_user(user_data):
 
 
 # ИТОГО: O(N)
-# Возможно более предпочтительный способ
+# 
 
 check_user({'name': 'Simon', 'pwd': 'asdaafdghetyjsd'})
 check_user({'name': 'Fred', 'pwd': 'Asfafddsf'})
 check_user({'name': 'Tom', 'pwd': 'gfdg5'})
 check_user({'name': 'Elena', 'pwd': 'asdu76u46uasd'})
 check_user({'name': 'Donald', 'pwd': 'asdaasfassd'})
+
+print('-----------------------------------------------------------')
+
+
+# ------------------- Четвертый способ ------------------------------
+
+
+def binary_search(lst, user_name):
+    start = 0  # O(1)
+    end = len(lst) - 1  # O(1) + O(1)
+
+    while start <= end:  # O(1)
+        mid = int((start + end) / 2)  # O(1) + O(1) + O(1)
+        if lst[mid]['name'] == user_name:  # O(1) + O(1)
+            return mid  # O(1)
+        elif lst[mid]['name'] < user_name:  # O(1) + O(1)
+            start = mid + 1  # O(1) + O(1)
+        else:
+            end = mid - 1  # O(1) + O(1)
+    return None  # O(1)
+
+
+def check_user_data_2(users_list, user_data):
+    new_lst = sorted(users_list, key=lambda x: x['name'])  # O(1) + O(N log N)
+    user = binary_search(new_lst, user_data['name'])  # O(1) + O(1)
+    if not user:  # O(1)
+        print(f'Пользователь с логином {user_data["name"]} не найден')  # O(1)
+        return  # O(1)
+    elif new_lst[user]['pwd'] != user_data['pwd']:  # O(1) + O(1) + O(1)
+        print(f'{user_data["name"]}, Вы неверно ввели пароль')  # O(1)
+        return  # O(1)
+    elif not new_lst[user]['is_activated']:  # O(1) + O(1)
+        print(f'{user_data["name"]}, Вам необходимо активировать учетную запись')  # O(1)
+        return  # O(1)
+    else:
+        print(f'{user_data["name"]}, добро пожаловать!')  # O(1)
+
+
+# ИТОГО: O(N log N)
+# Самый предпочтительный способ
+
+check_user_data_2(users, {'name': 'Simon', 'pwd': 'asdaafdghetyjsd'})
+check_user_data_2(users, {'name': 'Fred', 'pwd': 'Asfafddsf'})
+check_user_data_2(users, {'name': 'Tom', 'pwd': 'gfdg5'})
+check_user_data_2(users, {'name': 'Elena', 'pwd': 'asdu76u46uasd'})
+check_user_data_2(users, {'name': 'Donald', 'pwd': 'asdaasfassd'})
