9a2f9612f64e8b1d3dd9e5bcb2dc06c78f1d41e0
Sebkd <sebkd@mail.ru>
2021-06-15 20:54:54 +0700

    rebuilded task7 after viewed

--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -16,6 +16,27 @@
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
 import random
+'''
+Возможные варианты решения
+def recur_method(numb):
+    if numb == 1:
+        return numb
+    else:
+        return recur_method(numb-1) + numb
+
+try:
+    NUMB = int(input('Введите число: '))
+    if recur_method(NUMB) == NUMB * (NUMB+1) / 2:
+        print(f'Равенство равно')
+except ValueError:
+    print(f'Вы вместо числа ввели строчку')
+
+
+и еще одно решение
+
+def recur_method (numb):
+    return numb if numb == 1 else numb + recur_method(numb-1)
+'''
 
 
 def func_recursion_seven(number, verification_number, the_sum=0, counter=1):
