c6cc5eedc3b621fe89fc13e889f7dfdf10383e2a
Sebkd <sebkd@mail.ru>
2021-06-15 19:52:41 +0700

    rebuilded task2 after viewed

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -18,7 +18,22 @@
 Введите число: 123
 Количество четных и нечетных цифр в числе равно: (1, 2)
 """
+# можно сделать следующим образом
+'''
+def func_recursion_two(number, even_numbers=0, odd_numbers=0):
+    if number == 0:
+        return even_numbers, odd_numbers
+    else:
+        cur_n = number % 10
+        number = number // 10
+        if cur_n % 2 == 0:
+            even_numbers += 1
+        else:
+            odd_numbers += 1
+        return func_recursion_two(number, even_numbers, odd_numbers)
 
+необходимо сделать проверку на try except ValueError
+'''
 
 def func_recursion_two(number):
     global even_numbers, odd_numbers
