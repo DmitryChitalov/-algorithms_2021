025de9c935bea0403047e310d8f252f390893139
Sebkd <sebkd@mail.ru>
2021-06-15 20:30:25 +0700

    rebuilded task5 after

--- Урок 2. Практическое задание/task_5.py
+++ Урок 2. Практическое задание/task_5.py
@@ -23,6 +23,17 @@
 и допускается комб-е - цикл и рекурсия
 """
 
+'''
+def chr_row(ascii_val=32):
+    if ascii_val == 128:
+        return True
+    print(f' {ascii_val} - {chr(ascii_val)}', end=' ')
+    if (ascii_val - 31) % 10 == 0:
+        print('\n')
+    chr_row(ascii_val + 1)
+
+chr_row()
+'''
 
 def func_recursion_five(start, stop):
     global my_list
