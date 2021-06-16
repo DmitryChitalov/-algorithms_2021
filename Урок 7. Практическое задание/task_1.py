d1869826a6cf2d4365c787b8db53abe0b2b9ad2e
Danilov.S <dobrblu74@mail.ru>
2021-06-16 10:12:40 +0300

    lesson_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,58 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+from random import randint
+from timeit import timeit
+
+
+# Сортировка по убыванию
+def bubble_sort_rev1(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i + 1] > lst_obj[i]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+# Доработанная сортировка
+def bubble_sort_rev2(lst_obj):
+    n = 1
+    lst_end = 0
+    while n < len(lst_obj):
+        break_counter = True
+        for i in range(len(lst_obj) - 1, lst_end, -1):
+            if lst_obj[i - 1] > lst_obj[i]:
+                lst_obj[i], lst_obj[i - 1] = lst_obj[i - 1], lst_obj[i]
+                break_counter = False
+        if break_counter is True:
+            return lst_obj
+        lst_end += 1
+        n += 1
+    return lst_obj
+
+
+# На каждом проходе цикла исключается отсортированная часть.
+# Цикл прерывается если за проход по списку не совершилась не одна перестановка.
+
+list_10 = [randint(-100, 100) for _ in range(10)]
+list_100 = [randint(-100, 100) for _ in range(100)]
+list_1000 = [randint(-100, 100) for _ in range(1000)]
+
+print('Сравнение стандартной пузырьковой сортировки и доработанной:')
+print(f'Стандартная сортировка список 10: {timeit("bubble_sort_rev1(list_10[:])", globals=globals(), number=1000)}')
+print(f'Доработанная сортировка список 10: {timeit("bubble_sort_rev2(list_10[:])", globals=globals(), number=1000)}')
+print('*' * 50)
+print(f'Стандартная сортировка список 100: {timeit("bubble_sort_rev1(list_100[:])", globals=globals(), number=1000)}')
+print(f'Доработанная сортировка список 100: {timeit("bubble_sort_rev2(list_100[:])", globals=globals(), number=1000)}')
+print('*' * 50)
+print(f'Стандартная сортировка список 1000: {timeit("bubble_sort_rev1(list_1000[:])", globals=globals(), number=1000)}')
+print(
+    f'Доработанная сортировка список 1000: {timeit("bubble_sort_rev2(list_1000[:])", globals=globals(), number=1000)}')
+
+print(list_10)
+print(bubble_sort_rev2(list_10))
+
+# Прервывание списка даст прирост производительности только если список близок к отсортированному состоянию
+# На это есть шанс только в маленьком списке
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,64 @@ for i in
 
 
 """
+
+from statistics import median
+import random
+from timeit import timeit
+
+
+def gnome(lst_obj):
+    i, size = 1, len(lst_obj)
+    index = (len(lst_obj) - 1) // 2
+    while i < size:
+        if lst_obj[i - 1] <= lst_obj[i]:
+            i += 1
+        else:
+            lst_obj[i - 1], lst_obj[i] = lst_obj[i], lst_obj[i - 1]
+            if i > 1:
+                i -= 1
+    return lst_obj[index]
+
+
+def sorted_median(lst_obj):
+    quotient, remainder = divmod(len(lst_obj), 2)
+    if remainder:
+        return sorted(lst_obj)[quotient]
+    return sum(sorted(lst_obj)[quotient - 1:quotient + 1]) / 2
+
+
+def not_sort(lst_obj):
+    left_side = []
+    right_side = []
+    for i in range(len(lst_obj)):
+        for j in range(len(lst_obj)):
+            if lst_obj[i] > lst_obj[j]:
+                left_side.append(lst_obj[j])
+            if lst_obj[i] < lst_obj[j]:
+                right_side.append(lst_obj[j])
+            if lst_obj[i] == lst_obj[j] and i > j:
+                left_side.append(lst_obj[j])
+            if lst_obj[i] == lst_obj[j] and i < j:
+                right_side.append(lst_obj[j])
+        if len(left_side) == len(right_side):
+            return lst_obj[i]
+        left_side.clear()
+        right_side.clear()
+
+
+m = int(input("Введите натуральное число: "))
+orig_list = [random.randint(0, 1000) for _ in range(2 * m + 1)]
+
+print(f'Исходный список: {orig_list}')
+# print(median(orig_list[:])) # Проверка правильно ли мы нашли медиану
+print(f'Поиск медианы Гномьей сортировкой: {gnome(orig_list[:])}\n'
+      f'Затраченное время: {timeit("median(orig_list[:])", globals=globals(), number=1000)}')
+print(f'Поиск медианы с помощью "divmod": {sorted_median(orig_list[:])}\n'
+      f'Затраченное время: {timeit("sorted_median(orig_list[:])", globals=globals(), number=1000)}')
+print(f'Поиск медианы без сортировки списка: {not_sort(orig_list[:])}\n'
+      f'Затраченное время: {timeit("not_sort(orig_list[:])", globals=globals(), number=1000)}')
+
+# С помощью сортировки поиск медианы происходит очень быстро.
+# Без сортировки возрастает в десятки раз.
+
+# P.S. c методом без сортировки очень помог интернет, но я постарался разобрать его.
