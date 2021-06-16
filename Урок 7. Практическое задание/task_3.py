61722559de5feff27e2a0c35fbe87b8e0138e92e
Dukemegaman <Dukemegaman@gmail.com>
2021-06-16 12:51:13 +0300

    task 1, 2, 3

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,38 @@ for i in
 
 
 """
+"""
+Вывод: Тут вывод явно напрашивается только один, встроенная функция работает намного быстрее любой сортировки написанной
+нами. В данном случае был рассмотрен метод Гномьей сортировки.
+"""
+from timeit import timeit
+import random
+from statistics import median
+
+
+def gnome_sort(lst_obj):
+    i = 1
+    j = 2
+    while i < len(lst_obj):
+        if lst_obj[i - 1] <= lst_obj[i]:
+            i = j
+            j += 1
+        else:
+            lst_obj[i - 1], lst_obj[i] = lst_obj[i], lst_obj[i - 1]
+            i -= 1
+            if i == 0:
+                i = j
+                j += 1
+    return lst_obj
+
+
+m = int(input('Введите натуральное число: '))
+orig_list = [random.randint(0, 100) for _ in range(2 * m + 1)]
+
+print(f'Медиана Гномьей сортировки: {gnome_sort(orig_list[:])[m]}')
+print(f'Поиск с помощью Гномьей сортировки: '
+      f'{timeit("gnome_sort(orig_list[:])[m]", globals=globals(), number=1000)}')
+print(f'Медиана из библиотеки statistics: {median(orig_list)}')
+print(f'Поиск функецией из библиотеки statistics: '
+      f'{timeit("median(orig_list[:])", globals=globals(), number=1000)}')
+
