281c9dc13b6af39f9e18b9d372986afcbe4e5faf
margarita-lomakina <margarita.v.i@mail.ru>
2021-06-17 13:01:59 +0300

    Algorithms homework7
    by Margarita Lomakina, group 897

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,159 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+from random import random, randint
+from timeit import default_timer
+
+
+def time_it(func):
+    def wrapper(numb):
+        start_time = default_timer()
+        res = func(numb)
+        print(default_timer() - start_time)
+        return res
+    return wrapper
+
+
+def standard_mergesort(lst):
+    if len(lst) == 1 or len(lst) == 1:
+        return
+    middle = len(lst) // 2
+    l_lst, r_lst = lst[:middle], lst[middle:]
+    standard_mergesort(l_lst)
+    standard_mergesort(r_lst)
+
+    # слияние массивов
+    l_ind = r_ind = i = 0
+    while l_ind < len(l_lst) and r_ind < len(r_lst):
+        if l_lst[l_ind] <= r_lst[r_ind]:
+            lst[i] = l_lst[l_ind]
+            l_ind += 1
+        else:
+            lst[i] = r_lst[r_ind]
+            r_ind += 1
+        i += 1
+    while l_ind < len(l_lst):
+        lst[i] = l_lst[l_ind]
+        l_ind += 1
+        i += 1
+    while r_ind < len(r_lst):
+        lst[i] = r_lst[r_ind]
+        r_ind += 1
+        i += 1
+
+
+def merge(left, right):
+    result = list()
+    while len(left) > 0 and len(right) > 0:
+        if left[0] <= right[0]:
+            result.append(left[0])
+            del left[0]
+        else:
+            result.append(right[0])
+            del right[0]
+    while len(left) > 0:
+        result.append(left[0])
+        del left[0]
+    while len(right) > 0:
+        result.append(right[0])
+        del right[0]
+    return result
+
+
+def mergesort(lst):
+    if len(lst) <= 1:
+        return lst
+    else:
+        middle = len(lst) // 2
+        left = lst[:middle]
+        right = lst[middle:]
+
+        left = mergesort(left)
+        right = mergesort(right)
+        result = merge(left, right)
+        return result
+
+
+@time_it
+def standard_mergesort_measure(lst):
+    standard_mergesort(lst)
+
+
+@time_it
+def mergesort_measure(lst):
+    return mergesort(lst)
+
+
+
+print("Массив из 10 элементов:")
+source_list = [round(random() * 50, 3) for _ in range(10)]
+sorted_list = source_list[:]
+print("Исходный массив: ", source_list)
+print("\nСтандартная сортировка слиянием: ")
+standard_mergesort_measure(sorted_list)
+print(sorted_list)
+print("Другая реализация: ")
+sorted_list = mergesort_measure(source_list)
+print(sorted_list)
+
+print("\nМассив из 100 элементов:")
+source_list = [round(random() * 50, 3) for _ in range(100)]
+sorted_list = source_list[:]
+print("Стандартная сортировка слиянием: ")
+standard_mergesort_measure(sorted_list)
+print("Другая реализация: ")
+sorted_list = mergesort_measure(source_list)
+
+print("\nМассив из 1000 элементов:")
+source_list = [round(random() * 50, 3) for _ in range(1000)]
+sorted_list = source_list[:]
+print("Стандартная сортировка слиянием: ")
+standard_mergesort_measure(sorted_list)
+print("Другая реализация: ")
+sorted_list = mergesort_measure(source_list)
+
+print("\nМассив из 10000 элементов:")
+source_list = [round(random() * 50, 3) for _ in range(10000)]
+sorted_list = source_list[:]
+print("Стандартная сортировка слиянием: ")
+standard_mergesort_measure(sorted_list)
+print("Другая реализация: ")
+sorted_list = mergesort_measure(source_list)
+
+
+'''
+Результаты замеров
+Массив из 10 элементов:
+Стандартная сортировка слиянием: 
+9.670000000000512e-05
+Другая реализация: 
+0.00010540000000000549
+
+Массив из 100 элементов:
+Стандартная сортировка слиянием: 
+0.0007101999999999942
+Другая реализация: 
+0.0005486999999999853
+
+Массив из 1000 элементов:
+Стандартная сортировка слиянием: 
+0.009278199999999986
+Другая реализация: 
+0.007194200000000012
+
+Массив из 10000 элементов:
+Стандартная сортировка слиянием: 
+0.07938820000000002
+Другая реализация: 
+0.11841120000000005
+
+Первый вариант сортировки слиянием (standard_mergesort) аналогичен варианту, предложенномуу на лекции.
+Второй вариант использует дополнительную память для создания вспомогательных списков. 
+
+Оба варианта выполняют сортировку за примерно одинаковое время.
+В приведенном замере на списках длины 100 и 1000, второй вариант работает быстрее. Со списком из 10000 - первый.
+Однако при многократном запуске можно увидеть, что то один, то другой вариант работает быстрее.
+Оба варианта имеют сложность n*log(n).
+'''
+
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,67 @@ for i in
 
 
 """
+
+from random import randint
+
+'''
+Первый вариант поиска медианы. Ищем максимальный элемент m раз и удаляем его из списка. 
+m+1-ый максимальный элемент - искомая медиана.
+
+Этот вариант имеет сложность 3n*(n/2) = 1,5n^2 = O(n^2)
+'''
+
+
+def get_median_1(lst):
+    new_list = lst[:]
+    for _ in range(m):
+        max_ind = new_list.index(max(new_list))
+        del new_list[max_ind]
+    return max(new_list)
+
+
+'''
+Второй вариант поиска медианы - взять средний элемент в отсортированном массива.
+Я выбрала сортировку кучей HeapSort, которая имеет сложность n*log(n).
+'''
+
+def heapify(lst, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and lst[largest] < lst[l]:
+        largest = l
+
+    if r < n and lst[largest] < lst[r]:
+        largest = r
+
+    if not (largest == i):
+        lst[largest], lst[i] = lst[i], lst[largest]
+        heapify(lst, n, largest)
+
+
+def heap_sort(list_to_sort):
+    lst = list_to_sort[:]
+    for i in range(round(len(lst) / 2 - 1), -1, -1):
+        heapify(lst, len(lst), i)
+
+    for i in range(len(lst) - 1, 0, -1):
+        lst[0], lst[i] = lst[i], lst[0]
+        heapify(lst, i,  0)
+    return lst
+
+
+m = int(input("Введите m (m - половина длины списка): "))
+source_list = [randint(0, 10) for _ in range(2*m + 1)]
+print(source_list)
+print("Медиана 1: ", get_median_1(source_list))
+print("Медиана 2: ", heap_sort(source_list)[m])
+
+'''
+Введите m (m - половина длины списка): 5
+[7, 9, 9, 1, 7, 0, 5, 1, 5, 7, 1]
+Медиана 1:  5
+Медиана 2:  5
+'''
+
