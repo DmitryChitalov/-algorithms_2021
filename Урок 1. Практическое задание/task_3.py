75a733dd4f66f602823b3ead77b3c729e1a75390
Aleksey Shmykov <shmykov.job@gmail.com>
2021-06-17 16:10:15 +0300

    task 3 done

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -21,34 +21,42 @@
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
 
-
-company_list = {'Рога и Копыта': 1000000, 'Тру.Бо.О.Ж.О.П': 20000000, 'Шарашкина контора': 24443425, 'МММ': 76646879,
-                'Братва и кольцо': 87858976, 'SpaceX': 999999999}
+company_list = {'Рога и Копыта': 1000000, 'Тру.Бо.О.Ж.О.П': 50000000, 'Шарашкина контора': 24443425, 'МММ': 76646879,
+                'Братва и кольцо': 47858976, 'SpaceX': 999999999}
 #############################################################################################
 """
 Вариант 1
 Сложность O(n^2)
 """
 
+sorted_values = sorted(company_list.values(), reverse=True)
+sorted_dict = {}
 
-# sorted_values = sorted(company_list.values(), reverse=True)
-# sorted_dict = {}
-#
-# for i in sorted_values:
-#     for k in company_list.keys():
-#         if company_list[k] == i:
-#             sorted_dict[k] = company_list[k]
-#             break
-# k = 3
-# for name,profit in sorted_dict.items():
-#     print(f"{name} {profit}\n")
-#     k -= 1
-#     if k == 0:
-#         break
+for i in sorted_values:
+    for k in company_list.keys():
+        if company_list[k] == i:
+            sorted_dict[k] = company_list[k]
+            break
+k = 3
+for name,profit in sorted_dict.items():
+    print(f"{name} {profit}$")
+    k -= 1
+    if k == 0:
+        break
+
 
 #############################################################################################
 """
 Вариант 2
-Сложность !!!!
+Сложность O(n)
 """
 
+import operator
+
+top_companies_list = company_list.copy()
+k = 3
+while k > 0:
+    name = max(top_companies_list.items(), key=operator.itemgetter(1))[0]
+    profit = top_companies_list.pop(name)
+    print(f'{name} {profit}')
+    k -= 1
