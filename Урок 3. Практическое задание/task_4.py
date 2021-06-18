3f3be16dae961fe1e0012fee0458c788818d6fa4
Dmitry <myxahev2@gmail.com>
2021-06-18 16:30:46 +0500

    complete_task_4

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,21 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+import hashlib
+from uuid import uuid4
+
+
+def func_url_check(url, bd={}, salt=uuid4().hex):
+    if url in bd.keys():
+        print(f'{url} есть в БД')
+    else:
+        hash = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+        bd.update({url: hash})
+        print(f'{url} добавлен в БД')
+
+
+func_url_check('https://gb.ru/')
+func_url_check('https://github.com/')
+func_url_check('https://python.org/')
+func_url_check('https://python.org/')
