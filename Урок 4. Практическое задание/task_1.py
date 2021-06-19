6aef15af17d92141aa70ee33b2199910cbb218d6
cyber-welder <cyber-welder@ya.ru>
2021-06-19 16:37:53 +0800

    Выполнены задания 4.1 - 4.5

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,8 @@
 Без аналитики задание не принимается
 """
 
+from timeit import timeit
+
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +22,14 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+print(timeit("func_1(range(100))", globals=globals(), number=100000))
+print(timeit("func_2(range(100))", globals=globals(), number=100000))
+
+# добавил функцию с тем же функционалом, только через list comprehension
+# работает процентов на 15-20 быстрее
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,6 +11,8 @@
 
 Без аналитики задание считается не принятым!
 """
+from timeit import timeit
+from collections import Counter
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -39,5 +41,29 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    result = max([(array.count(i), i) for i in set(array)])
+    return f'Чаще всего встречается число {result[1]}, оно появилось в массиве {result[0]} раз(а)'
+
+
+def func_4():
+    dig, cnt = Counter(array).most_common(1)[0]
+    return f'Чаще всего встречается число {dig}, оно появилось в массиве {cnt} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+
+print('Функция func_1 -', timeit("func_1()", globals=globals(), number=100000))
+print('Функция func_2 -', timeit("func_2()", globals=globals(), number=100000))
+print('Функция func_3 -', timeit("func_3()", globals=globals(), number=100000))
+print('Функция func_4 -', timeit("func_4()", globals=globals(), number=100000))
+
+# придумал пару функций не самых быстрых, но лаконичнее предложенных
+# самой медленной оказалась функция func_4 через класс Counter, видимо потому что наследник коллекций
+# зато самый лаконичный код
+# вторая по скорости и лаконичности получилась функция func_3, через list comprehension и встроенные функции
+# самой быстрой осталась func_1
+
