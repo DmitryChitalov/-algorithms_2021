e82553ad1eff7ce9fc89f6d0de2f7d88dddfd765
Aleksey Shmykov <shmykov.job@gmail.com>
2021-06-19 16:03:57 +0300

    tTask 1 done

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -28,3 +28,58 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+'''
+==================================================================================================
+Вариант решения не очень рекуривный
+'''
+def input_operation():
+    operation_list = ['+', '-', '*', '/', '0']
+    while True:
+        operation = input('Введите операцию (+, -, *, / или 0 для выхода): ')
+        operation = operation.replace(' ', '')
+        if operation in operation_list:
+            break
+        print('Вы ввели операцию не из списка или ошиблись. Исправьтесь')
+    return operation
+
+
+def validate_number(number):
+    if type(int(number)) == int:
+        return True
+
+
+def input_number(element: str):
+    while True:
+        number = input(f'Введите {element} число: ')
+        if validate_number(number):
+            number = int(number)
+            break
+        print(f'Вместо целого числа вы ввели {number}. Исправьтесь')
+    return number
+
+
+def calculator_recur(operation):
+    if operation == '0':
+        return
+    number_1 = input_number('первое')
+    number_2 = input_number('второе')
+    if operation == '+':
+        print(f'Ваш результат {number_1 + number_2}')
+        calculator_recur(input_operation())
+    if operation == '-':
+        print(f'Ваш результат {number_1 - number_2}')
+        calculator_recur(input_operation())
+    if operation == '*':
+        print(f'Ваш результат {number_1 * number_2}')
+        calculator_recur(input_operation())
+    if operation == '/':
+        if number_2 == 0 and operation == '/':
+            print('На ноль делить нельзя!')
+            calculator_recur(input_operation())
+        else:
+            print(f'Ваш результат {number_1 / number_2}')
+            calculator_recur(input_operation())
+
+
+calculator_recur(operation=input_operation())
