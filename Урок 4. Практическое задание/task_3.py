f305779c6ac78815a52d6a6af90a4ce6c3506c74
RuslanT <12herekeith@gmail.com>
2021-06-19 17:49:24 +0300

    Unit_3

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -15,6 +15,17 @@
 """
 
 
+# Вариант revers_4 является самым труднозатратным, так как используетя цикл и аппенд.
+# Менее затратным является revers_3,так как он по своей структуре превращает число в строку и
+# переварачивает его с помощью среза.
+# Другие варианты используют рекурсию, что является затратным по скорости
+
+from cProfile import run
+from timeit import timeit
+
+num = 15450
+
+
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -37,3 +48,31 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num, my_list=[]):
+    if enter_num == 0:
+        return ''.join(str(x) for x in my_list)
+    else:
+        c = enter_num % 10
+        my_list.append(c)
+        enter_num = enter_num // 10
+        return revers_4(enter_num)
+
+
+print('С помощью time it')
+
+print(timeit("revers_1(num)", globals=globals()))
+print(timeit("revers_2(num)", globals=globals()))
+print(timeit("revers_3(num)", globals=globals()))
+print(timeit("revers_4(num)", globals=globals()))
+
+
+def main():
+    res_1 = revers_1(num)
+    res_2 = revers_2(num)
+    res_3 = revers_3(num)
+    res_4 = revers_4(num)
+
+
+run('main()')
