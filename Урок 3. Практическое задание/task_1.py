aa497112a2fbb63f4f23ec3fe5fdb8a04ca0350e
Sergei Ryzhov <ryzhovs@tut.by>
2021-06-19 12:17:05 +0300

    Done the lesson-3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -1,18 +1,95 @@
 """
 Задание 1.
-
 Реализуйте свои пользовательские функции, в которых реализуйте:
-
 a) заполнение списка и словаря,
    сделайте замеры и сделайте выводы, что выполняется быстрее и почему
+   И укажите сложность каждой ф-ции, которую вы применяете для заполнения.
+   У этих ф-ций может быть разная сложность. Поэтому время заполнения списка и словаря может как совпадать, так и отличаться.
 b) выполните набор операций и со списком, и со словарем,
    сделайте замеры и сделайте выводы, что и где выполняется быстрее и почему
-
+   И укажите сложность ф-ций, которые вы используете для операций.
 Подсказка: для замеров воспользуйтесь модулем time (см. примеры урока 1)
-
 Примечание: eсли вы уже знаете, что такое декоратор и как его реализовать,
 то реализуйте ф-цию-декоратор для подсчета времени работы ваших пользовательских функций
 И примените ее к своим функциям!
-
 Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
+"""
+
+from time import time
+
+test_list = [1, 2, 4, 10]
+test_dict = {1: 'one', 2: 'two', 4: 'four', 10: 'ten'}
+
+
+def time_measure(func_to_decorate):
+    def the_wrapper(n):
+        for i in range(100000000):  # it's CPU power up
+            1 / 100
+        start_time = time()
+        execute = func_to_decorate(n)
+        print(f'The execution time is {time() - start_time}')
+        return execute
+    return the_wrapper
+
+
+@time_measure
+def list_filler_comprehension(k):       # Сложность функции O(N) - линейная
+    lst = [i for i in range(k)]         # O(N) - линейная
+    return 'Created list by comprehension'
+
+@time_measure
+def list_filler_append(k):              # Сложность функции O(N) - линейная
+    lst = []                            # O(1) - константная
+    for i in range(k):                  # O(N) - линейная
+        lst.append(i)                   # O(1) - константная
+    return 'Created list by append'
+
+@time_measure
+def dict_filler_comprehension(k):       # Сложность функции O(N) - линейная
+    dict = {i: i for i in range(k)}     # O(N) - линейная
+    return 'Created Dictionary by comprehension'
+
+@time_measure
+def dict_filler_setdefault(k):          # Сложность функции O(N) - линейная
+    dict = {}                           # O(1) - константная
+    for i in range(k):                  # O(N) - линейная
+        dict.setdefault(i, 1000)        # O(1) - константная
+    return 'Created Dictionary by setdefault'
+
+@time_measure
+def dict_filler_update(k):              # Сложность функции O(N) - линейная
+    dict = {}                           # O(1) - константная
+    for i in range(k):                  # O(N) - линейная
+        dict.update({i: i})             # O(1) - константная
+    return 'Created Dictionary by update'
+
+@time_measure
+def list_oper(k):                       # Сложность функции O(N) - линейная
+    num = 10                            # O(1) - константная
+    if num in k:                        # O(N) - линейная
+        None                            # O(1) - константная
+    k.pop(len(k)-1)                     # O(1) - константная
+    return 'list operations'
+
+@time_measure
+def dict_oper(k):                       # Сложность функции O(N) - линейная
+    num = 10                            # O(1) - константная
+    if num in k.keys():                 # O(N) - линейная
+        None                            # O(1) - константная
+    k.pop(num)                          # O(1) - константная
+    return 'Dictionary operations'
+
+print(list_filler_comprehension(1000000))
+print(list_filler_append(1000000))
+print(dict_filler_comprehension(1000000))
+print(dict_filler_setdefault(1000000))
+print(dict_filler_update(1000000))
+print(list_oper([1, 2, 4, 10]))
+print(dict_oper({1: 'one', 2: 'two', 4: 'four', 10: 'ten'}))
+
+"""
+Вывод: словарь работает медленнее при аналогичных функциях наполнения.
+Причина: использование бОльшего объема памяти для словаря чем для списка.
+Однако выполнение операций занимает примерно равное время, что говорит о том что
+для операций нет разницы в использовании типа данных словарь или список.
 """
--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -4,17 +4,121 @@
 Для этого пароля вам нужно получить хеш, используя функцию sha256
 Для генерации хеша обязательно нужно использовать криптографическую соль
 Обязательно выведите созданный хеш.
-
 Далее программа должна запросить пароль повторно
 Вам нужно проверить, совпадает ли пароль с исходным
-Для проверки необходимо сравнить хеши паролей
-
+Для проверки необходимо сравнить хеши паролей.
+Самый просто вариант хранения хешей - просто в оперативной памяти (в переменных).
 ПРИМЕР:
 Введите пароль: 123
 В базе данных хранится строка: 555a3581d37993843efd4eba1921f1dcaeeafeb855965535d77c55782349444b
 Введите пароль еще раз для проверки: 123
 Вы ввели правильный пароль
-
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+
+import hashlib
+import pandas as pd
+import MySQLdb
+import sshtunnel
+import logging
+from sshtunnel import SSHTunnelForwarder
+
+ssh_host = '192.168.1.58'
+ssh_username = 'user'
+ssh_password = '000000'
+database_username = 'root'
+database_password = '111111'
+database_name = 'sample'
+localhost = '127.0.0.1'
+
+
+def open_ssh_tunnel(verbose=False):
+    if verbose:
+        sshtunnel.DEFAULT_LOGLEVEL = logging.DEBUG
+
+    global tunnel
+    tunnel = SSHTunnelForwarder(
+        (ssh_host, 22),
+        ssh_username=ssh_username,
+        ssh_password=ssh_password,
+        remote_bind_address=('127.0.0.1', 3306)
+    )
+
+    tunnel.start()
+    print('ssh connected')
+
+
+def mysql_connect():
+    global connection
+
+    connection = MySQLdb.connect(
+        host=localhost,
+        user=database_username,
+        passwd=database_password,
+        db=database_name,
+        port=tunnel.local_bind_port
+    )
+    print('mysql connected')
+
+
+def run_query(sql):
+    return pd.read_sql_query(sql, connection)
+
+
+def mysql_disconnect():
+    connection.close()
+
+
+def close_ssh_tunnel():
+    tunnel.close
+
+
+def password_salt_hasher(password):
+    salt = 'salt_forever'
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+
+
+open_ssh_tunnel()
+mysql_connect()
+user_password = password_salt_hasher(input('Input the password: '))
+print(user_password)
+cursor = connection.cursor()
+
+# If you need to crete a table, you may execute the lines below
+
+# cursor.execute("DROP TABLE IF EXISTS passwords")
+# cursor.execute("""CREATE TABLE passwords(
+#           id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+#           pass_hash VARCHAR(255))""")
+
+sql = "INSERT INTO `passwords`(`pass_hash`) VALUES ('%s')" % (user_password)
+try:
+    cursor.execute(sql)
+    connection.commit()
+    print('Connection success')
+except:
+    connection.rollback()
+    print('Operation error')
+
+sql = "SELECT pass_hash FROM passwords WHERE pass_hash = '%s'" % (user_password)
+cursor.execute(sql)
+print('The new string saves in the db: '+str(cursor.fetchone()))
+
+user_password_check = password_salt_hasher(input('Confirm the password, input it again: '))
+sql = "SELECT * FROM passwords WHERE pass_hash = '%s'" % (user_password_check)
+answer = ''
+try:
+    answer = cursor.execute(sql)
+    connection.commit()
+    print('Connection success')
+except:
+    connection.rollback()
+    print('Operation error')
+if answer != 0:
+    print('Confirmed password is correct!')
+else:
+    print("Confirmed password isn't correct. Try again.")
+
+mysql_disconnect()
+close_ssh_tunnel()
