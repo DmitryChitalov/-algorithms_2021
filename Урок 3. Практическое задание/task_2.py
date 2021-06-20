f676616389b8a95ef52183359753410b6a3dc5a7
WhiIend <76176054+WhiIend@users.noreply.github.com>
2021-06-20 13:46:53 +0300

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -18,3 +18,35 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+
+import hashlib, uuid
+
+
+salt = uuid.uuid4().hex
+passwd = 'password'
+res_pass_hash = hashlib.sha256(salt.encode() + passwd.encode()).hexdigest()
+with open('hash_task2.txt', 'w') as f:
+    f.write(res_pass_hash)
+with open('hash_task2.txt', 'r') as f:
+        open_file = f.read()
+
+'''
+while True:
+    inp_pass = input('Введите пароль: ')
+    if open_file == hashlib.sha256(salt.encode() + inp_pass.encode()).hexdigest():
+        print('Welcome!')
+        break
+    else:
+        print('Invalid password. Try again.\n')
+        continue
+'''
+
+def func_inp_pass(open_file):
+    inp_pass = input('Введите пароль: ')
+    if open_file == hashlib.sha256(salt.encode() + inp_pass.encode()).hexdigest():
+        return 'Welcome!'
+    else:
+        print('Invalid password. Try again.\n')
+        return func_inp_pass(open_file)
+
+print(func_inp_pass(open_file))
