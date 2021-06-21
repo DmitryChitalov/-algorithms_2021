22aa542a0702ac502c54ce4693969dbf564745b2
Evgeny <ea.nikulin@yandex.ru>
2021-06-21 15:53:41 +0500

    //Homework_4 Complete

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,6 +11,8 @@
 
 Без аналитики задание считается не принятым!
 """
+from timeit import timeit
+from cProfile import run
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -39,5 +41,25 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    max_count = max([(array.count(i), i) for i in set(array)])
+    return f'Чаще всего встречается число {max_count[1]}, ' \
+           f'оно появилось в массиве {max_count[0]} раз(а)'
+
+
+print(timeit("func_1()", number=2500000, globals=globals()))
+run('func_1()')
+
+print(timeit("func_2()", number=2500000, globals=globals()))
+run('func_2()')
+
+print(timeit("func_3()", number=2500000, globals=globals()))
+run('func_3()')
+
 print(func_1())
 print(func_2())
+print(func_3())
+
+# Для убедительности увеличил число повторений кода в 2.5 раза от стандарта, сделал следующие выводы:
+# Из всех функций самая быстрая - функция 1
+# Функция 2 немного медленнее чем моя Функция 3
