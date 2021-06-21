24990f9538670e9d3c66836a1d87ed5ab970c288
Sebkd <sebkd@mail.ru>
2021-06-21 23:18:31 +0700

    decode

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -9,3 +9,35 @@
 Можете условжнить задачу, реализовав ее через ООП
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
+import hashlib
+import json
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        url_str= str(input('Введите адрес url, но не менее 4 букв или цифр ') + '\n')
+        if len(url_str) < 4:
+            print(f'Введено не то что нужно, если вы понимаете о чем это я')
+            break
+        salt = str(url_str[-4])
+        print(salt.encode () + url_str.encode (), end = '\n')
+        url_str_original = hashlib.sha256 (salt.encode () + url_str.encode ()).hexdigest ()
+
+        with open ('archive.txt', 'a') as file_one:
+            file_one.writelines (url_str_original)
+        with open('archive.txt', 'r') as file_one:
+            for file_inn in file_one:
+                print(file_inn)
+                if url_str_original != file_inn:
+                    print('уау')
+                    print(file_one.read())
+                else:
+                    print('ауа')
+
+
+
+
+
+
