5079c1dfc4895e6104e8dad3eece80007f1f2620
Evgeny <ea.nikulin@yandex.ru>
2021-06-21 11:07:18 +0500

    //Homework_4 task 1 Complete

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,9 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание не принимается
 """
+from timeit import timeit
+
+nums = [i * i for i in range(100)]
 
 
 def func_1(nums):
@@ -20,3 +23,19 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+print(timeit("func_1(nums)", number=1500000, globals=globals()))
+
+
+def func_1_mod(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+print(timeit("func_1_mod(nums)", number=1500000, globals=globals()))
+
+# 11.742759642000001
+# 9.680958492999999
+# Вывод: В исходном коде используется итерация с методом append (большая сложность), а в моей версии
+# я использовал list comprehension, что дало прибавку в скорости, так же код стал более красивым и стильным.
