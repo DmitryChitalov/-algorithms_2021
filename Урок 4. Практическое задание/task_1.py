062176708b036fb359277901e8b372ebf788bba4
Dmitry <myxahev2@gmail.com>
2021-06-22 12:14:28 +0500

    complete_task_1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,10 @@
 Без аналитики задание не принимается
 """
 
+from timeit import timeit
+from cProfile import run
+
+enter_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +24,16 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+print(timeit("func_1(enter_num)", number=3700000, globals=globals()))
+run('func_1(enter_num)')
+
+def func_2(nums):
+    return nums[::2]
+
+print(timeit("func_2(enter_num)", number=3700000, globals=globals()))
+run('func_2(enter_num)')
+
+"""
+Выводы: Добавил другой вариант более быстрого решения в котором используется механизм срезов которые считаются быстрыми т.к. они встроенные.
+"""
