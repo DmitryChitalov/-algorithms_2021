eaaad28257adce3afe4af9e5df1e93a840e37d40
soulparadise <soulparadiss@gmail.com>
2021-06-22 23:09:16 +0300

    Lesson 7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,77 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+from timeit import timeit
+from random import randint
+
+
+def sort_array(array):
+    """
+    Классическая функция сортировки пузырьком.
+    """
+    for _ in range(len(array)):
+        for element in range(len(array) - 1):
+            if array[element] < array[element + 1]:
+                array[element + 1], array[element] = array[element], array[element + 1]
+    return array
+
+
+def sort_array_upgrade(array):
+    """
+    Доработанная функция сортировки с использованием флага и уменьшения колличества иттераций.
+    """
+    len_list = len(array)
+    while len_list > 1:
+        flag = True
+        for element in range(len_list - 1):
+            if array[element] < array[element + 1]:
+                array[element], array[element + 1] = array[element + 1], array[element]
+                flag = False
+        if flag:
+            return array
+        len_list -= 1
+    return array
+
+
+random_array_10 = [randint(-100, 100) for _ in range(10)]
+random_array_100 = [randint(-100, 100) for _ in range(100)]
+random_array_1000 = [randint(-100, 100) for _ in range(1000)]
+
+print(random_array_10)
+print(sort_array(random_array_10.copy()))
+print(sort_array_upgrade(random_array_10.copy()))
+
+print(f'Стандартная сортировка список 10: '
+      f'{timeit("sort_array(random_array_10.copy())", globals=globals(), number=1000)}')
+print(f'Доработанная сортировка список 10: '
+      f'{timeit("sort_array_upgrade(random_array_10.copy())", globals=globals(), number=1000)}')
+
+print(f'Стандартная сортировка список 100: '
+      f'{timeit("sort_array(random_array_100.copy())", globals=globals(), number=1000)}')
+print(f'Доработанная сортировка список 100: '
+      f'{timeit("sort_array_upgrade(random_array_100.copy())", globals=globals(), number=1000)}')
+
+print(f'Стандартная сортировка список 1000: '
+      f'{timeit("sort_array(random_array_1000.copy())", globals=globals(), number=1000)}')
+print(f'Доработанная сортировка список 1000: '
+      f'{timeit("sort_array_upgrade(random_array_1000.copy())", globals=globals(), number=1000)}')
+
+
+'''
+[-49, 68, 5, -83, 19, 13, -97, -86, -25, 25]
+[68, 25, 19, 13, 5, -25, -49, -83, -86, -97]
+[68, 25, 19, 13, 5, -25, -49, -83, -86, -97]
+Стандартная сортировка список 10 элементов 0.011587399999999998
+Доработанная сортировка список 10 элементов 0.010045100000000001
+
+Стандартная сортировка список 100 элементов 0.882776
+Доработанная сортировка список 100 элементов 0.7582925
+
+Стандартная сортировка список 1000 элементов 106.5490396
+Доработанная сортировка список 1000 элементов 86.48994909999999
+
+Скорость сортировки повысилась при использование доработанной функции.
+Доработка вида возвращать список если не было произведено ни одной операции присваивания -  
+не имеет смысла, так как вероятность такого случая крайне мала.
+'''
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,69 @@ for i in
 
 
 """
+from random import randint
+from timeit import timeit
+from statistics import median
+
+m = int(input(f'Введите m: '))
+user_list = [randint(0, 10000) for i in range(2 * m + 1)]
+
+
+def median_delete(lst):
+    for i in range(len(lst) // 2):
+        lst.remove(max(lst))
+    return max(lst)
+
+
+def median_sort(lst):
+    lst.sort()
+    return lst[m]
+
+
+def gnome_sort(arr):
+    n = len(arr)
+    index = 0
+    while index < n:
+        if index == 0:
+            index += 1
+        if arr[index] >= arr[index - 1]:
+            index += 1
+        else:
+            arr[index], arr[index - 1] = arr[index - 1], arr[index]
+            index -= 1
+
+    return arr[m]
+
+
+print(
+    f'Функция удаления максимального элемента: {timeit("median_delete(user_list[:])", globals=globals(), number=1000)}')
+print(f'Обычная сортировка: {timeit("median_sort(user_list[:])", globals=globals(), number=1000)}')
+print(f'Функция median: {timeit("median(user_list[:])", globals=globals(), number=1000)}')
+print(f'Сортировка gnome: {timeit("gnome_sort(user_list[:])", globals=globals(), number=1000)}')
+
+'''
+m = 100
+Функция удаления максимального элемента: 0.26111289999999987
+Обычная сортировка: 0.008954000000000129
+Функция median: 0.009854099999999866
+Сортировка gnome: 3.9031618
+
+m = 300
+Функция удаления максимального элемента: 2.179484199999999
+Обычная сортировка: 0.04013850000000119
+Функция median: 0.0404605999999994
+Сортировка gnome: 36.39318
+
+m = 1000
+Функция удаления максимального элемента: 23.4133266
+Обычная сортировка: 0.1750606000000019
+Функция median: 0.1794165999999997
+Сортировка gnome: 423.85444040000004
+
+Самая эффективная как показывают замеры это штатная функция median(),
+или обычная сортировка sort() и возврат эелемента с индексом m.
+
+"Гномья сортировка" - крайне не эффективная при большом объеме входящих данных.
+Вычисление медианы путём удаления максимального элемента - показывает не плохой результат, 
+но все еще далеко от стандартного набора вычисление медианы.
+'''
