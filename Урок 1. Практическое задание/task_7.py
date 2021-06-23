115bd389c65cbf6df8a8939d767c892c7421488f
ASh1ne <77381910+ASh1ne@users.noreply.github.com>
2021-06-23 19:05:13 +0500

    Update task_7.py

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -1,23 +1,7 @@
-"""
-Задание 7.
-Задание на закрепление навыков работы с деком
-
-В рассмотренном на уроке листинге есть один недостаток
-Приведенный код способен "обработать" только строку без пробелов, например, 'топот'
-
-Но могут быть и такие палиндромы, как 'молоко делили ледоколом'
-
-Вам нужно доработать программу так, чтобы она могла выполнить проверку на палиндром
-и в таких строках (включающих пробелы)
-
-Примечание:
-Вам не нужно писать код с нуля. Вам нужно доработать пример с урока.
-"""
-
 class DequeClass:
     def __init__(self):
         self.elems = []
-      
+
     def is_empty(self):
         return self.elems == []
 
@@ -36,26 +20,26 @@ class DequeClass:
     def size(self):
         return len(self.elems)
 
-    def myprint(self):
+    def my_print(self):
         print(self.elems)
-      
-    
-poli = 'молоко делили ледоколом'
+
+
+phrase = 'молоко делили ледоколом'
 new_str = DequeClass()
 
-for el in poli:
-  if el == ' ':
-    continue
-  else:
-     new_str.add_to_rear(el)
+for el in phrase:
+    if el == ' ':
+        continue
+    else:
+        new_str.add_to_rear(el)
 
-new_str.myprint()
+new_str.my_print()
 still_equal = True
 
 while new_str.size() > 1 and still_equal:
-  first = new_str.remove_from_front()
-  last = new_str.remove_from_rear()
-  if first != last:
-      still_equal = False
+    first = new_str.remove_from_front()
+    last = new_str.remove_from_rear()
+    if first != last:
+        still_equal = False
 
-print (still_equal)
+print(still_equal)
