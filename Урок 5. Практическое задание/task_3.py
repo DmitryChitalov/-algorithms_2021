7b6393aa8ef6ba2e25e74652bdec69b290e5fb0c
RuslanT <12herekeith@gmail.com>
2021-06-23 16:45:59 +0300

    Unit_3

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,43 @@ deque – это обобщение стеков и очередей.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+# 1) Заполнение листа и очереди через append показали примерно одинаковые трудозатраты на исполнение функций;
+# 2) Appendleft дека работает быстрее, чем инсерт списка
+# 3) Через лист комприхеншен, дэк работает быстрее
+# 4) Разворот списка через дек работает быстрее, чем встр. функция reverse
+# 5) Добавление только чётных элементов через список работает быстрее
+# 6) Popleft и pop работают примерно одинаково
+
+from timeit import timeit
+from collections import deque
+
+print('Это список')
+
+fill_list = [el for el in range(1, 101)]
+insert_list = [el for el in range(1, 101)]
+insert_list.insert(0, [el for el in range(1, 100)])
+a = [el for el in range(1, 101)]
+reverse_list = a.reverse()
+even_numbers = [el for el in range(1, 101) if el % 2 == 0]
+pop_list = [el for el in range(1, 100)]
+pop_list.pop()
+
+print(timeit('fill_list', globals=globals()))
+print(timeit('insert_list', globals=globals()))
+print(timeit('reverse_list', globals=globals()))
+print(timeit('even_numbers', globals=globals()))
+print(timeit('pop_list', globals=globals()))
+
+print('Это дек')
+fill_deque = deque(range(1, 101))
+append_deque = deque(range(1, 101))
+append_deque.appendleft([el for el in range(1, 100)])
+reversed_deque = deque(reversed(range(1, 101)))
+even_numbers_deque = deque(range(2, 101, 2))
+pop_deque = deque(range(1, 101))
+pop_deque.popleft()
+print(timeit('fill_deque', globals=globals()))
+print(timeit('append_deque', globals=globals()))
+print(timeit('reversed_deque', globals=globals()))
+print(timeit('even_numbers_deque', globals=globals()))
+print(timeit('pop_deque', globals=globals()))
