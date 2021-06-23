fb2c260007bb41cb2077f6344c59e16802f932a9
Aaznutii <aaznutii@mail.ru>
2021-06-24 01:29:21 +0400

    less_3_task_1, 2,  4,

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -1,3 +1,4 @@
+# coding=utf8
 """
 Задание 2.
 Ваша программа должна запрашивать пароль
@@ -18,3 +19,73 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+import hashlib
+
+import mysql.connector
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+def get_passwd_lst_bd(col='passwd'):
+    con = mysql.connector.connect(user='user', passwd='any', database='ido')
+    sql = pd.read_sql(f'SELECT {col} FROM users', con)
+    return sql
+
+
+def get_passwd():
+    passwd = input('Введите пароль: \n')
+    salt_id = max(get_passwd_lst_bd('id')['id']) + 1
+    salt = f'{salt_id}kulebyaka'                    # Соль- вероятность повторения низкая. привязка к id пользователя
+    result = bytes(f"{passwd}{salt}", encoding='utf-8')
+    print(result)
+    result = hashlib.sha3_256(result).hexdigest()
+    print(result)
+    return result
+
+
+def get_df(diction):
+    df = pd.DataFrame([diction], columns=diction.keys())
+    return df
+
+
+def update_db(df):
+    passwd = input('Введи свой админовский пароль: \n')
+    # Создать соединение и указать кодировку
+    con = mysql.connector.connect(user='root', passwd=passwd, database='ido')
+    engine = create_engine(f'mysql://root:{passwd}@localhost/ido?charset=utf8', echo=True)
+
+    # Записать датафрейм в базу поверх данных с заменой (replace). Можно писать в конец имеющейся таблицы ('append')
+    # index=False - признак того, что индекс не участвует в качестве данных.
+    df.to_sql('users', con=engine, if_exists='append', index=False)
+    # Контроль таблицы
+    sql = pd.read_sql('SELECT * FROM users', con)
+    print(sql)
+
+
+def registration():
+    last_name = input('Введите Вашу фамилию: \n')
+    first_name = input('Введите Ваше имя: \n')
+    patronymic = input('Введите Ваше отчетсво: \n')
+    email = input('Укажите email: \n')
+    phone = input('Укажиет телефон контакта: \n')
+
+    def validation_pass():
+        passwd = get_passwd()
+        print('Для проверки пароля введите его еще раз. \n')
+        passwd_2 = get_passwd()
+        if passwd == passwd_2:
+            if passwd not in get_passwd_lst_bd():
+                result = {'last_name': last_name, 'first_name': first_name, 'patronymic': patronymic,
+                          'email': email, 'phone': phone, 'passwd': passwd}
+                update_db(get_df(result))
+            else:
+                print("Выберите другой пароль, этот слишком простой")
+                validation_pass()
+        else:
+            print('Пароль не совпадает. Введите пароль заново')
+            validation_pass()
+    validation_pass()
+
+
+registration()
+
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,90 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+import hashlib
+from datetime import datetime
+import time
+
+
+class UrlCache:
+    cache = {}
+    maximum = 50
+
+    def __init__(self):
+        self.set_max_cache()            # Контроль макимального числа инициализируется вместе с новым экземпляром
+
+    def get_hash(self, url):
+        salt = 'hash'
+        result = bytes(f"{url}{salt}", encoding='utf-8')
+        result = hashlib.sha3_256(result).hexdigest()
+        return result
+
+    def check_url(self, url):
+        key = self.get_hash(url)
+        if key not in self.cache:
+            self.cache.update({key: {'url': url, 'update_datetime': datetime.now(), 'object': 'json_linc'}})
+
+    def show_cache(self):
+        print(f'Всего в кэше элементов: {len(self.cache)}')
+        for key, el in self.cache.items():
+            print(f'{key}: url:{el["url"]}; update_datetime:{el["update_datetime"]}')
+        print('*' * 30)
+
+    def clear_cache(self):
+        count = len(self.cache)
+        self.cache.clear()
+        print(f'Cache clear. Cleared {count} rows')
+
+    def get_len_cache(self):
+        print(f'Всего записей: {len(self.cache)}')
+
+    def set_max_cache(self, maximum=50):
+        self.maximum = maximum
+        if len(self.cache) == self.maximum:
+            to_dell_max_time = datetime.now()
+            key_to_dell = ''
+            print(to_dell_max_time)
+            for key, el in self.cache.items():
+                if to_dell_max_time > el["update_datetime"]:
+                    to_dell_max_time = el["update_datetime"]
+                    key_to_dell = key
+            self.cache.pop(key_to_dell)
+
+
+if __name__ == '__main__':
+
+    #  Добавляем единичную запись, проверяем число записей
+    UrlCache().check_url('https://gb.ru/lessons/145595/homework')
+    UrlCache().get_len_cache()
+
+    #  Смотрим что есть в кэше. Получаем таблицу
+    UrlCache().show_cache()
+
+    # создаем url  и инициализируем экземпляры класса. с небольшой паузой, чтобы было отличие во времени инициализации
+    for i in range(49):
+        time.sleep(0.1)
+        url = f'https://samara.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={i}&region=4966'
+        UrlCache().check_url(url)
+
+    #  Добавляем единичную  повторяющуюся запись, проверяем число записей
+    UrlCache().check_url('https://gb.ru/lessons/145595/homework')
+    UrlCache().get_len_cache()
+    #     Число записей не изменилось. Проверим состав записей
+    #  Смотрим что есть в кэше
+    UrlCache().show_cache()
+    # повторяющаяся запись не включена в кэш.
+
+    #  Добавляем единичную уникальную запись, проверяем число записей, уникальная запись добавляется в конец очереди.
+    UrlCache().check_url('https://ria.ru/')
+    UrlCache().get_len_cache()
+
+    #  Смотрим что есть в кэше. Контроль числа записей работает.
+    UrlCache().show_cache()
+
+    # Очистим кэш. Посмотрим число записей. Все записи удалены.
+    UrlCache().clear_cache()
+    UrlCache().get_len_cache()
+
+
+
