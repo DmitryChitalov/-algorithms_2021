c23f98196d239dd60824da700ce9d61a59a24c61
ASh1ne <77381910+ASh1ne@users.noreply.github.com>
2021-06-23 19:01:25 +0500

    Update task_5.py

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -6,46 +6,46 @@ class PlateClass:
     def __str__(self):
         return str(self.elems)
 
-    def inputplates(self, el):
-      if len(self.elems) == 0:
-         self.elems.append([])
-      if len(self.elems[-1]) < self.size:
-         self.elems[-1].append(el)
-      else:
-         self.elems.append([])
-         self.elems[-1].append(el)
+    def input_plates(self, el):
+        if len(self.elems) == 0:
+            self.elems.append([])
+        if len(self.elems[-1]) < self.size:
+            self.elems[-1].append(el)
+        else:
+            self.elems.append([])
+            self.elems[-1].append(el)
 
-    def removeplate(self):
-      newplates = self.elems[-1].pop()
-      if len(self.elems[-1])==0:
-          self.elems.pop()
-      return newplates
+    def remove_plate(self):
+        new_plates = self.elems[-1].pop()
+        if len(self.elems[-1]) == 0:
+            self.elems.pop()
+        return new_plates
 
-    def steckcount(self):
+    def stack_count(self):
         return len(self.elems)
-    
-    def platecount(self):
+
+    def plate_count(self):
         if len(self.elems) == 0:
             return 'Ups, you have no plates.'
-        result = self.size * (len(self.elems)-1) + len(self.elems[-1])
+        result = self.size * (len(self.elems) - 1) + len(self.elems[-1])
         return result
+
 
 plates = PlateClass(5)
 
-print (plates.platecount())
-plates.inputplates('Plate1')
-plates.inputplates('plate2')
-plates.inputplates('plate3')
-plates.inputplates('plate4')
-plates.inputplates('plate5') 
-plates.inputplates('plate6')
-plates.inputplates('plate7')
-print (plates.steckcount())
-print (plates.platecount())
+print(plates.plate_count())
+plates.input_plates('Plate1')
+plates.input_plates('plate2')
+plates.input_plates('plate3')
+plates.input_plates('plate4')
+plates.input_plates('plate5')
+plates.input_plates('plate6')
+plates.input_plates('plate7')
+print(plates.stack_count())
+print(plates.plate_count())
 print(plates)
-
-print (plates.removeplate())
-print (plates.removeplate())
-print (plates)
-print (plates.steckcount())
-print (plates.platecount())
+print(plates.remove_plate())
+print(plates.remove_plate())
+print(plates)
+print(plates.stack_count())
+print(plates.plate_count())
