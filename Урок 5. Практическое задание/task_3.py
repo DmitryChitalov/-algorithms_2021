65e5b65fed70e6a534321348e3627f9a74cfcb0c
Evgeny <ea.nikulin@yandex.ru>
2021-06-24 11:52:45 +0500

    //Homework_5 Complete

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -60,9 +60,59 @@ def func_3_2():
     return simple_lst2
 
 
+lst = [x for x in range(1, 100)]
+DEQUE_OBJ = deque(lst)
+
+
+def func_4_1(lst):
+    for x in lst:
+        print(x, end=' ')
+    print()
+
+
+def func_4_2(lst):
+    print(lst, end=' ')
+    return None
+
+
+def func_5_1(DEQUE_OBJ):
+    for x in DEQUE_OBJ:
+        print(x, end=' ')
+    print()
+    return None
+
+
+def func_5_2(DEQUE_OBJ):
+    print(DEQUE_OBJ, end=' ')
+    return None
+
+
+def func_6_1(DEQUE_OBJ):
+    while len(DEQUE_OBJ) > 0:
+        DEQUE_OBJ.popleft()
+
+
 print('append(deque)', timeit("func_1_1()", number=2500000, globals=globals()))
 print('append(list)', timeit("func_1_2()", number=2500000, globals=globals()))
+
 print('appendleft(deque)', timeit("func_2_1()", number=1000, globals=globals()))
 print('insert(list)', timeit("func_2_2()", number=1000, globals=globals()))
+
 print('pop(deque)', timeit("func_3_1()", number=1000, globals=globals()))
 print('pop(list)', timeit("func_3_2()", number=1000, globals=globals()))
+
+print('iter(list)', timeit("func_4_1(lst)", globals=globals(), number=10000))
+print('print(list)', timeit("func_4_2(lst)", globals=globals(), number=10000))
+
+print('iterr(deque)', timeit("func_5_1(DEQUE_OBJ)", globals=globals(), number=10000))
+print('print(deque)', timeit("func_5_2(DEQUE_OBJ)", globals=globals(), number=10000))
+
+print('popleft(deque)', timeit("func_6_1(DEQUE_OBJ)", globals=globals(), number=1000000))
+
+# Вывод: Заполнение элементами списка и очереди через метод append по времени практически одинаково,
+# Вставка с помощью insert у списка происходит медленнее, чем у очереди appendleft, я думаю, из-за того
+# что при вставке в простой список в начало элемента, все элементы сдвигаются, по логике, вставка в конец
+# у списка будет более быстрая, чем вставка в начало.
+# Далее я сделал итеррацию листа и дека, а так просто вывод.
+# Итеррация списка медленнее, чем у очереди, а вот вывод заначений у списка быстрее.
+# Извлечение значений у очереди намного быстрее, чем у списка с любой стороны.
