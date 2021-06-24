4ca69fca6be159ee2312b9e4b3a7663695bd4b32
Aleksey Shmykov <shmykov.job@gmail.com>
2021-06-24 18:32:42 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -22,7 +22,6 @@ my_string = input('Вводи строку ')
 counts = set()
 for i in range(len(my_string)):
     for j in range(1, len(my_string) + 1):
-        if i < j:
+        if i < j and ((j - i) < (len(my_string) ) ):
             counts.add(hash(my_string[i:j]))
-counts.remove(hash(my_string))
 print(f'{my_string} - {len(counts)} уникальных подстрок')
