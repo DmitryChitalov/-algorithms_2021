b5202b58e5764723a5efead67326cde09b5a19d8
Lina <linavi96@yandex.ru>
2021-06-24 18:34:20 +0300

    Done all tasks

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,65 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+"""
+Задание 1.
+Реализуйте кодирование строки "по Хаффману".
+У вас два пути:
+1) тема идет тяжело? тогда вы можете, опираясь на пример с урока, сделать свою!!! версию алгоритма
+Разрешается и приветствуется изменение имен переменных, выбор других коллекций, различные изменения
+и оптимизации.
+
+2) тема понятна? постарайтесь сделать свою реализацию.
+Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
+"""
+
+from collections import Counter
+
+
+class Node:
+    __slots__ = ('left_branch', 'right_branch', 'value')
+
+    def __init__(self, first_val, second_val):
+        self.left_branch = first_val
+        self.right_branch = second_val
+        self.value = (first_val.value if isinstance(first_val, Node) else first_val[1]) + (
+            second_val.value if isinstance(second_val, Node) else second_val[1])
+
+
+class HaffmanTree:
+    __slots__ = ('base_str', 'coded_symb', 'sort_tumple')
+
+    def __init__(self, user_str):
+        self.base_str = user_str
+        self.coded_symb = {}
+        self.code_str(self.haff_tree())
+
+    def haff_tree(self):
+        frequence = Counter(self.base_str)
+        self.sort_tumple = sorted(frequence.items(), key=lambda x: x[1])
+        del frequence
+        while len(self.sort_tumple) > 1:
+            node = Node(self.sort_tumple.pop(0), self.sort_tumple.pop(0))
+            for ind, item in enumerate(self.sort_tumple):
+                if node.value < item[1] or node.value == item[1]:
+                    self.sort_tumple.insert(ind, (node, node.value))
+                    break
+            else:
+                self.sort_tumple.append((node, node.value))
+        return self.sort_tumple[0][0]
+
+    def code_str(self, tree, code=''):
+        if not isinstance(tree, Node):
+            self.coded_symb[tree] = code
+
+        else:
+            self.code_str(tree=tree.left_branch[0], code=f'{code}0')
+            self.code_str(tree=tree.right_branch[0], code=f'{code}1')
+
+    def __str__(self):
+        return f'{" ".join([self.coded_symb[char] for char in self.base_str])}'
+
+
+user_str = "beep boop beer!"
+code_str = HaffmanTree(user_str)
+print(code_str)
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,7 +10,14 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
+class TreeRuleError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
 class BinaryTree:
+    __slots__ = ('root', 'left_child', 'right_child')
     def __init__(self, root_obj):
         # корень
         self.root = root_obj
@@ -21,13 +28,18 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
+        if new_node > self.root:
+            raise TreeRuleError(f'Потомок слева не может быть больше корня! {new_node} > {self.root}')
         # если у узла нет левого потомка
-        if self.left_child == None:
+        if self.left_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.left_child = BinaryTree(new_node)
         # если у узла есть левый потомок
         else:
+            if new_node < self.left_child:
+                raise TreeRuleError(f'Невозможно добавить слева узел с потомком больше, чем корень узла'
+                                    f' {new_node} > {self.left_child}')
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
@@ -36,13 +48,19 @@ class BinaryTree:
 
     # добавить правого потомка
     def insert_right(self, new_node):
+        if new_node < self.root:
+            raise TreeRuleError(f'Потомок справа должен быть больше корня! {new_node} < {self.root}')
         # если у узла нет правого потомка
-        if self.right_child == None:
+        if self.right_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
             self.right_child = BinaryTree(new_node)
         # если у узла есть правый потомок
         else:
+            if new_node < self.right_child:
+                raise TreeRuleError(f'Невозможно добавить справа узел с потомком меньше, чем корень узла'
+                                    f'{new_node} < {self.right_child}')
+
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
@@ -59,6 +77,14 @@ class BinaryTree:
 
     # метод установки корня
     def set_root_val(self, obj):
+        if self.right_child is not None:
+            if obj > self.right_child.get_root_val():
+                raise TreeRuleError(f'Корень не может быть больше потомка справа: '
+                                    f'{obj} > {self.right_child.get_root_val()}')
+        if self.left_child is not None:
+            if obj < self.left_child.get_root_val():
+                raise TreeRuleError(f'Корень не может быть меньше потомка слева: '
+                                    f'{obj} < {self.left_child.get_root_val()}')
         self.root = obj
 
     # метод доступа к корню
@@ -66,14 +92,17 @@ class BinaryTree:
         return self.root
 
 
-r = BinaryTree(8)
+
+r = BinaryTree(25)
 print(r.get_root_val())
 print(r.get_left_child())
-r.insert_left(40)
+r.insert_left(12)
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
-r.insert_right(12)
+r.insert_right(40)
 print(r.get_right_child())
 print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
+r.get_right_child().insert_right(50)
+r.get_right_child().insert_left(30)
+r.get_right_child().set_root_val(42)
 print(r.get_right_child().get_root_val())
