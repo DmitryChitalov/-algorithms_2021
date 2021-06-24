6c487ea615d57cee684a2887a4eb0f743fe5a0d0
Alexandr <ricko28@ya.ru>
2021-06-25 02:14:40 +0300

    Update task_4.py

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -27,45 +27,92 @@
 """
 
 
-def authorization(username, password, user_list):
-    result = {'success': False, 'message': ''}
+def get_users_dict():
+    return {
+        'Alexandr': {'password': 'pass', 'active': True},
+        'Maria': {'password': 'pass', 'active': True},
+        'Marina': {'password': 'pass', 'active': True},
+        'Ivan': {'password': 'pass', 'active': False},
+        'Nicola': {'password': 'pass', 'active': True},
+        'Nadya': {'password': 'pass', 'active': True},
+        'Olga': {'password': 'pass', 'active': False}
+    }
 
-    user_data = user_list.get(username)
 
-    if user_data is None:
-        result['message'] = "User doesn't exists"
-    elif not user_data.get('active'):
-        result['message'] = "Your account is not activated, please confirm the activation"
-    elif not password == user_data.get('password'):
-        result['message'] = "Invalid password"
+# Способ через условие Сложность: O(13) - константная
+def authorization_elif(username, password):
+    result = {'success': False, 'message': ''}   # O(2) - константная
+    user_dict = get_users_dict()   # O(1) - константная
+    user_data = user_dict.get(username)  # O(1) - константная
+
+    if user_data is None:  # O(1) - константная
+        result['message'] = "User doesn't exists"  # O(1) - константная
+    elif not user_data['active']:  # O(1) - константная
+        result['message'] = "Your account is not activated, please confirm the activation"  # O(1) - константная
+    elif password != user_data['password']:  # O(1) - константная
+        result['message'] = "Invalid password"  # O(1) - константная
     else:
-        result['success'] = True
-        result['message'] = f'Welcome, {username}!'
+        result['success'] = True  # O(1) - константная
+        result['message'] = f'Welcome, {username}!'  # O(1) - константная
 
-    return result
+    return result  # O(1) - константная
 
 
-users_dict = {
-    'Alexandr': {'password': 'pass', 'active': True},
-    'Maria': {'password': 'pass', 'active': True},
-    'Marina': {'password': 'pass', 'active': True},
-    'Ivan': {'password': 'pass', 'active': False},
-    'Nicola': {'password': 'pass', 'active': True},
-    'Nadya': {'password': 'pass', 'active': True},
-    'Olga': {'password': 'pass', 'active': False}
-}
+# Способ через попытку Сложность: O(12) - константная
+def authorization_try(username, password):
+    result = {'success': False, 'message': ''}  # O(2) - константная
+    user_dict = get_users_dict()  # O(1) - константная
+
+    try:  # O(1) - константная
+        user_data = user_dict[username]  # O(1) - константная
+
+        if not user_data['active']:  # O(1) - константная
+            result['message'] = "Your account is not activated, please confirm the activation"  # O(1) - константная
+        elif password != user_data['password']:  # O(1) - константная
+            result['message'] = "Invalid password"  # O(1) - константная
+        else:
+            result['success'] = True  # O(1) - константная
+            result['message'] = f'Welcome, {username}!'  # O(1) - константная
+
+    except KeyError:
+        result['message'] = "User doesn't exists"
+
+    return result  # O(1) - константная
+
+
+# Результат работы условного алгоритма
 print('====================')
-user_doesnt_exist = authorization('Ilya', '123hja123', users_dict)
+user_doesnt_exist = authorization_elif('Ilya', '123hja123')
 print(user_doesnt_exist.get('message'))
 
 print('====================')
-not_active_acc = authorization('Ivan', 'pass', users_dict)
+not_active_acc = authorization_elif('Ivan', 'pass')
 print(not_active_acc.get('message'))
 
 print('====================')
-wrong_password = authorization('Maria', 'pass11', users_dict)
+wrong_password = authorization_elif('Maria', 'pass11')
 print(wrong_password.get('message'))
 
 print('====================')
-success_result = authorization('Alexandr', 'pass', users_dict)
+success_result = authorization_elif('Alexandr', 'pass')
 print(success_result.get('message'))
+
+# Результат работы попыточного алгоритма
+print('********************')
+user_doesnt_exist = authorization_try('Ilya', '123hja123')
+print(user_doesnt_exist.get('message'))
+
+print('********************')
+not_active_acc = authorization_try('Ivan', 'pass')
+print(not_active_acc.get('message'))
+
+print('********************')
+wrong_password = authorization_try('Maria', 'pass11')
+print(wrong_password.get('message'))
+
+print('********************')
+success_result = authorization_try('Alexandr', 'pass')
+print(success_result.get('message'))
+
+# Вывод: Хотя оба варианта имеют константную сложность, считаю оптимальным второй вариант, и.к. там меньшее
+# количество константных операций
