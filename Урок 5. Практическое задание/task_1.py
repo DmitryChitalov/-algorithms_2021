5cf03a657348c44bd0fa19937fef70f371d145b0
Evgeny <ea.nikulin@yandex.ru>
2021-06-24 10:44:08 +0500

    //Homework_5 tasks 1, 2 complete

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,36 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+
+from collections import namedtuple
+
+count_of_comp = int(input("Введите количество предприятий: "))
+company = namedtuple("company", "company_name pf_1 pf_2 pf_3 pf_4")
+count = count_of_comp
+company_profit = {}
+max_profit = []
+min_profit = []
+avg_profit = 0
+
+while count_of_comp != 0:
+    name_comp = input("Введите название предприятия: ")
+    profits = input("через пробел введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ")
+    profits = profits.split(' ')
+    profits = list(map(int, profits))
+    company_tuple = company(company_name=name_comp, pf_1=profits[0], pf_2=profits[1], pf_3=profits[2], pf_4=profits[3])
+    count_of_comp -= 1
+    company_profit[company_tuple.company_name] = company_tuple.pf_1 + company_tuple.pf_2 + company_tuple.pf_3 + company_tuple.pf_1
+
+for profit in company_profit.values():
+    avg_profit += profit
+avg_profit = avg_profit / count
+
+for key, value in company_profit.items():
+    if value > avg_profit:
+        max_profit.append(key)
+    elif value < avg_profit:
+        min_profit.append(key)
+
+print(f"Средняя прибыль для {count} компаний - {avg_profit}")
+print(f"Компании с прибылью выше среднего: {' '.join(max_profit)}")
+print(f"Компании с прибылью ниже среднего: {' '.join(min_profit)}")
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,58 @@ deque – это обобщение стеков и очередей.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+
+from timeit import timeit
+from collections import deque
+
+simple_lst1 = []
+simple_lst2 = []
+deq_obj = deque(simple_lst1)
+
+
+def func_1_1():
+    deq_obj.append(1)
+    return deq_obj
+
+
+def func_1_2():
+    simple_lst2.append(1)
+    return simple_lst2
+
+
+simple_lst1 = []
+simple_lst2 = []
+deq_obj = deque(simple_lst1)
+
+
+def func_2_1():
+    deq_obj.appendleft(1)
+    return deq_obj
+
+
+def func_2_2():
+    simple_lst2.insert(0, 1)
+    return simple_lst2
+
+
+simple_lst1 = [[i * i for i in range(100000)]]
+simple_lst2 = [[i * i for i in range(100000)]]
+deq_obj = deque(simple_lst1)
+
+
+def func_3_1():
+    deq_obj.pop()
+    return deq_obj
+
+
+def func_3_2():
+    simple_lst2.pop()
+    return simple_lst2
+
+
+print('append(deque)', timeit("func_1_1()", number=2500000, globals=globals()))
+print('append(list)', timeit("func_1_2()", number=2500000, globals=globals()))
+print('appendleft(deque)', timeit("func_2_1()", number=1000, globals=globals()))
+print('insert(list)', timeit("func_2_2()", number=1000, globals=globals()))
+print('pop(deque)', timeit("func_3_1()", number=1000, globals=globals()))
+print('pop(list)', timeit("func_3_2()", number=1000, globals=globals()))
