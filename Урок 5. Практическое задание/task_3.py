89a0d7255d1a281d7e196b062fc9689b2a4165d7
cyber-welder <cyber-welder@ya.ru>
2021-06-25 18:35:00 +0800

    Выполнил задания 5.1 - 5.4

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,82 @@ deque – это обобщение стеков и очередей.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+
+from time import time
+from collections import deque
+
+def timer(func):
+    def wrapped(*args):
+        start = time()
+        response = func(*args)
+        print(f'Время выполнения функции {func.__name__}: {time() - start}')  #
+        return response
+    return wrapped
+
+
+@timer
+def fill_list_insert(l):             # заполнение списка методом insert
+    for i in range(100000):
+        l.insert(0, f'el{i}')
+
+
+@timer
+def fill_deque(d):                   # заполнение deque методом appendleft
+    for i in range(100000):
+        d.appendleft(f'el{i}')
+
+
+@timer
+def operations_list_extend(l):       # расширяем список сначала переворачивая его
+    l.reverse()
+    l.extend(range(1000000))
+    l.reverse()
+
+
+@timer
+def operations_deque_extendleft(d):  # расширяем deque методом extendleft
+    d.extendleft(range(1000000))
+
+
+@timer
+def operations_list_pop(l):          # очищаем список с начала в конец
+    for i in range(len(l)):
+        l.pop(0)
+
+
+@timer
+def operations_deque_popleft(d):     # очищаем deque методом popleft
+    for i in range(len(d)):
+        d.popleft()
+
+
+test_list = []
+test_deque = deque()
+
+fill_list_insert(test_list)             # заполнение списка методом insert
+fill_deque(test_deque)                  # заполнение deque методом appendleft
+
+operations_list_extend(test_list)       # расширяем список сначала переворачивая его
+operations_deque_extendleft(test_deque) # расширяем deque методом extendleft
+
+operations_list_pop(test_list)          # очищаем список с начала в конец
+operations_deque_popleft(test_deque)    # очищаем deque методом popleft
+
+
+"""
+вывод
+заполнение deque происходит в разы быстрее
+> Время выполнения функции fill_list_insert: 2.739314079284668
+> Время выполнения функции fill_deque: 0.018988370895385742
+
+а вот мой аналог функции extendleft для списка работает с такой же скоростью как и в deque
+> Время выполнения функции operations_list_extend: 0.0199887752532959
+> Время выполнения функции operations_deque_extendleft: 0.02198624610900879
+
+popleft работает не сопоставимо быстрей чем аналог в списках
+> Время выполнения функции operations_list_pop: 279.8265538215637
+> Время выполнения функции operations_deque_popleft: 0.06396102905273438
+
+форумы врут
+"""
+
