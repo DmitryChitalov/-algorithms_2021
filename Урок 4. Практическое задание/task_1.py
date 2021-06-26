e07be175afeae993094c65eddef8a8256bb057d0
Andrey Belyaev <ianbel263@gmail.com>
2021-06-26 08:52:22 +0300

    ДЗ_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,6 +13,9 @@
 Без аналитики задание не принимается
 """
 
+from timeit import timeit
+from random import randint
+
 
 def func_1(nums):
     new_arr = []
@@ -20,3 +23,19 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+"""
+используем List Comprehension, так как это встроенный механизм 
+построения списков, и он будет работать быстрее
+"""
+
+
+def func_2(nums):
+    return [i for i, num in enumerate(nums) if num % 2 == 0]
+
+
+numbers = [randint(0, 100) for i in range(1000)]
+
+print(timeit('func_1(numbers)', globals=globals(), number=10000))
+print(timeit('func_2(numbers)', globals=globals(), number=10000))
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -53,6 +53,7 @@ def memoize(f):
         else:
             cache[args] = f(*args)
             return cache[args]
+
     return decorate
 
 
@@ -79,3 +80,8 @@ print(
         'recursive_reverse_mem(num_10000)',
         setup='from __main__ import recursive_reverse_mem, num_10000',
         number=10000))
+
+"""
+Мемоизация здесь не нужна, так как рекурсивная функция не будет использовать кэш, 
+поскольку здесь нет повторяющихся вычислений
+"""
