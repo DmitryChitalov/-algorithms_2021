48bec6ac1b745deed77671b2cb6e00f925679cf5
Evgeny <ea.nikulin@yandex.ru>
2021-06-28 18:01:08 +0500

    //Homework_6 task 1 complete

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -21,3 +21,262 @@
 Попытайтесь дополнительно свой декоратор используя ф-цию memory_usage из memory_profiler
 С одновременным замером времени (timeit.default_timer())!
 """
+from pympler import asizeof
+from sys import getsizeof
+from memory_profiler import profile
+import gc
+from sys import getrefcount
+import numpy
+import json
+from pympler import asizeof
+import random
+
+# # Пример № 1 (Использование Сериализации)
+# # Исходный код
+# my_dict = {}
+# my_base = []
+# with open('task5_6.txt', encoding='utf-8') as out_base_object:
+#     for word in out_base_object:
+#         word = word.split(':')
+#         my_base.append(word[0])
+#         my_dict[word[0]] = None
+#
+#     out_base_object.seek(0)
+#
+#     x = 0
+#     for word in out_base_object:
+#         digit = [int(i) for i in word.split(' ') if i.isdigit()]
+#         digit = sum(digit)
+#         my_dict[my_base[x]] = digit
+#         x += 1
+#
+#     print(my_dict)
+#     print(asizeof.asizeof(my_dict))
+#
+#     # Оптимизированный код
+#     my_dict = {}
+#     my_base = []
+#     with open('task5_6.txt', encoding='utf-8') as out_base_object:
+#         for word in out_base_object:
+#             word = word.split(':')
+#             my_base.append(word[0])
+#             my_dict[word[0]] = None
+#
+#         out_base_object.seek(0)
+#
+#         x = 0
+#         for word in out_base_object:
+#             digit = [int(i) for i in word.split(' ') if i.isdigit()]
+#             digit = sum(digit)
+#             my_dict[my_base[x]] = digit
+#             x += 1
+#
+#         my_dict = json.dumps(my_dict)  # Преобразуем словарь в строку (json)
+#         print(asizeof.asizeof(my_dict))
+#         my_dict = json.loads(my_dict)  # При необходимости возращаем исходное состояние.
+#         print(my_dict)
+#
+#
+# # Вывод: В итоге мы сократили использование памяти с 608 до 248:
+# # Словарь это Хеш-таблица, которая занимает большой объем памяти, используя json я "сжимаю" словарь,
+# # преобразовывая его в строку, а мы знаем, что строки занимают меньше памяти, (тип данных json в Python не существует)
+# # Так же потом можно восстановить исходный словарь методом json.loads.
+#
+#
+# # Пример № 2 (Использование слотов в ООП)
+# # Исходный код
+# class Road:
+#     def __init__(self, _length, _width):
+#         self._length = _length
+#         self._width = _width
+#
+#
+# class MassRoad(Road):
+#     def __init__(self, _length, _width, weight, height):
+#         super().__init__(_length, _width)
+#         self.weight = weight
+#         self.height = height
+#
+#     def MassCount(self):
+#         return self._length * self._width * self.weight * self.height / 1000
+#
+#
+# r = MassRoad(200000, 50000000, 250000, 50000)
+# print(f'Для покрытия всей дороги с толщиной {r.height} см. неободимо {int(r.MassCount())} т. асфальта')
+# print(r.__dict__)
+# print(asizeof.asizeof(r))
+#
+#
+# # Оптимизированный код
+# class Road:
+#     __slots__ = ['_length', '_width']  # Сохраниение атрибутов объекта класса в слоте (списке)
+#
+#     def __init__(self, _length, _width):
+#         self._length = _length
+#         self._width = _width
+#
+#
+# class MassRoad(Road):
+#     def __init__(self, _length, _width, weight, height):
+#         super().__init__(_length, _width)
+#         self.weight = weight
+#         self.height = height
+#
+#     def MassCount(self):
+#         return self._length * self._width * self.weight * self.height / 1000
+#
+#
+# r = MassRoad(200000, 50000000, 250000, 50000)
+# print(f'Для покрытия всей дороги с толщиной {r.height} см. неободимо {int(r.MassCount())} т. асфальта')
+# print(r.__slots__)
+# print(asizeof.asizeof(r))  # показывает размер используемой памяти объекта.
+#
+# # Вывод: В итоге мы сократили использование памяти с 504 до 408:
+# # Как известно, в ООП стандрартно для хранения атрибутов объекта используется словарь, под него
+# # выделяется моного памяти, оптимизацией служит использование слотов, то есть другой контейнер для хранения в памяти
+# # - список, как известно, он будет занимать меньше памяти, но есть минус, что пропадает возможность добавлять атрибуты
+# # динамически, придется дописывать в слотах их в ручную.
+#
+#
+# # Пример № 3 (Использование NumPy)
+# # Исходный код
+# nums = [i * i for i in range(100)]
+#
+#
+# def func_1(nums):
+#     new_arr = []
+#     for i in range(len(nums)):
+#         if nums[i] % 2 == 0:
+#             new_arr.append(i)
+#     print(asizeof.asizeof(new_arr))
+#     return new_arr
+# print(func_1(nums))
+#
+#
+# # Оптимизированный код
+# def func_1(nums):
+#     new_arr = []
+#     for i in range(len(nums)):
+#         if nums[i] % 2 == 0:
+#             new_arr.append(i)
+#     new_arr = numpy.array(new_arr) # преобразуем список в объект класса 'numpy.ndarray'
+#     print(asizeof.asizeof(new_arr))
+#     return new_arr
+# print(func_1(nums))
+
+
+# Вывод: В итоге мы сократили использование памяти с 2112 до 320:
+# Библиотека NumPy очень эффективна в использовании памяти, я передал список (массив) фунции array библиотеки NumPy,
+# для сжятия, в итоге получаем объект класса 'numpy.ndarray'
+
+
+# Пример № 4 (Использование Функции MAP )
+# Исходный код
+# nums = [random.randint(100, 999) for i in range(100)]
+# #
+# def recursive_reverse(number):
+#     if number != 0:
+#         return str(number % 10) + str(recursive_reverse(number // 10))
+#     else:
+#         return ('')
+#
+# nums = list(map(recursive_reverse, nums))
+# print(nums)
+
+
+nums = [random.randint(100, 999) for i in range(100)]
+#
+def recursive_reverse(list, new_list = []):
+    for i in list:
+        new_list.append(i // 2)
+    return new_list
+
+print(recursive_reverse(nums))
+
+
+def recursive_reverse(list, new_list = []):
+    for i in list:
+        new_list.append(i // 2)
+    return new_list
+
+print(recursive_reverse(nums))
+
+new_list = map(recursive_reverse, nums)
+#
+# nums = [random.randint(100, 999) for i in range(100)]
+
+# def recursive_reverse(number, new_array = []):
+#     for i in number:
+#         if i == 0:
+#             return ''
+#         new_array.append(f'{str(i % 10)}{recursive_reverse(i // 10)}')
+#         return new_array
+# # {str(i % 10)}{recursive_reverse(i // 10)}
+#
+# print(recursive_reverse(nums))
+
+# nums = list(map(recursive_reverse, nums))
+# print(nums)
+#
+# def reversed2(variable):
+#     res=[]
+#     for i in range(len(variable)-1,-1,-1):
+#         res.append(variable[i])
+#     return res
+#
+# print(reversed2('324'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# gen_dict = {i: i * 2 for i in range(100000)}
+# dumped_dict = json.dumps(gen_dict)
+#
+# print(type(gen_dict))
+# print(type(dumped_dict))
+#
+# print('Размер list: ', asizeof.asizeof(gen_dict))
+# print('Размер json: ', asizeof.asizeof(dumped_dict))
+
+
+
+
+
+
+
+
+
+
+
+
+# @profile
+# def new_func():
+#     x = list(range(100000))
+#     y = '324dfgdfgdrgdfg34'
+#     new_lst = [1, 2, 3]
+#     new_lst.append(new_lst)
+#     # del x
+#     return new_lst
+
+# print(new_func())
+
+# print(getrefcount(x))
+# obj = gc.collect()
+# print("Количество скрытых объектов, собранных GC:", obj)
