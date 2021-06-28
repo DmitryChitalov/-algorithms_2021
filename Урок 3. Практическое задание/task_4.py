a93697b5a583878b93b32deb57aa49bed6ec8d1a
Sebkd <sebkd@mail.ru>
2021-06-28 11:13:15 +0700

    final lesson-3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -10,34 +10,41 @@
 Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
 import hashlib
-import json
+
+'''Реализация через ОПП'''
 
 
+class Hash_Table:
+    def __init__(self):
+        self.hash_table_from_class = {}
+        self.url_str = ''
+        self.salt = ''
+
+    def input_url(self, url):
+        if len (url) < 4: # Не запоминается url если нет возможности реализовать соль
+            return False
+        self.url_str = url
+        self.salt = str (self.url_str[:3])  # Реализация соли через сам url
+        return True
+
+    def chek_url(self):
+        url_str_key = hashlib.sha256 (self.salt.encode () + self.url_str.encode ()).hexdigest ()
+        if url_str_key in self.hash_table_from_class.keys ():
+            return True
+        else:
+            self.hash_table_from_class.update ({url_str_key: self.url_str})
+            return False
 
 
 if __name__ == '__main__':
+    hash_table_obj = Hash_Table ()
     while True:
-        url_str= str(input('Введите адрес url, но не менее 4 букв или цифр ') + '\n')
-        if len(url_str) < 4:
-            print(f'Введено не то что нужно, если вы понимаете о чем это я')
-            break
-        salt = str(url_str[-4])
-        print(salt.encode () + url_str.encode (), end = '\n')
-        url_str_original = hashlib.sha256 (salt.encode () + url_str.encode ()).hexdigest ()
-
-        with open ('archive.txt', 'a') as file_one:
-            file_one.writelines (url_str_original)
-        with open('archive.txt', 'r') as file_one:
-            for file_inn in file_one:
-                print(file_inn)
-                if url_str_original != file_inn:
-                    print('уау')
-                    print(file_one.read())
-                else:
-                    print('ауа')
-
-
-
-
-
-
+        if hash_table_obj.input_url (str (input ('Введите адрес url, но не менее 4 букв или цифр '))):
+            if hash_table_obj.chek_url ():
+                print (f'Такой url уже есть')
+                continue
+            else:
+                print (f'Текущая HASH-таблица {hash_table_obj.hash_table_from_class}')
+                continue
+        print (f'Введено не то что нужно')
+        continue
