fb240a8cb7c164e23352d77d8686bd0dabbd1ce2
Necris45 <necris01@mail.ru>
2021-06-30 03:07:58 +0500

    самое легкое задание - 7, по сути убираем пробелы и все обрабатывается корректно. Самым сложным показалось 5, дольше всего отлавливал некоторые ошибки, связанные с индексацией и в итоге пришел к решению ввести параметр, который и будет подставляться вместо одного из индексов (индекс текущей стопки)

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -19,3 +19,33 @@
 Постарайтесь не использовать ф-ции min() и sort() и другие ф-ции!
 Подход должен быть максимально алгоритмическим.
 """
+
+
+def min_1(lst_obj):
+    """
+    Сложность: O(N^2)
+    """
+    min_number = lst_obj[0]  # O(1)
+    min_in_iter = lst_obj[0]
+    for el_1 in lst_obj:  # O(N)
+        for el_2 in lst_obj:  # O(N)
+            if el_1 < el_2:  # O(1)
+                min_in_iter = el_1  # O(1)
+        if min_in_iter < min_number:  # O(1)
+            min_number = min_in_iter  # O(1)
+    return min_number  # O(1)
+
+
+def min_2(lst_obj):
+    """
+    Сложность: O(N)
+    """
+    min_number = lst_obj[0]  # O(1)
+    for el in lst_obj:  # O(N)
+        if el < min_number:  # O(1)
+            min_number = el  # O(1)
+    return min_number  # O(1)
+
+
+print(min_1([1, 10, 15, 100, 20]))
+print(min_2([1, 10, 15, 100, 20]))
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,77 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class PlateStacks:
+    def __init__(self):
+        self.plates = [[]]
+        self.max_plate = 5
+        self.current_stack = 0
+
+    def is_empty(self):
+        return self.plates == [[]]
+
+    def put_in_stack(self, el):
+        if len(self.plates[self.current_stack]) == self.max_plate:
+            self.plates.append([])
+            self.current_stack += 1
+            self.plates[self.current_stack].append(el)
+        else:
+            self.plates[self.current_stack].append(el)
+
+    def get_from_stack(self):
+        taken_plates = self.plates[self.current_stack].pop()
+        if len(self.plates[self.current_stack]) == 0:
+            self.plates.pop()
+            self.current_stack -= 1
+        return taken_plates
+
+    def placed_last(self):
+        return self.plates[self.current_stack][len(self.plates[self.current_stack]) - 1]
+
+    def stack_size(self):
+        return len(self.plates[self.current_stack])
+
+    def plates_total(self):
+        plates = self.max_plate * (len(self.plates) - 1) + len(self.plates[self.current_stack])
+        return plates
+
+
+if __name__ == '__main__':
+    stacks_of_plate = PlateStacks()
+
+    print(stacks_of_plate.is_empty())  # -> весь стек пуст
+
+    # наполняем стопки, при превышении лимита новая стопка добавится самостоятельно
+    stacks_of_plate.put_in_stack(1)
+    stacks_of_plate.put_in_stack(2)
+    stacks_of_plate.put_in_stack(3)
+    stacks_of_plate.put_in_stack(4)
+    stacks_of_plate.put_in_stack(5)
+    stacks_of_plate.put_in_stack(6)
+    stacks_of_plate.put_in_stack(7)
+    stacks_of_plate.put_in_stack(8)
+
+    # получаем значение первого элемента с вершины стека, но не удаляем сам элемент из стопки
+    print(stacks_of_plate.placed_last())  # -> 8
+
+    # узнаем размер текущей стопки
+    print(stacks_of_plate.stack_size())  # -> 3
+
+    print(stacks_of_plate.is_empty())  # -> в стопках уже есть тарелки
+
+    # кладем еще один элемент в стек
+    stacks_of_plate.put_in_stack(9)
+
+    # убираем элемент с вершины стека и возвращаем его значение
+    print(stacks_of_plate.get_from_stack())  # -> 9
+
+    # снова убираем элемент с вершины стека и возвращаем его значение
+    print(stacks_of_plate.get_from_stack())  # -> 8
+
+    # вновь узнаем размер текущего стека
+    print(stacks_of_plate.stack_size())  # -> 2
+
+    # узнаем общее кол-во тарелок во всех стопках
+    print(stacks_of_plate.plates_total())   # -> 7
