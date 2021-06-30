49004a70fb5ecc3924e88fc0f8940e22a76faddc
Aleksey Shmykov <shmykov.job@gmail.com>
2021-06-30 20:53:37 +0300

    task 1 completed

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -22,6 +22,49 @@
 Предприятия, с прибылью выше среднего значения: Фирма_1
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
-"""
+Класс collections.namedtuple()"""
+from collections import namedtuple
 
 
+def companies_average(obj):
+    sum_all_average = sum(company.average for company in obj)
+    return sum_all_average / len(obj)
+
+
+def companies_up(obj, average):
+    up_list = []
+    for company in obj:
+        if company.average > average:
+            up_list.append(company.name)
+    return up_list
+
+
+def companies_down(obj, average):
+    down_list = []
+    for company in obj:
+        if company.average < average:
+            down_list.append(company.name)
+    return down_list
+
+
+RES = namedtuple('Company_profit', 'name profit_1 profit_2 profit_3 profit_4 average')
+companies_number = int(input('Введите количество предприятий для расчета прибыли: '))
+companies_list = []
+for i in range(companies_number):
+    temp_name = input('Введите название предприятия: ')
+    temp_profit = [int(x) for x in input('через пробел введите прибыль данного предприятия\
+за каждый квартал(Всего 4 квартала): ').split()]
+    companies_list.append(RES(
+        name=temp_name,
+        profit_1=temp_profit[0],
+        profit_2=temp_profit[1],
+        profit_3=temp_profit[2],
+        profit_4=temp_profit[3],
+        average=sum(temp_profit) / len(temp_profit)
+    ))
+    print(f'{companies_list[i].name, companies_list[i].average}')
+
+companies__average = companies_average(companies_list)
+print(f'\nСредняя годовая прибыль всех предприятий: {companies__average}')
+print(f'Предприятия, с прибылью выше среднего значения: {companies_up(companies_list, companies__average)}')
+print(f'Предприятия, с прибылью ниже среднего значения: {companies_down(companies_list, companies__average)}')
