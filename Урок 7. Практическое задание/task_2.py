d82ab8cee32a5600bf06b32bbda7e9202f6a6ae7
Evgeny <ea.nikulin@yandex.ru>
2021-07-01 09:09:22 +0500

    //Homework_7 task 1, 2 complete

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,89 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import timeit
+import random
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    # Длина списков часто используется, поэтому создадим переменные для удобства
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            # Сравниваем первые элементы в начале каждого списка
+            # Если первый элемент левого подсписка меньше, добавляем его
+            # в отсортированный массив
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            # Если первый элемент правого подсписка меньше, добавляем его
+            # в отсортированный массив
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        # Если достигнут конец левого списка, элементы правого списка
+        # добавляем в конец результирующего списка
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        # Если достигнут конец правого списка, элементы левого списка
+        # добавляем в отсортированный массив
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+
+def merge_sort(nums):
+    # Возвращаем список, если он состоит из одного элемента
+    if len(nums) <= 1:
+        return nums
+
+    # Для того чтобы найти середину списка, используем деление без остатка
+    # Индексы должны быть integer
+    mid = len(nums) // 2
+
+    # Сортируем и объединяем подсписки
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+
+    # Объединяем отсортированные списки в результирующий
+    return merge(left_list, right_list)
+
+
+# замеры 10
+orig_list = [random.uniform(1, 50) for _ in range(10)]
+print('Слияние 10',
+      timeit.timeit(
+          "merge_sort(orig_list[:])",
+          globals=globals(),
+          number=1000))
+print('Исходный список', orig_list)
+print('Отсортированный список', merge_sort(orig_list))
+
+# замеры 100
+orig_list = [random.uniform(1, 50) for _ in range(100)]
+print('Слияние 10',
+      timeit.timeit(
+          "merge_sort(orig_list[:])",
+          globals=globals(),
+          number=1000))
+print('Исходный список', orig_list)
+print('Отсортированный список', merge_sort(orig_list))
+
+# замеры 1000
+orig_list = [random.uniform(1, 50) for _ in range(1000)]
+print('Слияние 10',
+      timeit.timeit(
+          "merge_sort(orig_list[:])",
+          globals=globals(),
+          number=1000))
+print('Исходный список', orig_list)
+print('Отсортированный список', merge_sort(orig_list))
+
+# Вывод:
+# С увеличением длины массива, увеличивается время сортировки, имеет O (n Log n) сложность.
