9c7f156d94fc393df0bfb1d39372713b8e56653a
WhiIend <76176054+WhiIend@users.noreply.github.com>
2021-07-02 00:11:32 +0300

    Update task_3.py

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,39 @@
 Придумать как это решить!
 Есть очень простое решение
 """
+
+from memory_profiler import memory_usage
+
+
+# скрипт
+def shifter(num, result):
+    if num == 0:
+        return f'Перевернутое число: {result}'
+    remains = num % 10
+    return shifter(num // 10, result + str(remains))
+
+###########################################################################
+
+def mem_usage_decorator(some_func):
+    """Вычисляет память, выделяемую под выполнение декорируемой функции"""
+    def wrapper(*args, **kwargs):
+        result = some_func(*args, **kwargs)
+        my_list.append(str(memory_usage()))  # добавляем в список
+                                        # значение задействованной памяти
+        return result
+    return wrapper
+
+@mem_usage_decorator
+def shifter(num, result):
+    if num == 0:
+        return f'Перевернутое число: {result}'
+    remains = num % 10
+    return shifter(num // 10, result + str(remains))
+
+
+inp_number = int(input('Введите целое положительное число: '))
+my_list = []
+
+print(f'Задействованная память до запуска рекурсивной функции: '
+      f'{str(memory_usage())} MB')
+print(f'Перевёрнутое число: {shifter(inp_number, "")}')
