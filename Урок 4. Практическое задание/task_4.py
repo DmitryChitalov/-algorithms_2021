8dbf7dc98d4a1da2adc3c9c445b43b562a8eb45b
a2kat <mousedrw@ya.ru>
2021-07-01 12:56:39 +0300

    les4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,6 +11,7 @@
 
 Без аналитики задание считается не принятым!
 """
+from timeit import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -39,5 +40,29 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    sorted_array = sorted(array, key=lambda x: array.count(x), reverse=True)
+    return f'Чаще всего встречается число {sorted_array[0]}, ' \
+           f'оно появилось в массиве {array.count(sorted_array[0])} раз(а)'
+
+
+def func_4():
+    elem = max(array, key=lambda x: array.count(x))
+    return f'Чаще всего встречается число {elem}, ' \
+           f'оно появилось в массиве {array.count(elem)} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+
+print(timeit('func_1()', globals=globals()))
+print(timeit('func_2()', globals=globals()))
+print(timeit('func_3()', globals=globals()))
+print(timeit('func_4()', globals=globals()))
+
+""" В задании говорится найти более быстрый, но я увы не сумел, поэтому добавил 2 способа, чуть медлее
+    Если я правильно понял, то у первой фунеции линейная сложность, а у встроеных сорт и макс - линейно-логарифмическая
+    пробовал с большими массивами, протестировать время, посмотреть как изменится, но не ощутил разницы
+"""
