1a5af1137959cbcd01167413138e4b19c885f3dc
cyber-welder <cyber-welder@ya.ru>
2021-07-02 19:06:15 +0800

    Выполнил задания 7.1 - 7.3

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,76 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+from timeit import timeit
+from random import random
+from task_1 import sort_base, sort_optimaze
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+
+    return merge(left, right, arr.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+if __name__ == '__main__':
+
+    n = int(input('Введите число элементов: '))
+    test_list = [random() * 50 for _ in range(n)]
+
+    # print('Исходный массив', test_list, sep='\n')
+    # print('Отсортированный массив', merge_sort(test_list), sep='\n')
+
+    print('Замер для сортировки слиянием', timeit("merge_sort(test_list[:])", globals=globals(), number=1000))
+
+    print('Замер для сортировки "методом пузырька"', timeit("sort_base(test_list[:])", globals=globals(), number=1000))
+    print('Замер для сортировки оптимизированным "методом пузырька"', timeit("sort_optimaze(test_list[:])", globals=globals(), number=1000))
+
+"""
+вывод
+    Введите число элементов: 10
+    Замер для сортировки слиянием 0.01708540000000003
+    Замер для сортировки "методом пузырька" 0.009314899999999682
+    Замер для сортировки оптимизированным "методом пузырька" 0.009726000000000123
+    
+    Введите число элементов: 100
+    Замер для сортировки слиянием 0.25182569999999993
+    Замер для сортировки "методом пузырька" 0.7695783
+    Замер для сортировки оптимизированным "методом пузырька" 0.7606875
+    
+    Введите число элементов: 1000
+    Замер для сортировки слиянием 4.3110462
+    Замер для сортировки "методом пузырька" 87.1112053
+    Замер для сортировки оптимизированным "методом пузырька" 91.3456357
+
+Реализовал метод сортировки слиянием через функцию merge_sort, которая рекурсивно делит массив и при объединении
+вызывает функцию сортировки merge. 
+Как видно, из примеров вывода, метод сортировки слиянием выигрывает в скорости у метода сортировки "пузырьком" 
+при увеличении размера массива 
+"""
+
+
+
+
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,77 @@ for i in
 
 
 """
+from timeit import timeit
+from statistics import median
+from random import randint
+
+
+def sort_gnom(arr):             # метод сортировки Гномья
+    n = 1
+    k = 2
+    while n < len(arr):
+        if arr[n - 1] <= arr[n]:
+            n = k
+            k += 1
+        else:
+            arr[n - 1], arr[n] = arr[n], arr[n - 1]
+            n -= 1
+            if n == 0:
+                n = k
+                k += 1
+    return arr
+
+
+def median_left_right(arr):       # метод через построение левой и правой части
+    for elem in arr:
+        left = [elem2 for elem2 in arr if elem2 < elem]
+        right = [elem2 for elem2 in arr if elem2 > elem]
+        if len(left) == len(right) or abs(len(left) - len(right)) < arr.count(elem):
+            return elem
+
+
+def median_remove_max(arr):       # метод через удаление максимумов
+    for _ in range(len(arr) // 2):
+        arr.remove(max(arr))
+    return max(arr)
+
+
+if __name__ == '__main__':
+
+    m = int(input('Введите целое число: '))
+    test_arr = [randint(0, 100) for _ in range(2 * m + 1)]
+
+    print('-' * 50)
+    print('Медиана методом сортировки (Гномья):', sort_gnom(test_arr[:])[m])
+    print('\tСкорость поиска:', timeit("sort_gnom(test_arr[:])[m]", globals=globals(), number=1000))
+    print('-' * 50)
+    print('Медиана методом построения левой и правой частей:', median_left_right(test_arr[:]))
+    print('\tСкорость поиска:', timeit("median_left_right(test_arr[:])", globals=globals(), number=1000))
+    print('-' * 50)
+    print('Медиана методом удаления максимумов:', median_remove_max(test_arr[:]))
+    print('\tСкорость поиска:', timeit("median_remove_max(test_arr[:])", globals=globals(), number=1000))
+    print('-' * 50)
+    print('Медиана встроенной функцией:', median_remove_max(test_arr[:]))
+    print('\tСкорость поиска:', timeit("median(test_arr[:])", globals=globals(), number=1000))
+
+"""
+вывод
+    Введите целое число: 100
+    --------------------------------------------------
+    Медиана методом сортировки (Гномья): 49
+        Скорость поиска: 3.4506821999999997
+    --------------------------------------------------
+    Медиана методом построения левой и правой частей: 49
+        Скорость поиска: 2.4412177
+    --------------------------------------------------
+    Медиана методом удаления максимумов: 49
+        Скорость поиска: 0.28119020000000017
+    --------------------------------------------------
+    Медиана встроенной функцией: 49
+        Скорость поиска: 0.008138800000001112
+"""
+
+# встроенная функция победила (кто бы сомневался)
+# из НЕ встроенных методов самым быстрым оказался - метод удаления максимумов
+# а самым медленным, ожидаемо, метод с сортировкой
+
