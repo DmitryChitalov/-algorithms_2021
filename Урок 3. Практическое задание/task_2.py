bfd0cffc736571696a0e60fb979583faa99e38cc
Vyacheslav <lvv_1@list.ru>
2021-07-03 16:42:46 +0300

    Ready

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -20,3 +20,42 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+
+import hashlib
+from uuid import uuid4
+
+
+def create_hash(pass_hash, salt1, first=True):
+    pass_hash = hashlib.sha256(salt1.encode() + pass_hash.encode()).hexdigest()
+    if first:
+        print(f"Пароль задан, хеш пароля : {pass_hash}")
+        write_password(pass_hash)
+    else:
+        return pass_hash
+
+
+def write_password(pass_temp2):
+    with open('pass_hash.txt', 'w') as f:
+        f.write(pass_temp2)
+    print("Хеш пароля успешно записан в файл.")
+
+
+def open_password():
+    with open('pass_hash.txt', 'r') as f:
+        open_password_temp = f.read()
+    return open_password_temp
+
+
+print("Добро пожаловать в систему юзер.")
+password = input("Введите пароль: ")
+salt = uuid4().hex
+create_hash(password, salt)
+
+print("Введите пароль еще раз для проверки.")
+second_password = input("Подтвердите пароль: ")
+second_password = create_hash(second_password, salt, first=False)
+
+if open_password() == second_password:
+    print("Вы ввели правильный пароль. Добро пожаловать.")
+else:
+    print("Введённые пароли не совпадают. Пройдите процедуру заново.")
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,23 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+from uuid import uuid4
+import hashlib
+
+salt = uuid4().hex
+web_hash = {}
+
+
+def url_website(url):
+    if not web_hash.get(url):
+        web_hash[url] = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+        print(f"Страница {url} успешно добавлена. Хеш : {web_hash[url]}")
+    else:
+        print(f"Такая страница в кеше уже присутствует {url}")
+
+
+url_website('https://ru.tradingview.com/')
+url_website('https://wpcalc.com/')
+url_website('https://www.citilink.ru/')
+url_website('https://wpcalc.com/')
