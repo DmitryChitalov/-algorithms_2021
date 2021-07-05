128ba105442cc57cc7591483fdd4c843f471c2b9
poker4grig <poker4grig@yandex.ru>
2021-07-05 20:14:54 +0300

    done 1,2,4

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,32 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+import hashlib
+
+cash = dict()
+SALT = 'salt'.encode('UTF-8')
+
+
+def fill_url_cash(url):
+    hash_obj = hashlib.sha256(str(url).encode('UTF-8') + SALT)
+    hash_url = hash_obj.hexdigest()
+    return cash.setdefault(url, hash_url)
+
+
+def check_url_cash(url):
+    hash_obj = hashlib.sha256(str(url).encode('UTF-8') + SALT)
+    hash_url = hash_obj.hexdigest()
+    if url not in cash:
+        return cash.setdefault(url, hash_url)
+
+
+print(fill_url_cash('geekbrains.ru'))
+print(fill_url_cash('yandex.ru'))
+print(fill_url_cash('mail.ru'))
+print(fill_url_cash('google.com'))
+print(fill_url_cash('kinopoisk.ru'))
+print(cash)
+print(check_url_cash('geekbrains.ru'))
+print(check_url_cash('rambler.ru'))
+print(cash)
+
