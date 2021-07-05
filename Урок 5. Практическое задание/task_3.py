cc548504852260e0323e2af21995f5d2bc81fe7e
alexabolot1 <alexabolot1@gmail.com>
2021-07-05 16:27:20 +0300

    complete homework for lession5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,60 @@ deque – это обобщение стеков и очередей.
 
 Не забудьте, что сравнивать, например, можно операцию appendleft дека и insert списка и т.д.
 """
+from collections import deque
+from time import time
+
+my_list = []
+my_deque = deque()
+
+
+def timer(func):
+    def wrapped(*args):
+        start = time()
+        response = func(*args)
+        print(f'Время выполнения функции {func.__name__}: {time() - start}')  #
+        return response
+    return wrapped
+
+
+@timer
+def list_insert(lst):
+    for i in range(100000):
+        lst.insert(0, i)
+
+
+@timer
+def deque_appendleft(dq):
+    for i in range(100000):
+        dq.appendleft(i)
+
+
+@timer
+def list_pop(lst):
+    for i in range(len(lst)):
+        lst.pop(0)
+
+
+@timer
+def deque_popleft(dq):
+    for i in range(len(dq)):
+        dq.popleft()
+
+
+list_insert(my_list)
+deque_appendleft(my_deque)
+list_pop(my_list)
+deque_popleft(my_deque)
+"""
+Вывод:
+Время выполнения функции list_insert: 2.6165153980255127
+Время выполнения функции deque_appendleft: 0.005004405975341797
+Время выполнения функции list_pop: 0.9896032810211182
+Время выполнения функции deque_popleft: 0.00502467155456543
+
+Добавление элемента в начало работает гораздо быстрее в deque, чем в обычном списке. Также и удаление
+элемента из начала обычного списка занимает в тысячи раз больше времени, чем удаление элемента из начала списка
+в deque
+
+Следовательно, deque очень эффективно и быстро справляется с задачами добавления и извлечения объектов.
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,57 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from collections import OrderedDict
+from time import time
+from timeit import timeit
+
+my_dict = {}
+my_ordereddict = OrderedDict()
+
+
+def timer(func):
+    def wrapped(*args):
+        start = time()
+        response = func(*args)
+        print(f'Время выполнения функции {func.__name__}: {time() - start}')  #
+        return response
+    return wrapped
+
+
+@timer
+def filling_dict(dct):
+    for i in (range(1000000)):
+        dct[i] = i + 1
+
+
+@timer
+def filling_ordereddict(orddct):
+    for i in (range(1000000)):
+        orddct[i] = i + 1
+
+
+def get_el_dict(dct):
+    return dct[1]
+
+
+def get_el_ordereddict(orddct):
+    return orddct[1]
+
+
+filling_dict(my_dict)
+filling_ordereddict(my_ordereddict)
+print(f'Получение элемента из dict занимает: '
+      f'{timeit("get_el_dict(my_dict)", globals=globals(), number=1000000)}')
+print(f'Получение элемента из OrderedDict занимает: '
+      f'{timeit("get_el_ordereddict(my_ordereddict)", globals=globals(), number=1000000)}')
+
+"""
+Время выполнения функции filling_dict: 0.11410331726074219
+Время выполнения функции filling_ordereddict: 0.1711561679840088
+Получение элемента из dict занимает: 0.08189420000000003
+Получение элемента из OrderedDict занимает: 0.08686779999999994
+
+Вывод: Время заполнения OrderedDict занимает больше времени, чем заполнение dict. Времени на получение
+элемента в OrderedDict также затрачивается больше, чем в dict.
+Отсюда можно сделать вывод, что OrderedDict, начиная с Python 3.6 абсолютно не нужен.
+"""
