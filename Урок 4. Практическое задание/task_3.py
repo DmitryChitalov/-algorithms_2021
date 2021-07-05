55e8ca0098174cd54ef537f6b681b54f317921dd
Vyacheslav <lvv_1@list.ru>
2021-07-06 00:47:03 +0300

    Ready

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -14,7 +14,11 @@
 Без аналитики задание считается не принятым
 """
 
+from timeit import timeit
+from cProfile import run
 
+
+# O(n!) очень похоже
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -25,6 +29,7 @@ def revers_1(enter_num, revers_num=0):
         return revers_1(enter_num, revers_num)
 
 
+# O(n)
 def revers_2(enter_num, revers_num=0):
     while enter_num != 0:
         num = enter_num % 10
@@ -33,7 +38,40 @@ def revers_2(enter_num, revers_num=0):
     return revers_num
 
 
+# O(n)
 def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+# O(N)
+def revers_4(enter_num):
+    listed_num = list(str(enter_num))
+    # приводим к списку, т.к. функция применяется только к списку
+    listed_num.reverse()                    # O(N)
+    revers_num = "".join(listed_num)
+    return revers_num
+
+
+def main():
+    print("Рекурсия:", timeit('revers_1(number)', globals=globals(), number=20000))
+    print("While:", timeit('revers_2(number)', globals=globals(), number=20000))
+    print("Срез:", timeit('revers_3(number)', globals=globals(), number=20000))
+    print("my_func_reverse:", timeit('revers_4(number)', globals=globals(), number=20000))
+
+
+number = 564892365
+run('main()')
+"""
+Рекурсия: 0.0549181
+While: 0.023538300000000012
+Срез: 0.007375199999999998
+my_func_reverse: 0.016952400000000006
+
+Выводы: Рекурсия очень медленно за счёт сложности (4 место), 
+While медленно за счет итераций и сложности O(n) (3 место), 
+reverse 2 место за счет сложности O(1), но проигрывает за счёт примнения двух
+методов reverse and join, 
+срез быстрее всего. 
+"""
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,6 +11,8 @@
 
 Без аналитики задание считается не принятым!
 """
+from timeit import timeit
+from collections import Counter
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -32,12 +34,38 @@ def func_2():
     for el in array:
         count2 = array.count(el)
         new_array.append(count2)
-
     max_2 = max(new_array)
     elem = array[new_array.index(max_2)]
     return f'Чаще всего встречается число {elem}, ' \
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+# лаконично
+def func_3():
+    return f'Чаще всего встречается число {max(array, key=array.count)}, ' \
+           f'оно появилось в массиве {array.count(max(array, key=array.count))} раз(а)'
+
+
+# лаконично-2
+def func_4():
+    c = (Counter(array)).most_common(1)
+    return f'Чаще всего встречается число {c[0][0]}, ' \
+           f'оно появилось в массиве {c[0][1]} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+print(func_4())
+print(func_1.__name__, timeit('func_1()', globals=globals(), number=20000))
+print(func_2.__name__, timeit('func_2()', globals=globals(), number=20000))
+print(func_3.__name__, timeit('func_3()', globals=globals(), number=20000))
+print(func_4.__name__, timeit('func_4()', globals=globals(), number=20000))
+
+# func_1 0.019907700000000004
+# func_2 0.0273066
+# func_3 0.0352397
+# func_4 0.0459508
+
+# Увы я не придумал как ускорить код. Две написанные функции хоть и лаконичны,
+# но не быстродейственны.
