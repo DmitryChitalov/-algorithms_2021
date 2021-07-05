2b2b5cb3aa25a56d107f02b735649b1eb1977495
Vladimir <vovazakharov2@gmail.com>
2021-07-05 13:09:21 +0300

    done

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,50 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+
+from random import randint
+from timeit import timeit
+
+
+my_list = [randint(-100, 100) for i in range(1000)]
+
+
+def bubble_sort_1(some_list):
+    result_list = some_list[:]
+    n = 1
+    while n < len(result_list):
+        for i in range(len(result_list) - n):
+            if result_list[i] < result_list[i + 1]:
+                result_list[i], result_list[i + 1] = result_list[i + 1], result_list[i]
+        n += 1
+    return result_list
+
+
+def bubble_sort_2(some_list):
+    result_list = some_list[:]
+    n = 1
+    m = True
+    while m == True:
+        m = False
+        for i in range(len(result_list) - n):
+            if result_list[i] < result_list[i + 1]:
+                m = True
+                result_list[i], result_list[i + 1] = result_list[i + 1], result_list[i]
+        n += 1
+    return result_list
+
+my_list_1 = bubble_sort_1(my_list)
+
+print(timeit('bubble_sort_1(my_list)', globals=globals(), number=1))      # 0.22295078000000002
+print(timeit('bubble_sort_2(my_list)', globals=globals(), number=1))      # 0.25174984499999997
+print(timeit('bubble_sort_1(my_list_1)', globals=globals(), number=1))    # 0.11300570700000001
+print(timeit('bubble_sort_2(my_list_1)', globals=globals(), number=1))    # 0.00024055900000008457
+
+print(my_list)
+print(bubble_sort_1(my_list))
+print(bubble_sort_2(my_list))
+
+# Доработка не помогла, сортировка стала медленнее, но если массив уже отсортирован, то доработанная программа
+# выполняется значительно быстрее, то есть чем более упорядочен массив тем эффективнее будет выполнятся
+# доработанная программа
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,44 @@ for i in
 
 
 """
+
+
+from statistics import median
+from random import randint
+from timeit import timeit
+
+
+m = 100 # int(input('Введите число: '))
+my_list = [randint(1, 10000) for i in range(m*2+1)]
+
+
+def median_1(some_list):
+    while len(some_list) > m + 1:
+        some_list.remove(max(some_list))
+    return max(some_list)
+
+
+def median_2(lst):
+    i = 1
+    while i < len(lst):
+        if (lst[i - 1] <= lst[i]):
+            i += 1
+        else:
+            lst[i], lst[i - 1] = lst[i - 1], lst[i]
+            i -= 1
+            if (i == 0):
+                i = 1
+    return lst[m]
+
+
+print(median(my_list[:]))
+print(median_1(my_list[:]))
+print(median_2(my_list[:]))
+
+print(timeit('median(my_list[:])', globals=globals(), number=100))      # 0.0032846509999999995
+print(timeit('median_1(my_list[:])', globals=globals(), number=100))    # 0.091496676
+print(timeit('median_2(my_list[:])', globals=globals(), number=100))    # 1.165463713
+
+# Наиболее быстрая - встроеная функция. Функция median_1 имеет квадратичную сложность, поэтому работает значительно
+# медленнее. Алгоритм на базе сортировки также имеет квадратичную сложность, но работает еще медленнее, поскольку
+# производит дополнительную работу по перестановке значений во всем массиве.
