78f483b27ccee2f9fd6f76ebcb79c62e20944c99
ivan <ivan.pavlov.alk@gmail.com>
2021-07-05 21:13:32 +0200

    L4.T4 - Completed

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,6 +11,7 @@
 
 Без аналитики задание считается не принятым!
 """
+from timeit import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -39,5 +40,21 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    max_3 = max(array, key=array.count)
+    return f'Чаще всего встречается число {max_3}, ' \
+           f'оно появилось в массиве {array.count(max_3)} раз(а)'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+
+print(f"Func 1 time: {timeit('func_1()', globals=globals())}"
+      f"\nFunc 2 time: {timeit('func_2()', globals=globals())}"
+      f"\nFunc 3 time: {timeit('func_3()', globals=globals())}")
+
+"""
+Функция func_3 - лаконично, но по времени равносильно функции func_1, т.к функция count() заменяет цикл который 
+испольщован для перебора элементов списка
+"""
