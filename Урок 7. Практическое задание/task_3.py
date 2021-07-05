4a438d0973675d1caf6919e1a5c479b97e4ae7c8
WhiIend <76176054+WhiIend@users.noreply.github.com>
2021-07-05 18:29:31 +0300

    Update task_3.py

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,50 @@ for i in
 
 
 """
+
+import random
+import timeit
+
+# Сначала попробовал реализовать поиск медианы по уже заданному списку
+"""
+my_list = [-12, -12, -6, 1, 6, 9, 9]
+average_num = round(sum(my_list)/len(my_list))
+print(sum(my_list)/len(my_list))
+print(average_num)
+difference_el = [abs((i - average_num)) for i in my_list]
+print(difference_el)
+print(min(difference_el))
+print(difference_el.index((min(difference_el))))
+print(f'Медиана списка: {my_list[(difference_el.index((min(difference_el))))]}.')
+"""
+
+
+def func_median(my_list):
+    average_num = round(sum(my_list) / len(my_list))
+    difference_el = [abs((i - average_num)) for i in my_list]
+    return f'Медиана списка: {my_list[(difference_el.index((min(difference_el))))]}.'
+
+rand_num = 2 * ((random.randint(0, 10000))) + 1
+print(f'Случайное число равно: {rand_num}')
+orig_list = [random.randint(-100, 100) for i in range(rand_num)]
+print(func_median(orig_list))
+
+print('Время затраченное на поиск медианы:', timeit.timeit("""
+import random
+def func_median(my_list):
+    average_num = round(sum(my_list) / len(my_list))
+    difference_el = [abs((i - average_num)) for i in my_list]
+    return f'Медиана списка: {my_list[(difference_el.index((min(difference_el))))]}.'
+rand_num = 2 * ((random.randint(0, 10000))) + 1
+orig_list = [random.randint(-100, 100) for i in range(rand_num)]
+func_median(orig_list)
+""", number=1000))
+
+
+
+"""
+    Результаты замеров:
+        Случайное число равно: 15205
+        Медиана списка: 0.
+        Время затраченное на поиск медианы: 6.485537
+"""
