b52bc185bcfbe38925f62854fe2c85c9e86d8158
Lalluviadel <inspiracion@yandex.ru>
2021-07-06 19:40:22 +0300

    выполнены задания к уроку 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,13 +13,112 @@
 Без аналитики задание не принимается
 
 И прошу вас обратить внимание, что то, что часто ошибочно называют генераторами списков,
-на самом деле к генераторам отношения не имеет. Это называется "списковое включение" - list comprehension.
+на самом деле к генераторам отношения не имеет. Это называется "списковое включение" -
+list comprehension.
 """
+from random import randint
+from timeit import timeit
+import dis
+
+# создание исходного списка из 100 целых чисел
+my_rand_int_list = [randint(0, 50) for i in range(100)]
 
 
 def func_1(nums):
+    """Original function"""
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    """Improvement №1 - enumerate() is used here"""
+    new_arr = []
+    for i, val in enumerate(nums):
+        if val % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+def func_3(nums):
+    """Improvement №2 - list comprehension is involved here"""
+    new_arr = [i for i, val in enumerate(nums) if val % 2 == 0]
+    return new_arr
+
+
+def func_4(nums):
+    """Improvement №3 - if v %2 == 0 replaced by if not v % 2"""
+    new_arr = [i for i, val in enumerate(nums) if not val % 2]
+    return new_arr
+
+
+'''Для замеров каждой функции (и дальнейших действий в дополнение к домашнему заданию) я поместила
+их в словарь. На моей машине хорошо видно, что каждая последующая - с меньшим временем
+выполнения.
+Анализ:
+1. Улучшение 1 - использование enumerate(), которая добавляет счетчик к каждому элементу
+итерируемого объекта, таким образом в коде функции не требуется отдельно обращаться
+к элементу списка по индексу
+2. Улучшение 2 - list comprehensions быстрее, чем цикл for.
+3. Улучшение 3 - if not val % 2 лучше, чем if val % 2 == 0 потому, что val % 2 == 0
+эквивалентно not (val % 2 != 0), то есть выполняются лишние действия отрицания.
+
+'''
+
+my_functions = dict(
+    func_1="""
+new_arr = []
+for i in range(len(my_rand_int_list)):
+    if my_rand_int_list[i] % 2 == 0:
+        new_arr.append(i)""",
+    func_2="""
+new_arr = []
+for i, val in enumerate(my_rand_int_list):
+        if val % 2 == 0:
+            new_arr.append(i)""",
+    func_3='new_arr = [i for i, v in enumerate(my_rand_int_list) if v %2 == 0]',
+    func_4='new_arr = [i for i, v in enumerate(my_rand_int_list) if not v % 2]',
+)
+
+for name, text in my_functions.items():
+    print(name, timeit(stmt=text, number=1000, globals=globals()))
+
+
+'''
+Далее мне стало интересно разобраться, почему же на самом деле list comprehensions быстрее,
+чем цикл for.
+Я использовала модуль dis для того, чтобы получить доступ к байт-коду и сравнила отличия.
+Мне кажется, lc быстрее for благодаря тому, что при использовании for выполняются загрузка
+метода и его вызов:
+38 LOAD_METHOD(append) и 42 CALL_METHOD для func_01 и
+34 LOAD_METHOD(append) и 38 CALL_METHOD для func_02 соответственно.
+А для lc такой необходимости нет.
+'''
+
+for name, text in my_functions.items():
+    print(name)
+    code = compile(text, '<string>', 'exec')
+    dis.disassemble(code)
+
+'''
+Также я проверила if val % 2 == 0 и if not val % 2, у первого варианта ожидаемо
+оказалось 2 лишних операции:
+6 LOAD_CONST и 8 COMPARE_OP
+Отсюда 4 вариант заданной функции быстрее, чем 3.
+'''
+
+
+def not_not(val):
+    if val % 2 == 0:
+        return True
+
+
+def one_not(val):
+    if not val % 2:
+        return True
+
+
+dis.dis(not_not)
+dis.dis(one_not)
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,16 +13,64 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
+from functools import reduce
+from dis import dis
+'''
+Для реализации собственных вариантов решения этой задачи я выбрала метод с reduce() и классический
+метод с join.
+
+Анализ:
+1) Ранжирование по скорости от самой быстрой функции к самой медленной:
+1. revers_3() - метод срезов
+2. revers_2() - метод цикла
+3. revers_5() - метод с join
+4. revers_4() - метод с reduce
+5. revers_1() - метод рекурсии
+
+2) Я считаю, что revers_2() и revers_1() не должны участвовать в данном ранжировании скорости,
+так как по сути, они выдают некорректный результат (числа с плавающей точкой, что являет
+не совсем верный реверс исходного числа). Также они некорректно выполняют реверс чисел,
+оканчивающихся нулем/нулями. С этой точки зрения "пьедестал" должен был бы выглядеть так:
+1. revers_3() - метод срезов
+2. revers_5() - метод с join
+3. revers_4() - метод с reduce
+4. revers_2() - метод цикла
+5. revers_1() - метод рекурсии
+
+3) Метод срезов - наиболее быстрый для переворачивания строк. Поэтому сложно изобрести решение
+быстрее, чем revers_3(), если остаться в рамках возврата строки, а не выбрать какой-то другой путь.
+
+4) Причину наиболее высокой эффективности revers_3() я попыталась найти в байт-коде. Так как
+я пока не очень хорошо разобралась в обозначениях инструкций и их механизмов, надеюсь, что
+верно смогла сделать выводы.
+
+5.revers_1 - самая низкая скорость за счет множественных инструкций LOAD_FAST к переменным, а также
+и самое большое общее количество инструкций.
+4.revers_4 - (с reduce) четвертый по скорости за счет инструкции создания функции MAKE_FUNCTION
+и нескольких ее вызовов CALL_FUNCTION, общее количество инструкций невелико.
+3.revers_5 - (с join) все еще не самый быстрый за счет загрузки метода LOAD_METHOD, нескольких
+вызовов функции CALL_FUNCTION и вызова метода СALL_METHOD, общее количество инструкций самое
+низкое.
+2.revers_2 - множество инструкций LOAD_FAST и само количество инструкций, но у нее
+нет вызовов функций и методов.
+1.revers_3 - самый быстрый, мало инструкций LOAD_FAST и общее количество инструкций невелико.
+
+Думаю, что объяснение скорости каждой из функций кроется в этом.
+
+5) С точки зрения cProfile, - если я правильно поняла, что требуется указать в анализе, - проблем
+и слабых мест в целом нет ни в одном из вариантов.
+'''
 
 
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
-    else:
-        num = enter_num % 10
-        revers_num = (revers_num + num / 10) * 10
-        enter_num //= 10
-        return revers_1(enter_num, revers_num)
+    num = enter_num % 10
+    revers_num = (revers_num + num / 10) * 10
+    enter_num //= 10
+    return revers_1(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -37,3 +85,45 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    """My solution with reduce()"""
+    revers_num = reduce(lambda x, y:  y+x,  str(enter_num))
+    return revers_num
+
+
+def revers_5(enter_num):
+    """My solution with join()"""
+    revers_num = ''.join(reversed(str(enter_num)))
+    return revers_num
+
+
+my_num = 100
+
+print(f"revers_1:"
+      f"{timeit('revers_1(my_num)', number=10000, globals=globals()):.5f}")
+print(f"revers_2:"
+      f"{timeit('revers_2(my_num)', number=10000, globals=globals()):.5f}")
+print(f"revers_3:"
+      f"{timeit('revers_3(my_num)', number=10000, globals=globals()):.5f}")
+print(f"revers_4:"
+      f"{timeit('revers_4(my_num)', number=10000, globals=globals()):.5f}")
+print(f"revers_5:"
+      f"{timeit('revers_5(my_num)', number=10000, globals=globals()):.5f}")
+
+
+def main():
+    revers_1(my_num)
+    revers_2(my_num)
+    revers_3(my_num)
+    revers_4(my_num)
+    revers_5(my_num)
+
+
+run('main()')
+# dis(revers_1) # до 56 адресация инструкций в байт-коде
+# dis(revers_2) # до 44
+# dis(revers_3) # до 24
+# dis(revers_4) # до 20 + 6
+# dis(revers_5) # до 20
