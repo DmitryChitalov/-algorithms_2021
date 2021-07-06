4a0e8c54ab3439e57e04ff41ce6499aefe93ba48
konovalovdv <89204140266kon@gmail.com>
2021-07-06 13:32:56 +0300

    On branch lesson_7  Changes to be committed: 	new file:   .idea/-algorithms_2021.iml 	new file:   .idea/.gitignore 	new file:   .idea/aws.xml 	new file:   .idea/inspectionProfiles/Project_Default.xml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	modified:   task_1.py 	modified:   task_2.py 	modified:   task_3.py

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,87 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+
+import timeit
+import random
+
+
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - 1):
+            if lst_obj[i] > lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def reverse_bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        is_sort = True
+        for i in range(1, len(lst_obj)):
+            if lst_obj[-i] < lst_obj[-i - 1]:
+                lst_obj[-i], lst_obj[-i - 1] = lst_obj[-i - 1], lst_obj[-i]
+                is_sort = False
+        if is_sort:
+            break
+        n += 1
+    return lst_obj
+
+
+def reverse_bubble_sort_update(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        is_sort = True
+        for i in range(1, len(lst_obj) + 1 - n):
+            if lst_obj[-i] < lst_obj[-i - 1]:
+                lst_obj[-i], lst_obj[-i - 1] = lst_obj[-i - 1], lst_obj[-i]
+                is_sort = False
+        if is_sort:
+            break
+        n += 1
+    return lst_obj
+
+
+num = 15
+a = [random.randint(-100, 100) for _ in range(num)]
+b = [1, 2, 3, 4, 5, 6]
+print(bubble_sort(a[:]))
+print(reverse_bubble_sort(a[:]))
+print(reverse_bubble_sort_update(a[:]))
+print(reverse_bubble_sort(b[:]))
+
+stmt = [
+    'reverse_bubble_sort(a[:])',
+    'reverse_bubble_sort_update(a[:])'
+]
+
+for st in stmt:
+    print(f'на выполение функции {st} затрачено времени: '
+          f'{timeit.timeit(st, setup="a =[random.randint(-100, 100) for _ in range(10)]", number=1000, globals=globals())}')
+
+for st in stmt:
+    print(f'на выполение функции {st} затрачено времени: '
+          f'{timeit.timeit(st, setup="a =[random.randint(-100, 100) for _ in range(100)]", number=1000, globals=globals())}')
+
+for st in stmt:
+    print(f'на выполение функции {st} затрачено времени: '
+          f'{timeit.timeit(st, setup="a =[random.randint(-100, 100) for _ in range(1000)]", number=1000, globals=globals())}')
+"""
+Для улучшения алгоритма была создана переменная-флаг is_sort, которая в случае хотя бы одной перестановки принимала 
+значение False. В том случае, если перестановки не было, то она оставалсь со значение True, что приводит 
+к остановке внешнего цикла while и завершению функции
+
+В функции reverse_bubble_sort_update для оптимизации изменен порядок определения значения 
+крайней границы range len(lst_obj) + 1 - n), что позволяет не проверять уже отсортированные элементы.
+В результате мы получаем сущесвенный прирост скорости:
+
+на выполение функции reverse_bubble_sort(a[:]) затрачено времени: 0.0109859 (10 элементов)
+на выполение функции reverse_bubble_sort_update(a[:]) затрачено времени: 0.011357600000000002 (10 элементов)
+на выполение функции reverse_bubble_sort(a[:]) затрачено времени: 1.2977106999999999 (100 элементов)
+на выполение функции reverse_bubble_sort_update(a[:]) затрачено времени: 0.9809941999999998 (100 элементов)
+на выполение функции reverse_bubble_sort(a[:]) затрачено времени: 152.62518350000002 (1000 элементов)
+на выполение функции reverse_bubble_sort_update(a[:]) затрачено времени: 106.3371616 (1000 элементов)
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,68 @@ for i in
 
 
 """
+
+
+from statistics import median
+import random
+import timeit
+
+
+def del_max(lst_obj):
+    for _ in range(m):
+        lst_obj.remove(max(lst_obj))
+    return f'Для массива {a} значение медианы равно {max(lst_obj)}, индекс медианы равен {m}'
+
+
+def gnom_sort(lst_obj):
+    i = 1
+    j = 2  # индекс элемента, на котором остановился гном
+    size = len(lst_obj)
+    while i < size:
+        if lst_obj[i - 1] < (lst_obj[i]):
+            i = j
+            j += 1
+        else:
+            lst_obj[i - 1], lst_obj[i] = lst_obj[i], lst_obj[i-1]
+            i -= 1
+            if i == 0:
+                i = j
+                j += 1
+    return f'Для массива {a} значение медианы равно {lst_obj[m]}, индекс медианы равен {m}'
+
+
+m = int(input('Ведите значение m'))
+length = 2 * m + 1
+a = [random.randint(0, 50) for _ in range(length)]
+print(f'Медиана массива {a} равна {median(a[:])}, индекс медианы равен {m}')
+print(del_max(a[:]))
+print(gnom_sort(a[:]))
+
+stmt = [
+        'del_max(a[:])',
+        'gnom_sort(a[:])'
+    ]
+
+for st in stmt:
+    print(f'на выполение функции {st} затрачено времени: '
+          f'{timeit.timeit(st, setup="a =[random.randint(0, 50) for _ in range(10)]", number=1000, globals=globals())}')
+
+for st in stmt:
+    print(f'на выполение функции {st} затрачено времени: '
+          f'{timeit.timeit(st, setup="a =[random.randint(0, 50) for _ in range(100)]", number=1000, globals=globals())}')
+
+for st in stmt:
+    print(f'на выполение функции {st} затрачено времени: '
+          f'{timeit.timeit(st, setup="a =[random.randint(0, 50) for _ in range(1000)]", number=1000, globals=globals())}')
+
+
+"""
+Метод удаления максимальных значений намного эффективнее гномьей сортировки, т.к. метод гномье сортировки может
+достигать сложность O(n^2)
+на выполение функции del_max(a[:]) затрачено времени: 0.002595600000000031
+на выполение функции gnom_sort(a[:]) затрачено времени: 0.004588700000000001
+на выполение функции del_max(a[:]) затрачено времени: 0.008619000000000154
+на выполение функции gnom_sort(a[:]) затрачено времени: 0.3257730000000001
+на выполение функции del_max(a[:]) затрачено времени: 0.05258750000000001
+на выполение функции gnom_sort(a[:]) затрачено времени: 36.271869
+"""
