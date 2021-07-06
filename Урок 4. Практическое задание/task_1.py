8d3a01b256ae158bc46e47f706106447bc20e57c
Idel Yusupov <idel84@mail.ru>
2021-07-06 16:12:14 +0300

    lesson #4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,9 +12,11 @@
 Добавьте аналитику: что вы сделали и почему!!!
 Без аналитики задание не принимается
 
-И прошу вас обратить внимание, что то, что часто ошибочно называют генераторами списков,
-на самом деле к генераторам отношения не имеет. Это называется "списковое включение" - list comprehension.
+И прошу вас обратить внимание, что то, что часто ошибочно называют генераторами
+списков, на самом деле к генераторам отношения не имеет. Это называется
+"списковое включение" - list comprehension.
 """
+from timeit import timeit
 
 
 def func_1(nums):
@@ -23,3 +25,18 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    yield (i for i in nums if i % 2 == 0)
+
+def func_3(nums):
+    return [i for i in nums if i % 2 == 0]
+
+
+arr_basic = [i for i in range(100000)]
+
+print(timeit('func_1(arr_basic)', globals=globals(), number=1000))
+print(timeit('func_2(arr_basic)', globals=globals(), number=1000))
+print(timeit('func_3(arr_basic)', globals=globals(), number=1000))
+
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,9 +13,11 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
 
 
-def revers_1(enter_num, revers_num=0):
+def revers_1(enter_num, revers_num=0):  # рекурсия (ресорсоёмкий) O(n)
     if enter_num == 0:
         return revers_num
     else:
@@ -25,7 +27,7 @@ def revers_1(enter_num, revers_num=0):
         return revers_1(enter_num, revers_num)
 
 
-def revers_2(enter_num, revers_num=0):
+def revers_2(enter_num, revers_num=0):  # цикл (тоже много ресурсов) O(n)
     while enter_num != 0:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
@@ -33,7 +35,38 @@ def revers_2(enter_num, revers_num=0):
     return revers_num
 
 
-def revers_3(enter_num):
+def revers_3(enter_num):    # наиболее эфективный и лаконичный O(1)
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):    # быстрый простой O(1)
+    return reversed(str(enter_num))
+
+
+# фактически тоже метод 3, но выдает не число а ссылку на память,
+# однако по времени быстрее
+num_for_reverse = 8520316497
+print('Вариант №1: ', timeit('revers_1(num_for_reverse)', globals=globals()))
+print('Вариант №2: ', timeit('revers_2(num_for_reverse)', globals=globals()))
+print('Вариант №3: ', timeit('revers_3(num_for_reverse)', globals=globals()))
+print('Вариант №4: ', timeit('revers_4(num_for_reverse)', globals=globals()))
+
+
+def main():
+    revers_1(num_for_reverse)
+    revers_2(num_for_reverse)
+    revers_3(num_for_reverse)
+    revers_4(num_for_reverse)
+
+
+print()
+run('main()')
+
+"""
+по замерам лучшая функция 3 и 4, и с ростом входных данных 
+производительность у них выше, т.к. по O-нотации они занимают верхнюю ступень.
+из профилирофщиков лучше себя показывает timeit(гораздо нагляднее), 
+т.к. cProfile также как и на уроке мало информативный. 
+"""
