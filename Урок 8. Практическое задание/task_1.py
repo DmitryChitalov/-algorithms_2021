9aab86946a2d679b9489a67d21bee763850e88cb
ahalay-mahalay <57568587+ahalay-mahalay@users.noreply.github.com>
2021-07-08 21:49:06 +0300

    Home Work #8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,49 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+from collections import Counter, deque
+
+
+# 1)
+def haffman_tree(s):
+    count_string = Counter(s)
+    sorted_string = deque(sorted(count_string.items(), key=lambda item: item[1]))
+    if len(sorted_string) != 1:
+        while len(sorted_string) > 1:
+            duplication = sorted_string[0][1] + sorted_string[1][1]
+            merger = {0: sorted_string.popleft()[0],
+                      1: sorted_string.popleft()[0]}
+
+            for i, _count in enumerate(sorted_string):
+                if duplication > _count[1]:
+                    continue
+                else:
+                    sorted_string.insert(i, (merger, duplication))
+                    break
+            else:
+                sorted_string.append((merger, duplication))
+    else:
+        duplication = sorted_string[0][1]
+        merger = {0: sorted_string.popleft()[0], 1: None}
+        sorted_string.append((merger, duplication))
+
+    return sorted_string[0][0]
+
+
+code_table = dict()
+
+
+def haffman_code(tree, path=''):
+    if not isinstance(tree, dict):
+        code_table[tree] = path
+    else:
+        haffman_code(tree[0], path=f'{path}0')
+        haffman_code(tree[1], path=f'{path}1')
+
+
+s = "мама мыла раму!"
+
+haffman_code(haffman_tree(s))
+
+for _ in s:
+    print(code_table[_], end=' ')
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,58 +10,61 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
+class BinaryTreeChildChecker(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
 class BinaryTree:
     def __init__(self, root_obj):
-        # корень
         self.root = root_obj
-        # левый потомок
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        while True:
+            try:
+                if self.left_child == None and new_node < self.root:
+                    self.left_child = BinaryTree(new_node)
+                elif new_node < self.root:
+                    tree_obj = BinaryTree(new_node)
+                    tree_obj.left_child = self.left_child
+                    self.left_child = tree_obj
+                else:
+                    raise BinaryTreeChildChecker("*** Ошибка! Левый потомок должен быть меньше родителя ***")
+            except (ValueError, BinaryTreeChildChecker) as err:
+                print(err)
+                new_node = int(input(f'Введите число меньше {self.root}: '))
+            else:
+                break
 
-    # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        while True:
+            try:
+                if self.right_child == None and new_node > self.root:
+                    self.right_child = BinaryTree(new_node)
+                elif new_node > self.root:
+                    tree_obj = BinaryTree(new_node)
+                    tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+                else:
+                    raise BinaryTreeChildChecker("*** Ошибка! Правый потомок должен быть больше родителя ***")
+            except (ValueError, BinaryTreeChildChecker) as err:
+                print(err)
+                new_node = int(input(f'Введите число больше {self.root}: '))
+            else:
+                break
 
-    # метод доступа к правому потомку
     def get_right_child(self):
         return self.right_child
 
-    # метод доступа к левому потомку
     def get_left_child(self):
         return self.left_child
 
-    # метод установки корня
     def set_root_val(self, obj):
         self.root = obj
 
-    # метод доступа к корню
     def get_root_val(self):
         return self.root
 
@@ -72,7 +75,7 @@ print(r.get_left_child())
 r.insert_left(40)
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
-r.insert_right(12)
+r.insert_right(6)
 print(r.get_right_child())
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
