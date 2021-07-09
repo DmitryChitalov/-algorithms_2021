0de7187433ce9cf3da9e811e9b10003f450dbb7e
belapko <belapko23@gmail.com>
2021-07-09 17:35:03 +0400

    Update task_7.py

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -13,3 +13,62 @@
 Примечание:
 Вам не нужно писать код с нуля. Вам нужно доработать пример с урока.
 """
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_rear(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+#  Добавленное мной:
+    @staticmethod
+    def union(string):
+        union_str = ''
+        for i in string.split():
+            union_str += i
+        return union_str
+      
+###################################
+
+from task_16 import DequeClass
+
+
+def pal_checker(string):
+    dc_obj = DequeClass()
+
+    # x = ''
+    # for i in string.split():
+    #     x += i
+
+    for el in dc_obj.union(string):
+        dc_obj.add_to_rear(el)
+
+    still_equal = True
+
+    while dc_obj.size() > 1 and still_equal:
+        first = dc_obj.remove_from_front()
+        last = dc_obj.remove_from_rear()
+        if first != last:
+            still_equal = False
+
+    return still_equal
+
+
+print(pal_checker("молоко делили ледоколом"))
+      
+      
+  
