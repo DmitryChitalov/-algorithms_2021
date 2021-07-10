757a4c2ab897a94e724f0065914a86d87c7cc8f6
Viktor Yagodin <Viktor.v.yagodin@gmail.com>
2021-07-11 02:24:08 +0500

    Ex4 reviewed

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -12,12 +12,19 @@
 Без аналитики задание считается не принятым!
 """
 from timeit import timeit
+from random import randint
 
 
-array = [1, 3, 1, 3, 4, 5, 1]
+#array = [1, 1, 3, 1, 3, 4, 4, 4, 4, 4, 5, 1]
+arr_1 = [randint(0, 9) for i in range(10)]
+arr_2 = [randint(0, 9) for i in range(100)]
+arr_3 = [randint(0, 9) for i in range(1000)]
+print(arr_1)
+print(arr_2)
+print(arr_3)
 
 
-def func_1():
+def func_1(array):  # сложность - O(N^2)
     m = 0
     num = 0
     for i in array:
@@ -29,7 +36,7 @@ def func_1():
            f'оно появилось в массиве {m} раз(а)'
 
 
-def func_2():
+def func_2(array):  # сложность - O(N^2)
     new_array = []
     for el in array:
         count2 = array.count(el)
@@ -41,7 +48,7 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-def func_3():
+def func_3(array):  # сложность - O(N)
     cache = {}
     for num in array:
         if num in cache:
@@ -49,17 +56,43 @@ def func_3():
         else:
             cache[num] = 1
     keys = list(cache.keys())
-    return f'Чаще всего встречается число {keys[0]}, ' \
-           f'оно появилось в массиве {cache[keys[0]]} раз(а)'
+    values = list(cache.values())
+    max_count = values.index(max(values))
+    max_num = keys[max_count]
+    return f'Чаще всего встречается число {max_num}, ' \
+           f'оно появилось в массиве {values[max_count]} раз(а)'
 
 
-print(func_1())
-print(func_2())
-print(func_3())
-print(timeit('func_1()', globals=globals(), number=10000))
-print(timeit('func_2()', globals=globals(), number=10000))
-print(timeit('func_3()', globals=globals(), number=10000))
+print(func_1(arr_1))
+print(func_2(arr_1))
+print(func_3(arr_1))
+print('Массив из 10 эл-тов:')
+print('1 функция:', timeit('func_1(arr_1)', globals=globals(), number=10000))
+print('2 функция:', timeit('func_2(arr_1)', globals=globals(), number=10000))
+print('3 функция:', timeit('func_3(arr_1)', globals=globals(), number=10000))
+print('Массив из 100 эл-тов:')
+print('1 функция:', timeit('func_1(arr_2)', globals=globals(), number=10000))
+print('2 функция:', timeit('func_2(arr_2)', globals=globals(), number=10000))
+print('3 функция:', timeit('func_3(arr_2)', globals=globals(), number=10000))
+print('Массив из 1000 эл-тов:')
+print('1 функция:', timeit('func_1(arr_3)', globals=globals(), number=10000))
+print('2 функция:', timeit('func_2(arr_3)', globals=globals(), number=10000))
+print('3 функция:', timeit('func_3(arr_3)', globals=globals(), number=10000))
 
 '''
-    Удалось написать чуть более быструю функцию
+Массив из 10 эл-тов:
+1 функция: 0.017991200000000002
+2 функция: 0.0243702
+3 функция: 0.0176722
+Массив из 100 эл-тов:
+1 функция: 0.9729201999999999
+2 функция: 1.0175169000000002
+3 функция: 0.09427610000000008
+Массив из 1000 эл-тов:
+1 функция: 90.0322089
+2 функция: 90.37662
+3 функция: 0.8478375999999912
+
+При малых длинах входного массива моя функция показывает сопоставимое время выполнения.
+Ожидаемо при увеличении кол-ва данных время выполнения растет на порядки медленее.
 '''
