fc82db5cf38071c2523c62d33f897d0bd01e233d
Lalluviadel <inspiracion@yandex.ru>
2021-07-10 16:46:41 +0300

    Выполнены задания к уроку 5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,49 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+from collections import namedtuple
+
+
+def sorting_by_profit(some_list, quant_num):
+    """Function sorts companies by its profit and outputs values"""
+    total_annual_profit = sum(i.sum for i in some_list) / quant_num
+    winners, loosers, middlings = [], [], []
+    for company in some_list:
+        if company.sum > total_annual_profit:
+            winners.append(company.name)
+        elif company.sum < total_annual_profit:
+            loosers.append(company.name)
+        else:
+            middlings.append(company.name)
+    print(f"Средняя годовая прибыль всех предприятий: {total_annual_profit}\n"
+          f"Предприятия, с прибылью выше среднего значения: {', '.join(map(str,winners))}\n"
+          f"Предприятия, с прибылью ниже среднего значения: {', '.join(map(str,loosers))}\n")
+    if len(middlings) > 1:
+        print(f"Предприятия с прибылью, равной среднему значению:"
+              f" {', '.join(map(str,middlings))}\n")
+
+
+def data_collection():
+    """Function collects initial data and calculates average total profit"""
+    quantity = int(input("Введите количество предприятий для расчета прибыли:"))
+    my_storage = []
+    for elem in range(quantity):
+        company_name = input("Введите название предприятия: ")
+        RES = namedtuple('Information', ['name', 'sum'])
+        annual_profit = sum(map(float, input("Через пробел введите прибыль данного предприятия "
+                                             "за каждый отработанный квартал года:").split()))
+        my_storage.append(RES(company_name, annual_profit))
+    sorting_by_profit(my_storage, quantity)
+
+
+def annual_report():
+    """A startup function that checks the input data"""
+    while True:
+        try:
+            data_collection()
+            break
+        except ValueError:
+            print('Вводите только числа. Попробуйте еще раз')
+
+
+annual_report()
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,82 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from collections import OrderedDict
+from random import randint
+from timeit import timeit
+
+'''
+Анализ:
+1) Операция создания и заполнения обычного словаря несколько быстрее, чем OrderedDict.
+Предполагаю, что не только за счет формы хранения данных, но и потому, что он при создании
+обрабатывает уже некую имеющуюся конструкцию.
+
+2) Запрос элемента занимает примерно равное время.
+
+3) Добавление элемента в OrderedDict выполняется немного медленнее, возможно, потому что
+тратится время на присвоение ему порядкового номера в структуре и запись в нужном формате.
+
+4) Сортировка меня особенно интересовала, т.к. прежде я читала, что OrderedDict может быть
+оптимизирован для частого переупорядочивания. Но и здесь он показал результаты ниже, по всем
+типам сортировки.
+
+5) Метод popitem() тоже быстрее работает в рамках обычного словаря.
+
+Итог:
+У OrderedDict есть 2 интересных функции, .popitem(), в которую можно передавать False и менять
+порядок LIFO на FIFO, а также .move_to_end(), у которой есть аргумент last, позволяющий
+перемещать элемент в начало или конец (в зависимости от того, было ли передано False или True).
+Но этот функционал слишком мал для того, чтобы OrderedDict мог сохранять свое практическое
+значение.
+Если в будущем его не оптимизируют или не расширят уникальный функционал, то этот объект и
+термин уйдут в прошлое. В прошлом он, видимо, был необходим, но с версии 3.6 обычные словари
+получили возможность хранить порядок элементов, а также оптимизировали работу с памятью на
+20-30 процентов, как указано в документации, поэтому формат OrderedDict утерял значение.
+'''
+
+create_operations = dict(my_dict="""{k:randint(0, 50) for k in range(100)}""",
+                         my_ordered_dict="""OrderedDict({k:randint(0, 50) for k in range(100)})""")
+
+for name, text in create_operations.items():
+    print(f"Создание и заполнение {name}, {timeit(stmt=text, number=1000, globals=globals()):.5f}")
+
+my_dict = {k: randint(0, 50) for k in range(100)}
+my_ordered_dict = OrderedDict({k: randint(0, 50) for k in range(100)})
+
+
+requesting_data = dict(my_dict_inx="""my_dict[5]""",
+                       my_ordered_dict_inx="""my_ordered_dict[5]""")
+
+add_data = dict(my_dict_add="""my_dict[100] = 100""",
+                my_ordered_dict_add="""my_ordered_dict[100] = 100""")
+
+sort_data = dict(my_dict_sorted_key="""sorted(my_dict)""",
+                 my_ordered_dict_sorted_key="""sorted(my_ordered_dict)""",
+                 my_dict_sorted_value="""sorted(my_dict.values())""",
+                 my_ordered_dict_sorted_value="""sorted(my_ordered_dict.values())""",
+                 my_dict_sorted_items="""sorted(my_dict.items())""",
+                 my_ordered_dict_sorted_items="""sorted(my_ordered_dict.items())""")
+
+pop_data = dict(
+    my_dict_pop="""
+new_dict = {1:1, 2:2, 3:3}
+new_dict.popitem()""",
+    my_ordered_dict_pop="""
+new_ord_dict = OrderedDict({1:1, 2:2, 3:3})
+new_ord_dict.popitem()""",
+    my_ordered_dict_pop_F="""
+new_ord_dict = OrderedDict({1:1, 2:2, 3:3})
+new_ord_dict.popitem(last=False)""")
+
+for name, text in requesting_data.items():
+    print(f"Запрос элемента по индексу {name}, "
+          f"{timeit(stmt=text, number=1000,globals=globals()):.5f}")
+
+for name, text in add_data.items():
+    print(f"Добавление {name}, {timeit(stmt=text, number=1000, globals=globals()):.5f}")
+
+for name, text in sort_data.items():
+    print(f"Сортировка {name}, {timeit(stmt=text, number=1000, globals=globals()):.5f}")
+
+for name, text in pop_data.items():
+    print(f"Удаление popitem {name}, {timeit(stmt=text, number=1000, globals=globals()):.5f}")
