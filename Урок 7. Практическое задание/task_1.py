570b298d4b00fb83d39bd4dd38e6d54f6879e4b3
andrey sinitsyn <andrey-sin@yandex.ru>
2021-07-11 10:10:49 +0300

    lesson_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,86 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+import timeit
+import random
+
+def bubble_sort_desc(lst_obj):
+    for j in range(len(lst_obj)):
+        for i in range(len(lst_obj)-1):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+    return lst_obj
+
+
+def bubble_sort_desc_opt(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def bubble_sort_desc_opt_flag(lst_obj):
+    n = 1
+    flag = True
+    while n < len(lst_obj) and flag:
+        flag = False
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+                flag = True
+        n += 1
+    return lst_obj
+
+orig_list_10 = [random.randint(-100, 100) for _ in range(10)]
+orig_list_100 = [random.randint(-100, 100) for _ in range(100)]
+
+print('Без оптимизации')
+print(
+    timeit.timeit(
+        "bubble_sort_desc(orig_list_10[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "bubble_sort_desc(orig_list_100[:])",
+        globals=globals(),
+        number=1000))
+
+print('С оптимизацией на колличество операций в каждом проходе')
+print(
+    timeit.timeit(
+        "bubble_sort_desc_opt(orig_list_10[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "bubble_sort_desc_opt(orig_list_100[:])",
+        globals=globals(),
+        number=1000))
+
+print('С оптимизацией "выход, если не было изменений"')
+print(
+    timeit.timeit(
+        "bubble_sort_desc_opt_flag(orig_list_10[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit.timeit(
+        "bubble_sort_desc_opt_flag(orig_list_100[:])",
+        globals=globals(),
+        number=1000))
+
+
+print(orig_list_10)
+print(bubble_sort_desc_opt_flag(orig_list_10))
+
+'''Оптимизация с сокращением проходов после каждой итерации наружнего цикла, существенно влияет
+на время выполнения функции, т.к. количество проходов сокращается очень значительно. В последней операции с начального n до 1.
+Оптимизация с флагом помогает, но очень не сильно и при не большом колличестве данных, потому что при
+большом колличестве данных количество исключенных интераций очень не значительное'''
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,59 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+from timeit import timeit
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+    for _ in range(len(left_list) + len(right_list)):
+        if left_list_index < len(left_list) and right_list_index < len(right_list):
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+        elif left_list_index == len(left_list):
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        elif right_list_index == len(right_list):
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+    return sorted_list
+
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    mid = len(lst) // 2
+    left_list = merge_sort(lst[:mid])
+    right_list = merge_sort(lst[mid:])
+    return merge(left_list, right_list)
+
+n = int(input('Введите число элементов:'))
+lst = [random.uniform(0, 50) for _ in range(n)]
+
+lst_10 = [random.uniform(0, 50) for _ in range(10)]
+print(
+    timeit(
+        "merge_sort(lst_10[:])",
+        globals=globals(),
+        number=1000))
+
+lst_100 = [random.uniform(0, 50) for _ in range(100)]
+print(
+    timeit(
+        "merge_sort(lst_100[:])",
+        globals=globals(),
+        number=1000))
+
+lst_1000 = [random.uniform(0, 50) for _ in range(1000)]
+print(
+    timeit(
+        "merge_sort(lst_1000[:])",
+        globals=globals(),
+        number=1000))
+
+print(lst)
+print(merge_sort(lst))
