83dac08500f2a3262b197bc84070f671ecef1890
kroleg <kr.oleg@gmail.com>
2021-07-11 14:13:07 +0300

    Task 6, 7

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -13,3 +13,56 @@
 Примечание:
 Вам не нужно писать код с нуля. Вам нужно доработать пример с урока.
 """
+import string
+
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_rear(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+
+
+def pal_checker(pal_string):
+    dc_obj = DequeClass()
+
+    # Изменения внесены здесь.
+    pal_string = pal_string.translate(str.maketrans(dict.fromkeys(string.punctuation)))
+    pal_string = ''.join(pal_string.split()).lower()
+
+    for el in pal_string:
+        dc_obj.add_to_rear(el)
+
+    still_equal = True
+
+    while dc_obj.size() > 1 and still_equal:
+        first = dc_obj.remove_from_front()
+        last = dc_obj.remove_from_rear()
+        if first != last:
+            still_equal = False
+
+    return still_equal
+
+
+print(pal_checker("молоко делили ледоколом"))
+print(pal_checker("Лёша на полке клопа нашёл"))
+print(pal_checker("Молебен о коне белом"))
+print(pal_checker("Лег на храм, и дивен и невидим архангел!"))
+print(pal_checker("топот"))
+print(pal_checker("Привет"))
