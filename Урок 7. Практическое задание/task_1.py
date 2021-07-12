28da72bf3d9d8d30aae177ddd5941645aaf420bb
alexabolot1 <alexabolot1@gmail.com>
2021-07-12 13:41:17 +0300

    complete homework for lession 7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,76 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+import random
+import timeit
+
+
+# скрипт без доработки
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+print(f'Исходный список bubble_sort - {orig_list}')
+print(f'Отсортированный список bubble_sort - {bubble_sort(orig_list)}')
+
+
+# скрипт с доработкой
+def bubble_sort_2(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        flag = True
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+                flag = False
+        if flag:
+            break
+        n += 1
+    return lst_obj
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+print(f'Исходный список bubble_sort_2 - {orig_list}')
+print(f'Отсортированный список bubble_sort_2 - {bubble_sort_2(orig_list)}')
+
+"""
+ЗАМЕРЫ
+"""
+orig_list_10 = [random.randint(-100, 100) for _ in range(10)]
+print(f'10 элементов bubble_sort - '
+      f'{timeit.timeit("bubble_sort(orig_list_10[:])",globals=globals(), number=1000)}')
+print(f'10 элементов bubble_sort_2 - '
+      f'{timeit.timeit("bubble_sort_2(orig_list_10[:])",globals=globals(), number=1000)}')
+
+orig_list_100 = [random.randint(-100, 100) for _ in range(100)]
+print(f'100 элементов bubble_sort - '
+      f'{timeit.timeit("bubble_sort(orig_list_100[:])",globals=globals(), number=1000)}')
+print(f'100 элементов bubble_sort_2 - '
+      f'{timeit.timeit("bubble_sort_2(orig_list_100[:])",globals=globals(), number=1000)}')
+
+orig_list_1000 = [random.randint(-100, 100) for _ in range(1000)]
+print(f'1000 элементов bubble_sort - '
+      f'{timeit.timeit("bubble_sort(orig_list_1000[:])",globals=globals(), number=1000)}')
+print(f'1000 элементов bubble_sort_2 - '
+      f'{timeit.timeit("bubble_sort_2(orig_list_1000[:])",globals=globals(), number=1000)}')
+
+"""
+10 элементов bubble_sort - 0.007412000000000002
+10 элементов bubble_sort_2 - 0.0057853
+100 элементов bubble_sort - 0.6612747000000001
+100 элементов bubble_sort_2 - 0.7773409000000001
+1000 элементов bubble_sort - 73.46996779999999
+1000 элементов bubble_sort_2 - 80.60540069999999
+
+Моя доработка заключалась в том, что я добавил флаг, с помощью которого цикл будет досрочно прерываться,
+если список изначально был отсортирован,что сэкономит время выполнения функции. Однако, вероятность получения
+заранее отсортированного списка крайне мала, а время выполнения обоих функций примерно равно (в случае с 
+длинными списками доработанная функция отработала даже медленее), то практического смысла в данной доработке не было.
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -36,3 +36,40 @@ for i in
 
 
 """
+from timeit import timeit
+from random import randint
+from statistics import median
+
+
+def median_gnome(lst, m):
+    i, size = 1, len(lst)
+    while i < size:
+        if lst[i - 1] <= lst[i]:
+            i += 1
+        else:
+            lst[i - 1], lst[i] = lst[i], lst[i - 1]
+            if i > 1:
+                i -= 1
+    return f'Медиана данного массива - {lst[m]}'
+
+
+def get_median(lst):
+    return f'Медиана данного массива - {median(lst)}'
+
+
+m = int(input('Введите натуральное число: '))
+test_lst = [randint(1, 10) for i in range(2 * m + 1)]
+
+print(median_gnome(test_lst[:], m))
+print(get_median(test_lst[:]))
+
+print(f'Время работы median_gnome : {timeit("median_gnome(test_lst[:], m)", globals=globals(), number=1000)}')
+print(f'Время работы get_median : {timeit("get_median(test_lst[:])", globals=globals(), number=1000)}')
+
+"""
+Время работы median_gnome : 0.03290850000000001
+Время работы get_median : 0.0009292999999996887
+
+Вывод: оказалось, что встроенная в библиотеку statistics функция median выполняется в сотни раз быстрее, 
+чем фукция основанная по методу сортировки Гномья.
+"""
