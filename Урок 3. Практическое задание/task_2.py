05709b1688ce56aa524b0f36f0812858eb8ec80c
Alexandr <ricko28@ya.ru>
2021-07-12 22:50:10 +0300

    update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -21,22 +21,94 @@
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
 
+
 import sqlite3 as sqlite
-from tempfile import TemporaryFile, TemporaryDirectory
+from hashlib import sha256
+from uuid import uuid4
+from string import ascii_lowercase
 
 
-def sqlite3_connect():
+def sqlite3_connect(database_path):
+    connect = sqlite.connect(database_path)
 
-    with TemporaryDirectory() as tmpdirname:
-        connect = sqlite.connect(f'./volkovan_task2.db')
-        cursor = connect.cursor()
+    cursor = connect.cursor()
 
-        cursor.execute('CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, salt TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, salt TEXT)')
+    return connect
 
 
-def registration():
-    return 1
+def registration(connection):
+    __salt__ = uuid4().hex
+    __username__ = input_value('логин')
+    __password__ = input_value("пароль")
+    __password_str__ = f'{__password__}{__salt__}'
+    __password_obj__ = sha256(__password_str__.encode('utf-8'))
+    __pass_hex_dig__ = __password_obj__.hexdigest()
+
+    cursor = connection.cursor()
+
+    cursor.execute('INSERT INTO users(username, password, salt) VALUES(?, ?, ?)', (__username__.lower(), __pass_hex_dig__, __salt__))
+    connection.commit()
+
+    print(f'Вы успешно зарегистрировались. Ваш хеш-пароль: {__pass_hex_dig__}')
+
+
+def authorization(connection):
+    authorization_data = {'registered': False}
+
+    __username__ = input('Введите логин:\n')
+
+    cursor = connection.cursor()
+
+    if __username__ is not None:
+        cursor.execute('SELECT * FROM users WHERE username=:username', {'username': __username__.lower()})
+        query_result = cursor.fetchall()
+
+        if len(query_result):
+            authorization_data = {'registered': True, 'password': query_result[0][1], 'salt': query_result[0][2]}
+
+    return authorization_data
+
+
+def register_question():
+    result = input('Вы не зарегистрированы. Желаете зарегистрироваться? (Y/n): ')
+    if result == 'n':
+        return False
+    elif result == 'Y':
+        return True
+    else:
+        return register_question()
+
+
+def input_value(desc):
+    __value__ = input(f'Введите новый {desc}:\n')
+
+    if __value__ == '':
+        return input_value(desc)
+    else:
+        return __value__
+
+
+def check_password(__password__, __auth_data__):
+    __salt__ = __auth_data__.get('salt')
+    __password_str__ = f'{__password__}{__salt__}'
+    __password_obj__ = sha256(__password_str__.encode('utf-8'))
+    __pass_hex_dig__ = __password_obj__.hexdigest()
+
+    return __password_obj__.hexdigest() == __auth_data__.get('password')
 
 
 if __name__ == '__main__':
-    sqlite3_connect()
+
+    db_path = './volkovan_task2.db'
+    sqlite_connection = sqlite3_connect(db_path)
+    auth_data = authorization(sqlite_connection)
+    if not auth_data.get('registered'):
+        if register_question():
+            registration(sqlite_connection)
+    else:
+        input_password = input('Введите Ваш пароль для авторизации:\n')
+        if check_password(input_password, auth_data):
+            print('Вы успешно авторизовались')
+        else:
+            print('Неверный пароль')
