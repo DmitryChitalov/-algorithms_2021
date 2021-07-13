abb645b5e25ab8977acaf8593c1df2e6d754dc34
MyEternity <spddarkforce47@gmail.com>
2021-07-13 10:58:50 +0300

    Task 5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,84 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+# Класс будет обслуживать стопку тарелок. По дефолту в стопке может быть 10 тареклок и стопка на старте - пуста.
+class PlateTower:
+    def __init__(self):
+        self.max_h = 10
+        self.count = 0
+
+    def add_plate(self):
+        if self.space_left():
+            self.count = self.count + 1
+            return True
+        else:
+            return False
+
+    def remove_plate(self):
+        if self.count > 0:
+            self.count = self.count - 1
+
+    @property
+    def plate_count(self):
+        return self.count
+
+    def space_left(self):
+        if self.max_h - self.count > 0:
+            return True
+        else:
+            return False
+
+
+# А это наш стол. На нем стоят стопки, куда мы кладем тарелки.
+class Table:
+    def __init__(self):
+        self.place = []
+        self.place.append(PlateTower())
+
+    @property
+    def get_last_pos(self):
+        if len(self.place) == 0:
+            return None
+        else:
+            return self.place[len(self.place) - 1]
+
+    def add_tower(self):
+        self.place.append(PlateTower())
+
+    def rem_plates(self, count):
+        for x in range(count):
+            if self.get_last_pos != None:
+                self.get_last_pos.remove_plate()
+                if self.get_last_pos.plate_count == 0:
+                    self.place.pop()
+
+    def add_plates(self, count):
+        if self.get_last_pos is None:
+            self.add_tower()
+        for x in range(count):
+            if self.get_last_pos.space_left():
+                self.get_last_pos.add_plate()
+            else:
+                self.add_tower()
+                self.get_last_pos.add_plate()
+
+    def __str__(self):
+        f = ''
+        if self.get_last_pos is None:
+            f = 'Стол пустой.\n'
+        else:
+            for x in range(len(self.place)):
+                f = f + (f'Стопка: {x + 1} содержит тарелок {self.place[x].plate_count} шт.\n')
+        return f
+
+
+tt = Table()
+tt.add_plates(12)
+tt.add_plates(14)
+print(tt)
+tt.rem_plates(122)
+print(tt)
+tt.add_plates(11)
+print(tt)
