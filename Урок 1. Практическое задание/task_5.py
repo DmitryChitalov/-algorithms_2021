dd73141b62c24ba8c50bbd43bdadf03a4698d581
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2021-07-13 15:38:13 +0300

    Add files via upload

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,73 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class StackOfPlatesClass:
+
+    def __init__(self, max_quantity):
+        self.unit = [[]]
+        self.max_quantity = max_quantity  # Max количество тарелок в стопке
+
+    def __str__(self):
+        if self.unit == [[]]:
+            return 'Нет ни одной тарелки.'
+        return str(self.unit)
+
+    def is_empty(self):
+        self.unit = [[]]
+        return 'Тарелок больше нет'
+
+    def push_in(self, el):
+        """Предполагаем, что верхний элемент стека находится в конце списка"""
+        if len(self.unit[len(self.unit) - 1]) < self.max_quantity:
+            self.unit[len(self.unit) - 1].append(el)
+        else:
+            self.unit.append([])
+            self.unit[len(self.unit) - 1].append(el)
+
+    def pop_out(self):
+        result = self.unit[len(self.unit) - 1].pop()
+        if len(self.unit[len(self.unit) - 1]) == 0:
+            self.unit.pop()
+        return f'Из стопки {self.stack_count()} забрали {result}.'
+
+    def get_val(self):
+        return self.unit[len(self.unit) - 1]  # Содержимое 'активной' стопки
+
+    def stack_size(self):
+        elem_sum = 0
+        for stack in self.unit:
+            elem_sum += len(stack)
+        return f'Всего тарелок: {elem_sum}'
+
+    def stack_count(self):
+        return len(self.unit)
+
+
+plates = StackOfPlatesClass(5)
+print(plates)
+plates.push_in('plate_01')
+plates.push_in('plate_02')
+plates.push_in('plate_03')
+plates.push_in('plate_04')
+plates.push_in('plate_05')
+plates.push_in('plate_06')
+plates.push_in('plate_07')
+plates.push_in('plate_08')
+plates.push_in('plate_09')
+plates.push_in('plate_10')
+plates.push_in('plate_11')
+plates.push_in('plate_12')
+plates.push_in('plate_13')
+print(plates)
+print(plates.pop_out())
+print(plates.pop_out())
+print(plates.pop_out())
+print(plates.pop_out())
+print(plates.get_val())
+print(plates.stack_size())
+print(f'Всего стопок: {plates.stack_count()}')
+print(plates)
+print(plates.is_empty())
+print(plates)
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,3 +18,90 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class QueueClass:
+    def __init__(self):
+        self.tasks = []
+
+    def __str__(self):
+        return str(self.tasks)
+
+    def is_empty(self):
+        return self.tasks == []
+
+    def to_queue(self, task_name):
+        self.tasks.insert(0, task_name)
+
+    def from_queue(self):
+        return self.tasks.pop()
+
+    def size(self):
+        return len(self.tasks)
+
+    @staticmethod
+    def ng_check():
+        print('В очереди нет задач.')
+
+
+class TaskBoard:
+    def __init__(self):
+        self.main_queue = QueueClass()  # Базовая очередь
+        self.remake_queue = QueueClass()  # Очередь на доработку
+        self.completed = []  # Список решенных задач
+
+    def new_task(self, task_name):  # создаем новую задачу(задача попадает в главную очередь)
+        task = task_name
+        self.main_queue.to_queue(task)
+
+    def resolve_task(self):  # Перемещение задачи из главной очереди в список решенных
+        if len(self.main_queue.tasks) != 0:
+            task = self.main_queue.from_queue()
+            self.completed.append(task)
+        else:
+            self.ng_check('доработки')
+
+    def to_revision_task(self):  # Перемещение задачи из главной очереди в очередь доработки
+        if len(self.main_queue.tasks) != 0:
+            task = self.main_queue.from_queue()
+            self.remake_queue.to_queue(task)
+        else:
+            self.ng_check('"главная"')
+
+    def to_current_queue(self):  # Перемещаем задачу из доработки в главную очередь
+        if len(self.remake_queue.tasks) != 0:
+            task = self.remake_queue.from_queue()
+            self.main_queue.to_queue(task)
+        else:
+            self.ng_check('доработки')
+
+    def to_completed(self):  # Перемещаем задачу из доработки в список решенных.
+        if len(self.remake_queue.tasks) != 0:
+            task = self.remake_queue.from_queue()
+            self.completed.append(task)
+        else:
+            self.ng_check('доработки')
+
+    @staticmethod
+    def ng_check(queue_name):
+        name = queue_name
+        print(f'В очереди {name} нет задач.')
+
+
+new_board = TaskBoard()
+new_board.new_task('task_01')
+new_board.new_task('task_02')
+new_board.new_task('task_03')
+new_board.new_task('task_04')
+new_board.new_task('task_05')
+print(new_board.main_queue)
+new_board.to_revision_task()
+new_board.to_completed()
+new_board.to_completed()
+new_board.new_task('task_06')
+print(new_board.remake_queue)
+print(new_board.completed)
+print(new_board.main_queue)
+new_board.resolve_task()
+print(new_board.main_queue)
+print(new_board.completed)
