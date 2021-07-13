f2775811ed122825d96bb622faa35b222a9a9cd2
ivan <ivan.pavlov.alk@gmail.com>
2021-07-13 22:35:15 +0200

    L6.T3 - Completed

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -5,3 +5,34 @@
 Придумать как это решить!
 Есть очень простое решение
 """
+from timeit import timeit
+from memory_profiler import memory_usage
+
+
+def time_memory(func):
+    def wrap(*args, **kwargs):
+        start_time = timeit()
+        start_mem = memory_usage()
+        result = func(*args, **kwargs)
+        print(f'Time: {start_time - timeit()}\nMemory: {memory_usage()[0] - start_mem[0]}')
+        return result
+    return wrap
+
+
+@time_memory
+def ascii_table_main():
+    def ascii_table(_ascii_code=32, count=0):
+        if _ascii_code <= 127:
+            if count == 10:
+                print()
+                count = 0
+            print('{0:4} - {1:4}'.format(_ascii_code, chr(_ascii_code)), end="| ")
+            ascii_table(_ascii_code + 1, count + 1)
+
+
+ascii_table_main()
+
+"""
+Профилировка рекурсивных функций возможна в случае обертки рекурсивной функции в другую функцию. В том случае данные 
+профилировки будут точными. В обратном случае, мы будем получать данные на каждый вызов рекурсивной функции.
+"""
