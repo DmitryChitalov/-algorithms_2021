310657904d84b703f11cb1044c4044e343d1045e
kroleg <kr.oleg@gmail.com>
2021-07-13 19:31:22 +0300

    Task 6, 7

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -11,3 +11,28 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+import random
+
+
+def guess_number(hidden_number, attempts_count=0, is_guessed=False, attempts_number=10):
+    if attempts_count == attempts_number or is_guessed:
+        if is_guessed is False:
+            print(f"Сожалею, вы не угадали за {attempts_count} попыток!")
+        return hidden_number
+    else:
+        number = int(input("Введите ваш ответ: \n"))
+        attempts_count += 1
+        if number < hidden_number:
+            print("Загаданное число больше!")
+        elif number > hidden_number:
+            print("Загаданное число меньше!")
+        elif number == hidden_number:
+            print(f"Поздравляю, вы угадали за {attempts_count} попыток!")
+            is_guessed = True
+
+        return guess_number(hidden_number, attempts_count, is_guessed)
+
+
+hidden_num = random.randint(1, 100)
+print("Комьютер загадал число от 1 до 100. Попробуйте отгадать за 10 попыток")
+print(f"Было загадано: {guess_number(hidden_num)}")
--- Урок 2. Практическое задание/task_7.py
+++ Урок 2. Практическое задание/task_7.py
@@ -16,3 +16,21 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def get_sum(number, counter=0, sum_numbers=0):
+    if number == counter:
+        return sum_numbers
+    else:
+        counter += 1
+        sum_numbers += counter
+        return get_sum(number, counter, sum_numbers)
+
+
+my_number = 15
+calc_rec_function = get_sum(my_number)
+calc_formula = int(my_number*(my_number+1)/2)
+print(f"Сумма элементов от 1 до {my_number} = {calc_rec_function}")
+print(f"{my_number} * ({my_number} + 1) / 2 = {calc_formula}")
+print(f"Равенство верно. Сумма элементов от 1 до {my_number} = {my_number} * ({my_number} + 1) / 2") \
+    if calc_rec_function == calc_formula else print("Равенство неверно.")
