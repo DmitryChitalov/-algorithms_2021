dd9f30aa05c81abcd06ee6158de9f652232232fb
Aleksey Shmykov <shmykov.job@gmail.com>
2021-07-13 21:52:04 +0300

    task 1

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,76 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+
+
+class Node:
+    def __init__(self, val):
+        self.l = None
+        self.r = None
+        self.v = val
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def get_root(self):
+        return self.root
+
+    def add(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            self._add(val, self.root)
+
+    def _add(self, val, node):
+        if val < node.v:
+            if node.l is not None:
+                self._add(val, node.l)
+            else:
+                node.l = Node(val)
+        else:
+            if node.r is not None:
+                self._add(val, node.r)
+            else:
+                node.r = Node(val)
+
+    def find(self, val):
+        if self.root is not None:
+            return self._find(val, self.root)
+        else:
+            return None
+
+    def _find(self, val, node):
+        if val == node.v:
+            return node
+        elif (val < node.v and node.l is not None):
+            return self._find(val, node.l)
+        elif (val > node.v and node.r is not None):
+            return self._find(val, node.r)
+
+    def delete_tree(self):
+        self.root = None
+
+    def print_tree(self):
+        if self.root is not None:
+            self._print_tree(self.root)
+
+    def _print_tree(self, node):
+        if node is not None:
+            self._print_tree(node.l)
+            print(str(node.v) + ' ')
+            self._print_tree(node.r)
+
+
+tree = Tree()
+tree.add(3)
+tree.add(4)
+tree.add(0)
+tree.add(8)
+tree.add(2)
+tree.print_tree()
+print(tree.find(3).v)
+print(tree.find(10))
+tree.delete_tree()
+tree.print_tree()
