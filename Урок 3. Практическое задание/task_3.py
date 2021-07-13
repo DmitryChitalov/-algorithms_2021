4c89e01777461edecc2068340b0d1e548bff6155
Alexandr <ricko28@ya.ru>
2021-07-13 11:50:25 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -16,3 +16,24 @@
 р
 а
 """
+
+# Пытался решить через алгоритм МакКрейта, не получилось :(
+
+
+def uniq_subsrtring(user_string):
+    substrings_set = set()
+    max_index = len(user_string)
+
+    for i in range(max_index):
+        for j in range(max_index):
+            iteration_value_hash = hash(user_string[i:j + 1])
+            substrings_set.add(iteration_value_hash)
+
+    substrings_set.remove(hash(''))
+    substrings_set.remove(hash(user_string))
+
+    return len(substrings_set)
+
+
+target_string = 'рара'
+print (f'Количество уникальных подстрок в строке "{target_string}": {uniq_subsrtring(target_string)}')
