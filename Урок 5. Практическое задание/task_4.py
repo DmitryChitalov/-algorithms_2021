537fe385d888cd982d6134672e5b34f8241af2b6
Viktor Yagodin <Viktor.v.yagodin@gmail.com>
2021-07-14 23:55:07 +0500

    all done

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -11,18 +11,17 @@ from timeit import timeit
 
 
 dt = {}
-odt = OrderedDict
+odt = OrderedDict([])
 
 
 def fill_1(dt: dict):
-    for i in range(100):
-        dt.update({i: i})
+    for i in range(1000):
+        dt[i] = i
 
 
-def fill_2(odt: OrderedDict):  
-    for i in range(100):
+def fill_2(odt: OrderedDict):
+    for i in range(1000):
         odt[i] = i
-    print(odt)
 
 
 def get_el1(dt: dict, el):
@@ -30,14 +29,14 @@ def get_el1(dt: dict, el):
 
 
 def get_el2(odt: OrderedDict, el):
-    return dt.get(el)
+    return odt.get(el)
 
 
-def pop_1(dt: dict, el):
+def pop_1(dt: dict):
     return dt.popitem()
 
 
-def pop_2(odt: OrderedDict, el):
+def pop_2(odt: OrderedDict):
     return odt.popitem()
 
 
@@ -48,5 +47,10 @@ print('Получение элемента:')
 print(timeit('get_el1(dt, 1)', globals=globals(), number=1000))
 print(timeit('get_el2(odt, 1)', globals=globals(), number=1000))
 print('Выброс элемента:')
-print(timeit('pop_1(dt, 1)', globals=globals(), number=1000))
-print(timeit('pop_2(odt, 1)', globals=globals(), number=1000))
+print(timeit('pop_1(dt)', globals=globals(), number=1000))
+print(timeit('pop_2(odt)', globals=globals(), number=1000))
+
+'''
+Основные операции выполняются примерно одинаково, у OrderedDict чуть медленнее, смысл использовать OrderedDict 
+в новых версиях языка есть только если нужно подвигать элементы с помощью movetoend   
+'''
