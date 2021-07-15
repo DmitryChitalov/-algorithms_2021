46484d3fe9dae4a3ac0b667846b8fce8a5160078
Alexandr <ricko28@ya.ru>
2021-07-16 00:45:02 +0300

    update tsak_3.py

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -15,6 +15,10 @@
 """
 
 
+import timeit
+import cProfile
+
+
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -37,3 +41,49 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+# Взял свой же алгоритм из второго задания
+def revers_4(enter_num, result):
+    this_number = enter_num % 10
+
+    result = f'{result}{this_number}'
+
+    if len(str(enter_num)) > 1:
+        next_numbers = enter_num // 10
+        return revers_4(next_numbers, result)
+
+    return result
+
+
+# Алгоритм, предложенный гуглом
+def revers_5(enter_num):
+    return ''.join(reversed(str(enter_num)))
+
+
+numbers = 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+# numbers = 1234567890
+empty_str = ''
+
+print(f'Проверка TimeIt:\n'
+      f'1. reverse_1: {timeit.timeit("revers_1(numbers)", globals=globals())}\n'
+      f'2. reverse_2: {timeit.timeit("revers_2(numbers)", globals=globals())}\n'
+      f'3. reverse_3: {timeit.timeit("revers_3(numbers)", globals=globals())}\n'
+      f'4. reverse_4: {timeit.timeit("revers_4(numbers, empty_str)", globals=globals())}\n'
+      f'5. reverse_5: {timeit.timeit("revers_5(numbers)", globals=globals())}\n')
+
+cProfile.run('revers_1(numbers)')
+cProfile.run('revers_2(numbers)')
+cProfile.run('revers_3(numbers)')
+cProfile.run('revers_4(numbers, empty_str)')
+cProfile.run('revers_5(numbers)')
+
+"""
+Вывод:
+Самыми эффективной оказалась вторая функция. Не зависимо от объема данных, она всегда выполняется менее,чем за
+секунду. Обусловлено это тем, что сложность алгоритма константная. Исходя из данных профилирования, следует, что она так
+же, как и вторая функция, задействует всего 4 штатные функции. Но вторая медленнее, потому что алгоритм является линейным.
+Так же хорошо, на большом объеме данных, показывает себя пятая функция. При 109 символах, среднее время выполнения 1, 2
+функции составляет ~30 секунд, третья справляется за 0.7 сек, пятая за 2.4, а вот 4 за 70 секунд, т.к. рекурсия + 221 
+вызов стандартных функций
+"""
