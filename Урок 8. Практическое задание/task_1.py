00be92a8fbe34b064581e373565227abb5c95a34
a2kat <mousedrw@ya.ru>
2021-07-15 16:55:30 +0300

    les8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,56 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+
+"""
+    решил взять и разобрать кодировку из интернета, мне она показалась более понятной
+"""
+import heapq
+from collections import Counter, namedtuple
+
+
+class Node(namedtuple("Node", ["left", 'right'])):  # Класс для узлов дерева
+    def walk(self, code, acc):
+        self.left.walk(code, acc + "0")  # acc - код который мы накопили спускаясь от корня до данного узла
+        self.right.walk(code, acc + "1")
+
+
+class Leaf(namedtuple("Leaf", ["char"])):  # Класс для листьев
+    def walk(self, code, acc):
+        code[self.char] = acc or "0"  # or '0' нужен на случай если на вход подали только 1 символ
+
+
+def huffman_encode(s):
+    h = []  # создаем список и наполняем его нашими элементами как листьями
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+    heapq.heapify(h)  # преобразовываем список в кучу
+    # count необходим для heappush, без него в случае 2 одинаковых по количеству
+    # элемментов она начинает сравнивать строку с классом
+    count = len(h)
+    while len(h) > 1:  # строим узлы пока есть хотябы 2 элемента
+        freq1, _count1, left = heapq.heappop(h)  # доставем элемент с минимальной частотой
+        freq2, _count2, right = heapq.heappop(h)  # и следующий за ним с минимальной частотой
+        # создаем новый элемент равный сумме частот двух взятых
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+
+    code = dict()
+    # Обходим дерево только если есть строки, на случай если на вход подали пустую строку
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, "")
+    return code
+
+
+def main(): # запрашиваем строку и кодируем её
+    s = input()
+    code = huffman_encode(s)
+    encoded = "".join(code[ch] for ch in s) # соединяем код в 1 строку
+    print(len(code), len(encoded)) # выводим сперва сколько всего символов закодировано, затем длинну кода
+    for ch in sorted(code):
+        print("{}: {}".format(ch, code[ch])) # выводим код каждого символа
+    print(encoded) # выводим конечный код
+
+if __name__ == '__main__':
+    main()
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,28 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+"""
+Не совсем понял как происходит добавление узлов, допустим есть схема
+-----------корень----------------
+----------/-----\----------------
+-----узел1------узел2------------
+----/-----\---------\------------
+-узел3----узел4------узел5-------
+
+чтобы дерево оставалось бинарным узел 4 должен быть больше узла1 и узла3, но меньше корня
+в данном ООП при вставке insert_left значения вставляются слева... и понятно что так можно вставить
+узел1 и узел3, а как при этом вставить узел4 ?
+insert_right - как я понял вставляет значения направо и через него можно создать узел2 и узел5
+
+Не осилил задачу... в основном из за того что поздно взялся, была идея переписать польностью код,
+но времени на сдачу не остаётся поэтому сдаю как есть, а над решением буду ломать голову в перерыве между курсами
+Спасибо за курс, очень надеюсь встретиться с вами в дальнейшем.
+"""
+
+
+class ValidationError(Exception):
+    pass
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -21,33 +43,43 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        try:
+            if new_node >= self.get_root_val():
+                raise ValidationError()
+            # если у узла нет левого потомка
+            if self.left_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
+        except ValidationError:
+            print("Попытка вставить значение в неверный узел")
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        try:
+            if new_node < self.get_root_val():
+                raise ValidationError()
+            # если у узла нет правого потомка
+            if self.right_child == None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
+        except ValidationError:
+            print("Попытка вставить значение в неверный узел")
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -67,13 +99,14 @@ class BinaryTree:
 
 
 r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
+r.insert_left(9) # Попытка вставить значение в неверный узел
+r.insert_left(4)
+r.insert_left(2)
+r.insert_left(1)
+r.insert_right(7) # Попытка вставить значение в неверный узел
 r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+r.insert_right(16)
+print(f'Верхний корень дерева = {r.get_root_val()}')
+
+
+
