5353eef7d0b330817251aade83766e924649349c
Alexandr <ricko28@ya.ru>
2021-07-15 15:27:47 +0300

    update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -17,9 +17,24 @@
 """
 
 
+from timeit import timeit
+
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [idx for num, idx in enumerate(nums) if num % 2 == 0]
+
+
+nums_list = [i for i in range(0, 100)]
+
+print(timeit('func_1(nums_list)', globals=globals()))
+print(timeit('func_2(nums_list)', globals=globals()))
+
+"""Реализация алгоритма черех лист комприхеншн дает прирост к скорости и более компактный вариант написания"""
