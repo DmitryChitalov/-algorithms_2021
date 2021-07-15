0420a12592e0789d6a1fa7c373c2bb3d92c5030d
kroleg <kr.oleg@gmail.com>
2021-07-15 23:48:21 +0300

    Add Task 2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -20,3 +20,106 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+import hashlib
+import sqlite3
+import os.path
+
+
+class MyDatabaseConnector:
+    """
+    Класс для работы с БД sqlLite3.
+    """
+    __path_to_db = os.path.join(os.path.dirname(__file__), "test.db")
+
+    def __init__(self):
+        """
+        Инициализация.
+        """
+        self.__connect = None
+        try:
+            self.__connect = sqlite3.connect(self.__path_to_db)
+            self.cursor = self.__connect.cursor()
+        except sqlite3.Error as e:
+            print(e)
+
+    def create_table(self):
+        """
+        Создание/Пересоздание таблицы.
+        """
+        sql = """drop TABLE IF EXISTS credentials;
+                 CREATE TABLE IF NOT EXISTS credentials(
+                        id INTEGER NOT NULL UNIQUE,
+                        user_name	TEXT,
+                        password_hash	TEXT,
+                        PRIMARY KEY(id)
+                 );
+                 drop INDEX IF EXISTS index_of_user_name;
+                 CREATE UNIQUE INDEX IF NOT EXISTS index_of_user_name ON credentials(user_name);"""
+        try:
+            self.cursor.executescript(sql).fetchall()
+        except sqlite3.Error as e:
+            print(e)
+
+    def save_password_hash(self, user_name, password_hash):
+        """
+        Сохранение хеша введенного пароля.
+        :param user_name:
+        :param password_hash:
+        :return:
+        """
+        if user_name and password_hash:
+            sql = """insert into credentials(user_name, password_hash) values(?, ?);"""
+            try:
+                self.cursor.execute(sql, (user_name, password_hash))
+                self.__connect.commit()
+            except sqlite3.Error as e:
+                print(e)
+
+    def get_password_hash(self, user_name):
+        """
+        Получения хеша пароля по имени пользователя.
+        :param user_name:
+        :return:
+        """
+        sql = """select c.password_hash
+                 from credentials c
+                 where c.user_name = :user_name;"""
+
+        try:
+            password_hash_list = self.cursor.execute(sql, {"user_name": user_name}).fetchall()
+        except sqlite3.Error as e:
+            print(e)
+
+        password_hash = None
+
+        if len(password_hash_list) > 0:
+            password_hash = password_hash_list[0][0]
+
+        return password_hash
+
+
+# Подготовка БД.
+m_sql = MyDatabaseConnector()
+m_sql.create_table()
+
+salt = "user1"
+
+password = input("Введите пароль: \n")
+
+# Соль-часть хеша.
+res = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+
+m_sql.save_password_hash(salt, res)
+
+pass_hash = m_sql.get_password_hash(salt)
+
+print(f"В базе данных хранится строка: {pass_hash}\n")
+
+password = input("Введите пароль еще раз для проверки: \n")
+
+res2 = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+
+if pass_hash == res2:
+    print("Вы ввели правильный пароль!")
+else:
+    print("Вы ввели не правильный пароль!")
