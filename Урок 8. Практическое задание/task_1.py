23b66d68ad4c244385e64dcc44cad269377cf985
alexabolot1 <alexabolot1@gmail.com>
2021-07-15 15:20:00 +0300

    complete homework for lession 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,47 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+from collections import Counter
+
+
+class BinaryTree:
+    def __init__(self, root_obj='root', weight=0):
+        self.root = root_obj
+        self.weight = weight
+        self.left_child = None
+        self.right_child = None
+
+    def get_full_tree(self, path='', code_table={}):
+        if isinstance(self.left_child, BinaryTree):
+            self.left_child.get_full_tree(f'{path}0', code_table)
+        else:
+            code_table[self.left_child] = f'{path}0'
+        if isinstance(self.right_child, BinaryTree):
+            self.right_child.get_full_tree(f'{path}1', code_table)
+        else:
+            code_table[self.right_child] = f'{path}1'
+        return code_table
+
+
+def get_tree(left, right):
+    tree = BinaryTree(weight=left[1]+right[1])
+    tree.left_child = left[0]
+    tree.right_child = right[0]
+    return tree
+
+
+user_str = input("Введите строку: ")
+print(f'Ваша строка: {user_str}')
+count = Counter(user_str)
+freq_list = sorted(count.items(), key=lambda x: x[1], reverse=True)
+while len(freq_list) > 1:
+    min_1 = freq_list.pop()
+    min_2 = freq_list.pop()
+    tree = get_tree(min_1, min_2)
+    min_union = (tree, tree.weight)
+    freq_list.append(min_union)
+    freq_list = sorted(freq_list, key=lambda x: x[1], reverse=True)
+
+code_table = freq_list[0][0].get_full_tree()
+print(f'Кодирование по Хаффману {code_table}')
+print('Шифровка: ', " ".join(list(map(lambda x: code_table[x], user_str))))
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,7 +10,19 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
+class Validation(Exception):
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return f'{self.text}'
+
+
 class BinaryTree:
+    # Организация слотов
+    __slots__ = ('root', 'left_child', 'right_child')
+
     def __init__(self, root_obj):
         # корень
         self.root = root_obj
@@ -21,33 +33,45 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        try:
+            if self.root > new_node:
+                # если у узла нет левого потомка
+                if self.left_child is None:
+                    # тогда узел просто вставляется в дерево
+                    # формируется новое поддерево
+                    self.left_child = BinaryTree(new_node)
+                # если у узла есть левый потомок
+                else:
+                    # тогда вставляем новый узел
+                    tree_obj = BinaryTree(new_node)
+                    # и спускаем имеющегося потомка на один уровень ниже
+                    tree_obj.left_child = self.left_child
+                    self.left_child = tree_obj
+            else:
+                raise Validation(f'Потомок слева больше корня! Это недопустимо! {new_node} > {self.root}')
+        except Validation as text:
+            print(text)
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        try:
+            if self.root < new_node:
+                # если у узла нет правого потомка
+                if self.right_child is None:
+                    # тогда узел просто вставляется в дерево
+                    # формируется новое поддерево
+                    self.right_child = BinaryTree(new_node)
+                # если у узла есть правый потомок
+                else:
+                    # тогда вставляем новый узел
+                    tree_obj = BinaryTree(new_node)
+                    # и спускаем имеющегося потомка на один уровень ниже
+                    tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+            else:
+                raise Validation(f'Потомок справа больше корня! Это недопустимо! {new_node} < {self.root}')
+        except Validation as text:
+            print(text)
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -69,7 +93,7 @@ class BinaryTree:
 r = BinaryTree(8)
 print(r.get_root_val())
 print(r.get_left_child())
-r.insert_left(40)
+r.insert_left(7)
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
 r.insert_right(12)
