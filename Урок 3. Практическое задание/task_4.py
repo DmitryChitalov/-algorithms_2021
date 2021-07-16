521e3e4caa006a68cc49dc362a9b0901eb6f5954
kroleg <kr.oleg@gmail.com>
2021-07-16 13:40:24 +0300

    add Task 4

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,53 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+from uuid import uuid4
+from hashlib import sha256
+
+
+class URLCache:
+    """
+    Класс для работы с кешем. Храним данные в словаре url: hash.
+    """
+    def __init__(self):
+        self.__cache = dict()
+
+    def get_value(self, value):
+        if not self.__cache.get(value):
+            __salt = uuid4().hex
+            __value = value + __salt
+            __value_hash = sha256(__value.encode('utf-8'))
+            __value_hexdig = __value_hash.hexdigest()
+
+            self.__cache[value] = __value_hexdig
+
+        return self.__cache[value]
+
+    def get_cache(self):
+        return self.__cache
+
+    def clear_cash(self):
+        self.__cache.clear()
+
+
+url_cache = URLCache()
+
+urls = ["https://yandex.ru", "https://google.ru", "https://mail.ru", "https://stackoverflow.com",
+        "https://github.com", "https://mail.google.com/", "https://mail.ru", "https://google.ru"]
+
+for u in urls:
+    url_cache.get_value(u)
+
+print([f"{k}: {v};" for k, v in url_cache.get_cache().items()])
+
+url_cache.clear_cash()
+
+
+
+
+
+
+
+
+
