21c9f6bd0a81c9bc901170e6b53aae8e0888faad
Alexandr <ricko28@ya.ru>
2021-07-19 01:36:11 +0300

    Update task_2.py

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -4,3 +4,13 @@
 для оптимизации памяти и
 доказать!!! (наглядно, кодом) их эффективность (на примере профилировщика).
 """
+
+
+from timeit import timeit
+
+
+data_list = [i for i in range(1000)]
+
+
+#print(f'Стандартный цикл: {timeit("basic_func()", globals=globals())}')
+#print(f'Стандартный цикл: {timeit("optimized_func()", globals=globals())}')
