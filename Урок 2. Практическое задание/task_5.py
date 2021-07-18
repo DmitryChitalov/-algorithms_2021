9ec7ec421784b0f1ae4ec641aa69f963d745b8bb
MonakhovEvgeny <79536395+MonakhovEvgeny@users.noreply.github.com>
2021-07-18 14:39:36 +0300

    Update task_5.py

--- Урок 2. Практическое задание/task_5.py
+++ Урок 2. Практическое задание/task_5.py
@@ -21,3 +21,24 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+count = 32
+count_1 = 0
+res = []
+
+
+def table():
+    global count
+    global count_1
+    if count < 128:
+        print(f' {count} - {chr(count)}', end=' ')
+
+        count += 1
+        count_1 += 1
+        if count_1 % 10 == 0:
+            print('\n')
+        return table()
+
+
+table()
