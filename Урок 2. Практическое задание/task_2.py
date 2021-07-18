34b226c6e39d66acaa48dc0a15c9ec2620505abf
MonakhovEvgeny <79536395+MonakhovEvgeny@users.noreply.github.com>
2021-07-18 14:36:58 +0300

    Update task_2.py

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -18,3 +18,31 @@
 Решите через рекурсию. Решение через цикл не принимается.
 Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 """
+
+
+def even_odd_numbers_input():
+    try:
+        number = int(input('Введите число - '))
+    except ValueError:
+        print("Вводить можно только натуральные числа")
+        return even_odd_numbers_input()
+    else:
+        print(f'\nВ числе {number} ')
+        return number
+
+
+def even_odd_numbers(number, even=0, odd=0):
+    if number == 0:
+        print(f'четных чисел - {even}, нечетных - {odd}')
+
+    else:
+        remains = number % 10
+        number = number // 10
+        if remains % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+        return even_odd_numbers(number, even, odd)
+
+
+even_odd_numbers(even_odd_numbers_input())
