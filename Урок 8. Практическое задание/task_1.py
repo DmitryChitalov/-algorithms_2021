7918c784d8132e68b7888cb6a5efe28999bbc901
ivan <ivan.pavlov.alk@gmail.com>
2021-07-19 19:34:02 +0200

    L8.T1 - Completed

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,33 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+import heapq
+from collections import defaultdict
+
+
+def huff_tree(freq):
+    heap = [[weight, [char, '']] for char, weight in freq.items()]
+    heapq.heapify(heap)
+    while len(heap) > 1:
+        left = heapq.heappop(heap)
+        right = heapq.heappop(heap)
+        for val in left[1:]:
+            val[1] = '0' + val[1]
+        for val in right[1:]:
+            val[1] = '1' + val[1]
+        heapq.heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
+    return sorted(heapq.heappop(heap)[1:], key=lambda x: (len(x[-1]), x))
+
+
+s_string = 'Encode a String in Huffman Coding Using Python'
+
+frequency = defaultdict(int)
+
+for chars in s_string:
+    frequency[chars] += 1
+
+res_tree = huff_tree(frequency)
+
+print("Char".ljust(10) + "Count".ljust(10) + "Huffman Code")
+for el in res_tree:
+    print(el[0].ljust(10) + str(frequency[el[0]]).ljust(10) + el[1])
