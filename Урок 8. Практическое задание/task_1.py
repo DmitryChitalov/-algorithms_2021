98d1a6618397c76959c5ab84559ab448c525a98e
Vyacheslav <lvv_1@list.ru>
2021-07-19 23:35:48 +0300

    Ready

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,50 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+import heapq
+from collections import Counter, namedtuple
+
+
+# Из всех вариантов мне понравился этот. Лаконичный.
+
+
+class Node(namedtuple("Node", ["left", "right"])):
+    def walk(self, code, a):
+        self.left.walk(code, a + "0")
+        self.right.walk(code, a + "1")
+
+
+class Leaf(namedtuple("Leaf", ["char"])):
+    def walk(self, code, a):
+        code[self.char] = a or "0"
+
+
+def huffman(s):
+    t = []
+    for ch, freq in Counter(s).items():
+        t.append((freq, len(t), Leaf(ch)))
+    heapq.heapify(t)
+    count = len(t)
+    while len(t) > 1:
+        freq1, _count1, left = heapq.heappop(t)
+        freq2, _count2, right = heapq.heappop(t)
+        heapq.heappush(t, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+    code = {}
+    if t:
+        [(_freq, _count, root)] = t
+        root.walk(code, "")
+    return code
+
+
+def first():
+    s = input("Введите строку, которую необходимо закодировать: ")
+    code = huffman(s)
+    coded = "".join(code[ch] for ch in s)
+    print(f"В строке {len(code)} уникальных символов, каждому символу соответствует код: ")
+    for ch in sorted(code):
+        print(f"'{ch}' - {code[ch]}")
+    print(f"Закодированная строка, длиной {len(coded)} знаков: {coded}.")
+
+
+first()
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,70 +10,62 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
 class BinaryTree:
+    __slots__ = ["root", "left_child", "right_child"]
+
     def __init__(self, root_obj):
-        # корень
         self.root = root_obj
-        # левый потомок
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
-    def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
-
-    # добавить правого потомка
-    def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
-
-    # метод доступа к правому потомку
-    def get_right_child(self):
+    def get_right_child(self):  # метод доступа вправо
         return self.right_child
 
-    # метод доступа к левому потомку
-    def get_left_child(self):
+    def get_left_child(self):  # метод доступа влево
         return self.left_child
 
-    # метод установки корня
-    def set_root_val(self, obj):
+    def set_root_val(self, obj):  # корень
         self.root = obj
 
-    # метод доступа к корню
-    def get_root_val(self):
+    def get_root_val(self):  # метод доступа к корню
         return self.root
 
+    def insert_left(self, new_node):
+        if new_node > self.root:
+            print(f'{new_node} больше корня и будет добавлен вправо')
+            self.insert_right(new_node)
+        else:
+            if self.left_child is None:
+                self.left_child = BinaryTree(new_node)
+            else:
+                tree_obj = BinaryTree(new_node)
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
 
-r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+    def insert_right(self, new_node):
+        if new_node < self.root:
+            print(f'{new_node} меньше корня и будет добавлен влево')
+            self.insert_left(new_node)
+        else:
+            if self.right_child is None:
+                self.right_child = BinaryTree(new_node)
+            else:
+                tree_obj = BinaryTree(new_node)
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
+
+
+tree = BinaryTree(8)
+
+print(tree.get_root_val())
+print(tree.get_left_child())
+tree.insert_left(40)
+print(tree.get_left_child())
+print(tree.get_right_child())
+print(tree.get_right_child().get_root_val())
+tree.insert_right(6)
+print(tree.get_right_child())
+
+del tree
+
