3fb9e89974cdd8f8fe6b95e07934eb2398148c8c
kroleg <kr.oleg@gmail.com>
2021-07-21 01:30:43 +0300

    Task 1,2,3

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,8 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +39,51 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num, revers_num=0):
+    num = len(str(enter_num))
+    for _ in range(num):
+        num = enter_num % 10
+        revers_num = (revers_num * 10) + num
+        enter_num //= 10
+
+    return revers_num
+
+
+def revers_5(enter_num):
+    my_list = list(str(enter_num))
+    my_list.reverse()
+    # converting list into number
+    return ''.join(my_list)
+
+
+number = 98789697987
+
+print("revers_1: ", timeit(stmt="revers_1(number)", number=100000, globals=globals()))
+print("revers_2: ", timeit(stmt="revers_2(number)", number=100000, globals=globals()))
+print("revers_3: ", timeit(stmt="revers_3(number)", number=100000, globals=globals()))
+print("revers_4: ", timeit(stmt="revers_3(number)", number=100000, globals=globals()))
+print("revers_5: ", timeit(stmt="revers_3(number)", number=100000, globals=globals()))
+
+run('revers_1(number)')
+run('revers_2(number)')
+run('revers_3(number)')
+run('revers_4(number)')
+run('revers_5(number)')
+
+"""
+revers_1:  1.1993071
+revers_2:  0.4233123000000001
+revers_3:  0.0987146000000001
+revers_4:  0.09894999999999987
+revers_5:  0.09915370000000001
+
+Добавлены методы revers_4 и revers_5.
+Выводы:
+Самая эффективная реализация со срезами в функции revers_3.
+Имеет наименьшее время выполнения согласно библиотеки timeit и содержит 4 вызова функции на запуск которых ушло 
+0 секунд, согдасно библиотеки cProfile.
+  
+Так же, хорошее время показали методы revers_4 и revers_5, но у revers_4 - 5 вызовов функции, а у revers_5 - 6 вызовов.
+"""
