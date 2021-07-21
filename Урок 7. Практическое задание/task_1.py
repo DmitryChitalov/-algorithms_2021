d3a925182adb5086599e3e0ab6620596548e6eed
Viktor Yagodin <Viktor.v.yagodin@gmail.com>
2021-07-21 21:37:33 +0500

    task 1,2 task 3 in progress

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,66 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+import random
+from timeit import timeit
+
+
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def bubble_sort_mod(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        old_lst = set(lst_obj)
+        for i in range(len(lst_obj)-n):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        if old_lst == set(lst_obj):
+            return lst_obj
+        n += 1
+    return lst_obj
+
+
+orig_list = [random.randint(-100, 99) for n in range(50)]
+print(orig_list)
+print(bubble_sort(orig_list[:]))
+print(bubble_sort_mod(orig_list[:]))
+
+orig_list = [random.randint(-100, 99) for n in range(10)]
+print(timeit("bubble_sort(orig_list[:])", globals=globals(), number=100))
+orig_list = [random.randint(-100, 99) for n in range(100)]
+print(timeit("bubble_sort(orig_list[:])", globals=globals(), number=100))
+orig_list = [random.randint(-100, 99) for n in range(1000)]
+print(timeit("bubble_sort(orig_list[:])", globals=globals(), number=100))
+
+orig_list = [random.randint(-100, 99) for n in range(10)]
+print(timeit("bubble_sort_mod(orig_list[:])", globals=globals(), number=100))
+orig_list = [random.randint(-100, 99) for n in range(100)]
+print(timeit("bubble_sort_mod(orig_list[:])", globals=globals(), number=100))
+orig_list = [random.randint(-100, 99) for n in range(1000)]
+print(timeit("bubble_sort_mod(orig_list[:])", globals=globals(), number=100))
+
+'''
+Проведена модификация - проверка изменился ли сортируемый массив после прохода цикла с заданным n.
+Узкое место - правильно сравнить массивы, я применил перевод во множество и их сравнение. 
+Если провести неверно, результаты будут даже несколько хуже
+
+Результаты замеров:
+немодифицированная сортировка:
+0.0021225110000000005
+0.175764837
+19.663375577
+модифицированная сортировка:
+0.0005840130000009935
+0.006014940000000024
+0.0646748410000022
+
+Модификация дает огромную экономию времени, главное правильно провести сравнение списков
+'''
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,100 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+from timeit import timeit
+import random
+
+
+def merge_sort_recurs(lst_obj):  # исходный алгоритм - рекурсивный
+    if len(lst_obj) > 1:
+        center = len(lst_obj) // 2
+        left = lst_obj[:center]
+        right = lst_obj[center:]
+
+        merge_sort_recurs(left)
+        merge_sort_recurs(right)
+
+        # перестали делить
+        # выполняем слияние
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst_obj[k] = left[i]
+                i += 1
+            else:
+                lst_obj[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst_obj[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            lst_obj[k] = right[j]
+            j += 1
+            k += 1
+        return lst_obj
+
+
+def merge_sort_iter(lst_obj):  # альтернативный алгоритм - итеративный
+    support = lst_obj[:]
+    n = len(support)
+    size = 1
+    while size < n:
+        j = 0
+        while j < n - size:
+            merge(lst_obj, support, j, j + size - 1, j + size, min(j + 2 * size - 1, n - 1))
+            j += size * 2
+        size = size * 2
+    return lst_obj
+
+
+def merge(lst_obj, support, ls, le, rs, re):
+    for i in range(ls, re + 1):
+        support[i] = lst_obj[i]
+    l = ls
+    r = rs
+    for i in range(ls, re + 1):
+        if l > le:
+            lst_obj[i] = support[r]
+            r += 1
+        elif r > re:
+            lst_obj[i] = support[l]
+            l += 1
+        elif support[l] < support[r]:
+            lst_obj[i] = support[l]
+            l += 1
+        else:
+            lst_obj[i] = support[r]
+            r += 1
+    return None
+
+
+orig_list = [random.uniform(0, 50) for n in range(10)]
+print(orig_list)
+print(merge_sort_recurs(orig_list[:]))
+print(merge_sort_iter(orig_list[:]))
+
+orig_list = [random.uniform(0, 50) for n in range(10)]
+print('Рекурсивный алгоритм, 10 эл-тов:', timeit('merge_sort_recurs(orig_list[:])', globals=globals(), number=1000))
+print('Итеративный алгоритм, 10 эл-тов:', timeit('merge_sort_iter(orig_list[:])', globals=globals(), number=1000))
+orig_list = [random.uniform(0, 50) for n in range(100)]
+print('Рекурсивный алгоритм, 100 эл-тов:', timeit('merge_sort_recurs(orig_list[:])', globals=globals(), number=1000))
+print('Итеративный алгоритм, 100 эл-тов:', timeit('merge_sort_iter(orig_list[:])', globals=globals(), number=1000))
+orig_list = [random.uniform(0, 50) for n in range(1000)]
+print('Рекурсивный алгоритм, 1000 эл-тов:', timeit('merge_sort_recurs(orig_list[:])', globals=globals(), number=1000))
+print('Итеративный алгоритм, 1000 эл-тов:', timeit('merge_sort_iter(orig_list[:])', globals=globals(), number=1000))
+
+'''
+Алгоритмы имеют одинаковую сложность O(N logN), рекурсивный выполняется немного быстрее
+
+Рекурсивный алгоритм, 10 эл-тов: 0.028356761
+Итеративный алгоритм, 10 эл-тов: 0.037938022
+Рекурсивный алгоритм, 100 эл-тов: 0.46147415700000005
+Итеративный алгоритм, 100 эл-тов: 0.5196910820000001
+Рекурсивный алгоритм, 1000 эл-тов: 6.378412496999999
+Итеративный алгоритм, 1000 эл-тов: 7.882305422999999
+'''
