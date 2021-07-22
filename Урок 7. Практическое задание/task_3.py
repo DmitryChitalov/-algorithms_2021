992741caf8341005f582389a3f2d9a1b65432a03
Alexandr <ricko28@ya.ru>
2021-07-22 17:03:16 +0300

    Update task_3.py

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -11,7 +11,7 @@
 
 Важно: стройте массив именно по формуле 2m+1
 Потому что параметр m вам пригодится при поиске медианы, когда массив будет отсортирован.
-Этот парамет m вам нужно запрашивать у пользователя.
+Этот параметр m вам нужно запрашивать у пользователя.
 
 [5, 3, 4, 3, 3, 3, 3]
 
@@ -40,3 +40,30 @@ for i in
 
 
 """
+
+
+from random import randint
+from statistics import median
+
+
+def shell_sort(tagret_list):
+    gap = len(tagret_list) // 2
+    while gap > 0:
+        for i in range(gap, len(tagret_list)):
+            temp = tagret_list[i]
+            j = i
+            while j >= gap and tagret_list[j - gap] > temp:
+                tagret_list[j] = tagret_list[j - gap]
+                j = j - gap
+            tagret_list[j] = temp
+        gap = gap // 2
+    return tagret_list, tagret_list[m]
+
+
+m = int(input(f'Введите значение m:\n'))
+test_list = [randint(0, 100) for x in range(2*m + 1)]
+result, m_result = shell_sort(test_list.copy())
+print(f'Исходный список: {test_list}\n'
+      f'Отсортированный список: {result}\n'
+      f'Медиана: {m_result}\n'
+      f'Проверка медианы: {median(result)}')
