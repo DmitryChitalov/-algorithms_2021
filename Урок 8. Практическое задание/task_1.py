3ec6114e36eff5ea99d52bdfad3757bf6d146590
kosarev-sa <kosarev-sa@mail.ru>
2021-07-23 21:34:26 +0300

    2 задачи +

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,74 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+
+
+class Haffman:
+    def __init__(self, obj):
+        self.obj = obj
+        self.bin_tab = {}
+
+    def __str__(self):
+        self.haffman_bin(self.haffman_structure())
+        code = ''
+        for i in self.obj:
+            code += f'{self.bin_tab[i]} '
+        return code
+
+    def haffman_bin(self, dct, bin=''):
+        if isinstance(dct, dict):
+            self.haffman_bin(dct[0], bin=f'{bin}0')
+            self.haffman_bin(dct[1], bin=f'{bin}1')
+        else:
+            self.bin_tab[dct] = bin
+
+    def my_sort(self, dct):
+        sort_i_cnt = {}
+        for i in sorted(dct, key=dct.get, reverse=True):
+            sort_i_cnt[i] = dct[i]
+        return sort_i_cnt
+
+    def haffman_structure(self):
+        cnt = {}
+        for i in set(self.obj):
+            cnt[i] = 0
+            for j in self.obj:
+                if i == j:
+                    cnt[i] += 1
+        sort_i_cnt = self.my_sort(cnt)
+
+        if len(sort_i_cnt) == 1:
+            double = {0: sort_i_cnt.popitem()[0], 1: None}
+            return double
+        else:
+            while len(sort_i_cnt) > 1:
+                double_cnt = []
+                sum_double_cnt = 0
+                for i in range(2):
+                    double_cnt.append(sort_i_cnt.popitem())
+                    sum_double_cnt += double_cnt[-1][1]
+                double = {0: double_cnt[0][0],
+                          1: double_cnt[1][0]}
+                sort_i_cnt[str(double)] = sum_double_cnt
+                sort_i_cnt = self.my_sort(sort_i_cnt)
+            str_tree = list(sort_i_cnt)[0].replace("'", '')
+            for i in str_tree:
+                if i in self.obj and i != '' and i != ' ':
+                    str_tree = str_tree.replace('\\', '').replace('"', '').replace(i, f"'{i}'").replace(':  ', ": ' '")
+            tree = eval(str_tree)
+        return tree
+
+
+s = "extremes meet too"
+
+haf_obj = Haffman(s)
+
+print(haf_obj)
+print(haf_obj.obj)
+print(haf_obj.bin_tab)
+print(haf_obj.haffman_structure())
+
+# 11 1010 00 10111 11 011 11 10110 100 011 11 11 00 100 00 010 010
+# extremes meet too
+# {'t': '00', 'o': '010', 'm': '011', ' ': '100', 'x': '1010', 's': '10110', 'r': '10111', 'e': '11'}
+# {0: {0: 't', 1: {0: 'o', 1: 'm'}}, 1: {0: {0: ' ', 1: {0: 'x', 1: {0: 's', 1: 'r'}}}, 1: 'e'}}
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,70 +10,121 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
+class PathError(Exception):
+    def __str__(self):
+        return "Для установки данного потомка размер значения не соответствует размеру корня или потомка.\n" \
+               "Также проверьте тип (int?)."
+
+
+class TreeValueError(Exception):
+    def __str__(self):
+        return "В данном бинарном дереве используются только значения типа int."
+
+
 class BinaryTree:
     def __init__(self, root_obj):
-        # корень
-        self.root = root_obj
-        # левый потомок
+        try:
+            self.root = root_obj
+            if type(root_obj) != int:
+                raise TreeValueError
+        except TreeValueError as error:
+            print(error, 'Отмена установки корня.')
+            return
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
+        try:
+            if type(new_node) != int or new_node >= self.root:
+                raise PathError
+        except PathError as error:
+            print(error, 'Отмена установки левого потомка.')
+            return
         if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
             self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
         else:
-            # тогда вставляем новый узел
+            try:
+                if new_node <= self.right_child.root:
+                    raise PathError
+            except PathError as error:
+                print(error, 'Отмена установки левого потомка.')
+                return
             tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.left_child = self.left_child
             self.left_child = tree_obj
 
-    # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
+        try:
+            if type(new_node) != int or new_node <= self.root:
+                raise PathError
+        except PathError as error:
+            print(error, 'Отмена установки правого потомка.')
+            return
         if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
             self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
         else:
-            # тогда вставляем новый узел
+            try:
+                if new_node >= self.right_child.root:
+                    raise PathError
+            except PathError as error:
+                print(error, 'Отмена установки правого потомка.')
+                return
             tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.right_child = self.right_child
             self.right_child = tree_obj
 
-    # метод доступа к правому потомку
     def get_right_child(self):
         return self.right_child
 
-    # метод доступа к левому потомку
     def get_left_child(self):
         return self.left_child
 
-    # метод установки корня
     def set_root_val(self, obj):
+        try:
+            if type(obj) != int:
+                raise TreeValueError
+        except TreeValueError as error:
+            print(error, 'Отмена установки корня.')
+            return
         self.root = obj
 
-    # метод доступа к корню
     def get_root_val(self):
         return self.root
 
 
 r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
+print(r.get_root_val())  # 8
+print(r.get_left_child())  # None
+r.insert_left(4)
+print(r.get_left_child())  # <__main__.BinaryTree object at 0x0000000000529640>
+print(r.get_left_child().get_root_val())  # 4
 r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
+print(r.get_right_child())  # <__main__.BinaryTree object at 0x0000000001E8E250>
+print(r.get_right_child().get_root_val())  # 12
 r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+print(r.get_right_child().get_root_val())  # 16
+
+# Установим правого потомка со сдвигом предыдущего
+r.insert_right(15)
+print(r.get_right_child().get_right_child())  # <__main__.BinaryTree object at 0x000000000258E250>
+print(r.get_right_child().get_root_val())  # 15
+# Проверим сдвиг предыдущего правого потомка
+print(r.get_right_child().get_right_child().get_root_val())  # 16
+
+# # Проверка валидаций:
+
+# Попытка установки невалидного корня
+# r2 = BinaryTree('d')      # В данном бинарном дереве используются только значения типа int. Отмена установки корня.
+
+# Попытка установки правого потомка со значением меньшим, чем у корня
+# r.insert_right(7)         # Для установки данного потомка размер значения не соответствует размеру корня или потомка.
+#                           # Также проверьте тип (int?). Отмена установки правого потомка.
+
+# Попытка установки правого потомка (со сдвигом вниз существующего) со значением большим, чем у существующего
+# r.insert_right(17)        # Для установки данного потомка размер значения не соответствует размеру корня или потомка.
+#                           # Также проверьте тип (int?). Отмена установки правого потомка.
+
+# Попытка установки невалидного левого потомка
+# r.insert_left('d')        # Для установки данного потомка размер значения не соответствует размеру корня или потомка.
+#                           # Также проверьте тип (int?). Отмена установки левого потомка.
