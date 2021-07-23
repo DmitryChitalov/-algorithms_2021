ba4d0f7e7e1b47582d412c7e7d4c7b44d66bd4c1
Denis <dkozin@holmix.ru>
2021-07-23 05:50:56 +0300

    made all

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,9 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
+from random import randint
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +40,50 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):  # Ничего не сказано о строках, поэтому с удовольствем воспользуюсь
+    return int(str(abs(enter_num))[::-1]) * (-1 if enter_num < 0 else 1)
+
+
+def revers_5(enter_num):  # Еще вариант, тоже строка, то же самое, чуть иначе записано
+    return int('-' * (enter_num < 0) + str(abs(enter_num))[::-1])
+
+
+def revers_6(enter_num):  # Тоже красиво, тут создаем лист, который затем снова в строку складываем
+    return int('-' * (enter_num < 0) + ''.join(list(map(str, str(abs(enter_num))[::-1]))))
+
+
+if __name__ == '__main__':
+    a = randint(1000000000, 10000000000000000)
+    print(f'revers_1 отработала за {timeit("revers_1(a)", globals=globals())}')
+    run("revers_1(a)")  # revers_1 отработала за 3.0384119999999997
+    # 20 function calls (4 primitive calls) in 0.000 seconds
+    print('##################################################################')
+    print(f'revers_2 отработала за {timeit("revers_2(a)", globals=globals())}')
+    run("revers_2(a)")  # revers_2 отработала за 1.9866570000000001
+    #  4 function calls in 0.000 seconds
+    print('##################################################################')
+    print(f'revers_3 отработала за {timeit("revers_3(a)", globals=globals())}')
+    run("revers_3(a)")  # revers_3 отработала за 0.2687531999999999
+    # 4 function calls in 0.000 seconds
+    print('##################################################################')
+    print(f'revers_4 отработала за {timeit("revers_4(a)", globals=globals())}')
+    run("revers_4(a)")  # revers_4 отработала за 0.4191457999999999
+    #  5 function calls in 0.000 seconds
+    print('##################################################################')
+    print(f'revers_5 отработала за {timeit("revers_5(a)", globals=globals())}')
+    run("revers_5(a)")  # revers_5 отработала за 0.4062657000000005
+    #  5 function calls in 0.000 seconds
+    print('##################################################################')
+    print(f'revers_6 отработала за {timeit("revers_6(a)", globals=globals())}')
+    run("revers_6(a)")  # revers_6 отработала за 1.7177764999999994
+    #  6 function calls in 0.000 seconds
+    print('##################################################################')
+
+# Наилучшим образом себя показал алгоритм функции 3, хотя результат не совсем честный. Дело в том, что 1-3 алгоритмы
+# не учитывают отрицательные числа и работать корректно с ними не будут, а все остальные - будут. Самый быстрый
+# алгоритм - тот, где просто переворачивается строка, без каких-либо вычислений. Второй результат показывает алгоритм
+# 5, он медленнее, так как там добавляется модуль числа и умножение на знак, а также условие(для отрицательных). чуть
+# хуже 4 с тернарным оператором. Далее куча преобразований в 6 и первые два(они последние, поскольку используют
+# вычисления)
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,11 +11,13 @@
 
 Без аналитики задание считается не принятым!
 """
+from collections import Counter, OrderedDict
+from timeit import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
-def func_1():
+def func_1():  # 0.019444599999999996
     m = 0
     num = 0
     for i in array:
@@ -27,7 +29,7 @@ def func_1():
            f'оно появилось в массиве {m} раз(а)'
 
 
-def func_2():
+def func_2():  # 0.019044600000000002
     new_array = []
     for el in array:
         count2 = array.count(el)
@@ -39,5 +41,65 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+def func_3():  # Тут очевидно, на уроке говорилось о этой функции   0.0194137
+    my_dict = Counter(array)
+    mnum = max(array, key=my_dict.get)
+    return f'Чаще всего встречается число {mnum}, ' \
+           f'оно появилось в массиве {my_dict.get(mnum)} раз(а)'
+
+
+def func_4():  # сгруппируем одинаковые элементы 0.01602780000000001
+    m = [[n] * array.count(n) for i, n in enumerate(array) if array.index(n) == i]
+    return f'Чаще всего встречается число {m[0][0]}, ' \
+           f'оно появилось в массиве {len(m[0])} раз(а)'
+
+
+def func_5():  # Улучшаем (3) 0.0161631
+    return f'Чаще всего встречается число {Counter(array).most_common()[0][0]}, ' \
+           f'оно появилось в массиве {Counter(array).most_common()[0][1]} раз(а)'
+
+
+def func_6():  # ну и словарь, понятное дело 0.01700270000000001
+    my_dict = {i: array.count(i) for i in array}
+    m_num = max(my_dict, key=my_dict.get)
+    return f'Чаще всего встречается число {m_num}, ' \
+           f'оно появилось в массиве {my_dict[m_num]} раз(а)'
+
+
+class OrdCount(Counter, OrderedDict):  # Попробуем ООП, суть та же, что в(4) просто интересно
+    pass
+
+
+def take_most(array):  # 5.4411231
+    m_num = [[k] * v for k, v in OrdCount(array).items()]
+    return f'Чаще всего встречается число {m_num[0][0]}, ' \
+           f'оно появилось в массиве {len(m_num[0])} раз(а)'
+
+
+# Итого, самым быстрым стал вариант (5). но и результаты (4), (6) - на том же примерно уровне. Это можно легко
+# объяснить, функция Counter уже была оптимизирована разработчниками, а в (4) мы использовали быстрый lc + встроенный
+# enumerate. Словарь - отрабатывает быстро, потому что там lc + значение по ключу, которое мы получаем с константной
+# сложностью. Наиболее медленный в конкретной ситуации вариант с ООП, за счет создания экземпляра класса и обращений
+# к нему
+if __name__ == '__main__':
+    print(func_1())
+    print(timeit('func_1', globals=globals()))
+    print('##################################################################')
+    print(func_2())
+    print(timeit('func_2', globals=globals()))
+    print('##################################################################')
+    print(func_3())
+    print(timeit('func_3', globals=globals()))
+    print('##################################################################')
+    print(func_4())
+    print(timeit('func_4', globals=globals()))
+    print('##################################################################')
+    print(func_5())
+    print(timeit('func_5', globals=globals()))
+    print('##################################################################')
+    print(func_6())
+    print(timeit('func_6', globals=globals()))
+    print('##################################################################')
+    print(take_most(array))
+    print(timeit('take_most(array)', globals=globals()))
+    print('##################################################################')
