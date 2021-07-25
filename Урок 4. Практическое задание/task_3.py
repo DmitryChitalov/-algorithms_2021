8d4f30f388658a52b24d4fe9bf4a414e90ff50ca
vlad <kingbingping@gmail.com>
2021-07-25 17:25:20 +0500

    done Lesson_4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,8 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +39,37 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    num = []
+    for i in str(enter_num):
+        num.insert(0, i)
+    return ''.join(num)
+
+
+number = 18561 * 1234
+
+
+def func():
+    revers_1(number)
+    revers_2(number)
+    revers_3(number)
+    revers_4(number)
+
+
+run('func()')
+print('Рекурсия', timeit('revers_1(number)', globals=globals()))
+print('Цикл', timeit('revers_2(number)', globals=globals()))
+print('Срез', timeit('revers_3(number)', globals=globals()))
+print('Вставка', timeit('revers_4(number)', globals=globals()))
+
+# №1 Срез выполняется быстрее так как сложность O(b-a)
+# №2 Вставка через insert  имеет сложность O(n)
+# №3 Цикл while  имеет сложность O(n) но выполняется медленнее чем вставка
+# №4 рекурсия имеет сложность O(n**2) и она самая медленная
+
+# Рекурсия 1.9772253
+# Цикл 1.1695951999999998
+# Срез 0.2995068000000001
+# Вставка 0.9359130999999996
