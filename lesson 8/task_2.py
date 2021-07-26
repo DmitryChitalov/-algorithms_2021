5b8312172027c63b06db0fd9534443677f714e2a
Seregaschukin <78037157+Seregaschukin@users.noreply.github.com>
2021-07-26 21:23:15 +0300

    Update task_2.py

--- lesson 8/task_2.py
+++ lesson 8/task_2.py
@@ -74,3 +74,66 @@ print(r.get_right_child())
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
 print(r.get_right_child().get_root_val())
+
+"""Валидация значений узлов"""
+class BinaryTree:
+    def __init__(self, root_obj):
+        self.root = root_obj
+        self.left_child = None
+        self.right_child = None
+
+    # добавить левого потомка
+    def insert_left(self, new_node):
+        # если у узла нет левого потомка
+        try:
+            if self.root >= new_node:
+                if self.left_child == None:
+                    self.left_child = BinaryTree(new_node)
+                else:
+                    tree_obj = BinaryTree(new_node)
+                    tree_obj.left_child = self.left_child
+                    self.left_child = tree_obj
+            else:
+                raise Exception
+        except Exception:
+            print('Число больше корня')
+
+    def insert_right(self, new_node):
+        try:
+            if self.root <= new_node:
+                if self.right_child == None:
+                    self.right_child = BinaryTree(new_node)
+                else:
+                    tree_obj = BinaryTree(new_node)
+                    tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+            else:
+                raise Exception
+        except Exception:
+            print('Число больше корня')
+    def get_right_child(self):
+        return self.right_child
+
+    def get_left_child(self):
+        return self.left_child
+
+    def set_root_val(self, obj):
+        self.root = obj
+
+    def get_root_val(self):
+        return self.root
+
+
+r = BinaryTree(8)
+print(r.get_root_val())
+print(r.get_left_child())
+r.insert_left(9)
+r.insert_left(7)
+print(r.get_left_child())
+print(r.get_left_child().get_root_val())
+r.insert_right(1)
+r.insert_right(19)
+print(r.get_right_child())
+print(r.get_right_child().get_root_val())
+r.get_right_child().set_root_val(21)
+print(r.get_right_child().get_root_val())
