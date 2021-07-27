b4a29d8950d6dc5cd18e334178765057c56678c7
kerleada7 <kerleada7@mail.ru>
2021-07-27 19:51:57 +0300

    lesson 5

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,24 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+
+from timeit import timeit
+from collections import OrderedDict
+
+dict_simple = {a: a ** 2 for a in range(50)}
+dict_ordered = OrderedDict({a: a ** 2 for a in range(5)})
+print(dict_ordered)
+print(dict_simple)
+
+print(f'Заполнения словаря {timeit("dict_simple", globals=globals())}.')
+print(f'Заполнения OrderedDict {timeit("dict_ordered", globals=globals())}.')
+
+print(f'Получения элемента из словаря {timeit("dict_simple.items()", globals=globals())}.')
+print(f'Получения элемента из OrderedDict {timeit("dict_ordered.items()", globals=globals())}.')
+"""
+время заполнения почти одинаково так как одинаковые методы
+
+начиная с версии Python 3.6 и позднее словари - упорядоченная коллекция,
+нет смысла использовать OrderedDict 
+"""
