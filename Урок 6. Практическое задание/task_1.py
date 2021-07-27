23b1029468bbf7a64e597a0441050716e8ef5788
vlad <kingbingping@gmail.com>
2021-07-27 21:55:04 +0500

    выполнил 6 задание

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -21,3 +21,107 @@
 Попытайтесь дополнительно свой декоратор используя ф-цию memory_usage из memory_profiler
 С одновременным замером времени (timeit.default_timer())!
 """
+from timeit import default_timer
+from memory_profiler import memory_usage
+from pympler import asizeof
+from collections import namedtuple
+from recordclass import recordclass
+from sys import getsizeof
+
+
+# __slot__
+class HexClass:
+
+    def __init__(self, number):
+        self.number = number
+
+
+name_2 = HexClass(54558)
+print(asizeof.asizeof(name_2))  # asizeof.asizeof = 240
+
+
+class HexClassSlot:
+    __slots__ = 'number'
+
+    def __init__(self, number):
+        self.number = number
+
+
+name_1 = HexClassSlot(54558)
+print(asizeof.asizeof(name_1))  # asizeof.asizeof = 40
+
+# Уходим от хеша словаря и  получаем уменьшение занимаемой памяти.
+# -----------------------------------------------------------------------
+# namedtuple Vs recordclass
+
+
+company = namedtuple('Company', 'one two tree four')
+firms = {'Фирма1': company(one=235, two=345634, tree=55, four=235)}
+print(f'namedtuple : {getsizeof(firms)}')  # 72
+print(f'namedtuple : {asizeof.asizeof(firms)}')  # 168
+# recordclass
+company_1 = recordclass('Company', 'one two tree four')
+firms_1 = company_1(one=235, two=345634, tree=55, four=235)
+print(f'recordclass : {getsizeof(firms_1)}')  # 56
+print(f'recordclass : {asizeof.asizeof(firms_1)}')  # 864
+
+# Сильной разницы в памяти нет. А если мерить asasizeof наоборот recordclass больше памяти требует.
+#  к примеру добавим объекты в словарь firms = {'Фирма1': company(one=235, two=345634, tree=55, four=235)}
+# и показатели сравняются  232 б.
+
+# -----------------------------------------------------
+# Цикл vs Генератор vs list comprehension
+numb = [i for i in range(100000)]
+
+
+def decor(func):
+    def wrapper(*args):
+        m1 = memory_usage()
+        s1 = default_timer()
+        res = func(args[0])
+        m2 = memory_usage()
+        s2 = default_timer()
+        mem = m2[0] - m1[0]
+        sec = s2 - s1
+        return mem, sec
+
+    return wrapper
+
+
+@decor
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+mem, sec = func_1(numb)
+print(f'Память {mem}, Время {sec} : Цикл')
+
+
+@decor
+def func_2(nums):
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            yield i
+
+
+mem, sec = func_2(numb)
+print(f'Память {mem}, Время {sec} : Генератор')
+
+
+@decor
+def func_3(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+mem, sec = func_3(numb)
+print(f'Память {mem}, Время {sec} : list comprehension')
+
+# Память 1.94921875, Время 0.14106090000000004 : Цикл
+# Память 0.0, Время 0.10869960000000001 : Генератор
+# Память 1.6328125, Время 0.12466069999999996 : list comprehension
+
+# И победитель генератор поскольку он не вычисляет значение всех элементов  а работает только по запросу.
--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -4,3 +4,22 @@
 для оптимизации памяти и
 доказать!!! (наглядно, кодом) их эффективность (на примере профилировщика).
 """
+from memory_profiler import profile
+from pympler import asizeof
+import itertools
+
+b = [1, 4, 6, 4, 1]
+a = itertools.filterfalse(lambda x: x % 2, b)
+
+new_arr = []
+for i in b:
+    if i % 2 == 0:
+        new_arr.append(i)
+print(f'Память list: {asizeof.asizeof(new_arr)}')
+print(f'Память itertools : {asizeof.asizeof(a)}')
+print(*a)
+print(new_arr)
+
+# itertools сборник итераторов (полезен)
+
+
