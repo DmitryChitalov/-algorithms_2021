d8ab85c315e48acd870c3d609b1ee56ad07cd30f
EuGeN-PYTHON <varlaea2@icloud.com>
2021-07-30 14:27:16 +0300

    HW_7_Evgeny_Varlamov

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -40,3 +40,60 @@ for i in
 
 
 """
+import random
+
+# Через сортировку (Гномья)
+
+def gnome(arr,i = 1):
+    while i < len(arr):
+        if arr[i - 1] <= arr[i]:
+            i += 1
+        else:
+            arr[i - 1], arr[i] = arr[i], arr[i - 1]
+            if i > 1:
+                i -= 1
+    return arr
+
+def sort_med(arr):
+    arr = gnome(arr)
+    return arr[m]
+    #если бы массив мог быть четным:
+    # if len(arr) % 2 == 1:
+    #     return arr[len(arr) // 2]
+    # else:
+    #     return 0.5 * (arr[len(arr) // 2 - 1] + arr[len(arr) // 2])
+
+# Без сортировки
+
+def quickselect_median(arr, pivot_fn=random.choice):
+    return quickselect(arr, m, pivot_fn)
+    # if len(l) % 2 == 1:
+    #     return quickselect(l, len(l) / 2, pivot_fn)
+    # else:
+    #     return 0.5 * (quickselect(l, len(l) / 2 - 1, pivot_fn) +
+    #                   quickselect(l, len(l) / 2, pivot_fn))
+
+
+def quickselect(arr, k, pivot_fn):
+
+    pivot = pivot_fn(arr)
+
+    lows = [el for el in arr if el < pivot]
+    highs = [el for el in arr if el > pivot]
+    pivots = [el for el in arr if el == pivot]
+
+    if k < len(lows):
+        return quickselect(lows, k, pivot_fn)
+    elif k < len(lows) + len(pivots):
+        return pivots[0]
+    else:
+        return quickselect(highs, k - len(lows) - len(pivots), pivot_fn)
+
+m = int(input('Введите число median (m):'))
+ln_arr = 2 * m + 1
+print(ln_arr)
+sim_arr = [random.randint(-100, 100) for _ in range(ln_arr)]
+print(sim_arr)
+print(sorted(sim_arr))
+print(sort_med(sim_arr))
+print(quickselect_median(sim_arr))
