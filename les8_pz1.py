044a341d6b63b7f4f2bbb4767c2b49581a97b94f
Ekaterina <e-kondra@mail.ru>
2021-08-06 18:03:38 +0300

    added lesson_8

---
+++ les8_pz1.py
@@ -0,0 +1,64 @@
+"""
+Задание 1.
+Реализуйте кодирование строки "по Хаффману".
+У вас два пути:
+1) тема идет тяжело? тогда вы можете, опираясь на пример с урока, сделать свою!!! версию алгоритма
+Разрешается и приветствуется изменение имен переменных, выбор других коллекций, различные изменения
+и оптимизации.
+2) тема понятна? постарайтесь сделать свою реализацию.
+Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
+"""
+
+from collections import Counter, deque
+from operator import itemgetter
+
+haffman_table = {}
+
+
+def form_haffman_tree(string):
+    count = Counter(string)
+    # получится отсортированный список deque, где элементы - кортежи(буква, кол-во)
+    sorted_lst = deque(sorted(count.items(), key=itemgetter(1)))
+    # print(sorted_lst)
+    if len(sorted_lst) != 1:
+        while len(sorted_lst) > 1:  # строим дерево
+            w = sorted_lst[0][1] + sorted_lst[1][1]  # объединенный вес
+            noda = {0: sorted_lst.popleft()[0],  # создаем элемент, вырезая из списка первые элементы в наш словарь
+                    1: sorted_lst.popleft()[0]}
+            # Ищем место для вставки объединенного элемента в тот же список
+            for i, c in enumerate(sorted_lst):
+                if w > c[1]:
+                    continue
+                else:
+                    # Вставляем объединенный элемент
+                    sorted_lst.insert(i, (noda, w))
+                    break
+            else:
+                # если наш новый элемент весит больше всех, то добавляем его в хвост
+                sorted_lst.append((noda, w))
+    else:
+        # приравниваем значение 0 к одному повторяющемуся символу
+        w = sorted_lst[0][1]
+        noda = {0: sorted_lst.popleft()[0], 1: None}
+        sorted_lst.append((noda, w))
+
+    return sorted_lst
+
+
+def create_hafm_table(tree_dict, path=''):
+    if not isinstance(tree_dict, dict):  # получили лист бинарного дерева
+        haffman_table[tree_dict] = path
+    else:
+        create_hafm_table(tree_dict[0], path=f'{path}0')
+        create_hafm_table(tree_dict[1], path=f'{path}1')
+
+
+# строка для кодирования
+s = "have you ever seen the rain"
+
+create_hafm_table(form_haffman_tree(s)[0][0])
+# for key, val in haffman_table.items():
+#     print(f'{key}: {val}')
+
+for i in s:
+    print(haffman_table[i], end='')
---
+++ les8_pz2.py
@@ -0,0 +1,130 @@
+"""
+Задание 2.
+Доработайте пример структуры "дерево",
+рассмотренный на уроке.
+Предложите варианты доработки и оптимизации
+(например, валидация значений узлов в соответствии с требованиями для бинарного дерева).
+Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
+"""
+from accessify import protected
+
+
+class BinaryTree:
+    def __init__(self, root_obj, path='root'):
+        # корень
+        self.root = root_obj
+        # левый потомок
+        self.left_child = None
+        # правый потомок
+        self.right_child = None
+        # путь
+        self.path = path
+
+    # добавить потомка
+    def insert_child(self, root_obj, path='root'):
+        # разделяем от корня на левую или правую ветки
+        if root_obj >= self.root:
+            self.insert_right(root_obj, path)
+        else:
+            self.insert_left(root_obj, path)
+
+    # добавить левого потомка
+    @protected
+    def insert_left(self, new_node, path):
+        # если у узла нет левого потомка
+        path = f'{path}-l'
+        if self.left_child == None:
+            # тогда узел просто вставляется в дерево
+            # формируется новое поддерево
+            self.left_child = BinaryTree(new_node, path=path)
+        # если у узла есть левый потомок
+        else:
+            old_node = self.get_left_child()
+            # большие или равные идут направо
+            if new_node >= old_node.root:
+                old_node.insert_right(new_node, path)
+            else:  # остальные налево
+                old_node.insert_left(new_node, path)
+
+    # добавить правого потомка
+    @protected
+    def insert_right(self, new_node, path):
+        path = f'{path}-r'
+        # если у узла нет правого потомка
+        if self.right_child == None:
+            # тогда узел просто вставляется в дерево
+            # формируется новое поддерево
+            self.right_child = BinaryTree(new_node, path=path)
+        # если у узла есть правый потомок
+        else:
+            old_node = self.get_right_child()
+            # большие или равные идут направо
+            if new_node >= old_node.root:
+                old_node.insert_right(new_node, path)
+            else:  # остальные налево
+                old_node.insert_left(new_node, path)
+
+    # метод доступа к правому потомку
+    def get_right_child(self):
+        return self.right_child
+
+    # метод доступа к левому потомку
+    def get_left_child(self):
+        return self.left_child
+
+    # метод установки корня
+    def set_root_val(self, obj):
+        self.root = obj
+
+    # метод доступа к корню
+    def get_root_val(self):
+        return self.root
+
+    def __str__(self):
+        return f'{self.path}: {self.root}'
+
+    # поиск элемента по значению
+    def find_el(self, node):
+        try:
+            if node == self.root:
+                print(self)
+            elif node >= self.root:
+                new_self = self.get_right_child()
+                new_self.find_el(node)
+            else:
+                new_self = self.get_left_child()
+                new_self.find_el(node)
+        except AttributeError:
+            print(f'Значение {node} отсутствует')
+
+
+r = BinaryTree(10)
+print(r.get_root_val())
+print(r.get_left_child())
+r.insert_child(40)
+print(f'{r.get_right_child()}')
+r.insert_child(3)
+print(f'{r.get_left_child()}')
+r.insert_child(5)
+print(f'{r.get_left_child().get_right_child()}')
+r.insert_child(15)
+print(f'{r.get_right_child().get_left_child()}')
+r.insert_child(45)
+print(f'{r.get_right_child().get_right_child()}')
+r.insert_child(12)
+r.insert_child(30)
+print(f'{r.get_right_child().get_left_child().get_right_child()}')
+print(f'{r.get_right_child().get_left_child().get_left_child()}')
+r.insert_child(41)
+print(f'{r.get_right_child().get_right_child().get_left_child()}')
+r.insert_child(31)
+print(f'{r.get_right_child().get_left_child().get_right_child().get_right_child()}')
+r.find_el(12)
+r.find_el(7)
+
+#  Предлагаемая оптимизация: Методы insert_right и insert_left делаем защищенными, а пользователю предоставляем
+#  для добавления узлов метод insert_child, который принимает на вход значение узла, определяет основную ветвь
+#  (правую или левую) и далее вызывает методы insert_right и insert_left, которые рекурсивно
+#  вызывают друг друга в процессе определения места в дереве в зависимости от значения нового узла и уже ранее
+#  добавленных узлов дерева. Для проверки корректности построения дерева добавила атрибут path, куда пишется путь
+#  размещения узла. В интерфейс дообавила еще метод поиска узла по значению
