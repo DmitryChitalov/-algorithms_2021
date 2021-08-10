c1617b202152727e4e13166f3e1a30e6db4151bf
kerleada7 <kerleada7@mail.ru>
2021-08-10 17:59:18 +0300

    lesson 8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,70 +10,80 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
+class OwnError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
 class BinaryTree:
     def __init__(self, root_obj):
-        # корень
         self.root = root_obj
-        # левый потомок
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        try:
+            if self.root < new_node:
+                raise OwnError(f"Левый потомок = {new_node} не может быть больше родителя = {self.root}")
+            if self.left_child is None:
+                print(f'Левая ветка отсутствует, лист {new_node} установлен')
+                self.left_child = BinaryTree(new_node)
+            else:
+                if self.left_child.get_root_val() < new_node:
+                    print(f'Левая ветка существует. Ее голова = {self.left_child} '
+                          f'меньше {new_node}, поэтому вставляем выше')
+                    tree_obj = BinaryTree(new_node)
+                    tree_obj.left_child = self.left_child
+                else:
+                    raise OwnError(f"Левая ветка существует ее корень = {self.left_child.set_root_val()} "
+                                   f"больше нашего  = {new_node}, - сюда не вставить")
+        except OwnError as err:
+            print(err)
 
-    # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        try:
+            if self.root > new_node:
+                raise OwnError(f"Левый потомок = {new_node} не может быть больше родителя = {self.root}")
+            if self.right_child is None:
+                print(f'Правая ветка отсутствует, лист {new_node} установлен')
+                self.right_child = BinaryTree(new_node)
+            else:
+                if self.right_child.get_root_val() < new_node:
+                    tree_obj = BinaryTree(new_node)
+                    tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+                else:
+                    raise OwnError(f"Правая ветка существует ее корень = {self.right_child.get_root_val()} "
+                                   f"больше нашего  = {new_node}, - сюда не вставить")
+        except OwnError as err:
+            print(err)
 
-    # метод доступа к правому потомку
     def get_right_child(self):
         return self.right_child
 
-    # метод доступа к левому потомку
     def get_left_child(self):
         return self.left_child
 
-    # метод установки корня
-    def set_root_val(self, obj):
+    def set_root_val(self, obj=None):
         self.root = obj
 
-    # метод доступа к корню
     def get_root_val(self):
         return self.root
 
 
-r = BinaryTree(8)
+r = BinaryTree(18)
+print(r)
 print(r.get_root_val())
 print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
+r.insert_right(140)
 print(r.get_right_child())
 print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
+r.insert_right(112)
+r.get_right_child().set_root_val(112)
+r.insert_right(140)
+print(r.get_right_child())
 print(r.get_right_child().get_root_val())
+r.get_right_child().set_root_val(116)
+print(r.get_right_child().get_root_val())
+print(r.get_right_child().get_right_child().get_root_val())
