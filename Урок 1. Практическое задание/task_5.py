9dfbf61a1d51f7f83d853b05a6f86453a853ced8
Anna <annaosipchik03@gmail.com>
2021-08-21 17:30:48 +0300

    сделала дз 1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,51 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class Stack:
+    __list_of_stacks = list()
+    __counter = -2
+
+    def __init__(self, n: int):
+        self.__size = n
+        self.stack = list()
+        Stack.__counter += 1
+
+    def push(self, el):
+        if not Stack.__list_of_stacks:
+            Stack.__list_of_stacks.append(Stack(self.__size))
+        elif len(Stack.__list_of_stacks[Stack.__counter].stack) >= self.__size:
+            Stack.__list_of_stacks.append(Stack(self.__size))
+        Stack.__list_of_stacks[Stack.__counter].stack.append(el)
+
+    @staticmethod
+    def pop():
+        if not Stack.__list_of_stacks:
+            print('Стек пуст')
+            return
+        else:
+            Stack.__list_of_stacks[Stack.__counter].stack.pop()
+
+    def __str__(self):
+        string = ''
+        for element in Stack.__list_of_stacks:
+            string += '\n'
+            for stack in element.stack:
+                string += str(stack)
+                string += '\n'
+        string += '\n'
+        return string
+
+
+stack_1 = Stack(3)
+stack_1.push(1)
+stack_1.push(2)
+stack_1.push(3)
+stack_1.push(4)
+stack_1.push(5)
+stack_1.pop()
+stack_1.push(5)
+stack_1.push(6)
+stack_1.push(7)
+print(stack_1)
