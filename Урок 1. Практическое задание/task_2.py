33d89743f90021d49d1276358153379e9b377500
Mrkorg1000 <korg1000@yandex.ru>
2021-08-23 18:05:33 +0300

    Python подзабыл, поэтому сделал на все. Но наверстаю.

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -19,3 +19,19 @@
 Постарайтесь не использовать ф-ции min() и sort() и другие ф-ции!
 Подход должен быть максимально алгоритмическим.
 """
+ls = [2, 3, 45, 21, 1, 100]
+
+""" Алгоритм 2. Сложность O(n) """
+
+
+def min_in_list_1(list_obj):
+    min_list = list_obj[0]
+    for i in list_obj[1:]:
+        if i < min_list:
+            min_list = i
+    return min_list
+
+
+""" Проверка """
+m = min_in_list_1(ls)
+print(m)
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -25,3 +25,56 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+users = {
+    'Al': {'passwd': 12345, 'activated': True},
+    'Robert': {'passwd': 54321, 'activated': True},
+    'Joe': {'passwd': 9876, 'activated': False},
+    'Clint': {'passwd': 77777, 'activated': True}
+}
+
+""" Вариант 1. Сложность О(n) (за счет цикла). Затратный по времени. """
+
+
+def auth_1(users_file, user_name, user_passwd):
+    for k, v in users_file.items():
+        if k == user_name and v['passwd'] == user_passwd and v['activated']:
+            return f'Добро пожаловать {user_name}!'
+        elif k == user_name and v['passwd'] != user_passwd:
+            return f'{user_name}, Вы ввели не правильный пароль'
+        elif k == user_name and v['passwd'] == user_passwd and not v['activated']:
+            return f'{user_name}, Ваша запись еще не активирована'
+
+    return f'{user_name}, Ваc нет в системе. Зарегистрируйтесь или До свидания!'
+
+
+u1 = auth_1(users, 'Clint', 77777)
+u2 = auth_1(users, 'Joe', 9876)
+u3 = auth_1(users, 'John', 77777)
+
+print(u1, u2, u3, sep='\n')
+
+""" 
+Вариант 2. Сложность О(1) (за счет хэша). Предпочтительный вариант.
+Решение подсмотрел (идею) 
+"""
+
+
+def auth_1(users_file, user_name, user_passwd):
+    if users_file.get(user_name) and users_file[user_name]['passwd'] == user_passwd \
+            and users_file[user_name]['activated']:
+        return f'Добро пожаловать {user_name}!'
+    elif users_file.get(user_name) and users_file[user_name]['passwd'] != user_passwd:
+        return f'{user_name}, Вы ввели не правильный пароль'
+    elif users_file.get(user_name) and users_file[user_name]['passwd'] == user_passwd \
+            and not users_file[user_name]['activated']:
+        return f'{user_name}, Ваша запись еще не активирована'
+
+    return f'{user_name}, Ваc нет в системе. Зарегистрируйтесь или До свидания!'
+
+
+u4 = auth_1(users, 'Clint', 77777)
+u5 = auth_1(users, 'Joe', 9876)
+u6 = auth_1(users, 'John', 77777)
+
+print(u4, u5, u6, sep='\n')
