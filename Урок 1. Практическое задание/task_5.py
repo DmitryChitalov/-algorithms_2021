d9f0da58dd9ae8b35285b8abe947c67ad8bbba26
Anton Eseneev <shaman_kub@mail.ru>
2021-08-27 00:05:31 +0300

    Домашнее задание к 1 уроку.

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,57 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class PlateStack:
+    def __init__(self, maximum):
+        self.el = []
+        self.maximum = maximum
+
+    def __str__(self):
+        return str(self.el)
+
+    def push_in(self, plate):
+        """Добавляем тарелки в стопки. Если стопка наполняется - добавляем новую стопку"""
+        if len(self.el) == 0:
+            self.el.append([plate])
+        elif len(self.el[len(self.el) - 1]) < self.maximum:
+            self.el[len(self.el) - 1].append(plate)
+        else:
+            self.el.append([])
+            self.el[len(self.el) - 1].append(plate)
+
+    def pop_out(self):
+        """Удаляем верхний элемент стопки"""
+        self.el[len(self.el) - 1].pop()
+        if len(self.el[len(self.el) - 1]) == 0:
+            self.el.pop()
+
+    def last_plate(self):
+        """Получаем значение верхнего элемента стопки"""
+        return f"Последняя тарелка № {self.el[len(self.el) - 1][-1]}"
+
+    def stack_height(self):
+        """Получаем высоту крайней стопки"""
+        return f"Высота стопки - {len(self.el[len(self.el) - 1])}"
+
+    def stacks_count(self):
+        """Получаем количество стопок"""
+        return f"Количество стопок - {len(self.el)}"
+
+
+plates = PlateStack(5)
+print(type(plates))
+
+for i in range(1, 7):
+    plates.push_in(i)
+
+print(plates)
+print(plates.last_plate())
+print(plates.stack_height())
+print(plates.stacks_count())
+plates.pop_out()
+print(plates)
+print(plates.last_plate())
+print(plates.stack_height())
+print(plates.stacks_count())
