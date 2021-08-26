890c244ad21cfc7d3c168d5f6d0fe9517fe11f39
Igor <Igor489@yandex.ru>
2021-08-27 01:54:01 +0300

    Решение четвертой задачи

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -12,3 +12,32 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+from hashlib import sha256
+web_cache_hash = {}
+
+
+class MyWebCache:
+
+    def __init__(self):
+        self.salt = 'my_salt'
+
+    def chk_cache(self, url):
+        """
+        :param url: web page address to check
+        calc_hash: hash computation
+        """
+        calc_hash = sha256(self.salt.encode() + url.encode()).hexdigest()
+        if calc_hash not in web_cache_hash.keys():
+            web_cache_hash[calc_hash] = url
+            print(f'{url} - добавлен в кэш.')
+        else:
+            print(f'{url} - уже присутствует в кэше.')
+
+
+cache = MyWebCache()
+
+cache.chk_cache('https://habr.com/ru/post/196560/')
+cache.chk_cache('http://www.gp.com/Roku/HLS/X264_Settings.htm#no-8x8dct')
+cache.chk_cache('http://www.gp.com/Roku/HLS/X264_Settings.htm#no-8x8dct')
+print(web_cache_hash)
