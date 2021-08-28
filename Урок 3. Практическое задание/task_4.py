17c0c0ef9c426a3a83464ed10bf835cd15466a81
Denis Dudnik <Denis.Dudnik@gmail.com>
2021-08-28 11:40:26 +0300

    Homework 3 done. Check it please

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,26 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+import hashlib
+
+
+def get_hash(url, salt):
+    """Генерируется соленый hash"""
+    return hashlib.sha256(url.encode() + salt.encode()).hexdigest()
+
+
+cache_data = {}
+SALT = 'AnY SeCrEt SaLt'
+
+while True:
+    url = input('Enter URL (For exit type Q): ')
+    if url.lower() == 'q':
+        break
+    url_hash = get_hash(url, SALT)
+
+    if cache_data.get(url_hash):
+        print(f'URL {cache_data[url_hash]} already added to cache.')
+    else:
+        cache_data[url_hash] = url
+        print(f'URL {cache_data[url_hash]} was not find in cache. It will be add to cache now.')
