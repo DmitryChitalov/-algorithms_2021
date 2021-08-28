dcd1dbe02e3b8e9e99ccb19b5696a3a7c7741a08
Aleksei Synkov <synkovaleksei@yandex.ru>
2021-08-28 12:46:22 +0300

    lesson_3

---
+++ Урок 3. Практическое задание/task_1.py
@@ -0,0 +1,89 @@
+"""
+Задание 1.
+
+Реализуйте свои пользовательские функции, в которых реализуйте:
+
+a) заполнение списка и словаря,
+   сделайте замеры и сделайте выводы, что выполняется быстрее и почему
+   И укажите сложность каждой ф-ции, которую вы применяете для заполнения.
+   У этих ф-ций может быть разная сложность. Поэтому время заполнения списка и словаря может как совпадать, так и отличаться.
+b) выполните набор операций и со списком, и со словарем,
+   сделайте замеры и сделайте выводы, что и где выполняется быстрее и почему
+   И укажите сложность ф-ций, которые вы используете для операций.
+
+Подсказка: для замеров воспользуйтесь модулем time (см. примеры урока 1)
+
+Примечание: eсли вы уже знаете, что такое декоратор и как его реализовать,
+то реализуйте ф-цию-декоратор для подсчета времени работы ваших пользовательских функций
+И примените ее к своим функциям!
+
+Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
+"""
+
+
+from time import time
+from random import randint
+
+
+def timer(function):                # O(1)
+    def wrapper(value):
+        start_time = time()
+        result = function(value)
+        print(f' - {round(time() - start_time, 3)} сек.')
+        return result
+    return wrapper
+
+
+@timer
+def get_list(number):       #O(n)
+    print(f'Наполнение списка {number} элементами.', end=' ')
+    result = [el for el in range(number)]
+    return result
+
+
+@timer
+def get_dict(number):       # O(n)
+    print(f'Наполнение словаря {number} элементами.', end=' ')
+    result = {el: el for el in range(number)}
+    return result
+
+
+number_of_elements = 1000000
+my_list = get_list(number_of_elements)  # 0.047 сек.
+my_dict = get_dict(number_of_elements)  # 0.085 сек.
+# Медленнее происходит заполнение словаря из-за создания хешей
+
+
+@timer
+def get_value_from_list(some_list: list):       # O(1)
+    print('Получение индекса рандомного числа в списке.', end=' ')
+    some_list.index(randint(0, number_of_elements))
+
+
+@timer
+def get_value_from_dict(some_dict: dict):       # O(1)
+    print('Поиск рандомного числа по ключу в словаре.', end=' ')
+    some_dict.get(randint(0, number_of_elements))
+
+
+get_value_from_list(my_list)  # 0.016 сек.  O(1)
+get_value_from_dict(my_dict)  # 0.0 сек.    O(1)
+# Поиск в словаре происходит быстрее благодаря хешам, сложность - константная
+# В списке сложность поиска элемента - линейная
+
+
+@timer
+def clear_list(some_list: list):    # O(1)
+    print('Очищение списка.', end=' ')
+    some_list.clear()
+
+
+@timer
+def clear_dict(some_dict: dict):    # O(1)
+    print('Очищение словаря.', end=' ')
+    some_dict.clear()
+
+
+clear_list(my_list)  # 0.0 сек.  O(1)    аналог l = []
+clear_dict(my_dict)  # 0.016 сек.  O(1)  аналогично s = {} или s = dict()
+# Cловарь будет очищаться дольше из-за удаления хешей. Время константное
---
+++ Урок 3. Практическое задание/task_3.py
@@ -0,0 +1,35 @@
+"""
+Задание 3.
+Определить количество различных (уникальных) подстрок с использованием хеш-функции.
+Дана строка S длиной N, состоящая только из строчных латинских букв.
+
+Подсказка: примените вычисление хешей для подстрок с помощью хеш-функций и множества
+Можно воспользоваться ф-цией hash() (см. материалы к уроку)
+
+Пример:
+рара - 6 уникальных подстрок
+
+рар
+ра
+ар
+ара
+р
+а
+"""
+
+
+def unique(word):
+    bd_hash = {}
+    for i in range(len(word)):
+        for j in range(i, len(word)):
+            upd = word[i:j+1]
+            bd_hash[hash(upd)] = upd
+    bd_hash.pop(hash(word))
+    return bd_hash
+
+
+in_word = 'papa'
+split_word = unique(in_word)
+print(f'{in_word} - {len(split_word)} уникальных подстрок:\n')
+for key in split_word:
+    print(split_word[key])
