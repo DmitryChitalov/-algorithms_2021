9393d123ecc9a45c387c31b0cb7a03282618b497
Sergei <serguchers@gmail.com>
2021-08-30 20:01:07 +0300

    hw#4 first 4 tasks

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,11 @@
 
 Без аналитики задание считается не принятым
 """
+import cProfile
+from random import randint
+from timeit import timeit
+
+TEST_NUM = randint(100000, 1000000)
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +42,53 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    enter_num = str(enter_num)
+    res = ''
+    for i in range(len(enter_num)-1, -1, -1):
+        res += enter_num[i]
+    return res
+
+
+def revers_5(enter_num):
+    enter_num = list(str(enter_num))
+    i = 0
+    while i != len(enter_num):
+        last = enter_num.pop()
+        enter_num.insert(i, last)
+        i += 1
+    return ''.join(enter_num)
+
+
+print(f'Фукнция revers_1', end=' ---> ')
+print(timeit('revers_1(TEST_NUM)', globals=globals( )))
+cProfile.run('revers_1(TEST_NUM)')
+print(f'Фукнция revers_2', end='---> ')
+print(timeit('revers_2(TEST_NUM)', globals=globals( )))
+cProfile.run('revers_2(TEST_NUM)')
+print(f'Фукнция revers_3', end='---> ')
+print(timeit('revers_3(TEST_NUM)', globals=globals( )))
+cProfile.run('revers_3(TEST_NUM)')
+print(f'Фукнция revers_4', end='---> ')
+print(timeit('revers_4(TEST_NUM)', globals=globals( )))
+cProfile.run('revers_4(TEST_NUM)')
+print(f'Фукнция revers_5', end='---> ')
+print(timeit('revers_5(TEST_NUM)', globals=globals( )))
+cProfile.run('revers_5(TEST_NUM)')
+
+"""
+Рекурсивная функция revers_1 показала один из самых медленных результатов, так как для реверса числа нам необходимо
+будет осуществить такое количество вызова функции, которое было бы равно "длине" числа, что существенно замедляет время
+выполнения.
+Revers_2 по смыслу похожа на revers_1, однако, функция не требует дополнительных вызовов и просто осуществляет расчеты
+и перезаписывает значения по ходу выполнения цикла.
+Revers_3 оказалась самой быстрой. Мы пользуемся встроенными функциями, которые уже являются оптимизированными, что 
+позволяет сократить время выполнения, также функция не производит никаких расчетов, а просто возвращает обратную строку.
+Revers_4 по задумке похожа на revers_3, однако, реализует тот же функционал путём обхода от последнего индекса строки
+к первому. В данной функции производится конкатенация строк, а также осуществляется больше вызовов сторонних функций.
+Revers_5 является медленной функцией так как во время её выполнения мы вызовем выполним len(N) вызовов сторонних функций
+особенное влияние на время выполнения окажет insert(), который будет производить вставку по индексу.
+Таким образом, самой оптимальной функцией оказалась revers_3. 
+"""
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -11,6 +11,7 @@
 
 Без аналитики задание считается не принятым!
 """
+from timeit import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
@@ -39,5 +40,41 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+def func_3():
+    num = max(set(array), key=lambda i: array.count(i))
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {array.count(num)} раз(а)'
+
+
+def func_4():
+    num = sorted(set(array), key=lambda i: array.count(i))[-1]
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {array.count(num)} раз(а)'
+
+
+def func_5():
+    m, num = 0, 0
+    for i in set(array):
+        count = array.count(i)
+        if count > m:
+            m = count
+            num = i
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {m} раз(а)'
+
+
+print(func_1( ) == func_2( ) == func_3( ) == func_4( ) == func_5( ))
+print(timeit('func_1()', globals=globals( )))
+print(timeit('func_2()', globals=globals( )))
+print(timeit('func_3()', globals=globals( )))
+print(timeit('func_4()', globals=globals( )))
+print(timeit('func_5()', globals=globals( )))
+
+"""
+Из 3-ёх написанных мной алгоритмов func_3, func_4 и func_5, func_5 оказался самым быстрым так как является доработанной
+версией func_1. Создавая множество от интересующего нас массива при проходе, мы избегаем повторных вызовов метода 
+count() для уже посчитанных значений.
+func_3 оказался медленней,но лаконичней, чем func_2.
+func_4 показывает примерно такие же результаты как и func_2, так как мы сортируем множество, но берем срез, 
+а не используем max().
+"""
