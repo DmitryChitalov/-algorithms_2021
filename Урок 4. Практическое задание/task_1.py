55021f6116cf95f97ae474f4a66d918cb890e134
MrHipster38 <79633359+MrHipster38@users.noreply.github.com>
2021-08-30 13:08:43 +0800

    update task 1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -16,6 +16,8 @@
 на самом деле к генераторам отношения не имеет. Это называется "списковое включение" - list comprehension.
 """
 
+import timeit
+
 
 def func_1(nums):
     new_arr = []
@@ -23,3 +25,57 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+test_arr = [i for i in range(100)]
+
+print(timeit.timeit('func_1(test_arr)', globals=globals(), number=100000))
+# 1.2354482629999999
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+print(timeit.timeit('func_2(test_arr)', globals=globals(), number=100000))
+# 0.9334593440000001
+# Попробовал использовать lc, увидел значительную прибавку к скорости выполнения
+
+
+def func_3(nums):
+    new_arr = []
+    test = new_arr.append
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            test(i)
+    return new_arr
+
+
+print(timeit.timeit('func_3(test_arr)', globals=globals(), number=100000))
+# 1.047493346
+# Попробовал вынести функцию append в отдельную переменную, заметил увеличение скорости
+
+
+def func_4(nums):
+    new_arr = []
+    test = new_arr.append
+    for idx, val in enumerate(nums):
+        if val % 2 == 0:
+            test(idx)
+    return new_arr
+
+
+print(timeit.timeit('func_4(test_arr)', globals=globals(), number=100000))
+# 0.923437748
+# функция enumerate и вынесение функции append дают прибавку к скорости на уровне lc
+
+
+def func_5(nums):
+    new_arr = [i for i, val in enumerate(nums) if val % 2 == 0]
+    return new_arr
+
+
+print(timeit.timeit('func_5(test_arr)', globals=globals(), number=100000))
+# 0.8308847449999996
+# объединил два лучших варианта, получил оптимальное значение
