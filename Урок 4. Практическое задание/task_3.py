1b1a16c81ed113c5248f90e45582cecc972fe79f
Aleksei Synkov <synkovaleksei@yandex.ru>
2021-08-31 22:42:56 +0300

    lesson_4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -15,6 +15,10 @@
 """
 
 
+from timeit import timeit
+from cProfile import run
+
+
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -37,3 +41,43 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    if enter_num == 0:
+        return ''
+    else:
+        revers = str(enter_num % 10)
+        enter_num = enter_num // 10
+        return revers + revers_4(enter_num)
+
+
+num = 13840
+
+print(timeit(stmt="revers_1(num)", setup="from __main__ import revers_1, num"))     # 1.5491
+print(timeit(stmt="revers_2(num)", setup="from __main__ import revers_2, num"))     # 1.1577
+print(timeit(stmt="revers_3(num)", setup="from __main__ import revers_3, num"))     # 0.4387
+print(timeit(stmt="revers_4(num)", setup="from __main__ import revers_4, num"))     # 2.1658
+
+# Самая быстрая функция - revers_3. С использованием среза. Самая медленная - revers_4 т.к. О(n!)
+
+def main():
+    revers_1()
+    revers_2()
+    revers_3()
+    revers_4()
+
+run('main()')
+
+"""         4 function calls in 0.000 seconds              
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 task_3.py:64(main)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+"""
+
+# насчитал 4 вызова функции. Не нашел узких мест, на выполнение кода потрачено 0,000 сек., отсекая числа после 0,000
