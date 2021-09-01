b95297aacfd422b89707d7ddbe63a25c40a8f37a
makar1758 <MKR1758@yandex.ru>
2021-09-01 22:33:24 +0300

    1,2,3,4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -14,8 +14,12 @@
 Без аналитики задание считается не принятым
 """
 
+from time import sleep
+from timeit import timeit
+from cProfile import run
 
 def revers_1(enter_num, revers_num=0):
+    sleep(2)
     if enter_num == 0:
         return revers_num
     else:
@@ -26,6 +30,7 @@ def revers_1(enter_num, revers_num=0):
 
 
 def revers_2(enter_num, revers_num=0):
+    sleep(2)
     while enter_num != 0:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
@@ -34,6 +39,38 @@ def revers_2(enter_num, revers_num=0):
 
 
 def revers_3(enter_num):
+    sleep(2)
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+    
+def revers_4(enter_num):
+    sleep(2)
+    list_1 = str(enter_num).split()
+    return list_1[0][::-1]
+
+    
+def main(enter_num):
+    revers_1(enter_num)
+    revers_2(enter_num)
+    revers_3(enter_num)
+    revers_4(enter_num)
+
+
+enter_num = 123456789
+
+print(timeit("revers_1(enter_num)", globals=globals(),number=1))
+print(timeit("revers_2(enter_num)", globals=globals(),number=1))
+print(timeit("revers_3(enter_num)", globals=globals(),number=1))
+print(timeit("revers_4(enter_num)", globals=globals(),number=1))
+
+run('main(enter_num)')
+
+"""
+Cамой эффективной можно считать 3 функцию, за ней идет 4, потом 2, а самой неэффективной можно считать 1.
+1 является неэффективной из-за решения через рекурсию, 2 более эффективна так как решена через цикл,
+3 самая эффективная так как в ней используется всего 2 присваивания, одна функия и разворот, 4 же
+является не самой эффективной, так как там используется создание списка, использование функции str
+и метода split, а так же возврат определенного элемента с разворотом этого элемента.
+"""
