59b2a7d5f2d04b6551de9213b4a02e505b7d954f
Alex <krounix@gmail.com>
2021-09-03 20:20:46 +0300

    task_3 completed

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,9 @@
 
 Без аналитики задание считается не принятым
 """
+from random import randint
+from timeit import timeit
+from cProfile import run
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +40,33 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def my_reverse(enter_num):
+    return ''.join(reversed(f'{enter_num}'))
+
+
+def main(num):
+    revers_1(num)
+    revers_2(num)
+    revers_3(num)
+    my_reverse(num)
+
+
+NUMBER = randint(100000, 100000000)
+print('revers_1 -',
+      timeit('revers_1(NUMBER)', globals=globals(), number=100000))
+print('revers_2 -',
+      timeit('revers_2(NUMBER)', globals=globals(), number=100000))
+print('revers_3 -',
+      timeit('revers_3(NUMBER)', globals=globals(), number=100000))
+print('my_reverse -',
+      timeit('my_reverse(NUMBER)', globals=globals(), number=100000))
+
+run('main(NUMBER)')
+
+# Самая эффективная - reverse 3, потому что она самая быстрая.
+# reverse_1 - рекурсивная, всем известно что рекурсии медленные
+# reverse_2 - много переменных и вычислений
+# my_reverse - тратится время на преобразование итератора в строку,
+# зато самое краткое решение
