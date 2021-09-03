6e753c0ddeab9e9c93a94ccdaa49693bba5d61e6
Sergei <serguchers@gmail.com>
2021-09-04 02:29:52 +0300

    hw#5

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -34,3 +34,38 @@ hx = HexNumber
 hx + hx
 hex()
 """
+from collections import defaultdict
+
+
+def hex_calc():
+    num_dct = defaultdict(list)
+    first_num = input('Введите первое число:')
+    second_num = input('Введите второе число:')
+
+    num_dct[first_num].extend(hex(int(first_num, 16)))
+    num_dct[second_num].extend(hex(int(second_num, 16)))
+
+    num_dct[hex(int(first_num, 16) + int(second_num, 16))].extend(hex(int(first_num, 16) + int(second_num, 16)))
+    num_dct[hex(int(first_num, 16) * int(second_num, 16))].extend(hex(int(first_num, 16) * int(second_num, 16)))
+
+    return f'Сумма чисел - {num_dct[hex(int(first_num, 16) + int(second_num, 16))]} \n' \
+           f'Произведение - {num_dct[hex(int(first_num, 16) * int(second_num, 16))]}'
+
+
+class HexNumber:
+    def __init__(self, num: str):
+        self.number = int(num, 16)
+
+    def __add__(self, other):
+        return list(hex(self.number + other.number))
+
+    def __mul__(self, other):
+        return list(hex(self.number * other.number))
+
+if __name__ == '__main__':
+    #print(hex_calc())
+
+    a1 = HexNumber('A2')
+    a2 = HexNumber('C4F')
+    print(a1 + a2)
+    print(a1 * a2)
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,100 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+from collections import deque
+from timeit import timeit
+
+
+def fill_list():
+    test_list = []
+    for i in range(10000):
+        test_list.append(i)
+    return test_list
+
+
+def fill_deque():
+    test_deque = deque( )
+    for i in range(10000):
+        test_deque.append(i)
+    return test_deque
+
+
+print('Заполнение')
+print(timeit('fill_list()', globals=globals( ), number=1000))
+print(timeit('fill_deque()', globals=globals( ), number=1000))
+"""
+Deque заполняется быстрее, чем list.
+"""
+my_list = fill_list( )
+my_deque = fill_deque( )
+
+
+def left_insert_deque():
+    my_deque.appendleft(100)
+    return my_deque
+
+
+def left_insert_list():
+    my_list.insert(0, 100)
+    return my_list
+
+
+print('Вставка в начало массива')
+print(timeit('left_insert_list()', globals=globals( ), number=10000))
+print(timeit('left_insert_deque()', globals=globals( ), number=10000))
+"""
+Вставка в начало deque выполняется в разы быстрее, чем в list. 
+"""
+
+
+def pop_left_deque():
+    my_deque.popleft( )
+    return my_deque
+
+
+def pop_left_list():
+    my_list.pop(0)
+    return my_list
+
+
+print('Достать значение из начала массива')
+print(timeit('pop_left_list()', globals=globals( ), number=10000))
+print(timeit('pop_left_deque()', globals=globals( ), number=10000))
+"""
+Получение значения из начала массива также в несколько раз быстрее осуществляется у deque.
+"""
+
+
+def extend_left_deque():
+    my_deque.extendleft([1, 2, 3])
+    return my_deque
+
+
+def extend_left_list():
+    my_list[:0] = [1, 2, 3]
+    return my_list
+
+
+print('Extendleft list/deque')
+print(timeit('extend_left_list()', globals=globals( ), number=10000))
+print(timeit('extend_left_deque()', globals=globals( ), number=10000))
+"""
+Добавление массива в начало deque выполняется в несколько раз быстрее.
+"""
+
+
+def index_deque():
+    return my_deque[100]
+
+
+def index_list():
+    return my_list[100]
+
+
+print('Indexing list/deque')
+print(timeit('index_list()', globals=globals( )))
+print(timeit('index_deque()', globals=globals( )))
+"""
+Получение элемента по индексу deque выполняет дольше.
+"""
+
