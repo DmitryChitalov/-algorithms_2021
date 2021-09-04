38e11c90f60f23c50b5df8ddbc2ec9448bcb7110
valeripanf <valeripanf@gmail.com>
2021-09-04 17:01:41 +0300

    homework_3

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -53,6 +53,7 @@ def memoize(f):
         else:
             cache[args] = f(*args)
             return cache[args]
+
     return decorate
 
 
@@ -79,3 +80,6 @@ print(
         'recursive_reverse_mem(num_10000)',
         setup='from __main__ import recursive_reverse_mem, num_10000',
         number=10000))
+
+# Вывод: Так как вызов функции совершается однократно и рекурсивная функция не будет использовать кэш, то
+# мемоизация не нужна.
--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -12,6 +12,8 @@
 Без аналитики задание считается не принятым!
 """
 
+from timeit import timeit
+
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
@@ -39,5 +41,18 @@ def func_2():
            f'оно появилось в массиве {max_2} раз(а)'
 
 
+def func_3():
+    return f'Чаще всего встречается число {max(array, key=array.count)}, ' \
+           f'число появилось в массиве {array.count(max(array, key=array.count))} раз.'
+
+
 print(func_1())
 print(func_2())
+print(func_3())
+
+print(timeit('func_1()', globals=globals(), number=1000000))
+print(timeit('func_2()', globals=globals(), number=1000000))
+print(timeit('func_3()', globals=globals(), number=1000000))
+
+# Вывод: Несмотря на визуальную величину и отсутсвие в коде встроенной функции, самой быстрой является функция func_1().
+# Функция func_2() выполняется в 1,5 раза дольше, а функция func_3() в 2 раза дольше.
