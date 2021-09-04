34b5c04316081ae7c7c0e5e16e8f0fe69492634f
Aleksei Synkov <synkovaleksei@yandex.ru>
2021-09-04 21:45:18 +0300

    lesson_5

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -17,9 +17,28 @@
 """
 
 
+from timeit import timeit
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [num for num in range(len(nums)) if nums[num] % 2 == 0]
+    return new_arr
+
+
+nums = [i for i in range(1000)]
+
+print(timeit("func_1(nums)", "from __main__ import func_1, nums", number=1000))     # 0.1125
+print(timeit("func_2(nums)", "from __main__ import func_2, nums", number=1000))     # 0.0849
+print()
+print(timeit("func_1(nums)", "from __main__ import func_1, nums", number=10000))    # 1.1919
+print(timeit("func_2(nums)", "from __main__ import func_2, nums", number=10000))    # 0.8784
+
+# Вывод: Преимущество в производительсности у list comprehension (func_2) примерно в 1,5 раза,
+# чем цикл с функцией добавления.
