03f5f2e15237b8a32bdf594ae1b1f1c7583321e3
Aleksei Synkov <synkovaleksei@yandex.ru>
2021-09-05 17:21:52 +0300

    lesson_5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,84 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+
+
+from collections import deque
+from timeit import timeit
+
+lst_to_test = list(range(10000))
+deq_to_test = deque(lst_to_test)
+
+
+def lst_append(length):
+    lst_test = list()
+    for i in range(length):
+        lst_test.append(i)
+
+
+def deq_append(length):
+    deq_test = deque()
+    for i in range(length):
+        deq_test.append(i)
+
+
+def lst_append_left(length):
+    lst_test = list()
+    for i in range(length):
+        lst_test.insert(0, i)
+
+
+def deq_append_left(length):
+    deq_test = deque()
+    for i in range(length):
+        deq_test.appendleft(i)
+
+
+def lst_get(lst_arg):
+    test = lst_arg[5000]
+
+
+def deq_get(deq_arg):
+    test = deq_arg[5000]
+
+
+def lst_pop(length, lst_arg):
+    for i in range(length):
+        lst_arg.pop()
+
+
+def deq_pop(length, deq_arg):
+    for i in range(length):
+        deq_arg.pop()
+
+
+functions = ['lst_append(1000)',
+             'deq_append(1000)',
+             'lst_append_left(1000)',
+             'deq_append_left(1000)',
+             'lst_get(lst_to_test)',
+             'deq_get(deq_to_test)'
+             ]
+
+functions_2 = ['lst_pop(1000, lst_to_test)', 'deq_pop(1000, deq_to_test)']
+
+for item in functions:
+    print(f"Функция {item}\t - {timeit(item, globals=globals(), number=10000)} сек")
+
+for item in functions_2:
+    print(f"Функция {item}\t -  {timeit(item, globals=globals(), number=1)} сек")
+
+"""
+Результаты: добавление элементов (append) примерно одинаковы; добавление в начало списка deque работает 
+существенно быстрее чем lst.
+По удалению цифры тоже одинаковые.
+
+Функция lst_append(1000)	 - 0.8019821 сек
+Функция deq_append(1000)	 - 0.8846677 сек
+Функция lst_append_left(1000)	 - 3.1922279 сек
+Функция deq_append_left(1000)	 - 0.8146319000000002 сек
+Функция lst_get(lst_to_test)	 - 0.0021290000000000475 сек
+Функция deq_get(deq_to_test)	 - 0.0025978000000002055 сек
+Функция lst_pop(1000, lst_to_test)	 -  5.230000000011614e-05 сек
+Функция deq_pop(1000, deq_to_test)	 -  5.780000000044083e-05 сек
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,48 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+
+from timeit import timeit
+from collections import OrderedDict
+
+
+def simply_create():
+    return {k: v for (k, v) in zip([x for x in range(100)],
+                                   [k for k in range(100)])}
+
+
+def or_create():
+    return OrderedDict({k: v for (k, v) in zip([x for x in range(100)],
+                                               [k for k in range(100)])})
+
+
+
+print(f'Создаение обычного словаря: {timeit("simply_create()", globals=globals(), number=10000)} сек')
+print(f'Создание OrderedDict    : {timeit("or_create()", globals=globals(), number=10000)} сек')
+
+simply_dict = simply_create()
+or_dict = or_create()
+
+key, value = 'key', 'value'
+
+print()
+print(f'Добавление пары: обычный словарь: {timeit("simply_dict[key] = value", globals=globals(), number=1000000)} сек')
+print(f'Добавление пары: OrderedDict    : {timeit("or_dict[key] = value", globals=globals(), number=1000000)} сек')
+
+print()
+print(f'Доступ по ключу: обычный словарь: {timeit("a = simply_dict[key]", globals=globals(), number=1000000)} сек')
+print(f'Доступ по ключу: OrderedDict    : {timeit("a = or_dict[key]", globals=globals(), number=1000000)} сек')
+
+"""
+Результат замеров показывает, что использовать OrderedDict нет необходимости, т.к. выполняется медленее чем dict
+
+Создаение обычного словаря: 0.1905281 сек
+Создание OrderedDict    : 0.35047740000000005 сек
+
+Добавление пары: обычный словарь: 0.0741117 сек
+Добавление пары: OrderedDict    : 0.09317799999999998 сек
+
+Доступ по ключу: обычный словарь: 0.06283539999999999 сек
+Доступ по ключу: OrderedDict    : 0.06172840000000002 сек
+"""
