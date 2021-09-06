39a75666828f06883e95546498c7e195d7211246
Denis Dudnik <Denis.Dudnik@gmail.com>
2021-09-06 14:45:33 +0300

    Homework 5 done. Add task 2 (function and class) + correct task 4

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,45 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+
+from collections import namedtuple
+
+
+def avg_profit(lst):
+    """Возвращает средний годовой доход списка фирм"""
+    sum_profit = 0
+    for el in lst:
+        sum_profit += sum(el[1:])
+    return sum_profit / len(lst)
+
+
+COMPANY = namedtuple('Company', 'name profit_q_1 profit_q_2 profit_q_3 profit_q_4')
+companies = []
+
+try:
+    cnt = int(input('Введите количество предприятий для расчета прибыли: '))
+except ValueError:
+    print('Вводить следует число.')
+else:
+    for idx in range(cnt):
+        comp_name = input('Введите название предприятия: ')
+        print('Через пробел введите прибыль данного предприятия за каждый квартал (Всего 4 квартала): ', end='')
+        try:
+            profit_q_1, profit_q_2, profit_q_3, profit_q_4 = map(int, input().split())
+        except ValueError:
+            print('Следует ввести 4 числа через пробел')
+        else:
+            companies.append(
+                COMPANY(name=comp_name, profit_q_1=profit_q_1, profit_q_2=profit_q_2, profit_q_3=profit_q_3,
+                        profit_q_4=profit_q_4))
+
+avg_profit_per_year = avg_profit(companies)
+print(f'Средняя годовая прибыль всех предприятий: {avg_profit_per_year:.2f}')
+
+print(
+    'Предприятия, с прибылью выше среднего значения: {}'.format(
+        ', '.join([el.name for el in companies if sum(el[1:]) >= avg_profit_per_year])))
+
+print(
+    'Предприятия, с прибылью ниже среднего значения: {}'.format(
+        ', '.join([el.name for el in companies if sum(el[1:]) < avg_profit_per_year])))
