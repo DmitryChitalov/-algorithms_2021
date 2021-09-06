3a43907ad19755e085ee177115610a4b02c3fb98
61003 <g100m8888@gmail.com>
2021-09-06 13:07:12 +0400

    Урок 5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,31 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+from timeit import timeit
+import string
+from collections import deque
+
+print('Заполнение. list быстрее deque')
+print('deque time', timeit("NEW_DEQUE = deque(string.ascii_uppercase)", globals=globals()))
+print('list time', timeit("NEW_LIST = list(string.ascii_uppercase)", globals=globals()))
+print('---')
+NEW_DEQUE = deque(string.ascii_uppercase)
+NEW_LIST = list(string.ascii_uppercase)
+print('Добавление слева. deque намного быстрее чем list')
+print('deque time', timeit("NEW_DEQUE.appendleft('left')", globals=globals(), number=10000))
+print('list time', timeit("NEW_LIST.insert(0, 'left')", globals=globals(), number=10000))
+print('---')
+print('Извлечение слева. deque намного быстрее чем list')
+print('deque time', timeit("left = NEW_DEQUE.popleft()", globals=globals(), number=10000))
+print('list time', timeit("left = NEW_LIST.pop(0)", globals=globals(), number=10000))
+print('---')
+print('Включение слева. list быстрее чем deque')
+print('deque time', timeit("""
+NEW_DEQUE_1 = deque('1234567890')
+NEW_DEQUE.extendleft(NEW_DEQUE_1)
+""", globals=globals()))
+print('list time', timeit("""
+NEW_LIST_1 = list('1234567890')
+NEW_LIST_1.reverse()
+NEW_LIST_1.extend(NEW_LIST)
+""", globals=globals()))
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,22 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+from timeit import timeit
+import collections
+
+print('Заполнение. dict намного быстрее ordereddict')
+print('dict time', timeit("NEW_DICT = {'a': 1, 'b': 2, 'c': 3}", globals=globals()))
+print('ordereddict time',
+      timeit("NEW_ODICT = collections.OrderedDict([('a', 1), ('b', 2), ('c', 3)])", globals=globals()))
+print('---')
+NEW_DICT = {'a': 1, 'b': 2, 'c': 3}
+NEW_ODICT = collections.OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+print('Извлечение. нет существенной разницы')
+print('dict time', timeit("item = NEW_DICT['a']", globals=globals()))
+print('ordereddict time', timeit("item = NEW_ODICT['a']", globals=globals()))
+
+'''
+Нет смысла использовать OrderedDict в Python 3.6 и более поздних версиях,
+так как стандартный Словарь умеет теперь тоже самое, и еще и быстрее работает
+'''
