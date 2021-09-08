918b00a3b6bf41f1875ae43cff0705b0e818bf28
Svetlana <svetik.tomina@mail.ru>
2021-09-08 12:48:12 +0300

    дз5 1,3,4 задание

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -23,3 +23,64 @@
 
 Предприятия, с прибылью ниже среднего значения: Фирма_2
 """
+
+# name tuple
+# defaultdict
+from collections import defaultdict
+
+def parce_value(list):
+    if len(list) <= 4:
+        for i in list:
+            if not i.isdigit():
+                print("Ошбика")
+                input_info()
+                # break
+        # print("Верный ввод прибыли.")
+
+# def analis(dict_comp_info):
+#     dict_comp_middle = defaultdict(int)
+#     for key, value in dict_comp_info.items():
+#         summ = 0
+#         for elem in value:
+#             middle_value = summ + elem
+#         dict_comp_middle[key] = middle_value
+#     print("средняя прибыль (за год для всех предприятий)", dict_comp_middle)
+
+
+def input_info():
+    comp_num = input("Введите количество предприятий для расчета прибыли:")
+    if comp_num.isdigit():
+        comp_num = int(comp_num)
+        dict_comp_info = defaultdict(int)
+        dict_comp_middle = {}
+        for _ in range(comp_num):
+            key = input("Введите название предприятия:")
+            value = input("Через пробел введите прибыль данного предприятия \nза каждый квартал(Всего 4 квартала): ").split(' ')
+            # print(value)
+            parce_value(value)
+            dict_comp_info[key] = value
+            summ = 0
+            for elem in value:
+                summ = summ + int(elem)
+            dict_comp_middle[key] = summ / 4
+
+        print("dict_comp_info", dict_comp_info)
+        print("dict_comp_middle", dict_comp_middle)
+        middle_val_all = 0
+        for val_year_one_comp in dict_comp_middle.values():
+            middle_val_all = middle_val_all + val_year_one_comp
+        middle_val_all = middle_val_all / comp_num
+        print("Средняя прибыль (за год для всех предприятий): ", middle_val_all)
+
+        list_above, list_lower = [], []
+        for key, value in dict_comp_middle.items():
+            list_above.append(key) if value > middle_val_all else list_lower.append(key)
+        print("Предприятия, чья прибыль выше средней:", list_above)
+        print("Предприятия, чья прибыль ниже средней:", list_lower)
+
+    else:
+        print("Некорректный ввод! Повторите ввод.")
+        return input_info()
+
+input_info()
+
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,104 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+
+from time import time
+from datetime import timedelta
+import random
+from collections import deque
+
+def log_time(func):  # O(1)
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        result = func(*args, **kwargs)
+        end_time = time()
+        print(f"Время выполнения ф-ции {func.__name__} составило: {timedelta(seconds=end_time - start_time)}")
+        return result
+    return wrapper
+
+
+@log_time
+def create_list(count):  # O(n)
+    new_list = [random.randint(0, 100) for _ in range(count)]
+    return new_list
+
+@log_time
+def create_deque(count):
+    return deque([random.randint(0, 100) for _ in range(count)])
+
+
+@log_time
+def list_append(list, elem):
+    for i in range(15000):
+        list.insert(0, elem)
+    return list
+
+@log_time
+def deque_append(deque, elem):
+    for i in range(15000):
+        deque.appendleft(elem)
+    return deque
+
+@log_time
+def list_popleft(list):
+    for i in range(10000):
+        list.pop(0)
+    return list
+
+@log_time
+def deque_popleft(deque):
+    for i in range(10000):
+        deque.popleft()
+    return deque
+
+@log_time
+def list_extendleft_1(list, list_ext):
+    for elem in list_ext[0:100]:
+        list.insert(0, elem)
+    return list
+
+@log_time
+def list_extendleft_2(list, list_ext):
+    list_ext.extend(list)
+    return list_ext
+
+@log_time
+def deque_extendleft(deque, deque_ext):
+    deque.extendleft(deque_ext)
+    return deque
+#  popleft, extendleft
+
+length = 100000
+list_1 = create_list(length)
+deque_1 = create_deque(length)
+# список создается дольше, чем очередь
+
+# list_append(list_1, 'word')
+list_2 = list_append(list_1, 'word')
+# print(list_2)
+# print(len(list_2))
+deque_2 = deque_append(deque_1, 'word')
+print(deque_2)
+''' 
+добавление элемента в дек в начало гораздо быстрее, чем в начало списка.
+Добавление 10 000 элементов
+     Время выполнения ф-ции list_append составило: 0:00:00.679185
+    Время выполнения ф-ции deque_append составило: 0:00:00.001000   
+    
+Добавление 15 000 элементов
+     Время выполнения ф-ции list_append составило: 0:00:01.219707
+    Время выполнения ф-ции deque_append составило: 0:00:00.002039'''
+
+list_3 = list_popleft(list_2)
+# print(list_3)
+# print(len(list_3))
+deque_3 = deque_popleft(deque_1)
+# аналогично, дек работает гораздо бытсрее, чем словарь
+
+list_extendleft_1(list_2, list_1)
+list_extendleft_2(list_2, list_1)
+deque_extendleft(deque_2, deque_1)
+print(len(deque_1))
+# добавление даже 100 элементов в начало списка дольше дека, но
+# можно похитрить и добавлять не в начало нашего списка список, а в добавляющийся список наш список,
+# либо использовать reverse(), но у него сложность O(n), что тоже долго
