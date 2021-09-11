2503532b5df6fd963abef0bb8fdc08a41f9ba199
makar1758 <MKR1758@yandex.ru>
2021-09-11 18:01:04 +0300

    1,3,4

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,77 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+
+from collections import deque
+from timeit import timeit
+
+
+def pull_list():
+    xperimnt_list = list(range(10000))
+    return xperimnt_list
+
+def popleft_list(xperimnt_list):
+    xperimnt_list.pop(0)
+    return xperimnt_list
+
+def appendleft_list(xperimnt_list):
+    xperimnt_list.insert(0, 10)
+    return xperimnt_list
+
+def extendleft_list(xperimnt_list):
+    new_lst = [1, 2, 3, 4, 5]
+    for i, num in enumerate(new_lst):
+        xperimnt_list.insert(i, num)
+    return xperimnt_list
+
+def append_list(xperimnt_list):
+    xperimnt_list.append(1)
+    return xperimnt_list
+
+def pull_deque():
+    xperimnt_deque = deque(range(10000))
+    return xperimnt_deque
+
+def popleft_deque(xperimnt_deque):
+    xperimnt_deque.popleft()
+    return xperimnt_deque
+
+def appendleft_deque(xperimnt_deque):
+    xperimnt_deque.appendleft(10)
+    return xperimnt_deque
+
+def extendleft_deque(xperimnt_deque):
+    new_lst = [1, 2, 3, 4, 5]
+    xperimnt_deque.extendleft(new_lst)
+    return xperimnt_deque
+
+def append_deque(xperimnt_deque):
+    xperimnt_deque.append(1)
+    return xperimnt_deque
+
+
+xperimnt_list = pull_list()
+xperimnt_deque = pull_deque()
+
+
+
+print(timeit("pull_list()", globals=globals(),number=10000))
+print(timeit("pull_deque()", globals=globals(),number=10000))
+print('-' * 30)
+print(timeit("popleft_list(xperimnt_list)", globals=globals(),number=10000))
+print(timeit("popleft_deque(xperimnt_deque)", globals=globals(),number=10000))
+print('-' * 30)
+print(timeit("appendleft_list(xperimnt_list)", globals=globals(),number=10000))
+print(timeit("appendleft_deque(xperimnt_deque)", globals=globals(),number=10000))
+print('-' * 30)
+print(timeit("extendleft_list(xperimnt_list)", globals=globals(),number=10000))
+print(timeit("extendleft_deque(xperimnt_deque)", globals=globals(),number=10000))
+print('-' * 30)
+print(timeit("append_list(xperimnt_list)", globals=globals(),number=10000))
+print(timeit("append_deque(xperimnt_deque)", globals=globals(),number=10000))
+"""
+Мы можем видеть, что список заполняется быстрее цем дека, однако все действия связанные с левой частью
+в деке соверщаются в разы быстрее. И в обычном действии типо append у деки все равно сохраняется небольшое 
+преимущество перед списком.
+"""
+
