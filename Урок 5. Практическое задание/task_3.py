72603f93af06f8b7119499a0ab94660394df42dd
mmsolovev <solomix92@gmail.com>
2021-09-11 11:06:32 +0300

    Lesson 5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,118 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+
+from collections import deque
+import timeit
+
+
+my_list = []
+my_deque = deque()
+
+
+def fill_list(some_list):
+    some_list = [i for i in range(10000)]
+    return some_list
+
+
+print('Заполнение list')
+print(timeit.timeit('fill_list(my_list)', globals=globals(), number=1000))
+
+
+def fill_deque(some_deque):
+    some_deque = [i for i in range(10000)]
+    return some_deque
+
+
+print('Заполнение deque')
+print(timeit.timeit('fill_deque(my_deque)', globals=globals(), number=1000))
+
+
+def app_list(some_list):
+    for i in range(100):
+        some_list.insert(0, i)
+    return some_list
+
+
+print('Добавление в начало list')
+print(timeit.timeit('app_list(my_list)', globals=globals(), number=1000))
+
+
+def app_deque(some_deque):
+    for i in range(100):
+        some_deque.appendleft(i)
+    return some_deque
+
+
+print('Добавление в начало deque')
+print(timeit.timeit('app_deque(my_deque)', globals=globals(), number=1000))
+
+
+def pop_list(some_list):
+    for i in range(100):
+        some_list.pop(0)
+    return some_list
+
+
+print('Удаление из начала list')
+print(timeit.timeit('pop_list(my_list)', globals=globals(), number=1000))
+
+
+def pop_deque(some_deque):
+    for i in range(100):
+        some_deque.popleft()
+    return some_deque
+
+
+print('Удаление из начала deque')
+print(timeit.timeit('pop_deque(my_deque)', globals=globals(), number=1000))
+
+new_list = [i for i in range(1000)]
+new_deque = deque([i for i in range(1000)])
+
+
+def ext_list(some_list):
+    some_list.reverse()
+    new_list.reverse()
+    some_list.extend(new_list)
+    some_list.reverse()
+    return some_list
+
+
+print('Добавление элементов в начало list')
+print(timeit.timeit('ext_list(my_list)', globals=globals(), number=1000))
+
+
+def ext_deque(some_deque):
+    some_deque.extendleft(new_deque)
+    return some_deque
+
+
+print('Добавление элементов в начало deque')
+print(timeit.timeit('ext_deque(my_deque)', globals=globals(), number=1000))
+
+
+def el_list(some_list):
+    for i in range(100, 200):
+        some_list[i] = 7
+    return some_list
+
+
+print('Изменение случайного элемента list')
+print(timeit.timeit('el_list(my_list)', globals=globals(), number=1000))
+
+
+def el_deque(some_deque):
+    for i in range(100, 200):
+        some_deque[i] = 7
+    return some_deque
+
+
+print('Изменение случайного элемента deque')
+print(timeit.timeit('el_deque(my_deque)', globals=globals(), number=1000))
+
+"""
+Вывод: По результатам замеров deque заполняется чуть быстрее, чем list.
+Добавление элемента в начало, удаление из начала и добавление группы элементов значительно быстрее в deque.
+А вот доступ к заданному элементу в list оказался быстрее, чем в deque.
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,75 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+import collections
+import timeit
+
+
+my_dict = {}
+my_ordered_dict = collections.OrderedDict()
+
+
+def fill_dict(some_dict):
+    for i in range(1000):
+        some_dict[i] = i + 10
+    return some_dict
+
+
+print('Заполнение dict')
+print(timeit.timeit('fill_dict(my_dict)', globals=globals(), number=1000))
+
+
+def fill_od(some_od):
+    for i in range(1000):
+        some_od[i] = i + 10
+    return some_od
+
+
+print('Заполнение OrderedDict')
+print(timeit.timeit('fill_dict(my_ordered_dict)', globals=globals(), number=1000))
+
+my_list = []
+
+
+def get_from_dict(some_dict):
+    for i in range(100, 200):
+        my_list.append(some_dict[i])
+    return some_dict
+
+
+print('Получение элемента dict')
+print(timeit.timeit('get_from_dict(my_dict)', globals=globals(), number=1000))
+
+
+def get_from_od(some_od):
+    for i in range(100, 200):
+        my_list.append(some_od[i])
+    return some_od
+
+
+print('Получение элемента OrderedDict')
+print(timeit.timeit('get_from_od(my_ordered_dict)', globals=globals(), number=1000))
+
+
+def pop_dict(some_dict):
+    some_dict.pop(1, 'default')
+    return some_dict
+
+
+print('Удаление элемента dict')
+print(timeit.timeit('pop_dict(my_dict)', globals=globals(), number=1000))
+
+
+def pop_od(some_od):
+    some_od.popitem(last=False)
+    return some_od
+
+
+print('Удаление элемента OrderedDict')
+print(timeit.timeit('pop_od(my_ordered_dict)', globals=globals(), number=1000))
+
+"""
+Вывод: Из замеров следует что OrderedDict немного медленнее обычного словаря во всех рассмотренных операциях. Вероятно
+его использование в версиях Python 3.6 и выше может быть уместно только когда важна обратная совместимость кода.
+"""
