a751b416beabdb30b372ecbbaa138157e7759d55
mmsolovev <solomix92@gmail.com>
2021-09-12 17:32:45 +0300

    Lesson 7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,40 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+import random
+import timeit
+
+
+def bubble_sort(some_list):
+    n = 1
+    while n < (len(some_list)):
+        for i in range(len(some_list) - n):
+            if some_list[i] < some_list[i + 1]:
+                some_list[i], some_list[i + 1] = some_list[i + 1], some_list[i]
+        n += 1
+    return some_list
+
+
+def new_bubble_sort(some_list):
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(some_list) - 1):
+            if some_list[i] < some_list[i + 1]:
+                some_list[i], some_list[i + 1] = some_list[i + 1], some_list[i]
+                flag = True
+    return some_list
+
+
+my_list = [random.randint(-100, 100) for i in range(10)]
+
+
+print(my_list)
+print(timeit.timeit('bubble_sort(my_list[:])', globals=globals(), number=1000))
+print(timeit.timeit('new_bubble_sort(my_list[:])', globals=globals(), number=1000))
+
+"""
+Выводы: Для завершения алгоритма в момент, в котором массив окажется полностью отсортированным создаем переменную flag.
+По результатам замеров оптимизация ощутимого выиграша во времени не дает.
+"""
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,45 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+import random
+import timeit
+
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+    return sorted_list
+
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+    mid = len(nums) // 2
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+    return merge(left_list, right_list)
+
+
+num = int(input('Введите число элементов: '))
+my_list = [random.randint(0, 50) for i in range(num)]
+print(f'Исходный - {my_list}')
+print(f'Отсортированный - {merge_sort(my_list[:])}')
+print(timeit.timeit('merge_sort(my_list[:])', globals=globals(), number=1000))
