81c369c6eaa09366c60f3a1946b586d0297aae26
LeraSidorova <valeralera22@icloud.com>
2021-09-12 12:37:50 +0300

    Lesson5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,98 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+
+import time
+from collections import deque
+
+num = 10000
+
+new_list = [el for el in range(num)]
+new_deque = deque(new_list)
+
+
+def time_check(action):
+    def wrapper(*args):
+        start = time.time()
+        result = action(*args)
+        finish = time.time()
+        print(f'Время выполнения: {finish - start}\n')
+        return result
+
+    return wrapper
+
+
+@time_check
+def append_list(num):
+    for i in range(num):
+        new_list.insert(0, i)
+
+
+@time_check
+def append_deque(num):
+    for i in range(num):
+        new_deque.appendleft(i)
+
+
+@time_check
+def extend_list():
+    a = [1, 2]
+    for i in range(len(a)):
+        new_list.insert(0, a[i])
+
+
+@time_check
+def extend_deque():
+    new_deque.extendleft([1, 2])
+
+
+@time_check
+def pop_list(new_list):
+    for i in range(len(new_list)):
+        new_list.pop(0)
+
+
+@time_check
+def pop_deque(new_deque):
+    for i in range(len(new_deque)):
+        new_deque.popleft()
+
+
+print('Cписок appendleft: ')
+new_list_1 = append_list(num)
+
+print('Очередь appendleft: ')
+new_deque_1 = append_deque(num)
+
+print('Cписок extendleft: ')
+new_list_2 = extend_list()
+
+print('Очередь extendleft: ')
+new_deque_2 = extend_deque()
+
+print('Cписок popleft: ')
+new_list_3 =pop_list(new_list)
+
+print('Очередь popleft: ')
+new_deque_3 = pop_deque(new_deque)
+
+
+"""
+Cписок appendleft: 
+Время выполнения: 0.07506704330444336
+Очередь appendleft: 
+Время выполнения: 0.0
+
+Cписок extendleft: 
+Время выполнения: 0.0009829998016357422
+Очередь extendleft: 
+Время выполнения: 0.0
+
+Cписок popleft: 
+Время выполнения: 0.5515656471252441
+Очередь popleft: 
+Время выполнения: 0.0010111331939697266
+
+На основании замеров можно сделать вывод, что deque имеет преимущество над списком.
+Все операции left, которые были выполнены, очередь выполняла быстрее списка.
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,62 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from timeit import timeit
+from collections import OrderedDict
+
+num = 100000
+my_dict = dict()
+for i in range(num):
+    my_dict[num] = num
+my_o_dict = OrderedDict()
+for i in range(num):
+    my_dict[num] = num
+
+
+def new_dict(num):
+    my_dict_1 = dict()
+    for i in range(num):
+        my_dict_1[num] = num
+
+
+def new_o_dict(num):
+    my_dict_1 = OrderedDict()
+    for i in range(num):
+        my_dict_1[num] = num
+
+
+def get_dict(my_dict):
+    for i in range(len(my_dict)):
+        item = my_dict.get(i)
+
+def get_o_dict(my_o_dict_1):
+    for i in range(len(my_o_dict_1)):
+        item = my_o_dict_1.get(i)
+
+
+print(f"new_dict({num}): ",
+      timeit(f"new_dict({num})", globals=globals(), number=1000))
+print(f"new_o_dict({num}): ",
+      timeit(f"new_o_dict({num})", globals=globals(), number=1000))
+
+
+print(f"get_dict(my_dict1): ",
+      timeit(f"get_dict(my_dict)", globals=globals(), number=1000))
+print(f"get_o_dict(my_o_dict1): ",
+      timeit(f"get_o_dict(my_o_dict)", globals=globals(), number=1000))
+
+
+"""
+new_dict(100000):  3.3567807000000003
+new_o_dict(100000):  4.4613515
+
+Заполнение обычного словаря происходит быстрее, чем OrderedDict.
+
+get_dict(my_dict1):  0.00024170000000012237
+get_o_dict(my_o_dict1):  0.00016139999999964516
+
+Получение элемента происходит практически с одинаковой скоростью.
+
+Вывод: применение OrderedDict в Python 3.6 и более поздних версиях не имеет смысла.
+
+"""
