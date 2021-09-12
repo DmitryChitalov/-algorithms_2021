1fae41e91ba29e6ffae3d69a05dce3a26a6a77a1
Sergei <serguchers@gmail.com>
2021-09-12 14:40:38 +0300

    hw#7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,41 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+from random import randint
+from timeit import timeit
+
+
+ARR = [randint(-100, 99) for _ in range(10)]
+ARR1 = [randint(-100, 99) for _ in range(10)]
+
+def bubble_sort_reverse(arr):
+    for i in range(1, len(arr) - 1):
+        already_sorted = True
+        for j in range(len(arr) - 1, -1 + i, -1):
+            if arr[j] > arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+                already_sorted = False
+        if already_sorted:
+            return arr
+    return arr
+
+
+def bubble_non_flagged(arr):
+    for i in range(1, len(arr) - 1):
+        for j in range(len(arr) - 1, -1 + i, -1):
+            if arr[j] > arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+    return arr
+
+
+print(ARR)
+print(bubble_sort_reverse(ARR))
+
+print(timeit("bubble_sort_reverse(ARR)", globals=globals()))    # 0.7598002
+print(timeit("bubble_non_flagged(ARR1)", globals=globals()))     # 3.5343288000000004
+"""
+Даже на таком небольшом массиве мы видим, что установка флага позволяет ускорить сортировку в 5 раз.
+Если увеличить количество элементов массива до 100 и не изменять параметр 'number' функции timeit, 
+то можно будет смело отойти от компьюетра, так как функция без флага 'не видит', 
+что массив уже был ею отсортирован и продолжает совершать обходы.
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -40,3 +40,58 @@ for i in
 
 
 """
+from statistics import median
+from random import randint
+
+
+def find_median(Arr):
+    stat_median = median(Arr)
+    for i in range(len(Arr) // 2):
+        Arr.remove(max(Arr))
+    if stat_median == max(Arr):
+        return max(Arr)
+
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+
+        heapify(arr, n, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+    return arr
+
+
+if __name__ == '__main__':
+    for i in range(100):
+        size = randint(0, 100)
+        TEST = [randint(-100, 100) for _ in range(2 * size + 1)]
+        if not median(TEST) == find_median(TEST) == heapSort(TEST)[size]:
+            print('Ошибка')
+
+    size = int(input( ))
+    TEST = [randint(-100, 100) for _ in range(2 * size + 1)]
+
+    print(median(TEST[:]))
+    print(find_median(TEST[:]))
+    print(heapSort(TEST[:])[size])
