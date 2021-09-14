029ed2ae572591b1585dacec36ce7092cfc332df
Anna <annaosipchik03@gmail.com>
2021-09-14 16:38:26 +0300

    делаю дз 7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,54 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import timeit
+import random
+import operator
+
+
+def generate_list():
+    return [random.uniform(0, 50) for _ in range(100)]
+
+
+def merge_sort(L, compare=operator.lt):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = int(len(L) / 2)
+        left = merge_sort(L[:middle], compare)
+        right = merge_sort(L[middle:], compare)
+        return merge(left, right, compare)
+
+
+def merge(left, right, compare):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+l = generate_list()
+print(l)
+new = merge_sort(l)
+print(new)
+print(
+    f"Сортировка списка функцией merge_sort составила {timeit.timeit('merge_sort(l[:])', globals=globals(), number=1000)} секунд")
+print(
+    f"Сортировка списка функцией merge_sort составила {timeit.timeit('merge_sort(l[:])', globals=globals(), number=10000)} секунд")
+print(
+    f"Сортировка списка функцией merge_sort составила {timeit.timeit('merge_sort(l[:])', globals=globals(), number=100000)} секунд")
+
+# Сортировка списка функцией merge_sort составила 0.5412807000000001 секунд
+# Сортировка списка функцией merge_sort составила 4.6127882 секунд
+# Сортировка списка функцией merge_sort составила 46.2698552 секунд
