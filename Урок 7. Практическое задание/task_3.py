9e01fd8df069d752d19ec3e2df0331f9f6a85885
Alex <krounix@gmail.com>
2021-09-14 21:39:11 +0300

    task_3 completed

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -40,3 +40,67 @@ for i in
 
 
 """
+from random import randint
+from statistics import median
+from timeit import timeit
+
+
+def median_no_sorted(arr):
+    """Поиск медианы без сортировки"""
+    for _ in range(len(arr) // 2):
+        arr.remove(max(arr))
+    return max(arr)
+
+
+def gnome_sort(arr):
+    """Гномья сортировка"""
+    i, size = 1, len(arr)
+    while i < size:
+        if arr[i - 1] <= arr[i]:
+            i += 1
+        else:
+            arr[i - 1], arr[i] = arr[i], arr[i - 1]
+            if i > 1:
+                i -= 1
+    return arr
+
+
+def gnome_median(arr):
+    """Поиск медианы в отсортированом массиве"""
+    return gnome_sort(arr)[len(arr) // 2]
+
+
+m = int(input('Ввведите m: '))
+array = [randint(0, 100) for n in range(2 * m + 1)]
+
+print(array)
+print(f'statistics.median() = {median(array[:])}')
+print(f'median_no_sorted() = {median_no_sorted(array[:])}')
+print(f'gnome_median() = {gnome_median(array[:])}')
+
+# Ввведите m: 5
+# [5, 15, 48, 28, 74, 87, 28, 16, 84, 63, 36]
+# statistics.median() = 36
+# median_no_sorted() = 36
+# gnome_median() = 36
+
+print('#' * 100)
+print(
+    f'statistics.median() = {timeit("median(array[:])", globals=globals(), number=10000)}')
+print(
+    f'median_no_sorted() = {timeit("median_no_sorted(array[:])", globals=globals(), number=10000)}')
+print(
+    f'gnome_median_sort() = {timeit("gnome_median(array[:])", globals=globals(), number=10000)}')
+
+# m = 10
+# statistics.median() = 0.010674514998754603
+# median_no_sorted() = 0.06205638399842428
+# gnome_median_sort() = 0.42656386100134114
+
+# m = 100
+# statistics.median() = 0.07472337000035623
+# median_no_sorted() = 3.1434248460009258
+# gnome_median_sort() = 33.82871456900102
+
+# Поиск медианы через гномью сортировку в данном случае самый медленный,
+# лучший результат у statistics.median()
