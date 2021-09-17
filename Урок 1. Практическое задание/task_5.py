ffd077bab065a78a0d2b1b68264a3b2004cca64a
CommanderRick <rustam.tahamtan@gmail.com>
2021-09-17 22:32:42 +0300

    Домашнее задание. Курс: Алгоритмы Python. Урок 1. Начало курса: 15.09.2021 Рустам Ахмедов

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,57 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class StackOfPlates:
+    def __init__(self, max_plates_in_stack=10):
+        self.elems = [[]]
+        self.stackcount = 0
+        self.maxinstack = max_plates_in_stack
+        self.curr_stack = self.elems[self.stackcount]
+
+    def clear(self):
+        self.curr_stack = []
+        self.elems = [[]]
+        self.stackcount = 0
+        return
+
+    def push_in(self, el):
+        for i in range(el):
+            if sum(self.curr_stack) == self.maxinstack:
+                self.elems.append([])
+                self.stackcount += 1
+                self.curr_stack = self.elems[self.stackcount]
+                self.curr_stack.append(1)
+            else:
+                self.curr_stack.append(1)
+
+    def pop_out(self):
+        result = self.curr_stack.pop()
+        if (len(self.curr_stack) == 0) and (self.stackcount > 0):
+            self.stackcount -= 1
+            self.elems.pop()
+            self.curr_stack = self.elems[self.stackcount]
+        return result
+
+    def countstacks(self):
+        return self.stackcount + 1
+
+    def totalplates(self):
+        return self.stackcount * self.maxinstack + len(self.curr_stack)
+
+
+tmp = StackOfPlates(5)
+
+tmp.push_in(16)
+tmp.push_in(5)
+print(f'Текущая кол-во: {tmp.totalplates()}\n'
+      f'Текущее стопка: {tmp.countstacks()}\n\n')
+
+tmp.pop_out()
+print(f'Текущая кол-во: {tmp.totalplates()}\n'
+      f'Текущее стопка: {tmp.countstacks()}\n\n')
+
+tmp.clear()
+print(f'\nТекущее кол-во: {tmp.totalplates()}\n'
+      f'Текущая стопка: {tmp.countstacks()}\n')
