f1f5a3243d9781e6203133e62f6bf6c9870ec876
py0trandreev <py0trandreev@yandex.ru>
2021-09-18 16:55:34 +0300

    ДЗ08

---
+++ ДЗ_8/dz_8_1.py
@@ -0,0 +1,60 @@
+from collections import Counter
+
+
+string = "beep boop beer!"
+
+
+#Узлы дерева
+class NodeTree(object):
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return (self.left, self.right)
+
+    def nodes(self):
+        return (self.left, self.right)
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+
+# Хафман кодировка
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
+
+
+# Частоты символов
+freq = dict(Counter(string))
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+nodes = freq
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+huffmanCode = huffman_code_tree(nodes[0][0])
+
+# Словарь кодировки
+dict_coding = {}
+for (char, frequency) in freq:
+    dict_coding[char] = huffmanCode[char]
+    
+# Результат кодировки
+for char in string:
+    print(dict_coding[char], end=' ')
