b907adf903b66053055ef8886a3ec1b9f98b86e3
Pavel <pasha050598@mail.ru>
2021-09-22 18:32:59 +0300

    Task 4 completed

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -26,3 +26,51 @@
 Для реализации хранилища можно применить любой подход,
 который вы придумаете, например, реализовать словарь.
 """
+
+
+######################################
+# O(n)
+
+def sign_in(login, password):
+    if login in users.keys() and users[login] == password and is_activate[login] is True:  # O(n)
+        print('Sign in is successful!')
+    elif login not in users.keys():  # O(n)
+        print('Please create a new account!')
+    elif users[login] != password:  # O(1)
+        print('Your password is incorrect!')
+    elif is_activate[login] is False:  # O(1)
+        print('Your account is not activated, please check your email')
+
+
+#######################################
+# O(1)
+
+def sign_in_2(login, password):
+    if users.get(login):  # O(1)
+        if users[login] == password:  # O(1)
+            if is_activate[login]:  # O(1)
+                print('Sign in is successful!')
+            else:
+                print('Your account is not activated, please check your email')
+        else:
+            print('Your password is incorrect!')
+    else:
+        print('Please create a new account!')
+
+
+users = {
+    'Gector': '28336279',
+    'Bobr': '1276579',
+    'Pussy': 'weytuy4'
+}
+
+is_activate = {
+    'Gector': True,
+    'Bobr': True,
+    'Pussy': False
+}
+
+sign_in('Gector', 'user6')
+sign_in('Pussy', 'weytuy4')
+sign_in_2('Gector', 'user6')
+sign_in_2('Pussy', 'weytuy4')
