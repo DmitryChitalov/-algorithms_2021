7a844a8037bfb05ce5fcab667338daca42bccd4b
gige <KartashovVD@yandex.ru>
2021-09-23 22:39:36 +0700

    hope to success

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -1,27 +1,64 @@
+#!/usr/bin/python3
+
+
 """
 Задание 4.
-
 Для этой задачи:
 1) придумайте 2-3 решения (не менее двух)
 2) оцените сложность каждого решения в нотации О-большое
 3) сделайте вывод, какое решение эффективнее и почему
-
 Сама задача:
 Пользователи веб-ресурса проходят аутентификацию.
 В системе хранятся логин, пароль и отметка об активации учетной записи.
-
 Нужно реализовать проверку, может ли пользователь быть допущен к ресурсу.
 При этом его учетка должна быть активирована.
 А если нет, то польз-лю нужно предложить ее пройти.
-
 Приложение должно давать ответы на эти вопросы и быть реализовано в виде функции.
 Для реализации хранилища можно применить любой подход,
 который вы придумаете, например, реализовать словарь.
-
 Примечание:
 Без выполнения пунктов 2 и 3 задание считается нерешенным. Пункты 2 и 3 можно выполнить
 через строки документации в самом коде.
 Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
-
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+users = {
+    'alex': {'activate': True, 'password': 'password'},
+    'okss': {'activate': False, 'password': 'mustang'},
+    'craig': {'activate': False, 'password': 'superman'},
+    'nicksl': {'activate': False, 'password': 'trustno1'},
+    'kendlamar': {'activate': True, 'password': 'tigger'},
+    'hellatr': {'activate': True, 'password': 'biteme'},
+    'itsme': {'activate': False, 'password': 'cplusplus'}
+}	#O(1)
+
+
+# сложность O(1)
+def authorize(login, password, users):
+    contained = users.setdefault(login) #O(1)
+    if contained: # O(1)
+        if contained['activate']: # O(1)
+            if contained.get('password') == password: print('you\'re welcome')
+            else: print('invalid pass')
+        else:
+            print('you need to activate your account')
+
+authorize('hellatr', 'biteme', users)
+
+
+def authorize2(login, password, users):
+    if login in users.keys(): # O(n)
+        cont = users.get(login) # O(1)
+        if cont['activate'] and not cont['password'] == password: # O(1)
+            print('invalid password')
+        elif cont['password'] == password and not cont['activate']: # O(1)
+            print('you need to activate your acc first')
+        else:
+            print('you\'re welcome')
+# O(n)
+
+
+authorize2('hellatr', 'biteme', users)
+
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -1,30 +1,135 @@
 """
 Задание 5.
 Задание на закрепление навыков работы со стеком
-
 Реализуйте структуру "стопка тарелок".
-
 Мы можем складывать тарелки в стопку и при превышении некоторого значения
 нужно начать складывать тарелки в новую стопку.
-
 Структура должна предусматривать наличие нескольких стеков.
 Создание нового стека происходит при достижении предыдущим стеком порогового значения.
 Реализуйте по аналогии с примером, рассмотренным на уроке, необходимые методы,
 для реализации это структуры, добавьте новые методы (не рассмотренные в примере с урока)
 для реализации этой задачи.
-
 После реализации структуры, проверьте ее работу на различных сценариях
-
 Подсказка:
 Отдельне стопки можно реализовать через:
 1) созд-е экземпляров стека (если стопка - класс)
 или
 2) lst = [[], [], [], [],....]
-
 Примечание: в этом задании вспомните ваши знания по работе с ООП
 и опирайтесь на пример урока
-
 Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
-
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class StackClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def push_in(self, el):
+        self.elems.append(el)
+
+    def pop_out(self):
+        return self.elems.pop()
+
+    def get_val(self):
+        return self.elems[len(self.elems) - 1]
+
+    def stack_size(self):
+        return len(self.elems)
+
+
+class PlatesStack:
+    def __init__(self, max_val):
+        self.max_val = max_val
+        self.stacks = []
+        self.plates = []
+
+    def push(self, plate):
+        if len(self.plates) < self.max_val:
+            self.plates.append(plate)
+        else: 
+            self.stacks.append(self.plates)
+            print('here\'s too much plates. let\'s make new stack')
+            self.plates = []
+
+    def stacksnum(self):
+        return len(self.stacks)
+
+    def stackcontained(self):
+        for stack in self.stacks:
+            print(stack)
+
+    def popout(self):
+        poped = self.stacks[-1].pop()
+        if len(self.stacks[-1]) == 0:
+            self.stacks.pop()
+        return poped
+
+
+    def present_stack_eval(self):
+        return len(self.stacks[-1])
+
+
+
+
+sm = PlatesStack(9)
+print(sm.stacksnum())
+sm.push('red')
+sm.push('BLACK')
+sm.push('GREEN')
+sm.push('yellow')
+sm.push('purple')
+sm.push('BLACK')
+sm.push('GREEN')
+sm.push('yellow')
+sm.push('BLACK')
+sm.push('GREEN')
+sm.push('yellow')
+sm.push('white')
+sm.push('yellow')
+sm.push('red')
+sm.push('yellow')
+sm.push('purple')
+sm.push('white')
+sm.push('yellow')
+sm.push('red')
+sm.push('yellow')
+sm.push('purple')
+sm.push('white')
+sm.push('yellow')
+sm.push('red')
+sm.push('yellow')
+sm.push('purple')
+sm.push('white')
+sm.push('yellow')
+sm.push('red')
+sm.push('yellow')
+sm.push('purple')
+sm.push('white')
+sm.push('yellow')
+sm.push('red')
+sm.push('yellow')
+sm.push('purple')
+sm.push('white')
+sm.push('yellow')
+sm.push('red')
+sm.push('yellow')
+sm.push('purple')
+sm.push('white')
+sm.push('yellow')
+sm.push('red')
+sm.push('yellow')
+sm.push('purple')
+sm.push('white')
+sm.push('yellow')
+
+print(sm.stacksnum())
+print(sm.stackcontained())
+print(sm.present_stack_eval())
+print(sm.popout())
+print(sm.present_stack_eval())
+
