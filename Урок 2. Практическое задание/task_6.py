9e6d9689da96c1dab2a2b0dcd428492efb6ed99b
Pavel <pasha050598@mail.ru>
2021-09-24 15:02:11 +0300

    Task 6 completed

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -1,3 +1,5 @@
+import random
+
 """
 6.	В программе генерируется случайное целое число от 0 до 100.
 Пользователь должен его отгадать не более чем за 10 попыток. После каждой
@@ -11,3 +13,25 @@
 Подсказка:
 Базовый случай здесь - угадали число или закончились попытки
 """
+
+
+def game(n, count=1):
+    try:
+        user_answer = int(input(f'{11 - count} attempts\nEnter an integer from 0 to 100: '))
+    except ValueError:
+        print('You entered str-class!')
+        return game(n, count)
+    if count == 10:
+        print('You are failed!')
+    elif user_answer == n:
+        print('You are win!')
+    elif user_answer > n:
+        print('The number is less!')
+        return game(n, count + 1)
+    else:
+        print('The number is bigger!')
+        return game(n, count + 1)
+
+
+a = random.randint(0, 100)
+game(a)
