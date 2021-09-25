7c305535218027775b6ccd23e73695ecfc649f60
Victor Kozlov <vick23@mail.ru>
2021-09-25 16:04:03 +0300

    Программист Python, начало курса 16 сентября 2021 Урок 1. Козлов В. Н.

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,11 +18,10 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
-# ##
-# ##  Не доделал
-# ##
-import datetime
+
+from _datetime import datetime, timedelta
 from collections import namedtuple
+from pprint import pprint
 
 
 # user = User(0, "John", "McClane", "die.hard@hollywood.com")
@@ -40,8 +39,8 @@ class TaskBoard:
         """
         Standard class initializer
         """
-        self.__tasks = dict()
-        self.__users = dict()
+        self.__tasks = dict()  # key - id
+        self.__users = dict()  # key - login
         self.__last_tid = -1
 
     def add_user(self, login: str, first_name: str, last_name: str, email: str) -> bool:
@@ -77,14 +76,14 @@ class TaskBoard:
         """
 
         if deadline is None:
-            datetime.datetime.now() + datetime.timedelta(days=21)
+            datetime.now() + timedelta(days=21)
         if supervisor is None:
             supervisor = author
 
         self.__last_tid += 1
         self.__tasks[self.__last_tid] = (TaskBoard.Task(TaskBoard.STATUS[0], TaskBoard.STATUS[0], title, description,
                                                         author, supervisor, performer, deadline,
-                                                        datetime.datetime.now(), datetime.datetime.now(), None))
+                                                        datetime.now(), datetime.now(), None))
         return True
 
     def update_task(self, task_id: int, status: int, editor: str, performer: str = None,
@@ -95,7 +94,7 @@ class TaskBoard:
         :param task_id: int; ID of the task for update
         :param status: int; new status of the task. Only author or supervisor can set status 'done'(5)
         :param editor: str; login of the user, who edit the task
-        :param performer: str; new task performer TODO:
+        :param performer: str; new task performer
         :param description: str; new description (prepend with '+' to add text to the end of current description).
                                 Only author or supervisor can edit that!
         :param deadline: datetime; new deadline date and time for the task. Only author or supervisor can edit that!
@@ -106,9 +105,21 @@ class TaskBoard:
             print(f"There is no task with id = {task_id}")
             return False
         # Looking for user ID
-        if editor not in self.__users.keys():
-            print("There is no user in the users list with such login")
+        if editor not in (self.__tasks[task_id].author, self.__tasks[task_id].supervisor,
+                          self.__tasks[task_id].performer):
+            print(f"Permission denied! Only author, supervisor or performer can change status! {editor} can not!")
             return False
+        if performer is None:
+            if self.__tasks[task_id].performer is None:
+                performer = self.__tasks[task_id].performer
+            else:
+                if editor != self.__tasks[task_id].author and editor != self.__tasks[task_id].supervisor:
+                    print(f"Only author or supervisor can set new performer! {editor} can not!")
+                    return False
+        else:
+            if editor != self.__tasks[task_id].author and editor != self.__tasks[task_id].supervisor:
+                print(f"Only author or supervisor can set new performer! {editor} can not!")
+                return False
 
         if status < 0:
             status = 0
@@ -118,12 +129,16 @@ class TaskBoard:
                 and self.__tasks[task_id].supervisor != editor:
             print("Only author or supervisor can set status 'done'(5)")
             return False
+        # current_status, previous_status
+        previous_status = self.__tasks[task_id].current_status
+
         if description is not None:
             if self.__tasks[task_id].author != editor and self.__tasks[task_id].supervisor != editor:
                 print("This user has no permission to change description in this task!")
                 return False
         else:
             description = self.__tasks[task_id].description
+
         if deadline is not None:
             if self.__tasks[task_id].author != editor and self.__tasks[task_id].supervisor != editor:
                 print("This user has no permission to change deadline in this task!")
@@ -131,4 +146,53 @@ class TaskBoard:
         else:
             deadline = self.__tasks[task_id].deadline
 
-        self.__tasks[task_id]._replace(status=status, performer=editor, description=description, deadline=deadline)
+        # date_edited, date_finished
+        if status == TaskBoard.STATUS[5]:
+            date_finished = datetime.now()
+        else:
+            date_finished = None
+        date_edited = datetime.now()
+
+        self.__tasks[task_id]._replace(current_status=status, previous_status=previous_status,
+                                       performer=performer, description=description,
+                                       deadline=deadline, date_edited=date_edited, date_finished=date_finished)
+        print(f"Task updated: task_id: {task_id}, status: {status}.{TaskBoard.STATUS[status]}, editor: {editor}, "
+              f"performer: {performer}, description: {description}, "
+              f"deadline: {deadline.isoformat() if deadline is not None else None}")
+        return True
+
+    def show_tasks(self, status: int = None) -> list or None:
+        """
+        Показывает имеющиеся задачи
+        :param status: str; статус задач, которые надо показать
+        :return: список кортежей (id, задача)
+        """
+        task_list = []
+        if status is None:
+            for id, task in self.__tasks.items():
+                task_list.append((id, task))
+        elif status < 0 or status > len(TaskBoard.STATUS) - 1:
+            print(f"Wrong argument: status can be only between 0 and {len(TaskBoard.STATUS) - 1} (both inclusive)!")
+            return None
+        else:
+            for id, task in self.__tasks.items():
+                if task.current_status == status:
+                    task_list.append((id, task))
+
+        pprint(task_list)
+        return task_list
+
+
+if __name__ == '__main__':
+    tb = TaskBoard()
+    tb.add_user('login1', 'fname1', 'lname1', 'email1')
+    tb.add_user('login2', 'fname2', 'lname2', 'email2')
+    tb.add_user('login3', 'fname3', 'lname3', 'email3')
+    tb.add_task('Some task1', 'Some description1', 'login1', supervisor='login1')
+    tb.add_task('Some task2', 'Some description2', 'login1', supervisor='login2')
+    tb.add_task('Some task3', 'Some description3', 'login2', supervisor='login2', performer='login1',
+                deadline=datetime(2021, 9, 23, 19, 0, 0))
+    tb.show_tasks()
+    tb.update_task(0, 2, 'login1', 'login3')
+    tb.update_task(1, 3, 'login2', performer='login2', deadline=datetime.now()+timedelta(days=20))
+    tb.show_tasks()
