09e8cee00590a59c54e5d87af352e2f37a55e160
Sergey Titov <titovsb@gmail.com>
2021-09-25 11:03:40 +0300

    dz3 is done

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -16,3 +16,54 @@
 р
 а
 """
+from pprint import pprint
+
+TEST_STRING = 'papa'
+
+
+def permutation1(s: str, d=dict()) -> dict:
+    if len(s):
+        # if len(s) != 1:
+        d[hash(s)] = s
+        return d | permutation1(s[:-1], d) | permutation1(s[1:], d)
+    return d
+
+
+def permutation2(st: str, s=set()) -> set:
+    if len(st):
+        s.add(hash(st))
+        return s | permutation2(st[:-1], s) | permutation2(st[1:], s)
+    return s
+
+
+if __name__ == '__main__':
+    res = permutation1(TEST_STRING)
+    print(f'Count of unique substring is {len(res.keys())}')
+    pprint(res)
+    print()
+    res = permutation2(TEST_STRING)
+    print(f'Count of unique substring is {len(res)}')
+    pprint(res)
+    exit(0)
+
+'''
+выходной словарь включает саму исходную строку, которая частный случай подстроки:
+
+Count of unique substring is 7
+{-8947889323220907942: 'apa',
+ -4288541607835941840: 'pa',
+ 1330555953908181415: 'ap',
+ 2140981377012498533: 'pap',
+ 3741590063541246851: 'papa',
+ 4299977071224642671: 'p',
+ 7226091154185768689: 'a'}
+
+Count of unique substring is 7
+{-8947889323220907942,
+ -4288541607835941840,
+ 1330555953908181415,
+ 2140981377012498533,
+ 3741590063541246851,
+ 4299977071224642671,
+ 7226091154185768689}
+ '''
--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -15,3 +15,37 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+from pprint import pprint
+
+
+class CacheURL:
+    def __init__(self):
+        self.cache = dict()
+
+    def throw_url(self, url) -> tuple:
+        is_cached = False
+        tmp = hash(url)
+        if not tmp in self.cache:
+            self.cache[tmp] = url
+            is_cached = True
+        return (self.cache[tmp], 'added to cache' if is_cached else 'already in cache')
+
+    def print_cache(self):
+        pprint(self.cache)
+
+
+if __name__ == '__main__':
+    request = CacheURL()
+    print('Site {} {}'.format(*request.throw_url('address1')))
+    print('Site {} {}'.format(*request.throw_url('address2')))
+    print('Site {} {}'.format(*request.throw_url('address1')))
+    print('Site {} {}'.format(*request.throw_url('address2')))
+    request.print_cache()
+
+'''
+Site address1 added to cache
+Site address2 added to cache
+Site address1 already in cache
+Site address2 already in cache
+{-3721402707588848217: 'address1', 1960924240048146215: 'address2'}
+'''
