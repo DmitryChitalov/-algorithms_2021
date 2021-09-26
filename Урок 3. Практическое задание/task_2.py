f1001a23e36b97360e3705ad45094ded31a1d422
ruslan4432013 <ruslan4432013@gmail.com>
2021-09-26 04:58:12 +0300

    Сделал 2-е задание через БД MySQL

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -20,3 +20,36 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+import hashlib
+import pymysql.cursors
+
+# Подключиться к базе данных.
+connection = pymysql.connect(host='localhost', user='root', password='*****', db='user_pass', charset='utf8mb4')
+print("Подключение выполнено!")
+
+passwd = input('Введите пароль: ')
+salt = 'login_user'
+hash_obj = hashlib.sha256(passwd.encode('utf-8') + salt.encode('utf-8'))
+# 3b2aded5e8d001845710769929773d9c3fa579615eb2e1897b1da5841437ce32 (if passwd == 'password')
+try:
+    # SQL
+    cursor = connection.cursor()
+    sql = f"INSERT into user_password (hash_pas) VALUES ('{hash_obj.hexdigest()}')"  # Сохраняем хэш в столбец с паролем
+    cursor.execute(sql)
+    connection.commit()
+    print('Регистрация завершена')
+    # SQL
+    sql_check = "SELECT hash_pas FROM user_password"
+    # Выполнить команду запроса (Execute Query).
+    cursor.execute(sql_check)
+    all_pas = []
+    for row in cursor:
+        all_pas.extend(row)
+    passwd_1 = input('Повторите пароль: ')
+    hash_obj_1 = hashlib.sha256(passwd_1.encode('utf-8') + salt.encode('utf-8'))
+    while hash_obj_1.hexdigest() not in all_pas:
+        passwd_1 = input('Вы не верно ввели пароль, попробуйте еще раз: ')
+        hash_obj_1 = hashlib.sha256(passwd_1.encode('utf-8') + salt.encode('utf-8'))
+    print('Вы ввели правильный пароль')
+finally:
+    connection.close()
