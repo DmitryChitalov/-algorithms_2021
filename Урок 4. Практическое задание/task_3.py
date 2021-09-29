563a173b4aadc98109b51511a7bcca61adbe5e8d
OptikRUS <optikrus61@gmail.com>
2021-09-29 10:50:45 +0300

    СДелал задания к 4 уроку, кроме 5(сжелаю позже)

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,7 +13,8 @@
 
 Без аналитики задание считается не принятым
 """
-
+from timeit import timeit
+from cProfile import run
 
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
@@ -37,3 +38,73 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(n: int, count=''):             # моё решение из ДЗ
+    if n == 0:
+        return f'Перевернутое число: {count}'
+    else:
+        count += str(n % 10)
+        return revers_4(n // 10, count)
+
+
+def revers_5(n: int):                       # ещё один вариант решения, который нельзя было использовать в ДЗ
+    return ''.join(reversed(str(n)))
+
+
+def count(n):       # обёртка кол-ва запусков функций
+    def decorator(func):
+        def wrapper(numb):
+            for i in range(n):
+                func(numb)
+        return wrapper
+    return decorator
+
+@count(100000)
+def main(n):
+    revers_1(n)
+    revers_2(n)
+    revers_3(n)
+    revers_4(n)
+    revers_5(n)
+
+# def main(n):                  # ещё способ запустить функцию много раз
+#     for i in range(100000):
+#         revers_1(n)
+#         revers_2(n)
+#         revers_3(n)
+#         revers_4(n)
+#         revers_5(n)
+
+run('main(2021)')
+
+print(
+    timeit(
+        "revers_1",
+        globals=globals(),
+        number=100000),
+    timeit(
+        "revers_2",
+        globals=globals(),
+        number=100000),
+    timeit(
+        "revers_3",
+        globals=globals(),
+        number=100000),
+    timeit(
+        "revers_4",
+        globals=globals(),
+        number=100000),
+    timeit(
+        "revers_5",
+        globals=globals(),
+        number=100000), sep='\n'
+)
+
+"""
+Все решения приблизительно одинаковы по времени, но решения через рекурсии немного выигрывают, 
+т.к. там не используются встроенные функции, которые значительно влияют на время(в других вариантах они есть), 
+а также нет повторяющихся элементов.
+Также добавил пятое решение, которое нельзя было использовать в ДЗ.
+Через cProfile видно количество раз выполнения рекурсий, что очень удобно.
+"""
