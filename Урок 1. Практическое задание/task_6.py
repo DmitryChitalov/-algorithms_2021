247babead7fe7094af6fe5f43be2a3b9075c6a47
Pavel <stodumoff@yandex.ru>
2021-09-29 20:40:04 +0300

    all compleate

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -29,19 +29,42 @@ class Turn:
     def add_task(self, task):
         self.tasks.append(task)
 
-    def finish(self):
+    def compleate(self):
         self.compleated.append(self.tasks.pop(0))
 
-    def debagging(self):
+    def debagg(self):
         self.debagging.append(self.tasks.pop(0))
 
     def show(self):
         print(f'tasks: {self.tasks}', f'debagging: {self.debagging}', f'compleated: {self.compleated}', sep='\n')
 
-
-
+    def debaging_compleate(self):
+        self.compleated.append(self.debagging.pop(0))
 
 
 if __name__ == '__main__':
     obj = Turn()
+    obj.show()
+    print('доска задач пустая\n')
+
+    # добавим задачи:
+    obj.add_task('task1')
+    obj.add_task('task2')
+    obj.add_task('task3')
+    obj.add_task('task4')
+
+    obj.show()
+    print()
+    # допустим что задачу task1 мы выполнили,
+
+    obj.compleate()
+
+    # а task2 и task3 отправим на доработку
+    obj.debagg()
+    obj.debagg()
+    obj.show()
+    print()
+
+    # теперь доделали task2
+    obj.debaging_compleate()
     obj.show()
