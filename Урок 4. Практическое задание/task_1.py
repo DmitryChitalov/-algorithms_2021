4786e341e1bfdb418e4ca48087a1fbb52183f353
Anton <4qguns@gmail.com>
2021-09-30 14:31:38 +0300

    Алгоритмы и структуры данных на Python. Базовый курс, 17.09.21, урок 3,Галкин Антон. 5-ую задачу не успел, хотя пытался.

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -17,9 +17,39 @@
 """
 
 
+from timeit import timeit
+
+setup = 'numbers = [i for i in range(100)]'
+# numbers = [i for i in range(100)]
+
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]  # <range_iterator object at 0x00...>
+    return new_arr
+
+
+def func_3(nums):
+    new_arr = []
+    gen = (i for i in range(0, len(nums)) if nums[i] % 2 == 0)  # <generator object func_3.<locals>.<genexpr> at 0x0..>
+    while True:
+        try:
+            new_arr.append(next(gen))
+        except StopIteration:
+            break
+    return new_arr
+
+
+# print(func_1(numbers))
+print(timeit('func_1(numbers)', globals=globals(), setup=setup))
+# print(func_2(numbers))
+print(timeit('func_2(numbers)', globals=globals(), setup=setup))
+# print(func_3(numbers))
+print(timeit('func_3(numbers)', globals=globals(), setup=setup))
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -13,8 +13,10 @@
 
 from timeit import timeit
 from random import randint
+from functools import cache
 
 
+# @cache
 def recursive_reverse(number):
     if number == 0:
         return ''
@@ -24,6 +26,9 @@ def recursive_reverse(number):
 num_100 = randint(10000, 1000000)
 num_1000 = randint(1000000, 10000000)
 num_10000 = randint(100000000, 10000000000000)
+# num_100 = 123456
+# num_1000 = 1234567
+# num_10000 = 1234567890
 
 print('Не оптимизированная функция recursive_reverse')
 print(
@@ -40,7 +45,11 @@ print(
     timeit(
         "recursive_reverse(num_10000)",
         setup='from __main__ import recursive_reverse, num_10000',
-        number=10000))
+        number=1))
+
+# print(recursive_reverse(num_100))
+# print(recursive_reverse(num_1000))
+# print(recursive_reverse(num_10000))
 
 
 def memoize(f):
@@ -78,4 +87,33 @@ print(
     timeit(
         'recursive_reverse_mem(num_10000)',
         setup='from __main__ import recursive_reverse_mem, num_10000',
-        number=10000))
+        number=1))
+
+# print(recursive_reverse_mem(num_100))
+# print(recursive_reverse_mem(num_1000))
+# print(recursive_reverse_mem(num_10000))
+
+'''
+Мемоизация в данном случае не нужна, во-первых рекурсивные функции в маленьким числом вызовов (до 13 раз),
+во-вторых вычисляет и записывает кэш в свой словарь (хеш-таблицу) и ускорит быстродействие функции только
+если число на входе в функцию будет состоять из максимально много повторяющихся цифр, но опять же 13 цифр
+это слишком мало, для ощутимого прироста. С мемоизацией кажется, что быстрее так, как число запусков функции
+10000, при первом запуске время на выполнение функции без декоратора уходит меньше (видно из эксперимента ниже),
+а 9999 запусков уже все цифры есть в кэше функции и вычисления не происходят, а только берутся значения из кэша.
+
+Не оптимизированная функция recursive_reverse
+0.018330600000000002
+0.020033200000000008
+6.899999999990247e-06 #  numbers = 1
+Оптимизированная функция recursive_reverse_mem
+0.0014903
+0.0014805999999999986
+1.6900000000000248e-05  # numbers = 1
+
+>>> 6.899999999990247e-06 < 1.6900000000000248e-05
+True
+>>> 6.899999999990247e-06 * 10000
+0.06899309999990248 # реальное время выполнения 
+>>> 1.6900000000000248e-05 * 10000
+0.1689831000000025 # реальное время выполнения 
+'''
