5ec71b29219c8fdef2d3e6f83b3bb3f2b070eb36
Victor Kozlov <vick23@mail.ru>
2021-10-01 05:43:46 +0300

    Программист Python, начало курса 16 сентября 2021, Козлов В. Н. Урок 5.

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -34,3 +34,77 @@ hx = HexNumber
 hx + hx
 hex()
 """
+
+import collections
+
+hx_dec = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+          'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+
+dec_hx = {'0': '0', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9',
+          '10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
+
+# 1. вариант
+
+
+def get_dec(hx_in):
+    dec_out = 0
+    for i in range(len(hx_in)):
+        dec_out += hx_dec[hx_in.pop()] * pow(16, i)
+    return dec_out
+
+
+def get_hx(dec_in):
+    hx_out = collections.deque()
+    while dec_in != 0:
+        hx_out.appendleft(dec_hx[str(dec_in % 16)])
+        dec_in //= 16
+    return hx_out
+
+
+# 2. вариант
+
+
+class HexNumber:
+
+    def __init__(self, num: str):
+        try:
+            self.hex = int(num, 16)
+        except ValueError as ex:
+            self.hex = int('FF', 16)
+
+    def __add__(self, other):
+        if type(other) is HexNumber:
+            return f"{(self.hex + other.hex):X}"
+        else:
+            return f"{(int(str(other), 16) + self.hex):X}"
+
+    def __mul__(self, other):
+        if type(other) is HexNumber:
+            return f"{(self.hex * other.hex):X}"
+        else:
+            return f"{(int(str(other), 16) * self.hex):X}"
+
+
+if __name__ == '__main__':
+    s_in1 = list('A2')
+    s_in2 = list('C4F')
+    hx1 = collections.deque(s_in1)
+    hx2 = collections.deque(s_in2)
+
+    dec1 = get_dec(hx1)
+    dec2 = get_dec(hx2)
+    print(f"Got numbers {s_in1}, {s_in2}")
+
+    summ_dec = dec1 + dec2
+    summ_hx = get_hx(summ_dec)
+    print(f"The sum of {s_in1} and {s_in2} is {str(summ_hx)}")
+
+    mul_dec = dec1 * dec2
+    mul_hx = get_hx(mul_dec)
+    print(f"The product of {s_in1} and {s_in2} is {str(mul_hx)}")
+
+    num1 = HexNumber('A2')
+    num2 = HexNumber('C4F')
+
+    print(num1 + num2)
+    print(num1 * num2)
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,106 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+
+from collections import deque
+from timeit import timeit
+
+lst = []
+dq = deque()
+
+
+def fill_list(number: int):
+    for i in range(number):
+        lst.append(i)
+
+
+def fill_deque(number: int):
+    for i in range(number):
+        dq.append(i)
+
+
+def pop_list():
+    for i in range(len(lst)//2):
+        lst.pop()
+
+
+def pop_deque():
+    for i in range(len(dq)//2):
+        dq.pop()
+
+
+def extend_list(arr: list):
+    lst.extend(arr)
+
+
+def extend_deque(arr: list):
+    dq.extend(arr)
+
+
+def appendleft_list(number: int):
+    for i in range(number):
+        lst.insert(0, i)
+
+
+def appendleft_list_2(number: int, in_lst: list) -> list:
+    for i in range(number):
+        in_lst = [i] + in_lst
+
+    return in_lst
+
+
+def appendleft_deque(number: int):
+    for i in range(number):
+        dq.appendleft(i)
+
+
+def popleft_list(number: int):
+    for i in range(number):
+        lst.pop(0)
+
+
+def popleft_deque(number: int):
+    for i in range(number):
+        dq.popleft()
+
+
+def extandleft_list(arr: list, in_lst: list) -> list:
+    in_lst = arr + in_lst
+
+    return in_lst
+
+
+def extandleft_deque(arr: list):
+    dq.extendleft(arr)
+
+
+if __name__ == '__main__':
+    NUMBER = 10000
+    SIZE = 1000
+    ARR = [23, 34, 45, 56, 67]
+    print(f"Execution time of fill_list: "
+          f"{timeit('fill_list(SIZE)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of fill_deque: "
+          f"{timeit('fill_deque(SIZE)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of pop_list: "
+          f"{timeit('pop_list()', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of pop_deque: "
+          f"{timeit('pop_deque()', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of extend_list: "
+          f"{timeit('extend_list(ARR)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of extend_deque: "
+          f"{timeit('extend_deque(ARR)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of appendleft_list: "
+          f"{timeit('lst = appendleft_list(SIZE//10)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of appendleft_list_2: "
+          f"{timeit('lst = appendleft_list_2(SIZE//10, lst)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of appendleft_deque: "
+          f"{timeit('appendleft_deque(SIZE//10)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of popleft_list: "
+          f"{timeit('popleft_list(SIZE)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of popleft_deque: "
+          f"{timeit('popleft_deque(SIZE)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of extandleft_list: "
+          f"{timeit('lst = extandleft_list(ARR, lst)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of extandleft_deque: "
+          f"{timeit('extandleft_deque(ARR)', globals=globals(), number=NUMBER)}")
