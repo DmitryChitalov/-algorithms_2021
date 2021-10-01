869792062bb3e1fe759d4ad720bb462cd3d19274
ruslan4432013 <ruslan4432013@gmail.com>
2021-10-01 22:40:32 +0300

    Сделал 4-е дз

--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,32 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from collections import OrderedDict
+from timeit import timeit
+
+dict_obj = {12: 'Двенадцать'}
+ord_dict_obj = OrderedDict([(12, 'Двенадцать')])
+
+
+# Функция, выполняющая заполнение словаря
+def app_to_dict(dct):
+    for i, el in enumerate(list(range(10)), start=1):
+        dct[i] = el
+    return dct
+
+
+# Замеры функции заполнения обычного словаря и OrderedDict
+# print(timeit('app_to_dict(dict_obj)', number=1000000, globals=globals()))  # 1.808
+# print(timeit('app_to_dict(ord_dict_obj)', number=1000000, globals=globals()))  # 2.258
+
+# Замеры получения элемента обычного словаря и OrderedDict
+print(timeit('dict_obj[12]', number=100000000, globals=globals()))  # 6.8445566
+print(timeit('ord_dict_obj[12]', number=100000000, globals=globals()))  # 5.6500784
+
+
+'''
+Аналитика:
+Начиная от версии Python 3.6 нет смысла использовать OrderedDict, операция получения
+элементов из OrderedDict, будет чуть быстрее, но не значительно, а в операциях заполнения 
+оказывается еще и медленее.
+'''
