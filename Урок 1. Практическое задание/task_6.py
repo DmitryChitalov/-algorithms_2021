7a4cb01b7e2a3af0fa90ce72786171baecd00ca4
Roman <s-3m@yandex.ru>
2021-10-01 16:10:39 +0300

    Сделал ДЗ 1, 2, 3, 5, 6.

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,3 +18,35 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class Queue:
+    def __init__(self):
+        self.current_tasks = []
+        self.finish_tasks = []
+        self.tasks_on_modify = []
+
+    def push_task(self, task):
+        self.current_tasks.append(task)
+
+    def show_next_task(self):
+        print(f'Задача: {self.current_tasks[0]}')
+        while True:
+            status = input('Укажите статус задачи: 1 - Задача решена 2 - Отправить задачу на доработку')
+            if status == '1':
+                self.finish_tasks.append(self.current_tasks.pop(0))
+                break
+            elif status == '2':
+                self.tasks_on_modify.append(self.current_tasks.pop(0))
+                break
+            else:
+                print('Не корректный ввод статуса!')
+
+    def finish(self):
+        return self.finish_tasks
+
+    def modify(self):
+        return self.tasks_on_modify
+
+    def all_current(self):
+        return self.current_tasks
