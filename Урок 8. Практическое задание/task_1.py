93fba6b85bf6817a4dc68e56220e9549a685c06e
Dmitry Chitalov <35995629+dmitrychitalov@users.noreply.github.com>
2021-10-01 22:35:43 +0500

    lesson_8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,51 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+import heapq
+from collections import Counter
+
+
+class Node:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def walk(self, code, acc):
+        self.left.walk(code, acc + '0')
+        self.right.walk(code, acc + '1')
+
+
+class Leaf:
+    def __init__(self, char):
+        self.char = char
+
+    def walk(self, code, acc):
+        code[self.char] = acc or '0'
+
+
+def huffman_encode(s):
+    h = []
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+    heapq.heapify(h)
+    count = len(h)
+    while len(h) > 1:
+        freq1, _count1, left = heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+
+    code = {}
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, '')
+    return code
+
+
+if __name__ == "__main__":
+    source_string = 'beep boop beer!'
+    tree = huffman_encode(source_string)
+    encode = " ".join(tree[ch] for ch in source_string)
+    for ch in sorted(tree):
+        print(f'{ch}: {tree[ch]}')
+    print(encode)
