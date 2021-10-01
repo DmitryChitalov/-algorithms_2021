780c7fcb10d5b042a044406896f1cca6df9b5822
Lexan <sheklford2015@yandex.ru>
2021-10-01 14:08:30 +0300

    Solved tasks 1,2,3,5,6,7

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -28,3 +28,55 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class StackPlates:
+    # список стопок
+    list_plate_stacks = []
+
+    def __init__(self):
+        self.plates = []
+
+    # кол-во стопок
+    @staticmethod
+    def len_plate_stacks():
+        return len(StackPlates.list_plate_stacks)
+
+    # метод копирования стопки в список
+    def add_stack_to_list(self):
+        stack = self.plates.copy()
+        return StackPlates.list_plate_stacks.append(stack)
+
+        # нахождение высоты/длинны/кол-ва(одна суть) тарелок в стопке
+    def size(self):
+        return len(self.plates)
+
+    # добавление тарелки в стопку
+    def add_plate(self, plate):
+        if self.size() <= 7:  # Думаю при семи стопка не свалится
+            return self.plates.append(plate)
+        else:
+            self.add_stack_to_list()
+            self.plates.clear()
+            return self.add_plate(plate)
+
+
+# Проверим работу
+if __name__ == '__main__':
+
+    plates_stack = StackPlates()
+    plates_stack.add_plate('Фарфоровая1')
+    plates_stack.add_plate('Фарворовая2')
+    plates_stack.add_plate('Глиняная1')
+    plates_stack.add_plate('Стеклянная1')
+    plates_stack.add_plate('Стеклянная2')
+    plates_stack.add_plate('Стеклянная3')
+    plates_stack.add_plate('Фарфоровая3')
+    plates_stack.add_plate('Фарфоровая4')
+    plates_stack.add_plate('Глиняная2')
+    # Список заполненных стопок
+    print(list(StackPlates.list_plate_stacks))
+    # Последняя стопка
+    print(plates_stack.plates)
+    # Кол-во полных стопок
+    print(StackPlates.len_plate_stacks())
--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,3 +18,54 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+class QueueTasks:
+
+    # Список задач
+    complete_tasks = []
+
+    @staticmethod
+    def get_all_solved():
+        print(QueueTasks.complete_tasks)
+
+    # Создание очереди с наименованием
+    def __init__(self, name):
+        self.queue = []
+        self.name = name
+
+    # Создание задачи
+    def add_task(self, task):
+        return self.queue.insert(0, task)
+
+    # Забрать первую задачу в очереди
+    def get_task(self):
+        return self.queue.pop()
+
+    # Перенос задачи в другую очередь
+    def transfer_task(self, queue):
+        return queue.add_task(self.get_task())
+
+    # Вывод очереди на экран
+    def get_queue(self):
+        print(self.queue)
+
+    # перенос задачи в решенные
+    def solve_task(self):
+        return QueueTasks.complete_tasks.append(self.get_task())
+
+
+if __name__ == '__main__':
+    tasks_1 = QueueTasks('Базовая очередь')
+    tasks_2 = QueueTasks('Очередь на доработку')
+    tasks_1.add_task('прочитать войну и мир')
+    tasks_1.add_task('прочитать мифы древней греции')
+    tasks_1.add_task('прогуляться')
+    tasks_1.add_task('сварить борщ')
+    tasks_1.add_task('лечь пораньше')
+    tasks_1.get_queue()
+    tasks_1.transfer_task(tasks_2)
+    tasks_2.get_queue()
+    tasks_2.solve_task()
+    tasks_2.get_queue()
+    QueueTasks.get_all_solved()
