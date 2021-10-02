0205044018ec0735e91e0da00fa943616c704505
Pavel <pasha050598@mail.ru>
2021-10-02 19:35:41 +0300

    Task 3 completed

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,3 +1,5 @@
+from cProfile import run
+from timeit import timeit
 """
 Задание 3.
 
@@ -36,3 +38,24 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(num):
+    return str(num) if num < 10 else str(num % 10) + revers_4(num // 10)
+
+
+run('revers_1(1000000000)')
+run('revers_2(1000000000)')
+run('revers_3(1000000000)')
+run('revers_4(1000000000)')
+
+print(timeit('revers_1(1000000000)', globals=globals(), number=1000))
+print(timeit('revers_2(1000000000)', globals=globals(), number=1000))
+print(timeit('revers_3(1000000000)', globals=globals(), number=1000))
+print(timeit('revers_4(1000000000)', globals=globals(), number=1000))
+
+"""
+1)функция 3 сработала быстрее всех, ак как используется срез строк;
+2)Неплохой результуат у функции 2 с циклами;
+3)Рекурсия на последнем месте.
+"""
