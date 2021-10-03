2908cbe0a9c13357cc590d30f138643bda8bad3c
Victor Kozlov <vick23@mail.ru>
2021-10-03 21:02:12 +0300

    Программист Python, начало курса 16 сентября 2021, Козлов В. Н. Урок 5.

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -65,12 +65,12 @@ def appendleft_deque(number: int):
 
 
 def popleft_list(number: int):
-    for i in range(number):
+    for i in range(number//2):
         lst.pop(0)
 
 
 def popleft_deque(number: int):
-    for i in range(number):
+    for i in range(number//2):
         dq.popleft()
 
 
@@ -85,13 +85,27 @@ def extandleft_deque(arr: list):
 
 
 if __name__ == '__main__':
-    NUMBER = 10000
+    NUMBER = 1000
     SIZE = 1000
     ARR = [23, 34, 45, 56, 67]
     print(f"Execution time of fill_list: "
           f"{timeit('fill_list(SIZE)', globals=globals(), number=NUMBER)}")
     print(f"Execution time of fill_deque: "
           f"{timeit('fill_deque(SIZE)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of appendleft_list: "
+          f"{timeit('appendleft_list(SIZE//10)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of appendleft_list_2: "
+          f"{timeit('appendleft_list_2(SIZE//10, lst)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of appendleft_deque: "
+          f"{timeit('appendleft_deque(SIZE//10)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of popleft_list: "
+          f"{timeit('popleft_list(SIZE)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of popleft_deque: "
+          f"{timeit('popleft_deque(SIZE)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of extandleft_list: "
+          f"{timeit('extandleft_list(ARR, lst)', globals=globals(), number=NUMBER)}")
+    print(f"Execution time of extandleft_deque: "
+          f"{timeit('extandleft_deque(ARR)', globals=globals(), number=NUMBER)}")
     print(f"Execution time of pop_list: "
           f"{timeit('pop_list()', globals=globals(), number=NUMBER)}")
     print(f"Execution time of pop_deque: "
@@ -100,17 +114,29 @@ if __name__ == '__main__':
           f"{timeit('extend_list(ARR)', globals=globals(), number=NUMBER)}")
     print(f"Execution time of extend_deque: "
           f"{timeit('extend_deque(ARR)', globals=globals(), number=NUMBER)}")
-    print(f"Execution time of appendleft_list: "
-          f"{timeit('lst = appendleft_list(SIZE//10)', globals=globals(), number=NUMBER)}")
-    print(f"Execution time of appendleft_list_2: "
-          f"{timeit('lst = appendleft_list_2(SIZE//10, lst)', globals=globals(), number=NUMBER)}")
-    print(f"Execution time of appendleft_deque: "
-          f"{timeit('appendleft_deque(SIZE//10)', globals=globals(), number=NUMBER)}")
-    print(f"Execution time of popleft_list: "
-          f"{timeit('popleft_list(SIZE)', globals=globals(), number=NUMBER)}")
-    print(f"Execution time of popleft_deque: "
-          f"{timeit('popleft_deque(SIZE)', globals=globals(), number=NUMBER)}")
-    print(f"Execution time of extandleft_list: "
-          f"{timeit('lst = extandleft_list(ARR, lst)', globals=globals(), number=NUMBER)}")
-    print(f"Execution time of extandleft_deque: "
-          f"{timeit('extandleft_deque(ARR)', globals=globals(), number=NUMBER)}")
+
+"""
+Execution time of fill_list: 0.055555499999999994
+Execution time of fill_deque: 0.044700100000000006
+=========================================================
+Execution time of appendleft_list: 111.8603032
+Execution time of appendleft_list_2: 848.7672498000001
+Execution time of appendleft_deque: 0.041238099999986844
+=========================================================
+Execution time of popleft_list: 323.66882439999995
+Execution time of popleft_deque: 0.01934920000007878
+=========================================================
+Execution time of extandleft_list: 4.676811400000133
+Execution time of extandleft_deque: 0.03451039999981731
+=========================================================
+Execution time of pop_list: 0.026632800000015777
+Execution time of pop_deque: 0.02691939999999704
+=========================================================
+Execution time of extend_list: 0.00022439999997914128
+Execution time of extend_deque: 0.0001257999999779713
+
+ВЫВОД:
+Все операции с левой (начальной) стороны списка выполняются на порядки медленнее, чем аналогичные операции в деке.
+Заполнение дека происходит не сильно быстрее списка, расширение с конца списка почти в два раза медленнее дека, а вот 
+операция рор с конца примерно одинакова до тысячных.
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,109 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+from collections import OrderedDict as od
+from timeit import timeit
+
+
+class DictVSOderedDict:
+
+    def __init__(self, size):
+        self.dct = {}
+        self.odct = od()
+        self.size = size
+
+    def fill_dict(self):
+        for i in range(self.size):
+            self.dct[i] = i
+
+    def fill_odict(self):
+        for i in range(self.size):
+            self.odct[i] = i
+
+    def show_dict_items(self):
+        for key, val in self.dct.items():
+            if int(key) % 10 == 0:
+                print()
+            print(f"{key:>5}:{val:>5}", end='\t')
+
+    def show_odict_items(self):
+        for key, val in self.odct.items():
+            if int(key) % 10 == 0:
+                print()
+            print(f"{key:>5}:{val:>5}", end='\t')
+
+    def write_dict_items_to_file(self, filename: str):
+        with open(filename, mode='w', encoding='utf-8') as f:
+            for key, val in self.dct.items():
+                if int(key) % 10 == 0:
+                    f.write('\n')
+                f.write(f"{key:>5}:{val:>5}\t")
+
+    def write_odict_items_to_file(self, filename: str):
+        with open(filename, mode='w', encoding='utf-8') as f:
+            for key, val in self.odct.items():
+                if int(key) % 10 == 0:
+                    f.write('\n')
+                f.write(f"{key:>5}:{val:>5}\t")
+
+    def pop_dict(self, keys: list):
+        for key in keys:
+            self.dct.pop(key)
+
+    def pop_odict(self, keys: list):
+        for key in keys:
+            self.odct.pop(key)
+
+
+if __name__ == '__main__':
+    battle = DictVSOderedDict(10000)
+    dict_file = 'dct.txt'
+    odict_file = 'odct.txt'
+
+    print(f"Fill dict time: "
+          f"{timeit('battle.fill_dict()', number=1000, globals=globals())}")
+
+    print(f"Fill ordered dict time: "
+          f"{timeit('battle.fill_odict()', number=1000, globals=globals())}")
+
+    print(f"Show elements of the dict time: "
+          f"{timeit('battle.write_dict_items_to_file(dict_file)', number=1000, globals=globals())}")
+
+    print(f"Show elements of the ordered dict time: "
+          f"{timeit('battle.write_odict_items_to_file(odict_file)', number=1000, globals=globals())}")
+
+    keys_dct = list(battle.dct.keys())
+    print(f"Pop elements of the dict time: "
+          f"{timeit('battle.pop_dict(keys_dct)', number=1, globals=globals())}")
+
+    keys_odct = list(battle.odct.keys())
+    print(f"Pop elements of the ordered dict time: "
+          f"{timeit('battle.pop_odict(keys_odct)', number=1, globals=globals())}")
+
+"""
+Fill dict time: 0.5122118
+Fill ordered dict time: 0.9826965000000001
+Show elements of the dict time: 7.6706311000000005
+Show elements of the ordered dict time: 8.084804899999998
+Pop elements of the dict time: 0.0007030999999990684
+Pop elements of the ordered dict time: 0.001321900000000653
+
+Fill dict time: 0.508626
+Fill ordered dict time: 0.9801002999999999
+Show elements of the dict time: 7.703318
+Show elements of the ordered dict time: 8.1342484
+Pop elements of the dict time: 0.0007145999999984554
+Pop elements of the ordered dict time: 0.0013319999999978904
+
+Fill dict time: 0.5139958
+Fill ordered dict time: 0.9917861
+Show elements of the dict time: 7.7828919
+Show elements of the ordered dict time: 8.1589096
+Pop elements of the dict time: 0.0007260000000002265
+Pop elements of the ordered dict time: 0.0013684000000004914
+
+Во всех проведённых операциях OrderedDict оказался медленнее, при этом dict в полной мере выполняет тот же функционал.
+Таким образом, нет особого смысла использовать OrderedDict в современных версиях python. Более того, по полученным 
+данным получается, что стоит даже избегать использования OrderedDict.
+"""
