5db2973e78e3d1a284afa47cbf827ae20e2bc6f4
F1ourish <flourishstream@gmail.com>
2021-10-04 22:32:02 +0300

    Выполнение заданий к Уроку 1.

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -18,3 +18,78 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+
+class QueueClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def push_in(self, item):
+        self.elems.insert(0, item)
+
+    def pop_out(self):
+        return self.elems.pop()
+
+    def stack_size(self):
+        return len(self.elems)
+
+
+class TaskBoardClass:
+    def __init__(self):
+        self.current = QueueClass()
+        self.rework = QueueClass()
+        self.resolved = []
+
+    def to_rework(self):
+        """Отправляем текущую на доработку"""
+        task = self.current.pop_out()
+        self.rework.push_in(task)
+
+    def to_current(self, item):
+        """Добавляем задачу в текущую очередь"""
+        self.current.push_in(item)
+
+    def current_task(self):
+        """Текущая задача"""
+        return self.current.elems[len(self.current.elems) - 1]
+
+    def from_rework(self):
+        """Возвращаем задачу из доработки в текущую очередь"""
+        task = self.rework.pop_out()
+        self.current.push_in(task)
+
+    def current_rework(self):
+        """На доработке"""
+        return self.rework.elems[len(self.rework.elems) - 1]
+
+    def resolved_task(self):
+        """текущую задачу добавляем в решёные"""
+        task = self.current.pop_out()
+        self.resolved.append(task)
+
+
+
+task_board = TaskBoardClass()
+task_board.to_current("Task1")
+task_board.to_current("Task2")
+task_board.to_current("Task3")
+task_board.to_current("Task4")
+task_board.to_current("Task5")
+
+print(task_board.current.elems)
+print(task_board.current_task())
+
+task_board.to_rework()
+task_board.to_current()
+task_board.from_rework()
+task_board.current_task()
+task_board.current_rework()
+task_board.resolved_task()
+
+print(task_board.current.elems)
+print(task_board.current_task())
+print(task_board.resolved_task)
