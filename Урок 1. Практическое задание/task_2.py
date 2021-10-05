436ad5cdf054facecc9ec223f999af9bb14dc280
RomanGeekBrains <Roman_Lebedev91@mail.ru>
2021-10-05 21:11:29 +0600

    DeadLine for homework # 1. Lebedev R

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -19,3 +19,46 @@
 Постарайтесь не использовать ф-ции min() и sort() и другие ф-ции!
 Подход должен быть максимально алгоритмическим.
 """
+
+# ------------------------------algorithm no. 1------------------------------
+def get_min_value(any_list_numb):
+    '''
+    param any_list_numb: a list of numbers and / or real numbers
+    return: minimum value from a list of integers and / or real numbers
+
+    complexity: O(N^2)
+    '''
+    list_length = len(any_list_numb) # O(1)
+    for i in range(1, list_length): # O(N)
+        for j in range(i, 0, -1): # O(N)
+            if any_list_numb[j] < any_list_numb[j-1]: # O(1)
+                any_list_numb[j], any_list_numb[j-1] = any_list_numb[j-1], any_list_numb[j] # O(1)
+            else:
+                break
+    return any_list_numb[0] # O(1)
+
+    # Почему-то сомневался в if any_list_numb[j] < any_list_numb[j-1]: # O(1)
+    # по сути это ведь сравнение двух переменных, в которых одиночные значения
+    # значит и сложность константная.
+    # По сути вложенность циклов друг в друга дают O(N)*O(N)=O(N^2). 
+
+# ------------------------------algorithm no. 2------------------------------
+def get_min_value_(any_list_numb):
+    '''
+    param any_list_numb: a list of numbers and / or real numbers
+    return: minimum value from a list of integers and / or real numbers
+
+    complexity: O(N)
+    '''
+    inferior_limit = any_list_numb[0] # need to start with some value # O(1)
+    for i in any_list_numb: # O(N)
+        if i < inferior_limit: # O(1)
+            inferior_limit = i # O(1)
+    return inferior_limit # O(1)
+
+
+some_list_with_numbers = [4, 345, 3, 456, 65, -2.5, 0, 0, 3, -55, 2345, 345.578, 267]
+
+print(get_min_value(some_list_with_numbers))
+
+print(get_min_value_(some_list_with_numbers))
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -25,3 +25,68 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+# data = {
+#     'log0':[['passw0'], True],
+#     'log1':[['passw1'], True],
+#     'log2':[['passw2'], True],
+#     'log3':[['passw3'], True],
+#     'log4':[['passw4'], False],
+#     'log5':[['passw5'], True],
+# }
+
+# arr = list(data.items())
+# print(arr[0]) # info user
+# print(arr[0][0]) # log user
+# print(arr[0][1][0][0]) # passw user
+# print(arr[0][1][1]) # metka user
+
+data = [
+    ['log', 'passw', 'qeushens???', 'answer!!!', 'key', True],
+    ['log1', 'passw1', 'qeushens???1', 'answer!!!1', 'key1', True],
+    ['log2', 'passw2', 'qeushens???2', 'answer!!!2', 'key2', False],
+]
+
+
+
+# print(data[0]) #info from user
+# print(data[0][0]) # log
+# print(data[0][1]) # passw
+# print(data[0][2]) # qeushens
+# print(data[0][3]) # answer
+# print(data[0][4]) # key
+# print(data[0][5]) # metka
+
+
+user_inp = input('для входа в ресурс выполните: 0-вход, 1-аут, 2-региср')
+
+if int(user_inp) == 0:
+    us_log = input('введите логин')
+    us_passw = input('введите пароль')
+
+    def inter(log, passw, database):
+        for inf_users in database:
+            if us_log == inf_users[0] and us_passw == inf_users[1] and True in inf_users:
+                print('hello!')
+            elif us_log == inf_users[0] and us_passw == inf_users[1] and False in inf_users:
+                sys_qeshens = input('вы не прошли аут! хотите пройти? у/н')
+                if sys_qeshens == 'y':
+                    us_key = input('введите ключ аут ')
+                    if us_key == inf_users[4]:
+                        data[data.index(inf_users)][5] = True
+                        print('аутен пройдена!')
+                        print('hello!!')
+                else:
+                    print('пока пока')
+
+
+    inter(us_log, us_passw, data)
+
+
+# ПОМЕТКА: к сожалению, мне совсем не хватило времени, чтобы сделать это задание
+# Я представил его сделать с активацией, восстановление пароля и так далее
+# очень грандиозно и скорей всего громоздко =0
+
+
+
