52774826f3097d6d722dd92237cd62f9aead9b8d
ruslan4432013 <ruslan4432013@gmail.com>
2021-10-07 03:08:19 +0300

    Сделал 1-е дз

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,53 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+import random
+from timeit import timeit
+
+test_massive = [random.randint(-100, 100) for _ in range(100)]
+
+
+def bubble_sort_old(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def bubble_sort(lst_obj: list):
+    n = 1
+    while n < len(lst_obj):
+        sort = True
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+                sort = False
+        if sort is True:
+            break
+        n += 1
+
+    return lst_obj
+
+
+# Выводим список до и после сортировки
+print(f'Список до сортировки {test_massive}')
+print(f'Список после сортировки {bubble_sort(test_massive.copy())}')
+# Делаем замеры
+# 100 измерений
+print(timeit('bubble_sort_old(test_massive.copy())', number=100, globals=globals()))  # 0.08
+print(timeit('bubble_sort(test_massive.copy())', number=100, globals=globals()))  # 0.13
+# 1000 измерений
+print(timeit('bubble_sort_old(test_massive.copy())', number=1000, globals=globals()))  # 1.06
+print(timeit('bubble_sort(test_massive.copy())', number=1000, globals=globals()))  # 1.05
+# 10000 измерений
+print(timeit('bubble_sort_old(test_massive.copy())', number=10000, globals=globals()))  # 10.06
+print(timeit('bubble_sort(test_massive.copy())', number=10000, globals=globals()))  # 10.27
+
+'''
+Аналитика:
+Если за весь цикл не выполняется ни одной сортировки, то цикл завершается
+Особого смысла в этой доработке нет, т.к получить отсортированный массив практически невозможно
+'''
