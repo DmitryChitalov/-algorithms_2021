321d0a2dbdfb6de52a7cb5baac9cebe42caffd44
OptikRUS <optikrus61@gmail.com>
2021-10-09 15:45:39 +0300

    Выполнил задания 7 урока

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,43 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+from random import randint
+from timeit import timeit
+
+not_sorted_list = [randint(-100, 100) for i in range(100)]
+
+def bubble(lst: list):
+    n = 1
+    while n < len(lst):
+        for i in range(len(lst) - n):
+            if lst[i] < lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+        n += 1
+    return lst
+
+
+def bubble_mod(lst: list):
+    n = 1
+    while n < len(lst):
+        count = False
+        for i in range(len(lst) - n):
+            if lst[i] < lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+                count = True
+        if count:
+            break
+        n += 1
+    return lst
+
+print(bubble(not_sorted_list))
+print(bubble_mod(not_sorted_list))
+
+print(
+    timeit("bubble(not_sorted_list)", globals=globals(), number=1000),
+    timeit("bubble_mod(not_sorted_list)", globals=globals(), number=1000), sep='\n'
+)
+
+"""
+Не вижу особого смысла в доработке, т.к. скорость выполнение зависит от везения
+"""
