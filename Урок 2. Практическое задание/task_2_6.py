407418df7273a5b18ce1f80b6e1e96cf130bf89a
Max <setborg@inbox.ru>
2021-10-10 23:52:49 +0500

    Обновил сложность в 6 файле

--- Урок 2. Практическое задание/task_2_6.py
+++ Урок 2. Практическое задание/task_2_6.py
@@ -2,20 +2,20 @@ from random import randint
 
 def ygadaika(pk_numb, numb_att=10):
     if numb_att == 0:
-        return print(f'Вы использовали все попытки, загаданое число было = {pk_numb}')
+        return print(f'Вы использовали все попытки, загаданое число было = {pk_numb}')                                  #O(1)
 
     num = int(input(f'Введите число которое загадал компьютер от 0 до 10, у вас осталось {numb_att} попыток : '))
     if num == pk_numb:
-        return print(f'Вы угадачи, это число -  {pk_numb}')
+        return print(f'Вы угадачи, это число -  {pk_numb}')                                                             #O(1)
     else:
         if num > pk_numb:
             print('Вы ввели больше чем загаданое число')
             print(f'Осталось попыток = {numb_att - 1}')
-            ygadaika(pk_numb,numb_att - 1)
+            ygadaika(pk_numb, numb_att - 1)
         else:
             print('Вы ввели меньше чем загаданое число')
             print(f'Осталось попыток = {numb_att - 1}')
-            ygadaika(pk_numb,numb_att - 1)
+            ygadaika(pk_numb, numb_att - 1)
 
 pk_random = randint(0,10)
 ygadaika(pk_random)
