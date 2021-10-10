da626b96793e19fc7ba065616f37b72480e723e4
Vadim <vadim2387@gmail.com>
2021-10-10 21:00:46 +0300

    lesson_3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -20,3 +20,57 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+import sqlite3
+from uuid import uuid4
+import hashlib
+
+
+class DbHash:
+    def __init__(self):
+        self.conn = sqlite3.connect("test_al.db")
+        self.cursor = self.conn.cursor()
+
+    def create_table(self):
+        self.cursor.execute("DROP TABLE IF EXISTS users;")
+        self.cursor.execute("CREATE TABLE users(u_login varchar(255), password_hash varchar(255))")
+
+    def create_hash(self):
+        login = input('Введите логин: ')
+        us_pass = input('Введите пароль: ')
+        hash_passwd = hashlib.sha256(login.encode() + us_pass.encode()).hexdigest()
+        return login, hash_passwd
+
+    def sql_insert(self):
+
+        login, hash_pass = self.create_hash()
+
+        insert_log_hash = 'INSERT INTO users(u_login, password_hash) VALUES(?,?)'
+        log_hash = (login, hash_pass)
+        try:
+            self.cursor.execute(insert_log_hash, log_hash)
+        except sqlite3.IntegrityError:
+            print('login уже существует. Войдите в систему или введите другой login.')
+        else:
+            self.conn.commit()
+            print('Вы успешно зарегестрированы')
+
+    def log_in(self):
+
+        login, hash_pass = self.create_hash()
+
+        select_log_hash = 'SELECT password_hash FROM users WHERE u_login = ?'
+
+        self.cursor.execute(select_log_hash, (login,))
+
+        db_hash = self.cursor.fetchone()
+        db_hash = str(*db_hash)
+
+        if hash_pass == db_hash:
+            print('Пароль верен')
+        else:
+            print('Пароль неверен')
+
+data_base_1 = DbHash()
+data_base_1.create_table()
+data_base_1.sql_insert()
+data_base_1.log_in()
