7a4fe94ef6cdee4897cd2a5eee2580b379b3030e
Mikhail <mikhail.nazarov@internet.ru>
2021-10-10 22:33:51 +0300

    4 Lesson

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -16,6 +16,9 @@
 на самом деле к генераторам отношения не имеет. Это называется "списковое включение" - list comprehension.
 """
 
+from timeit import timeit
+from numpy import number
+
 
 def func_1(nums):
     new_arr = []
@@ -23,3 +26,69 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+numbers_10 = [i for i in range(10)]
+numbers_100 = [i for i in range(100)]
+numbers_1000 = [i for i in range(1000)]
+
+print('func_1(numbers_10): ')
+print(
+    timeit(
+        "func_1(numbers_10)",
+        setup="from __main__ import func_1, numbers_10", number=100000))
+
+print('func_1(numbers_100): ')
+print(
+    timeit(
+        "func_1(numbers_100)",
+        setup="from __main__ import func_1, numbers_100", number=100000))
+
+print('func_1(numbers_1000): ')
+print(
+    timeit(
+        "func_1(numbers_1000)",
+        setup="from __main__ import func_1, numbers_1000", number=100000))
+
+print('func_2(numbers_10): ')
+print(
+    timeit(
+        "func_2(numbers_10)",
+        setup="from __main__ import func_2, numbers_10", number=100000))
+
+print('func_2(numbers_100): ')
+print(
+    timeit(
+        "func_2(numbers_100)",
+        setup="from __main__ import func_2, numbers_100", number=100000))
+
+print('func_2(numbers_1000): ')
+print(
+    timeit(
+        "func_2(numbers_1000)",
+        setup="from __main__ import func_2, numbers_1000", number=100000))
+
+'''
+func_1() оптимизирована через list comprehension.
+Время исполнения на 100 000 вызовах функции: 
+
+Для списка из 10 элементов:
+func_1 = 0.10630044800200267
+func_2(LC) = 0.09646925700144493
+
+Для списка из 100 элементов:
+func_1 = 0.7473548349989869
+func_2(LC) = 0.6210089519991016
+
+Для списка из 1000 элементов:
+func_1 = 7.462719033999747
+func_2(LC) = 6.460678071001894
+
+ВЫВОД: List comprehension позволяет оптимизировать значительно время только при увеличении вводимых элементов.
+       Так же при работе с малым количеством элементов, ЦИКЛ проявляет себя незначительно быстрее LS.
+'''
--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -15,6 +15,10 @@
 """
 
 
+from timeit import timeit
+from cProfile import run
+
+
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -37,3 +41,66 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num, revers_num = ''):
+    enter_num = str(enter_num)
+    for i in range(len(enter_num)):
+        revers_num = revers_num + enter_num[(len(enter_num) - 1 ) - i]
+    return revers_num
+
+
+def main():
+    revers_1(number_ent)
+    revers_2(number_ent)
+    revers_3(number_ent)
+    revers_4(number_ent)
+
+
+number_ent = 12345678909876543212341234123412341234
+number_str = str(number_ent)
+print(len(number_str))
+
+
+print(
+    timeit(
+        "revers_1(number_ent)",
+        setup="from __main__ import revers_1, number_ent", number=10000))
+
+
+print(
+    timeit(
+        "revers_2(number_ent)",
+        setup="from __main__ import revers_2, number_ent", number=10000))
+
+
+print(
+    timeit(
+        "revers_3(number_ent)",
+        setup="from __main__ import revers_3, number_ent", number=10000))
+
+
+print(
+    timeit(
+        "revers_4(number_ent)",
+        setup="from __main__ import revers_4, number_ent", number=10000))
+
+
+run('main()')
+
+'''
+Наиболее быстрая функция является разворот СТРОКИ через срезы O(b-a).
+
+На втором месте по скорости исполнения (при небольшом количестве элементов): цикл WHILE пока число не будет равно 0, 
+остаток от целочисленного деления на 10 записывается в новую переменную, а исчисляемое сокращается на один порядок. O(n)
+
+При большом количестве элементов на втором месте ЦИКЛ FOR... IN... со сложностью O(n)
+
+Последнее место по производительности это рекурсивная функция с экспоненциальной сложностью O(2**n)
+Вычисления для числа длиной 38 цифр:
+
+revers_1 = 0.12520021200180054
+revers_2 = 0.06585694200111902
+revers_3 = 0.004694117000326514
+revers_4 = 0.05080128400004469
+'''
