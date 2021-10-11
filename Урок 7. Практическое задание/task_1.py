ccc06fb13cc298c08f54c65cadd857183a45a60a
Anton <4qguns@gmail.com>
2021-10-11 15:37:13 +0300

    Алгоритмы и структуры данных на Python. Базовый курс, 17.09.21, урок 7,Галкин Антон.

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,78 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+
+import timeit
+import random
+
+
+def bubble_sort(lst_obj):
+    for _ in range(len(lst_obj) - 1):
+        for i in range(len(lst_obj) - 1):
+            if lst_obj[i] < lst_obj[i+1]:  # Поменяли знак и сортировка по убыванию
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+    return lst_obj
+
+
+def bubble_sort_2(lst_obj):
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(lst_obj) - 1):
+            if lst_obj[i] < lst_obj[i+1]:
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+                flag = True
+    return lst_obj
+
+
+def bubble_sort_3(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i+1]:  # Поменяли знак и сортировка по убыванию
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+def bubble_sort_4(lst_obj):
+    n = 1
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i+1]:  # Поменяли знак и сортировка по убыванию
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]
+                flag = True
+        n += 1
+    return lst_obj
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+print(orig_list)
+
+print(bubble_sort(orig_list[:]))
+print(timeit.timeit("bubble_sort(orig_list[:])", globals=globals(), number=10000))
+
+print(bubble_sort_2(orig_list[:]))
+print(timeit.timeit("bubble_sort_2(orig_list[:])", globals=globals(), number=10000))
+
+print(bubble_sort_3(orig_list[:]))
+print(timeit.timeit("bubble_sort_3(orig_list[:])", globals=globals(), number=10000))
+
+print(bubble_sort_4(orig_list[:]))
+print(timeit.timeit("bubble_sort_4(orig_list[:])", globals=globals(), number=10000))
+
+print(orig_list)
+
+'''
+bubble_sort_2: добавлено переменная булевая flag, идея такая: если не происходит перемена (своп) элементов,
+значит список отсортирован и можно раньше выходить из цикла. Поможет если только массив хотя бы частично отсортирован.
+
+bubble_sort_2: после прохода каждого цикла, число элементов проверяемого на один элемент меньше, так, как самый
+максимальный уже находится в конце списка и его нет смысла проверять.
+
+bubble_sort_4: реализованы способы описанные в bubble_sort_2 и bubble_sort_2, соответственно и прирост по скорости
+заметен
+'''
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,52 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+
+from random import uniform
+from timeit import timeit
+
+
+def merge_sort(arr):
+    def merge(first, second):
+        result = []
+        i, j = 0, 0
+        while i < len(first) and j < len(second):
+            if first[i] < second[j]:
+                result.append(first[i])
+                i += 1
+            else:
+                result.append(second[j])
+                j += 1
+
+        result.extend(first[i:] if i < len(first) else second[j:])
+
+        return result
+
+    def div_half(lst):
+        if len(lst) == 1:
+            return lst
+        elif len(lst) == 2:
+            return lst if lst[0] <= lst[1] else lst[::-1]
+        else:
+            return merge(div_half(lst[:len(lst)//2]), div_half(lst[len(lst)//2:]))
+    return div_half(arr)
+
+
+def array_size():
+    return int(input('Введите число элементов массива: '))
+
+
+array = [round(uniform(-50, 50), 2) for _ in range(array_size())]
+
+print(f'Исходный массив: {array}')
+print(f'Отсортированный массив: {merge_sort(array[:])}')
+print(timeit("merge_sort(array[:])", globals=globals(), number=1000))
+
+'''
+Замеры производились на 1000 запусков:
+Массив на 
+    10 элементов: сортировка происходит за 0.009118999999999877,
+    100 элементов: сортировка происходит за 0.15257450000000006,
+    1000 элементов: сортировка происходит за 2.0540494000000002
+'''
