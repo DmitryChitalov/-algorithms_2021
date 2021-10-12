edcb8bb819d6d73ed3f50c73f26494a74f026591
RomanGeekBrains <Roman_Lebedev91@mail.ru>
2021-10-12 23:49:06 +0600

    lesson_3 Lebedev R

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -20,3 +20,51 @@
 Обязательно усложните задачу! Добавьте сохранение хеша в файле и получение его из файла.
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
+
+import hashlib
+from os import path
+
+
+def create_hash_from(password):
+    salt = 'salt'
+    return hashlib.sha256(salt.encode()+password.encode()).hexdigest()
+
+
+def create_new_user():
+    us_login = input('Придумайте логин: ')
+    us_password = input('Придумайте пароль: ')
+    us_hash = create_hash_from(us_password)
+
+    with open('data.txt', 'a+') as file:
+        file.write(f'{us_login}={us_hash}\n')
+
+
+def authentication(path_to_BD):
+    us_log = input('Введите логин: ')
+    with open(path_to_BD, 'r') as file:
+        for el in file.readlines():
+            log, passw = el.split('=')
+            if us_log == log:
+                us_pass = input(f'Пользователь {us_log} теперь введите пароль: ')
+                if passw == create_hash_from(us_pass) + '\n':
+                    return '\033[92m{}\033[00m'.format(f'======>>>>> Welcome {us_log} <<<<<======')
+                    # return f'Welcome {us_log}'
+                else:
+                    return '\033[91m{}\033[00m'.format(f'======>>>>> Пароль указан не верно! <<<<<======')
+                    # return f'Пароль указан не верно!'    
+        else:
+            return '\033[93m{}\033[00m'.format(f'======>>>>> Пользователя с ником {us_log} не существует! <<<<<======')
+            # return f'Пользователя с ником {us_log} не существует!'
+            
+
+create_new_user()
+print(authentication(path_to_BD='data.txt'))
+
+
+
+
+# Это оставлю здесь, чтобы не потерять, как с Python к MySQL
+# from getpass import getpass
+# from mysql.connector import connect, Error
+# https://proglib.io/p/python-i-mysql-prakticheskoe-vvedenie-2021-01-06
+
--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -21,3 +21,56 @@
 скорость доступа вместе с уникальностью элементов,
 которые даёт множество, сделают решение коротким и эффективным.
 """
+
+
+
+def get_unique_substrings(some_srting):
+    return {
+        some_srting[start_ind: end_ind] for start_ind in range(len(some_srting)) \
+            for end_ind in range(start_ind + 1, len(some_srting) + 1) \
+                if len(some_srting) > len(some_srting[start_ind: end_ind])
+    }
+
+
+
+some_srting = input('Введите строку, состоящию только из строчных латинских букв: ')
+
+print(get_unique_substrings(some_srting))
+
+print(f'Число уникальных подстрок в строке "{some_srting}" = {len(get_unique_substrings(some_srting))}')
+
+# f'В строке {} {len({some_srting[start_ind: end_ind] for start_ind in range(len(some_srting)) for end_ind in range(start_ind + 1, len(some_srting) + 1) if len(some_srting) > len(some_srting[start_ind: end_ind])})}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# some_set = set()
+
+
+# def some_func(value):
+#     for i in range(len(value)):
+#         for z in range(len(value), i, -1):
+#             hash_str = hash(value[i:z])
+#             some_set.add(hash_str)
+
+
+# string = input('Введите строку (маленькие латинские буквы; ')
+# some_func(string)
+
+# print(f'В строке {string} находится {len(some_set)} уникальных значений.')
