40c3fbabcf7fbb00c2d33713f4be0bf5a4be3d5b
Кузнецов Алексей <alecKuz@yandex.ru>
2021-10-13 20:19:28 +0700

    Угадать генерируемое в программе случайное целое число от 0 до 100

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -11,3 +11,26 @@
 Подсказка:
 Базовый случай здесь - угадали число или закончились попытки
 """
+
+from random import random
+
+
+def name_number(secret_number, number_attempts):
+    if number_attempts == 0:
+        print('Вы не угадали. Ответ -', secret_number)
+    else:
+        if number_attempts == 10:
+            print('Угадайте число, загаданное компьютером')
+        number_user = int(input("Введите число от 0 до 100 "))
+        if number_user < secret_number:
+            print('Не угадали. Ваше число меньше')
+            return name_number(secret_number, number_attempts - 1)
+        elif number_user > secret_number:
+            print('Не угадали. Ваше число больше')
+            return name_number(secret_number, number_attempts - 1)
+        elif number_user == secret_number:
+            print('Ура! Число угадано!')
+
+
+if __name__ == '__main__':
+    name_number(round(random() * 100), 10)
