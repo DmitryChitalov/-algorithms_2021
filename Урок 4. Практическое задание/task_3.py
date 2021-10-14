c87b9aaebdd3f79e5ab04b2cc078d12d6ebaf553
Roman <s-3m@yandex.ru>
2021-10-14 10:53:49 +0300

    ДЗ к 4 уроку

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,8 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +39,34 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def my_revers(enter_num):
+    enter_num = list(str(enter_num))
+    enter_num.reverse()
+    revers_num = int(''.join(enter_num))
+    return revers_num
+
+
+print(timeit('revers_1(enter_num)', 'enter_num=12345', globals=globals()))
+print(timeit('revers_2(enter_num)', 'enter_num=12345', globals=globals()))
+print(timeit('revers_3(enter_num)', 'enter_num=12345', globals=globals()))
+print(timeit('my_revers(enter_num)', 'enter_num=12345', globals=globals()))
+
+run('revers_1(12345)')
+print('--' * 50)
+run('revers_1(12345)')
+print('--' * 50)
+run('revers_1(12345)')
+print('--' * 50)
+run('my_revers(12345)')
+
+"""
+Самая быстрая функция оказалась № 3, на мой взгляд, из-за того, что при ее исполнении происходит меньше всего 
+вызовов встроенных функций и методов. Но я заметил, что тип данных, полученного результата в этой функции - str, 
+а если перевести его в int, то она станет чуть помедленнее из-за дополнительного вызова встроенной функции, 
+хотя все равно будет на первом месте.
+На втором месте функция написанная мной, полагаю, что из-зи того, что в ней используются только встроенные функции
+и методы достаточно простой сложности и не тратится время на математические операции.
+Рекурсивная функция - самая медленная, т.к. рекурсия, в принципе, медленная штука из-зи своего уровня сложности.
+"""
