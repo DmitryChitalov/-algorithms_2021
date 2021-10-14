d01351945beb204475dc8b91ae88757cc6d815a6
Anton <4qguns@gmail.com>
2021-10-14 14:08:03 +0300

    Алгоритмы и структуры данных на Python. Базовый курс, 17.09.21, урок 7,Галкин Антон.

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,59 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+
+
+from collections import Counter, deque
+
+
+def haffman_tree(string):
+    frequency = Counter(string)
+    sort_frequency = deque(sorted(frequency.items(), key=lambda item: item[1]))
+    if len(sort_frequency) != 1:
+        while len(sort_frequency) > 1:
+            weight = sort_frequency[0][1] + sort_frequency[1][1]
+            sub_tree = {0: sort_frequency.popleft()[0],
+                        1: sort_frequency.popleft()[0]}
+            for i, elem in enumerate(sort_frequency):
+                if weight > elem[1]:
+                    continue
+                else:
+                    sort_frequency.insert(i, (sub_tree, weight))
+                    break
+            else:
+                sort_frequency.append((sub_tree, weight))
+    else:
+        weight = sort_frequency[0][1]
+        sub_tree = {0: sort_frequency.popleft()[0],
+                    1: None}
+    return sort_frequency[0][0]
+
+
+table_code = {}
+
+
+def haffman_encode(tree, symbol=''):
+    # tree = haffman_tree(line)
+    if not isinstance(tree, dict):
+        table_code[tree] = symbol
+    else:
+        haffman_encode(tree[0], symbol=f'{symbol}0')
+        haffman_encode(tree[1], symbol=f'{symbol}1')
+
+
+line = input('Введите строку, которую необходимо закодировать: ') or 'beep boop beer!'
+
+haffman_encode(haffman_tree(line))
+
+encode_str = ''.join([f'{table_code[i]} ' for i in line])
+
+print(f'Исходная строка: {line}\n'
+      f'Закодированная строка: {encode_str}\n'
+      f'Таблица кодировки: {table_code}')
+
+
+'''
+Тема понятна не до конца, много прочитал в сети про кодирование по хаффману, 
+для общего развития интересно.
+Задание сделал по примеру, в коде разобрался.
+'''
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,7 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -19,43 +20,64 @@ class BinaryTree:
         # правый потомок
         self.right_child = None
 
+    def __str__(self):
+        return str(self.root)
+
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+        try:
+            # если у узла нет левого потомка
+            if self.root >= new_node:
+                if self.left_child == None:
+                    # тогда узел просто вставляется в дерево
+                    # формируется новое поддерево
+                    self.left_child = BinaryTree(new_node)
+                # если у узла есть левый потомок
+                else:
+                    # тогда вставляем новый узел
+                    tree_obj = BinaryTree(new_node)
+                    # и спускаем имеющегося потомка на один уровень ниже
+                    tree_obj.left_child = self.left_child
+                    self.left_child = tree_obj
+            else:
+                raise Exception('Неверное значение (значение больше корня не может быть вставлено в левое поддерево!)')
+        except Exception as error:
+            print(error)
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+        try:
+            # если у узла нет правого потомка
+            if self.root <= new_node:
+                if self.right_child == None:
+                    # тогда узел просто вставляется в дерево
+                    # формируется новое поддерево
+                    self.right_child = BinaryTree(new_node)
+                # если у узла есть правый потомок
+                else:
+                    # тогда вставляем новый узел
+                    tree_obj = BinaryTree(new_node)
+                    # и спускаем имеющегося потомка на один уровень ниже
+                    tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+            else:
+                raise Exception('Неверное значение (значение меньше корня не может быть вставлено в правое поддерево!)')
+        except Exception as error:
+            print(error)
 
     # метод доступа к правому потомку
     def get_right_child(self):
-        return self.right_child
+        try:
+            return self.right_child
+        except AttributeError:
+            print('Ошибка доступа')
 
     # метод доступа к левому потомку
     def get_left_child(self):
-        return self.left_child
+        try:
+            return self.left_child
+        except AttributeError:
+            print('Ошибка доступа')
 
     # метод установки корня
     def set_root_val(self, obj):
@@ -63,17 +85,30 @@ class BinaryTree:
 
     # метод доступа к корню
     def get_root_val(self):
-        return self.root
+        try:
+            return self.root
+        except AttributeError:
+            print('Ошибка доступа')
 
 
 r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
+
+print(f'Значение корня: {r.get_root_val()}')
+print(f'Левый потомок: {r.get_left_child()}')
+r.insert_left(10)
+r.insert_left(1)
+print(f'Левый потомок: {r.get_left_child()}')
+print(f'Левый потомок: {r.get_left_child()}')
 r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+r.insert_right(2)
+r.insert_right(13)
+print(f'Правый потомок: {r.get_right_child()}')
+print(f'Правый потомок: {r.get_right_child().get_root_val()}')
+r.get_right_child().set_root_val(20)
+print(f'Правый потомок: {r.get_right_child()}')
+
+
+'''
+Сложная тема для понимания, пару дней разбирался, что именно и как работает, доработал по примеру.
+Пример task_2_2 очень интересен, построение графа наглядно отображает понимание структуры.
+'''
