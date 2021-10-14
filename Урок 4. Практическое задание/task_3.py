b67e48b888d810cdd94da0e87d556db6877a3957
Shevchenko-Py-lab <shevchenko.logist@mail.ru>
2021-10-14 15:36:27 +0300

    ДЗ к четвёртому занятию
    Наконец-то у меня появился хотя бы шанс подумать над дз до его разбора (это я про пятое)

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,18 +1,17 @@
 """
 Задание 3.
-
 Приведен код, формирующий из введенного числа
 обратное по порядку входящих в него
 цифр и вывести на экран.
-
 Сделайте профилировку каждого алгоритма через cProfile и через timeit
-
 Обязательно предложите еще свой вариант решения и также запрофилируйте!
-
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
-
 Без аналитики задание считается не принятым
 """
+
+from random import randint
+import cProfile
+import timeit
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +36,62 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def reverse_4(enter_num):
+    rest_numb, numeral = divmod(enter_num, 10)
+    if rest_numb == 0:
+        return str(numeral)
+    else:
+        return str(numeral) + str(reverse_4(rest_numb))
+
+
+def reverse_5(enter_num):
+    return "".join(reversed(str(enter_num)))
+
+
+ENTER_NUM = randint(10000, 1000000)
+# enter_num = int(input('Введите целов число'))
+print(revers_1(ENTER_NUM))
+print(revers_2(ENTER_NUM))
+print(revers_3(ENTER_NUM))
+print(reverse_4(ENTER_NUM))
+print(reverse_5(ENTER_NUM))
+print("revers_1. Время выполнения:")
+print(
+    timeit.timeit(
+        "revers_1(ENTER_NUM)",
+        setup='from __main__ import revers_1, ENTER_NUM',
+        number=10000))
+cProfile.run('revers_1(ENTER_NUM)')
+print("revers_2. Время выполнения:")
+print(
+    timeit.timeit(
+        "revers_2(ENTER_NUM)",
+        setup='from __main__ import revers_2, ENTER_NUM',
+        number=10000))
+cProfile.run('revers_2(ENTER_NUM)')
+print("revers_3. Время выполнения:")
+print(
+    timeit.timeit(
+        "revers_3(ENTER_NUM)",
+        setup='from __main__ import revers_3, ENTER_NUM',
+        number=10000))
+cProfile.run('revers_3(ENTER_NUM)')
+print("reverse_4. Время выполнения:")
+cProfile.run('reverse_4(ENTER_NUM)')
+print(
+    timeit.timeit(
+        "reverse_4(ENTER_NUM)",
+        setup='from __main__ import reverse_4, ENTER_NUM',
+        number=10000))
+print("reverse_5. Время выполнения:")
+print(
+    timeit.timeit(
+        "reverse_5(ENTER_NUM)",
+        setup='from __main__ import reverse_5, ENTER_NUM',
+        number=10000))
+cProfile.run('reverse_5(ENTER_NUM)')
+
+# по итогам профилирования самые эффективные алгоритмы revers_3 (через срез) и reverse_5 (через встроенный модуль),
+# вариант выполнения через срез как по времени выполняется быстрее всех, так и с наименьшим количеством вызовов функции
