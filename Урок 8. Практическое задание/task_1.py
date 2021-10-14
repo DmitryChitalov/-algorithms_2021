6c0d475e0363b6a69e59bd0b6613a47984f66175
Rafael <nem7552@mail.ru>
2021-10-14 19:29:20 +0700

    lesson 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,51 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+from collections import Counter, deque
+
+
+def generate(node, code=''):
+    if not node:
+        return
+    elif node.name is not None:
+        print(f'Кодировка символа "{node.name}" по Хаффману: {code}\n')
+        return
+    generate(node.left, code + '0')
+    generate(node.right, code + '1')
+
+
+class Node:
+    def __init__(self, name=None, value=None):
+        self.name = name
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class HuffmanTree:
+    def __init__(self, s):
+        count = Counter(s)
+        print(count)
+        branch = deque([Node(k, v) for k, v in count.items()])
+        while len(branch) > 1:
+            n = Node(value=(branch[-1].value + branch[-2].value))
+            n.left = branch.pop()
+            n.right = branch.pop()
+            for i in range(len(branch)):
+                if n.value < branch[i].value:
+                    continue
+                else:
+                    branch.insert(i, n)
+                    break
+            else:
+                branch.append(n)
+        self.root = branch[0]
+
+    def gen_code(self):
+        generate(self.root)
+
+
+obj_1 = HuffmanTree("beep boop beer!")
+obj_1.gen_code()
+obj_2 = HuffmanTree('111112222333445')
+obj_2.gen_code()
