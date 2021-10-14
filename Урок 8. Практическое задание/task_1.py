540ff0b9b82762dce79396c42a896847fa69615b
Pavel <pavel-zh@inbox.ru>
2021-10-14 19:46:43 +0300

    Lesson-8 Zhirnov Pavel

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,3 +9,102 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+
+from collections import Counter, deque
+
+
+class Haffman:
+
+    def __init__(self, *args):
+        self.code_table = dict()
+        self.decode_table = dict()
+        self.encoded_string = ''
+        self.my_str = ''
+        try:
+            if len(args) < 2:
+                self.my_str = args[0]
+            else:
+                self.code_table = args[0]
+                for key, val in self.code_table.items():
+                    self.decode_table[val] = key
+                self.encoded_string = args[1].split()
+        except Exception:
+            print('Исходные данные введены с ошибкой.')
+
+    def make_tree(self):
+        freq_of_char = Counter(self.my_str)
+        sorted_elements = deque(sorted(freq_of_char.items(), key=lambda item: item[1]))
+        if len(sorted_elements) != 1:
+            # Цикл для построения дерева
+            while len(sorted_elements) > 1:
+                weight = sorted_elements[0][1] + sorted_elements[1][1]
+                comb = {0: sorted_elements.popleft()[0],
+                        1: sorted_elements.popleft()[0]}
+                # Ищем место для ставки объединенного элемента
+                for i, _count in enumerate(sorted_elements):
+                    if weight > _count[1]:
+                        continue
+                    else:
+                        sorted_elements.insert(i, (comb, weight))
+                        break
+                else:
+                    sorted_elements.append((comb, weight))
+        else:
+            # приравниваемыем значение 0 к одному повторяющемуся символу
+            weight = sorted_elements[0][1]
+            comb = {0: sorted_elements.popleft()[0], 1: None}
+            sorted_elements.append((comb, weight))
+
+        return sorted_elements[0][0]
+
+    def haffman_code(self, tree, path=''):
+        # Если элемент не словарь, значит мы достигли самого символа
+        # и заносим его, а так же его код в словарь (кодовую таблицу).
+        if not isinstance(tree, dict):
+            self.code_table[tree] = path
+        # Если элемент словарь, рекурсивно спускаемся вниз
+        # по первому и второму значению (левая и правая ветви).
+        else:
+            self.haffman_code(tree[0], path=f'{path}0')
+            self.haffman_code(tree[1], path=f'{path}1')
+
+    def table_code(self):
+        self.code_table.clear()
+        self.haffman_code(self.make_tree())
+        return self.code_table
+
+    def str_encode(self):
+        if len(self.code_table) == 0:  # если таблица кодировки не была создана, то создадим
+            self.table_code()
+        return ' '.join([(lambda x: self.code_table.get(x))(i) for i in self.my_str])
+
+    def str_decode(self):
+        if len(self.decode_table) == 0:  # если таблица кодировки не была создана, то создадим
+            return 'В класс не была передана таблица кодировки.'
+        else:
+            self.my_str = ''.join([(lambda x: self.decode_table.get(x))(i) for i in self.encoded_string])
+            return self.my_str
+
+
+# -- Проверка решения:
+
+# Кодируем данные
+print(f'{"-" * 20} Кодируем данные {"-" * 20}')
+h = Haffman("Мой дядя самых честных правил")
+print(f'Кодируемый текст: {h.my_str}')
+print(f'Структура дерева: {h.make_tree()}')  # если нужно создать и посмотреть дерево
+print(f'Кодировочная таблица:  {h.table_code()}')  # если нужно посмотреть полученную кодировочную таблицу
+print(f'Результат кодирования: {h.str_encode()}')  # результат кодирования (можно запустить без make_tree и table_code)
+
+print(f'\n{"-" * 20} Декодируем данные {"-" * 20}')
+# Декодируем данные
+# В переменную класса передадим два параметра - словарь с кодировочной таблицей, закодированный текст
+table_code = {'х': '000', 'л': '0010', 'в': '00110', 'и': '00111', 'п': '01000', 'р': '01001',
+              'т': '01010', 'н': '01011', ' ': '011', 'М': '10000', 'о': '10001', 'д': '1001',
+              'ч': '10100', 'е': '10101', 'й': '10110', 'м': '10111', 'а': '1100', 'ы': '1101',
+              'я': '1110', 'с': '1111'}
+str_encode = '10000 10001 10110 011 1001 1110 1001 1110 011 1111 1100 10111 1101 000 011 10100 10101 1111 ' \
+             '01010 01011 1101 000 011 01000 01001 1100 00110 00111 0010'
+nh = Haffman(table_code, str_encode)
+print(f'Результат декодирования: {nh.str_decode()}')
+print(f'Структура дерева: {h.make_tree()}')
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,6 +10,13 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
+class ChangeDirection(Exception):
+    def __init__(self, new_node, root, branch):
+        side_one, side_two, compare_sign = ['left', 'right', '>'] if branch == 'left' else ['right', 'left', '<=']
+        self.msg = f'Значение {new_node} {compare_sign} {root} --> {side_one} заменен на {side_two}'
+
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -22,32 +29,64 @@ class BinaryTree:
     # добавить левого потомка
     def insert_left(self, new_node):
         # если у узла нет левого потомка
-        if self.left_child == None:
+        if self.left_child is None:
             # тогда узел просто вставляется в дерево
+            # если новое значение больше значения узла и добавляется в левую сторону, то меняем направление на право
             # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
+            try:
+                if new_node >= self.root:
+                    raise ChangeDirection(new_node, self.root, 'left')
+            except ChangeDirection as err:
+                print(err.msg)
+                self.right_child = BinaryTree(new_node)
+            else:
+                self.left_child = BinaryTree(new_node)
+
         # если у узла есть левый потомок
         else:
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            try:
+                if new_node >= self.root:
+                    raise ChangeDirection(new_node, self.root, 'left')
+            except ChangeDirection as err:
+                print(err.msg)
+                tree_obj.right_child = self.left_child
+                self.left_child = tree_obj
+            else:
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
 
     # добавить правого потомка
     def insert_right(self, new_node):
         # если у узла нет правого потомка
-        if self.right_child == None:
+        if self.right_child is None:
             # тогда узел просто вставляется в дерево
             # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
+            try:
+                if new_node < self.root:
+                    raise ChangeDirection(new_node, self.root, 'right')
+            except ChangeDirection as err:
+                print(err.msg)
+                self.left_child = BinaryTree(new_node)
+            else:
+                self.right_child = BinaryTree(new_node)
         # если у узла есть правый потомок
         else:
             # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
             # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            try:
+                if new_node < self.root:
+                    raise ChangeDirection(new_node, self.root, 'left')
+            except ChangeDirection as err:
+                print(err.msg)
+                tree_obj.left_child = self.right_child
+                self.right_child = tree_obj
+            else:
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -66,14 +105,107 @@ class BinaryTree:
         return self.root
 
 
+# --- Проверка результатов:
+
+# 1. Проверка валидации значений - 40 должно добавиться в правку ветку, а не в левую
 r = BinaryTree(8)
 print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
+print(r.get_left_child())   # пока пустая ветка
+r.insert_left(40)           # Значение 40 > 8 --> left заменен на right
+print(r.get_right_child())   # <__main__.BinaryTree object at 0x0000018D37850EE0>
+print(r.get_right_child().get_root_val())   # 40
+del r
+
+
+# ---- 2. Ручной вариант добавления данных в дерево
+# Взят пример из задания для демонстрации вывода результата
+
+r = BinaryTree(8)
+
+# --- Пройдемся по левой части дерева
+r.insert_left(1)
+r.insert_left(2)
+r.insert_left(4)
+
+# 2 ---> 1 - 3
+test_root = r.left_child.left_child
+print('2 -- ', test_root.get_root_val())  # 2
+test_root.insert_right(3)  # 3
+
+# 4 ---> 6
+test_root = r.left_child
+print('4 -- ', test_root.get_root_val())  # 4
+test_root.insert_right(6)  # 6
+test_root.right_child.insert_right(7)
+test_root.right_child.insert_left(5)
+
+# --- Пройдемся по правой части дерева
+r.insert_right(15)
+r.insert_right(14)
 r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+
+# 2 ---> 1 - 3
+test_root = r.right_child.right_child
+print('14 -- ', test_root.get_root_val())  # 14
+test_root.insert_left(13)  # 13
+
+# 10 --- 9 - 11
+test_root = r.right_child
+print('12 -- ', test_root.get_root_val())  # 12
+test_root.insert_left(10)  # 10
+test_root.left_child.insert_right(11)
+test_root.left_child.insert_left(9)
+#  test_root.left_child.left_child.insert_left(6)
+
+test_root = r.right_child.right_child.left_child
+print('13 -- ', test_root.get_root_val())  # 9
+test_root = r.right_child.left_child.left_child
+print('9 -- ', test_root.get_root_val())  # 9
+
+
+# ---- 3. Красивый вывод данных
+
+# Создадм словарь для хранения данных
+code_tree = dict()
+
+
+def view_tree(tree, level=1):
+    """
+    Функция заполняет словарь данными из дерева по уровням
+    :param tree:  дерево в виде созданного класса
+    :param level: уровень
+    """
+    if tree is None:
+        return None
+    else:
+        if code_tree.get(level) is None:
+            code_tree[level] = [tree.get_root_val()]
+        else:
+            f = code_tree.get(level)
+            f.append(tree.get_root_val())
+        view_tree(tree.left_child, level + 1)
+        view_tree(tree.right_child, level + 1)
+
+# Заполним словарь
+
+
+view_tree(r)
+
+print(code_tree)  # выведем полученный словарь в одну строку
+
+
+#  сделаем понятную визуально структуру дерева (упрощенный вариант вывода)
+level_in_tree = max([i for i in code_tree]) - 1
+max_num_val = 2**level_in_tree
+for key, val in code_tree.items():
+    g = '\t\t' * (max_num_val // len(val))
+    print(f'{key})', '\t\t' * (max_num_val // len(val) // 2), g.join(map(str, val)))
+
+"""
+Получается вот такой результат, что удобно для проверки и добавления данных:
+
+1) 								 8
+2) 				 4								12
+3) 		 2				6				10				14
+4)  1		3		5		7		9		11		13		15
+"""
