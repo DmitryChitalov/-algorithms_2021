978753996fa9cbe970baba450e455fa45dc779fc
KOMPOTISHE <danilvankov@yandex.ru>
2021-10-15 15:59:08 +0700

    add Hwlesson_5

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,90 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+from timeit import timeit
+from collections import deque
+
+lst_obj = [1, 2, 3]
+deq_obj = deque(lst_obj)
+
+
+def fill_lst():
+    lst_obj = [x for x in range(10000)]
+    return lst_obj
+
+
+def fill_deq():
+    deq_obj = [x for x in range(10000)]
+    return deq_obj
+
+
+def lst_in():
+    lst_obj.insert(0, 12)
+    return lst_obj
+
+
+def deq_al():
+    deq_obj.appendleft(12)
+    return deq_obj
+
+
+def lst_pop():
+    lst_obj.pop(0)
+    return lst_obj
+
+
+def deq_pop():
+    deq_obj.popleft()
+    return deq_obj
+
+
+def lst_extl():
+    a = [1, 2, 3]
+    for i in range(len(a)):
+        lst_obj.insert(0, a[i])
+    return lst_obj
+
+
+def deq_extl():
+    deq_obj.extendleft([1, 2, 3])
+    return deq_obj
+
+
+print(
+    timeit(
+        "fill_lst()",
+        setup="from __main__ import fill_lst", number=10000))
+
+print(
+    timeit(
+        "fill_deq()",
+        setup="from __main__ import fill_deq", number=10000))
+
+print(
+    timeit(
+        "lst_in()",
+        setup="from __main__ import lst_in", number=10000))
+
+print(
+    timeit(
+        "deq_al()",
+        setup="from __main__ import deq_al", number=10000))
+
+print(
+    timeit(
+        "lst_pop()",
+        setup="from __main__ import lst_pop", number=10000))
+
+print(
+    timeit(
+        "deq_pop()",
+        setup="from __main__ import deq_pop", number=10000))
+
+print(
+    timeit(
+        "lst_extl()",
+        setup="from __main__ import lst_extl", number=10000))
+print(
+    timeit(
+        "deq_extl()",
+        setup="from __main__ import deq_extl", number=10000))
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,69 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+from timeit import timeit
+from collections import OrderedDict
+
+dict_obj = {1: 'one', 2: 'two', 3: 'three'}
+ordict_obj = OrderedDict(dict_obj)
+
+print(type(dict_obj))
+
+
+def dict_in():
+    for i in range(4, 7):
+        dict_obj[i] = chr(93 + i)
+    return dict_obj
+
+
+def ordict_in():
+    for i in range(4, 7):
+        ordict_obj[i] = chr(93 + i)
+    return ordict_obj
+
+
+def dict_get():
+    dict_obj.get(3)
+    return dict_obj
+
+
+def ordict_get():
+    ordict_obj.get(3)
+    return ordict_obj
+
+
+print(
+    timeit(
+        "dict_in()",
+        setup="from __main__ import dict_in", number=100000))
+
+print(
+    timeit(
+        "ordict_in()",
+        setup="from __main__ import ordict_in", number=100000))
+
+print(
+    timeit(
+        "dict_get()",
+        setup="from __main__ import dict_get", number=100000))
+
+print(
+    timeit(
+        "ordict_get()",
+        setup="from __main__ import ordict_get", number=100000))
+
+'''
+Для 100 000 повторов:
+
+Заполнение:
+dict_in - 0.0531862
+ordict_in - 0.05982140000000001
+
+Получение элемента:
+dict_get - 0.01137740000000001
+ordict_get - 0.011561100000000019
+
+Тип данных OrderedDict после Python3.6 не актуален т.к. он стал запоминить последовательность!
+
+Но при работе с .csv файлами OrderedDict используется до сих пор. 
+'''
