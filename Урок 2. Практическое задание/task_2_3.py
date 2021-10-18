d7237db29445d573f42d15cda8ee0b11bb9e34f0
Max <setborg@inbox.ru>
2021-10-18 21:49:57 +0500

    Исправление ошибок стиля

--- Урок 2. Практическое задание/task_2_3.py
+++ Урок 2. Практическое задание/task_2_3.py
@@ -1,36 +1,36 @@
-reslt = ''
+result = ''
 
-def resiver(number,rlt):
+def resiver(number, rlt):
     res_str = len(str(number))
     if res_str == 1:
-        num = int(number)
-        result_int = str(num % 10)
+        numbers = int(number)
+        result_int = str(numbers % 10)
         rlt += result_int                       # Специально перевел rlt в строку, и тогда накопление работает.
         print(f'Обратный результат {rlt}')
         return
     else:
-        num = int(number)
-        result_int = str(num % 10)
-        rlt +=result_int
-        num = num // 10
-        resiver(num,rlt)
+        numbers = int(number)
+        result_int = str(numbers % 10)
+        rlt += result_int
+        numbers = numbers // 10
+        resiver(numbers, rlt)
 
 
-# num = int(input('Ввидите 3-4 значное число:  '))
-# print(f'Пользователь ввел число:  {num}')
-# resiver(num,reslt)
+num = int(input('Ввидите 3-4 значное число:  '))
+print(f'Пользователь ввел число:  {num}')
+resiver(num, result)
 
 
 
 
 # Вариант 2
 
-def revers_number(num):
-    return str(num) if num < 10 else str(num % 10) + revers_number(num // 10)
-    print(num)
-
-numbers = int(input('Введите число которое требуесят перевенуть :'))
-print(type(numbers))
-print(f' Перевернутое число: {revers_number(numbers)}, {type({revers_number(numbers)})}')
+# def revers_number(nums):
+#     return str(nums) if nums < 10 else str(nums % 10) + revers_number(nums // 10)
+#     print(nums)
+#
+# numbers = int(input('Введите число которое требуесят перевенуть :'))
+# print(type(numbers))
+# print(f' Перевернутое число: {revers_number(numbers)}, {type({revers_number(numbers)})}')
 
 
