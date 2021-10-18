a3267789447ef7bbe7651aeb572599ca32cfbdad
Julio <juliojm1310@gmail.com>
2021-10-18 21:11:10 +0300

    Homework 4
    HW 4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,9 @@
 
 Без аналитики задание считается не принятым
 """
+import timeit
+from cProfile import run
+import time
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +40,36 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def my_function(number, result=''):
+    if number == 0:
+        return result
+    x = str(number % 10)
+    result = result + x
+    return my_function(number // 10, result)
+
+
+enter_num = 17850
+
+
+def main():
+    enter_num = 17850
+    one = revers_1(enter_num)
+    two = revers_2(enter_num)
+    three = revers_3(enter_num)
+    four = my_function(enter_num)
+
+
+print("revers_1 time with timeit: ", timeit.timeit("revers_1(enter_num, revers_num=0)", globals=globals()), "secs")
+print("revers_2 time with timeit: ", timeit.timeit("revers_2(enter_num, revers_num=0)", globals=globals()), "secs")
+print("revers_3 time with timeit: ", timeit.timeit("revers_3(enter_num)", globals=globals()), "secs")
+print("my_function time with timeit: ", timeit.timeit("my_function(enter_num, result='')", globals=globals()), "secs")
+run("main()")
+run("revers_1(enter_num, revers_num=0)")
+run("revers_2(enter_num, revers_num=0)")
+run("revers_3(enter_num)")
+run("my_function(enter_num, result='')")
+# с функциец run() получаются другие резултаты, везде 0 не понимаю почему, я что-то не так делаю?...
+
+# функция revers_3 эффективнее, так как она быстрее работает и также она более локаничная
