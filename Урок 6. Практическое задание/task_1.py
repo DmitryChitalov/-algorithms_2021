04b0b6c99cbaae722ee5c05814f6bc701d19e888
Vadim <vadim2387@gmail.com>
2021-10-19 18:56:26 +0300

    lesson_6

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -21,3 +21,77 @@
 Попытайтесь дополнительно свой декоратор используя ф-цию memory_usage из memory_profiler
 С одновременным замером времени (timeit.default_timer())!
 """
+
+from timeit import timeit
+from memory_profiler import profile
+
+
+@profile
+def decor(func):
+    def wrapper(*argv):
+        return func(*argv)
+    return wrapper
+
+@ decor
+def get_rev_number(number, rev_number = ''):
+
+    if number == 0:
+        return rev_number
+    else:
+
+        last_number = number % 10
+        rev_number += str(last_number)
+        return get_rev_number(number // 10, rev_number)
+
+@profile
+def get_rev_number_2(number, rev_number = ''):
+    while number != 0:
+        last_number = number % 10
+        rev_number += str(last_number)
+        number = number // 10
+    return rev_number
+
+number_s = 1230
+print(get_rev_number(number_s))
+print(get_rev_number_2(number_s))
+# print(
+#     timeit(
+#         "get_rev_number(number_s)",
+#         setup="from __main__ import get_rev_number, number_s", number=1000000))
+# print(
+#     timeit(
+#         "get_rev_number_2(number_s)",
+#         setup="from __main__ import get_rev_number_2, number_s", number=1000000))
+
+'''
+1 000 000 повторений:
+get_rev_number = 1.291
+get_rev_number_2 = 1.012
+Аналитика:
+Замена Рекурсии на Цикл привела к снижению сложности и увеличению скорости 
+обработки операций с факториальной O(n!) на линейную O(n).
+@profile - показывает, что цикл не нагружает доп. элементы в Increment - что говорит о том,
+ что дополнительной Опертивной памяти для хранения Итерации не требуется. 
+'''
+
+@profile
+def get_revers_number(number, rev_number=''):
+    if number == 0:
+        return rev_number
+    else:
+        last_number = number % 10
+        rev_number = rev_number + str(last_number)
+        return get_revers_number(number // 10, rev_number)
+
+
+print(get_revers_number(123123890737460787))
+
+@profile
+def get_revers_number_2(number, rev_number_2=''):
+    while number != 0:
+        last_number = number % 10
+        rev_number = rev_number_2 + str(last_number)
+        number = number // 10
+    return rev_number_2
+
+print(get_revers_number_2(123123890737460787))
