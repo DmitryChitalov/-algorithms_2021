0023019a6becafcf6d6d68150e5f6a5dd46a3f46
Кузнецов Алексей <alecKuz@yandex.ru>
2021-10-20 11:27:55 +0700

    запрос пароля пользователя и его сравнение с паролем в БД

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -18,3 +18,28 @@
 Допускаются любые усложения задания - валидация, подключение к БД, передача данных в файл
 """
 # sqlite, postgres, db_api, orm
+
+import hashlib
+
+user_login = input("Введите логин ")    # логин будет солью (т.к. он неповторимый)
+user_password = input("Введите пароль ")
+
+
+def hash_password(password, login):
+    with open('example.txt', encoding='utf-8') as file_read:    # открываем для чтения
+        result = hashlib.sha256(login.encode() + password.encode()).hexdigest()
+        if result in file_read.read():
+            print("В базе данных уже есть хеш данного пароля: ", result)
+        else:
+            print("Информация о данном пароле отсутствует")
+            user_password_2 = input("Чтобы добавить пароль в БД, ведите его повторно ")
+            if password == user_password_2:
+                print("Пароли совпадают. Сохранён хеш: ", result)
+                with open('example.txt', 'a', encoding='utf-8') as file_write:    # открываем для записи
+                    file_write.write(result + '\n')
+            else:
+                print("Введёный пароли не совпадают")
+
+
+hash_password(user_password, user_login)
+"""можно было и логин в отдельную БД закинуть"""
