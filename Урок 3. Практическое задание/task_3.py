7992dfa0dab28225033437194282f40cb07e6cab
Кузнецов Алексей <alecKuz@yandex.ru>
2021-10-20 11:29:37 +0700

    определение количества различных (уникальных) подстрок

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -17,3 +17,20 @@
 """
 
 # hash?
+
+import hashlib
+
+subs_set = set()
+str_user = str(input("Введите строку: "))
+
+print(f"Строка {str_user} имеет длину {len(str_user)} сиволов.")
+
+for i in range(len(str_user)):
+    for j in range(i + 1, len(str_user) + 1):
+        if str_user[i:j] != str_user:
+            subs_set.add(hashlib.sha256(str_user[i:j].encode()).hexdigest())
+            print(str_user[i:j])
+
+print("количество элементов в множестве: ", len(subs_set))
+print("Хеш элементов: ")
+print(*subs_set, sep="\n")
