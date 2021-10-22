583d3c1519df7102a1ffd322aaebc5824674b2a4
Lexan <sheklford2015@yandex.ru>
2021-10-22 13:15:16 +0300

    tasks 1-3 done

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,42 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+
+
+from timeit import timeit
+from random import randint
+
+
+def bubble_sort(lst):
+    for j in range(len(lst)):
+        for i in range(len(lst)-1):
+            if lst[i] < lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+    return lst
+
+
+def bubble_sort_advance(lst):
+    for j in range(len(lst)):
+        lst_c = lst.copy()
+        for i in range(len(lst)-1):
+            if lst[i] < lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+        if lst_c == lst:
+            return lst
+    return lst
+
+
+need_list = [randint(-100, 100) for el in range(-100, 100)]
+print(f"Несортированный: {need_list}\nСортированный: {bubble_sort_advance(need_list[:])}\n")
+
+print("Время обычной " + str(timeit("bubble_sort(need_list[:])", "from __main__ import bubble_sort, need_list",
+                                    number=1000)))
+print("Время доработанной " + str(timeit("bubble_sort_advance(need_list[:])",
+                                         "from __main__ import bubble_sort_advance, need_list", number=1000)))
+
+"""
+Сделал вариант с завершением сортировки при уже сортированном массиве.
+Время обычной 2.2190600000000003
+Время доработанной 2.2268739
+Замерял несколько раз, в целом +- одинаково, так что смысла нет.
+"""
