07d9e463443abf7fe8b7f6935f897ff646a1e9b5
Roman <s-3m@yandex.ru>
2021-10-22 15:48:41 +0300

    Доработка ДЗ к 7 уроку

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -17,6 +17,38 @@ from random import uniform
 from timeit import timeit
 
 
+def merge_from_lesson(arr):
+    if len(arr) > 1:
+        center = len(arr) // 2
+        left = arr[:center]
+        right = arr[center:]
+
+        merge_from_lesson(left)
+        merge_from_lesson(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+        return arr
+
+
 def merge_sort(arr):
     n = len(arr)
     if n <= 1:
@@ -44,9 +76,34 @@ def merge(left, right):
     return result
 
 
+lst_test_100 = [uniform(0, 50) for i in range(100)]
+lst_test_1000 = [uniform(0, 50) for i in range(1000)]
+lst_test_10000 = [uniform(0, 50) for i in range(10000)]
+
+print('-' * 50, 'Сортировка слиянием из урока', '-' * 50)
+print(
+    f'Затраченное время на сортировку массива из 100 элементов:\n{timeit("merge_from_lesson(lst_test_100[:])", globals=globals(), number=1000)}\n'
+    f'Затраченное время на сортировку массива из 1000 элементов:\n{timeit("merge_from_lesson(lst_test_1000[:])", globals=globals(), number=1000)}\n'
+    f'Затраченное время на сортировку массива из 10000 элементов:\n{timeit("merge_from_lesson(lst_test_10000[:])", globals=globals(), number=1000)}')
+
+print('-' * 50, 'Альтернативная сортировка слиянием', '-' * 50)
+print(
+    f'Затраченное время на сортировку массива из 100 элементов:\n{timeit("merge_sort(lst_test_100[:])", globals=globals(), number=1000)}\n'
+    f'Затраченное время на сортировку массива из 1000 элементов:\n{timeit("merge_sort(lst_test_1000[:])", globals=globals(), number=1000)}\n'
+    f'Затраченное время на сортировку массива из 10000 элементов:\n{timeit("merge_sort(lst_test_10000[:])", globals=globals(), number=1000)}')
+
+print('-' * 50, 'Сортировка пользовательского массива', '-' * 50)
 user_number = int(input('Введите число: '))
 some_lst = [uniform(0, 50) for i in range(user_number)]
 
 print(
     f'Массив из {user_number} элементов:\nИсходный массив - {some_lst}\nОтсортированный массив - {merge_sort(some_lst)}')
 print('Затраченное время - ', timeit('merge_sort(some_lst)', globals=globals(), number=10000))
+
+"""
+Вывод:
+Пример рассмотренный на уроке оказался быстрее. И если на массивах до 100 элементов это почти незаметно, то на массиве
+в 10000 элементов скорость выше в 5 раз. 
+Полагаю, мой пример меленнее из-за постоянной работы со срезами (создаются много новых объектов), в то время как 
+пример из урока работает с индексами существующего объекта.
+"""
