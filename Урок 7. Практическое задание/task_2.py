f39b9affcd544812ad08637b1cc5b9376ac82bdc
Andrey <ab@inteb.pro>
2021-10-24 21:57:27 +0400

    lesson7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,51 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+from timeit import timeit
+
+
+def merge_sort(obj):
+    if len(obj) > 1:
+        median_list = len(obj) // 2
+        left_list = obj[:median_list]
+        right_list = obj[median_list:]
+
+        merge_sort(left_list)
+        merge_sort(right_list)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left_list) and j < len(right_list):
+            if left_list[i] < right_list[j]:
+                obj[k] = left_list[i]
+                i += 1
+            else:
+                obj[k] = right_list[j]
+                j += 1
+            k += 1
+
+        while i < len(left_list):
+            obj[k] = left_list[i]
+            i += 1
+            k += 1
+
+        while j < len(right_list):
+            obj[k] = right_list[j]
+            j += 1
+            k += 1
+        return obj
+
+
+list_len = int(input('Введите количество элементов: \n'))
+test_list = [random.random() for i in range(list_len)]
+print(f'Исходный - {test_list}')
+print(f'Отсортированный - {merge_sort(test_list)}')
+print(f'{list_len} элементов', timeit('merge_sort(test_list[:])', globals=globals(), number=1000))
+"""
+Для 10 элементов время выполнения - 0.014677999999999969
+Для 100 элементов время выполнения - 0.22962229999999995
+Для 1000 элементов время выполнения - 2.8496870000000003
+
+На 1000 элементов результаты уже не очень, на 10000 совсем плохо - 37.345229
+"""
