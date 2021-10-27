11f055892a7455605b273f768c4099f304373d17
Кузнецов Алексей <alecKuz@yandex.ru>
2021-10-27 16:08:49 +0700

    список (list) и очередь (deque) - проверка по времени

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -15,3 +15,59 @@ deque – это обобщение стеков и очередей.
 
 И добавить аналитику, так ли это или нет.!
 """
+
+from timeit import timeit
+from collections import deque
+
+number_cycles = 100000    # количество циклов
+list_obj = []
+deque_obj = deque([])
+
+
+def fill_list():    # заполнить обычный массив
+    list_obj = [i for i in range(1000)]
+    return list_obj
+
+
+def fill_deque():    # заполнить контейнер deque
+    deque_obj = [j for j in range(1000)]
+    return deque_obj
+
+
+def add_list():    # добавим элемент в массив
+    list_obj.insert(0, 1)
+    return list_obj
+
+
+def add_deque():    # добим элемент в deque контейнер
+    deque_obj.appendleft(1)
+    return deque_obj
+
+
+def del_list():    # удалим элемент 0 позиции
+    list_obj.pop(0)
+    return list_obj
+
+
+def del_deque():    # удалим элемент слева
+    deque_obj.popleft()
+    return deque_obj
+
+
+print(f'Заполнение массива: '
+      f'{timeit("fill_list()", globals=globals(), number=number_cycles)}')    # 5.258377099991776
+print(f'Заполнение контейнера: '
+      f'{timeit("fill_deque()", globals=globals(), number=number_cycles)}')    # 5.306759900006
+print(f'Добавление элемента в массив: '
+      f'{timeit("add_list()", globals=globals(), number=number_cycles)}')    # 4.352013099996839
+print(f'Добавление элемента в контейнер: '
+      f'{timeit("add_deque()", globals=globals(), number=number_cycles)}')    # 0.02476639999076724
+print(f'Удаление элемента из массива: '
+      f'{timeit("del_list()", globals=globals(), number=number_cycles)}')    # 1.4185422000009567
+print(f'Удаление элемента из контейнера: '
+      f'{timeit("del_deque()", globals=globals(), number=number_cycles)}')    # 0.02342400001361966
+
+
+"""В данном случае видно, что используя модуль deque,
+работа с массивами происходит гораздо быстрее, чем стандартными методами.
+лишь заполнение массива примерно одинаково по времени (и там и там LC используется)"""
