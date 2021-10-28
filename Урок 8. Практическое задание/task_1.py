050c89943936527326f9c4b26780e3e982b58760
Polina Nechaeva <polina2000_21@mail.ru>
2021-10-28 22:52:27 +0300

    complete_hw_8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -10,3 +10,57 @@
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 """
+from collections import Counter
+from operator import itemgetter
+
+
+class Offspring:
+
+    def __init__(self, left_ch=None, right_ch=None):
+        self.left_ch = left_ch
+        self.right_ch = right_ch
+
+    def child(self):
+        return self.left_ch, self.right_ch
+
+
+def create_tree(root, cypher=str()):
+    if type(root) is str:
+        return {
+            root: cypher
+        }
+    l, r = root.child()
+    result = {}
+    result.update(create_tree(l, cypher + "0"))
+    result.update(create_tree(r, cypher + "1"))
+    return result
+
+
+# Подсчет букв
+def count_rep_lett(data):
+    dct_quantity = Counter(data)
+    srt_dct = sorted(dct_quantity.items(), key=itemgetter(1))
+    while len(srt_dct) > 1:
+        srt_dct = sorted(srt_dct, key=itemgetter(1))
+        counter = 0
+        lett_1, cnt_1 = srt_dct[0]
+        lett_2, cnt_2 = srt_dct[1]
+        srt_dct = srt_dct[2:]
+        counter += cnt_1 + cnt_2
+        srt_dct.append(
+            (Offspring(lett_1, lett_2), counter))
+    return srt_dct
+
+
+data = "beep boop beer!"
+srt_dct = count_rep_lett(data)
+# print(srt_dct)
+
+code_letters = create_tree(srt_dct[0][0])
+# print(code_letters)
+
+# Закодированная строка
+for el in code_letters.values():
+    print(el, end=' ')
+
+
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -10,44 +10,15 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+
 class BinaryTree:
     def __init__(self, root_obj):
-        # корень
         self.root = root_obj
-        # левый потомок
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
-    def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
-
-    # добавить правого потомка
-    def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
-        else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+    def __str__(self):
+        return str(self.root)
 
     # метод доступа к правому потомку
     def get_right_child(self):
@@ -65,15 +36,67 @@ class BinaryTree:
     def get_root_val(self):
         return self.root
 
+    def insert(self, new_node):
+        try:
+            if type(new_node) is not int and type(new_node) is not float:
+                raise ValueError(f"{new_node} - не число")
+        except ValueError as err:
+            print(err)
+        else:
+
+            # Проверка больше ли новый объект чем корень
+            if new_node > self.root or new_node == self.root:
+                # Если правовго потомка еще нет, то вставляется новвый узел
+                if self.right_child is None:
+                    self.right_child = BinaryTree(new_node)
+                # Если потомок есть, то вставляем его в узел, а имеющего спускаем вниз
+                else:
+                    tree_obj = BinaryTree(new_node)
+                    if self.right_child.root < new_node:
+                        tree_obj.left_child = self.right_child
+                    else:
+                        tree_obj.right_child = self.right_child
+                    self.right_child = tree_obj
+            else:
+                if self.left_child is None:
+                    self.left_child = BinaryTree(new_node)
+                else:
+                    tree_obj = BinaryTree(new_node)
+                    if self.left_child.root < new_node:
+                        tree_obj.left_child = self.left_child
+                    else:
+                        tree_obj.right_child = self.left_child
+                    self.left_child = tree_obj
+
 
 r = BinaryTree(8)
+r.insert(7)
+r.insert(12)
+r.insert(13)
+r.insert(3.25)
+r.insert(2)
+r.insert(5)
+r.insert('hello')
+r.left_child.insert(6)
+
 print(r.get_root_val())
+# Левый и правый потомки от 8
+# --> 5
+# --> 13
 print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
-r.insert_right(12)
 print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+
+# --> 2
+# --> 6
+print(r.get_left_child().get_left_child())
+print(r.get_left_child().get_right_child())
+
+# -->12
+# None
+print(r.get_right_child().get_left_child())
+print(r.get_right_child().get_right_child())
+
+# --> 3.25
+# --> 7
+print(r.get_left_child().get_left_child().get_right_child())
+print(r.get_left_child().get_left_child().get_right_child().get_right_child())
