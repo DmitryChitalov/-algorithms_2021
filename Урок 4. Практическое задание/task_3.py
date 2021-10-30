27dba70b721516c8889824a3c3e62e900b3845c8
RomanGeekBrains <Roman_Lebedev91@mail.ru>
2021-10-30 22:10:05 +0600

    add t1 and t3

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -15,6 +15,13 @@
 """
 
 
+import sys
+from timeit import timeit
+from cProfile import run
+
+sys.setrecursionlimit(3000)
+
+
 def revers_1(enter_num, revers_num=0):
     if enter_num == 0:
         return revers_num
@@ -37,3 +44,100 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num):
+    return ''.join([str(i) for i in (x for x in str(enter_num)[::-1])])
+
+
+def main(arr):
+    revers_1(arr)
+    revers_2(arr)
+    revers_3(arr)
+    revers_4(arr)
+
+
+arr = int(''.join([str(el) for el in range(1000)]))
+
+
+print(f'''Время выполнения revers_1(arr)
+==>>{timeit(
+        'revers_1(arr)',
+        globals=globals(),
+        number=10000
+    )}
+# ''', end='\n')
+# run('revers_1')
+
+print(f'''Время выполнения revers_2(arr)
+==>>{timeit(
+        'revers_2(arr)',
+        globals=globals(),
+        number=10000
+    )}
+# ''', end='\n')
+# run('revers_2')
+
+print(f'''Время выполнения revers_3(arr)
+==>>{timeit(
+        'revers_3(arr)',
+        globals=globals(),
+        number=10000
+    )}
+# ''', end='\n')
+# run('revers_3')
+
+print(f'''Время выполнения revers_4(arr)
+==>>{timeit(
+        'revers_4(arr)',
+        globals=globals(),
+        number=10000
+    )}
+# ''', end='\n')
+# run('revers_4')
+
+run('main(arr)')
+
+
+# Время выполнения revers_1(arr)
+# ==>>57.58934165199753
+# # 
+# Время выполнения revers_2(arr)
+# ==>>37.516389247000916
+# # 
+# Время выполнения revers_3(arr)
+# ==>>1.312020579003729
+# # 
+# Время выполнения revers_4(arr)
+# ==>>5.189374495996162
+# # 
+#          5789 function calls (2900 primitive calls) in 0.013 seconds
+
+#    Ordered by: standard name
+
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.013    0.013 <string>:1(<module>)
+#    2890/1    0.008    0.000    0.008    0.008 task_3.py:24(revers_1)
+#         1    0.004    0.004    0.004    0.004 task_3.py:34(revers_2)
+#         1    0.000    0.000    0.000    0.000 task_3.py:42(revers_3)
+#         1    0.000    0.000    0.001    0.001 task_3.py:48(revers_4)
+#      2890    0.000    0.000    0.000    0.000 task_3.py:49(<genexpr>)
+#         1    0.000    0.000    0.001    0.001 task_3.py:49(<listcomp>)
+#         1    0.000    0.000    0.013    0.013 task_3.py:52(main)
+#         1    0.000    0.000    0.013    0.013 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'join' of 'str' objects}
+# 
+# Вывод: 
+# функция revers_3 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
