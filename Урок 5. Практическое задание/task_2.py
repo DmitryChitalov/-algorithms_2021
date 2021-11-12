0048b1a72381638de9cc16222e31f5a0c14ec501
RomanGeekBrains <Roman_Lebedev91@mail.ru>
2021-11-13 01:01:39 +0600

    done home work 5

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -34,3 +34,61 @@ hx = HexNumber
 hx + hx
 hex()
 """
+
+from collections import namedtuple
+
+
+class HexNumber:
+    def __init__(self, x = '', y = ''):
+        self.x = int(x, 16)
+        self.y = int(y, 16)
+
+    def add(self):
+        return list(hex(self.x + self.y).upper())[2:]
+
+    def mul(self):
+        return list(hex(self.x * self.y).upper())[2:]
+
+
+hx = HexNumber('A2', 'C4f')
+print(hx.add())
+print(hx.mul())
+
+# =====================================================
+
+
+class HexNumber_1:
+    def __init__(self, x = ''):
+        self.x = int(x, 16)
+
+    def __add__(self, other):
+        self.x += other.x
+        return list(hex(self.x)[2:].upper())
+
+    def __mul__(self, other):
+        self.x *= other.x
+        return list(hex(self.x)[2:].upper())
+
+
+hx1 = HexNumber_1('A2')
+hx2 = HexNumber_1('C4f')
+print(hx1 + hx2)
+print(hx1 * hx2)
+
+# =====================================================
+
+
+def add_and_multiply(num_1, num_2):
+    nums = namedtuple("nums", "num_1 num_2")
+    nums.num_1, nums.num_2 = int(num_1, 16), int(num_2, 16)
+    print(f'Сумма чисел: {list(hex(nums.num_1 + nums.num_2).upper())[2:]}')
+    print(f'Произведение чисел: {list(hex(nums.num_1 * nums.num_2).upper())[2:]}')
+
+add_and_multiply('A2', 'C4F')
+
+# =====================================================
+# Как по мне очень интересное задание
+# Специально искал решения через ООП - так как для меня это лес
+# Я хорошо уяснил что такое переопределение метода или атрибута.
+# Но к сожалению так и не понял до конца перегрузку..Первое решение через ООП - 
+# истинно мое, которое легко написал. Второе - много интернета, ошибок, тыканья наугад=)
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,43 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+from timeit import timeit
+from collections import OrderedDict
+
+experimental_dict = {i: i for i in range(10)}
+experemental_ord_dict = OrderedDict({i: i for i in range(10)})
+
+
+
+def fill_dict():   
+    dict = {i: i for i in range(10)}
+    return dict
+
+print(f"{fill_dict.__name__} \
+    {timeit('fill_dict()', globals=globals(), number=10000)}")
+
+
+def fill_ord_dict(): 
+    ord_dict = OrderedDict({i: i for i in range(10)})
+    return ord_dict
+
+print(f"{fill_ord_dict.__name__} \
+    {timeit('fill_ord_dict()', globals=globals(), number=10000)}")
+
+
+def get_elm_dict(experimental_dict):
+    return experimental_dict.get(7)
+
+print(f"{get_elm_dict.__name__} \
+    {timeit('get_elm_dict(experimental_dict)', globals=globals(), number=10000)}")
+
+
+def get_elm_ord_dict(experemental_ord_dict):
+    return experemental_ord_dict.get(7)
+
+print(f"{get_elm_ord_dict.__name__} \
+    {timeit('get_elm_ord_dict(experemental_ord_dict)', globals=globals(), number=10000)}")
+
+     
+
